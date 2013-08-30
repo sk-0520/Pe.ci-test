1d548e6bbcb99c3122245e2a498f2cfd53eaa4f0
sk <content.text.net@gmail.com>
2013-08-30 21:02:07 +0900

    ID

---
+++ Pe/Pe/Logic/Item/Item.cs
@@ -0,0 +1,226 @@
+﻿/*
+ * SharpDevelopによって生成
+ * ユーザ: sk
+ * 日付: 08/29/2013
+ * 時刻: 23:22
+ * 
+ * このテンプレートを変更する場合「ツール→オプション→コーディング→標準ヘッダの編集」
+ */
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.Xml;
+
+using Pe.IF;
+
+namespace Pe.Logic
+{
+	public abstract class Item: IImportExportXmlElement
+	{
+		static Regex _reg = new Regex(
+			""
+			+ "([" 
+			+ @"\s\\\^\$\(\)\|\.\[\-\]\*\+\?\{\,\}" 
+			+ "\"'=~@<>;:!#%&" 
+			+ "])"
+			, 
+			RegexOptions.None
+		);
+		
+		const string ID = "id";
+		
+		public static string UniqueItemId(string source, int index) 
+		{
+			return string.Format("{0}_{1}", source, index);
+		}
+		
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="id"></param>
+		/// <returns></returns>
+		public static bool IsSafeId(string id)
+		{
+			if(id.Length == 0) {
+				return false;
+			}
+			return !_reg.Match(id).Success;
+		}
+		public static string ToSafeId(string id) 
+		{
+			var result = _reg.Replace(id, "_");
+			return result;
+		}
+		
+		private string id;
+		
+		public Item()
+		{
+			
+		}
+		
+		/// <summary>
+		/// 
+		/// </summary>
+		public virtual string Id 
+		{ 
+			get { return this.id; }
+			set 
+			{
+				if(this.id != null && !IsSafeId(value)) {
+					throw new Exception(id);
+				}
+				this.id = value;  
+			}
+		}
+		
+		/// <summary>
+		/// XML要素出力。
+		/// 
+		/// メソッドをオーバーライドする場合、スーパークラスのメソッド戻り値を使用すること。
+		/// </summary>
+		/// <param name="xml"></param>
+		/// <returns></returns>
+		public virtual XmlElement ToXmlElement(XmlDocument xml, ExportArgs exExport)
+		{
+			var result = xml.CreateElement("item");
+			
+			result.SetAttribute(ID, Id);
+			
+			return result;
+		}
+		
+		/// <summary>
+		/// XML要素入力
+		/// 
+		/// メソッドをオーバーライドする場合、スーパークラスから先に呼び出すこと。
+		/// </summary>
+		/// <param name="xml"></param>
+		/// <returns></returns>
+		public virtual void FromXmlElement(XmlElement element)
+		{
+			var id = element.GetAttribute(ID);
+			Id = id;
+		}
+	}
+
+	public class ItemContainer<TItem>
+		where TItem: Item
+	{		
+		/// <summary>
+		/// 
+		/// </summary>
+		SortedDictionary<string, TItem> map = new SortedDictionary<string, TItem>();
+		
+		/// <summary>
+		/// 
+		/// </summary>
+		public ItemContainer()
+		{
+		}
+		
+		public int Count 
+		{
+			get { return this.map.Count; }
+		}
+		
+		/// <summary>
+		/// 
+		/// </summary>
+		public virtual TItem this[string id] 
+		{
+			get 
+			{
+				return this.map[id];
+			}
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		public SortedDictionary<string, TItem>.KeyCollection Ids
+		{
+			get { return this.map.Keys; }
+		}
+		/// <summary>
+		/// 
+		/// </summary>
+		public SortedDictionary<string, TItem>.ValueCollection Items
+		{
+			get { return this.map.Values; }
+		}
+		
+		public void Clear()
+		{
+			this.map.Clear();
+		}
+		
+		public void Valid()
+		{
+			var list = new List<string>();
+			foreach(var data in this.map)
+			{
+				if(data.Key != data.Value.Id) {
+					list.Add(data.Key);
+				} else if(!Item.IsSafeId(data.Key)) {
+					list.Add(data.Key);
+				}
+			}
+			if(list.Count > 0) {
+				throw new Exception(string.Join(Environment.NewLine, list));
+			}
+		}
+		
+		public bool ContainsId(string id)
+		{
+			return this.map.ContainsKey(id);
+		}
+		
+		public bool TryGetItem(string id, out TItem value)
+		{
+			return this.map.TryGetValue(id, out value);
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="item"></param>
+		public void Set(TItem item)
+		{
+			this.map[item.Id] = item;
+		}
+		
+		public void SetRange(IEnumerable<TItem> items)
+		{
+			foreach(var item in items) {
+				Set(item);
+			}
+		}
+		
+		public void ChangeId(string fromId, string toId)
+		{
+			Debug.Assert(this.map.ContainsKey(fromId));
+			Debug.Assert(!this.map.ContainsKey(toId));
+			
+			var tempItem = this.map[fromId];
+			this.map.Remove(fromId);
+			tempItem.Id = toId;
+			this.Set(tempItem);
+		}
+		
+		public TItem Remove(string itemId)
+		{
+			var item = this.map[itemId];
+			this.map.Remove(itemId);
+			return item;
+		}
+		
+		public TItem Remove(TItem item)
+		{
+			return Remove(item.Id);
+		}
+		
+	}
+
+}
---
+++ Pe/Pe/Logic/Item/LanguageItem.cs
@@ -0,0 +1,120 @@
+﻿/*
+ * SharpDevelopによって生成
+ * ユーザ: sk
+ * 日付: 2013/08/30
+ * 時刻: 20:40
+ * 
+ * このテンプレートを変更する場合「ツール→オプション→コーディング→標準ヘッダの編集」
+ */
+using System;
+using System.Text.RegularExpressions;
+using System.Xml;
+
+using Pe.IF;
+
+namespace Pe.Logic
+{
+	public class LanguageItem: Item
+	{
+		const string WORD = "word";
+		
+		private string text = null;
+		
+		public LanguageItem(LanguageItemContainer lang)
+		{
+			LanguageItemContainer = lang;
+		}
+		
+		public LanguageItemContainer LanguageItemContainer { get; internal protected set; }
+		
+		/// <summary>
+		/// 
+		/// </summary>
+		public string Word { get; set; }
+		
+		/// <summary>
+		/// Wordからの変換値
+		/// </summary>
+		public string Text
+		{
+			get
+			{
+				if(this.text == null) {
+					
+					var word = Word;
+					
+					if(word.IndexOf('$') != -1) {
+						var repResult = Regex.Replace(word, @"\$\{(.+)\}", (Match m) => {
+							string s = m.Value;
+							var id = s.Substring("${".Length, s.Length - "${}".Length);
+							//return Language[id].Text;
+							return string.Empty;
+						});
+						word = repResult;
+					}
+					
+					this.text = word;
+				}
+				
+				return this.text;
+			}
+		}
+		
+		public override XmlElement ToXmlElement(XmlDocument xml, ExportArgs exExport)
+		{
+			var result = base.ToXmlElement(xml, exExport);
+			
+			result.SetAttribute(WORD, Word);
+			
+			return result;
+		}
+		
+		public override void FromXmlElement(XmlElement element)
+		{
+			base.FromXmlElement(element);
+			
+			var word = element.GetAttribute(WORD);
+			
+			Word = word;
+		}
+	}
+	
+	
+	public class LanguageItemContainer: ItemContainer<LanguageItem>, IExportXmlElement, IImportXmlElement
+	{
+		public LanguageItemContainer()
+		{
+		}
+		
+		/// <summary>
+		/// 
+		/// </summary>
+		public override LanguageItem this[string id] {
+			get 
+			{
+				LanguageItem item = null;
+				if(TryGetItem(id, out item)) {
+					return item;
+				}
+				
+				item = new LanguageItem(this);
+				item.Id = id;
+				item.Word = string.Format("<{0}>", id);
+				return item; 
+			}
+		}
+		
+		public XmlElement ToXmlElement(XmlDocument xml, ExportArgs exExport)
+		{
+			throw new NotImplementedException();
+		}
+		public void FromXmlElement(XmlElement xml)
+		{
+			foreach(XmlElement element in xml.GetElementsByTagName("item")) {
+				var item = new LanguageItem(this);
+				item.FromXmlElement(element);
+				Set(item);
+			}
+		}
+	}
+}
