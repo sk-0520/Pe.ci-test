1286d61ff24d67771f774f9f35c7e880ec599c2c
sk <content.text.net@gmail.com>
2013-11-04 02:27:40 +0900

    utility

---
+++ Pe/PeUtility/Text.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * SharpDevelopによって生成
+ * ユーザ: sk
+ * 日付: 2013/11/04
+ * 時刻: 1:56
+ * 
+ * このテンプレートを変更する場合「ツール→オプション→コーディング→標準ヘッダの編集」
+ */
+using System;
+using System.Collections.Generic;
+
+namespace PeUtility
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public delegate string TextUnique(string source, int index);
+	
+	/// <summary>
+	/// 
+	/// </summary>
+	public static class Text
+	{
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="target"></param>
+		/// <param name="list"></param>
+		/// <param name="dg"></param>
+		/// <returns></returns>
+		public static string ToUnique(this string target, IEnumerable<string> list, TextUnique dg = null)
+		{
+			if(dg == null) {
+				dg = (string source, int index) => string.Format("{0}({1})", source, index);
+			}
+			var changeName = target;
+			int n = 1;
+			RETRY:
+			foreach(var value in list) {
+				if(value == changeName) {
+					changeName = dg(target, ++n);
+					goto RETRY;
+				}
+			}
+			return changeName;
+		}
+	}
+}
