b447ba4c84f15d0c68e9de59ad229556b3e85ce0
sk <content.text.net@gmail.com>
2014-02-15 13:05:39 +0900

    ちょこっと定義

--- Pe/PeMain/Logic/ScreenUtility.cs
+++ Pe/PeMain/Logic/ScreenUtility.cs
@@ -7,7 +7,9 @@
  * このテンプレートを変更する場合「ツール→オプション→コーディング→標準ヘッダの編集」
  */
 using System;
+using System.Diagnostics;
 using System.Linq;
+using System.Management;
 using System.Windows.Forms;
 
 namespace PeMain.Logic
@@ -17,9 +19,18 @@ namespace PeMain.Logic
 	/// </summary>
 	public static class ScreenUtility
 	{
-		public static string ToScreenName(Screen screen) 
+		public static string ToScreenName(Screen screen)
 		{
 			// TODO: ディスプレイ名称
+			/*
+			using(var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_DesktopMonitor")) {
+				foreach(var currentObj in searcher.Get()) {
+					String name = currentObj["Name"].ToString();
+					String device_id = currentObj["DeviceID"].ToString();
+					Debug.WriteLine("{0} - {1}, {2}", name, device_id.Length, currentObj);
+				}
+			}
+			*/
 			return screen.DeviceName;
 		}
 		public static string ToScreenName(string screenName)
