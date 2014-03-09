e5094da2e2939843e7099b521bd0f1c5d02ff1fd
sk <content.text.net@gmail.com>
2014-03-09 21:38:14 +0900

    M_NOTE_GROUP

---
+++ Pe/PeMain/Data/DB/Entity/M_NOTE_GROUP.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * SharpDevelopによって生成
+ * ユーザ: sk
+ * 日付: 2014/03/09
+ * 時刻: 21:29
+ * 
+ * このテンプレートを変更する場合「ツール→オプション→コーディング→標準ヘッダの編集」
+ */
+using System;
+using System.Drawing;
+using PeUtility;
+
+namespace PeMain.Data.DB
+{
+	/// <summary>
+	/// Description of M_NOTE_GROUP.
+	/// </summary>
+	[TargetName("M_NOTE_GROUP")]
+	public class MNoteGroupEntity: CommonDataEntity
+	{
+		[TargetName("GROUP_ID")]
+		public long Id { get; set ;}
+		[TargetName("GROUP_TITLE")]
+		public string Title {get; set; }
+		// mixinつかいてぇ
+		[TargetName("FONT_FAMILY")]
+		public string FontFamily { get; set; }
+		[TargetName("FONT_SIZE")]
+		public float FontHeight { get; set; }
+		[TargetName("FONT_ITALIC")]
+		public bool FontItalic { get; set; }
+		[TargetName("FONT_BOLD")]
+		public bool FontBold { get; set; }
+		[TargetName("COLOR_FORE")]
+		public Color ForeColor { get; set; }
+		[TargetName("COLOR_BACK")]
+		public Color BackColor { get; set; }
+	}
+}
