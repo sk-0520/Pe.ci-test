aedc2b97ad20d6f62f47a9fb744a48c965cb1512
sk <content.text.net@gmail.com>
2014-03-11 00:57:23 +0900

    コメント

--- Pe/PeUtility/DBManager.cs
+++ Pe/PeUtility/DBManager.cs
@@ -18,14 +18,31 @@ using System.Text.RegularExpressions;
 
 namespace PeUtility
 {
+	/// <summary>
+	/// 条件が偽の場合にコマンドと条件式のどちらを使用するか。
+	/// </summary>
 	public enum FalseCondition
 	{
+		/// <summary>
+		/// コマンドを使用する。
+		/// </summary>
 		Command,
+		/// <summary>
+		/// 条件式を使用する。
+		/// </summary>
 		Expression,
 	}
 	
+	/// <summary>
+	/// 条件式からコマンドの構築。
+	/// 
+	/// DBManagerに渡すコマンドに対してさらに実装側で条件分岐を行う。
+	/// </summary>
 	public class CommandExpression
 	{
+		/// <summary>
+		/// 条件式のデフォルト値生成。
+		/// </summary>
 		public CommandExpression()
 		{
 			Condition = false;
@@ -35,7 +52,13 @@ namespace PeUtility
 			FalseExpression = null;
 		}
 		
-		
+		/// <summary>
+		/// 条件式を指定値で生成。
+		/// 
+		/// 偽の場合は空文字列となる。
+		/// </summary>
+		/// <param name="condition">条件</param>
+		/// <param name="trueCommand">真の場合のコマンド。</param>
 		public CommandExpression(bool condition, string trueCommand): this()
 		{
 			Condition = condition;
@@ -43,6 +66,12 @@ namespace PeUtility
 			FalseCondition = FalseCondition.Command;
 		}
 		
+		/// <summary>
+		/// 条件式を指定値で生成。
+		/// </summary>
+		/// <param name="condition">条件</param>
+		/// <param name="trueCommand">真の場合のコマンド</param>
+		/// <param name="falseCommand">偽の場合のコマンド</param>
 		public CommandExpression(bool condition, string trueCommand, string falseCommand): this()
 		{
 			Condition = condition;
@@ -51,6 +80,12 @@ namespace PeUtility
 			FalseCommand = falseCommand;
 		}
 		
+		/// <summary>
+		/// 条件式を指定値で生成。
+		/// </summary>
+		/// <param name="condition">条件</param>
+		/// <param name="trueCommand">真の場合のコマンド</param>
+		/// <param name="commandExpression">偽の場合の条件式</param>
 		public CommandExpression(bool condition, string trueCommand, CommandExpression commandExpression): this()
 		{
 			Condition = condition;
@@ -79,6 +114,10 @@ namespace PeUtility
 		/// </summary>
 		public CommandExpression FalseExpression { get; private set; }
 		
+		/// <summary>
+		/// 条件をコマンドに落とし込む。
+		/// </summary>
+		/// <returns></returns>
 		public string ToCode()
 		{
 			if(Condition) {
@@ -98,7 +137,9 @@ namespace PeUtility
 	
 	
 	/// <summary>
+	/// DTO, Entity で使用するカラム名。
 	/// 
+	/// Entity の場合はテーブル名まで指定する。
 	/// </summary>
 	[AttributeUsage(
 		AttributeTargets.Class  | AttributeTargets.Property,
@@ -115,6 +156,9 @@ namespace PeUtility
 		public string TargetName { get; private set; }
 	}
 	
+	/// <summary>
+	/// エンティティをテーブル名・カラム名から構築する。
+	/// </summary>
 	public sealed class EntitySet
 	{
 		public EntitySet(string tableName, IDictionary<string, string> columnPropName)
@@ -122,11 +166,24 @@ namespace PeUtility
 			TableName = tableName;
 			ColumnPropertyMap = columnPropName;
 		}
-		
+		/// <summary>
+		/// テーブル名。
+		/// </summary>
 		public string TableName { get ; private set; }
+		/// <summary>
+		/// カラム名。
+		/// 
+		/// キー: カラム名
+		/// 値: プロパティ名
+		/// </summary>
 		public IDictionary<string, string> ColumnPropertyMap { get; private set; }
 	}
 	
+	/// <summary>
+	/// Entity, DTO の共通クラス。
+	/// 
+	/// 特に何もしない。
+	/// </summary>
 	public abstract class DbData
 	{ }
 	
@@ -149,11 +206,15 @@ namespace PeUtility
 
 	/// <summary>
 	/// DB接続・操作の一元化
-	/// 
-	/// シングルスレッドで使用しないとトランザクションあたりが爆発する。しゃあない。
 	/// </summary>
 	public abstract class DBManager
 	{
+		/// <summary>
+		/// 生成。
+		/// 
+		/// </summary>
+		/// <param name="connection">コネクション</param>
+		/// <param name="isOpened">コネクションは開いているか。閉じている場合は開く。</param>
 		public DBManager(DbConnection connection, bool isOpened)
 		{
 			Parameter = new Dictionary<string, object>();
@@ -167,39 +228,78 @@ namespace PeUtility
 				Connection.Open();
 			}
 		}
+		/// <summary>
+		/// DB接続。
+		/// </summary>
 		public DbConnection Connection { get; private set; }
+		/// <summary>
+		/// トランザクションの開始
+		/// </summary>
+		/// <returns></returns>
 		public DbTransaction BeginTransaction()
 		{
 			var tran = Connection.BeginTransaction();
 			
 			return tran;
 		}
-		
+		/// <summary>
+		/// 条件式パターン。
+		/// </summary>
 		public string ConditionPattern { get; set; }
-		
+		/// <summary>
+		/// パラメータ。
+		/// </summary>
 		public Dictionary<string, object> Parameter { get; private set; }
+		/// <summary>
+		/// 条件式。
+		/// </summary>
 		public Dictionary<string, CommandExpression> Expression { get; private set; }
-		
+		/// <summary>
+		/// 条件式の生成。
+		/// </summary>
+		/// <returns></returns>
 		public virtual CommandExpression CreateExpresstion()
 		{
 			return new CommandExpression();
 		}
-		
+		/// <summary>
+		/// パラメータ・条件式のクリア。
+		/// </summary>
 		public void Clear()
 		{
 			Parameter.Clear();
 			Expression.Clear();
 		}
-		
-		public DbCommand CreateCommand()
+		/// <summary>
+		/// コマンド生成。
+		/// 
+		/// ユーザーコードでは多分出番ない、はず。
+		/// </summary>
+		/// <returns></returns>
+		protected virtual DbCommand CreateCommand()
 		{
 			return Connection.CreateCommand();
 		}
 		
+		/// <summary>
+		/// 型変換。
+		/// 
+		/// キャストでなく実際の変換処理も担当する
+		/// </summary>
+		/// <param name="value"></param>
+		/// <param name="toType"></param>
+		/// <returns></returns>
 		protected virtual object To(object value, Type toType)
 		{
 			return value;
 		}
+		/// <summary>
+		/// 型変換。
+		/// 
+		/// 実際の型変換には object To(object, Type) を使用する。
+		/// </summary>
+		/// <param name="value"></param>
+		/// <returns></returns>
 		public T To<T>(object value)
 		{
 			return (T)(To(value, typeof(T)));
@@ -265,6 +365,13 @@ namespace PeUtility
 			throw new ArgumentException(type.ToString());
 		}
 		
+		/// <summary>
+		/// パラメータの生成。
+		/// </summary>
+		/// <param name="command">コマンド</param>
+		/// <param name="name">パラメータ名</param>
+		/// <param name="value">パラメータ値</param>
+		/// <returns>パラメータ</returns>
 		protected virtual DbParameter MakeParameter(DbCommand command, string name, object value)
 		{
 			var param = command.CreateParameter();
@@ -277,7 +384,11 @@ namespace PeUtility
 			}
 			return param;
 		}
-		
+		/// <summary>
+		/// 現在設定されているパラメータを作成。
+		/// </summary>
+		/// <param name="command">コマンド</param>
+		/// <returns>パラメータ一覧</returns>
 		protected DbParameter[] MakeParameterList(DbCommand command)
 		{
 			var list = new List<DbParameter>(Parameter.Count);
@@ -288,7 +399,11 @@ namespace PeUtility
 			
 			return list.ToArray();
 		}
-		
+		/// <summary>
+		/// 現在設定されているパラメータをコマンドに設定。
+		/// </summary>
+		/// <param name="command">コマンド</param>
+		/// <returns>設定の有無</returns>
 		protected bool SetParameter(DbCommand command)
 		{
 			if(Parameter.Count > 0) {
@@ -302,7 +417,11 @@ namespace PeUtility
 			
 			return false;
 		}
-		
+		/// <summary>
+		/// 条件式をパターンから置き換え。
+		/// </summary>
+		/// <param name="code"></param>
+		/// <returns></returns>
 		protected virtual string ExpressionReplace(string code)
 		{
 			if(Expression.Count == 0) {
@@ -315,6 +434,12 @@ namespace PeUtility
 			return replacedCode;
 		}
 		
+		/// <summary>
+		/// 現在の指定値からコマンド実行。
+		/// </summary>
+		/// <param name="func"></param>
+		/// <param name="code">コマンド</param>
+		/// <returns></returns>
 		private T Executer<T>(Func<DbCommand,T> func, string code)
 		{
 			using(var command = CreateCommand()) {
@@ -323,12 +448,20 @@ namespace PeUtility
 				return func(command);
 			}
 		}
-		
+		/// <summary>
+		/// 現在の指定値からコマンド実行。
+		/// </summary>
+		/// <param name="code"></param>
+		/// <returns></returns>
 		public DbDataReader ExecuteReader(string code)
 		{
 			return Executer(command => command.ExecuteReader(), code);
 		}
-		
+		/// <summary>
+		/// 現在の指定値からコマンド実行。
+		/// </summary>
+		/// <param name="code"></param>
+		/// <returns>影響を受けた行数</returns>
 		public int ExecuteCommand(string code)
 		{
 			return Executer(command => command.ExecuteNonQuery(), code);
@@ -358,6 +491,11 @@ namespace PeUtility
 			return propMap;
 		}
 		
+		/// <summary>
+		/// 指定値からコマンドを実行
+		/// </summary>
+		/// <param name="code"></param>
+		/// <returns>Tの集合</returns>
 		private IEnumerable<T> GetDtoListImpl<T>(string code)
 			where T: Dto, new()
 		{
@@ -377,17 +515,25 @@ namespace PeUtility
 			}
 		}
 		
-		
+		/// <summary>
+		/// 指定値からコマンドを実行
+		/// </summary>
+		/// <param name="code"></param>
+		/// <returns>T単体</returns>
 		public T GetDtoSingle<T>(string code)
 			where T: Dto, new()
 		{
 			return GetDtoListImpl<T>(code).Single();
 		}
-		
-		public List<T> GetDtoList<T>(string code)
+		/// <summary>
+		/// 指定値からコマンドを実行
+		/// </summary>
+		/// <param name="code"></param>
+		/// <returns>T集合</returns>
+		public IEnumerable<T> GetDtoList<T>(string code)
 			where T: Dto, new()
 		{
-			return GetDtoListImpl<T>(code).ToList();
+			return GetDtoListImpl<T>(code);
 		}
 		
 		private EntitySet GetEntitySet<T>()
@@ -432,19 +578,27 @@ namespace PeUtility
 				ExecuteCommand(code);
 			}
 		}
-		
+		/// <summary>
+		/// Entityの挿入。
+		/// </summary>
+		/// <param name="entityList"></param>
 		public void ExecuteInsert<T>(IList<T> entityList)
 			where T: Entity
 		{
 			ExecuteEntityCommand(entityList, CreateInsertCommandCode);
 		}
-		
+		/// <summary>
+		/// Entityの更新。
+		/// </summary>
+		/// <param name="entityList"></param>
 		public void ExecuteUpdate<T>(IList<T> entityList)
 			where T: Entity
 		{
 			ExecuteEntityCommand(entityList, CreateUpdateCommandCode);
 		}
-		
+		/// <summary>
+		/// とじるん。
+		/// </summary>
 		public void Close()
 		{
 			Connection.Close();
