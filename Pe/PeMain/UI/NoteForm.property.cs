ce5a75ef3c263fbe6b3c3f9d80a1bd81f2b81743
sk <content.text.net@gmail.com>
2014-03-12 04:32:33 +0900

    NoteForm

---
+++ Pe/PeMain/UI/NoteForm.property.cs
@@ -0,0 +1,16 @@
+﻿/*
+ * SharpDevelopによって生成
+ * ユーザ: sk
+ * 日付: 2014/03/12
+ * 時刻: 4:30
+ * 
+ * このテンプレートを変更する場合「ツール→オプション→コーディング→標準ヘッダの編集」
+ */
+using System;
+
+namespace PeMain.UI
+{
+	partial class NoteForm
+	{
+	}
+}
