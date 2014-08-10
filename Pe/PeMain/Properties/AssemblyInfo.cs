7c65c7ce483b4273e90a512bcc4e3463b5c494b6
sk <content.type.text.net@gmail.com>
2014-08-10 12:24:03 +0900

    #8

--- Pe/PeMain/Properties/AssemblyInfo.cs
+++ Pe/PeMain/Properties/AssemblyInfo.cs
@@ -9,8 +9,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PeMain")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Pe")]
+[assembly: AssemblyDescription("プログラムランチャー")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PeMain")]
