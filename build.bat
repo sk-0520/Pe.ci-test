33df1a5a53060e7bcc0cdc2842d0841f0aaf5a7d
sk <content.type.text.net@gmail.com>
2014-09-16 02:36:50 +0900

    7z -> cab にしてみたけどデブいのはともかく平べったいよこれ！

--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
 cd /d %~dp0
 
+set OUTPUT=output\Release
 set DOTNETVER=v4.0.30319
 if "%PROCESSOR_ARCHITECTURE%" NEQ "x86" (
 	set MB=%windir%\microsoft.net\framework64\%DOTNETVER%\msbuild
@@ -11,4 +12,16 @@ echo "x86"
 "%MB%" Pe\Pe.sln /p:Configuration=Release;Platform=x86 /t:Rebuild /m
 echo "x64"
 "%MB%" Pe\Pe.sln /p:Configuration=Release;Platform=x64 /t:Rebuild /m
+
+echo compression
+
+dir %OUTPUT%\x86 /s /b /a-d >%OUTPUT%\files.x86
+makecab /d "CabinetName1=Pe_x86.cab" /f %OUTPUT%\files.x86
+
+dir %OUTPUT%\x64 /s /b /a-d >%OUTPUT%\files.x64
+makecab /d "CabinetName1=Pe_x64.cab" /f %OUTPUT%\files.x64
+
+move /Y disk1\* %OUTPUT%
+move /Y setup.* %OUTPUT%
+rmdir disk1 /q
 
