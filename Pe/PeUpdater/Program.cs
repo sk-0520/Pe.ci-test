a82171a684b1c32f14c4ab0f6ec495c979aa543f
sk <content.type.text.net@gmail.com>
2014-09-25 22:54:36 +0900

    PeUpdater

---
+++ Pe/PeUpdater/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * SharpDevelopによって生成
+ * ユーザ: sk
+ * 日付: 2014/09/25
+ * 時刻: 22:43
+ * 
+ * このテンプレートを変更する場合「ツール→オプション→コーディング→標準ヘッダの編集」
+ */
+using System;
+
+namespace PeUpdater
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
