bae67c24481b58b20348ee16f09cfb1d20bdc3ff
sk <content.type.text.net@gmail.com>
2014-10-27 23:40:22 +0900

    GetScreens

--- Pe/PeMain/Logic/ScreenUtility.cs
+++ Pe/PeMain/Logic/ScreenUtility.cs
@@ -7,12 +7,12 @@
  * このテンプレートを変更する場合「ツール→オプション→コーディング→標準ヘッダの編集」
  */
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Management;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
-
 using PeMain.Data;
 using PeMain.IF;
 using PInvoke.Windows;
@@ -25,16 +25,38 @@ namespace PeMain.Logic
 	/// </summary>
 	public static class ScreenUtility
 	{
-		/// <summary>
-		/// 
-		/// </summary>
-		/// <param name="screen"></param>
-		/// <returns></returns>
-		public static string GetScreenName(Screen screen)
+		private static string NeviceToId(string deviceName)
 		{
-			return GetScreenName(screen, null);
+			return new string(deviceName.Trim().SkipWhile(c => !char.IsNumber(c)).ToArray());
 		}
 		
+		private static IEnumerable<Win32_DesktopMonitor> GetScreens(string deviceName, ILogger logger)
+		{
+			string query = "SELECT * FROM Win32_DesktopMonitor";
+			if(!string.IsNullOrWhiteSpace(deviceName)) {
+				//var id = new string(deviceName.Trim().SkipWhile(c => !char.IsNumber(c)).ToArray());
+				var id = NeviceToId(deviceName);
+				query = string.Format("SELECT * FROM Win32_DesktopMonitor where DeviceID like \"DesktopMonitor{0}\"", id);
+			}
+			using(var searcher = new ManagementObjectSearcher(query)) {
+				foreach(ManagementObject mng in searcher.Get()) {
+					var item = new Win32_DesktopMonitor();
+					try {
+						item.Import(mng);
+					} catch(Exception ex) {
+						if(logger != null) {
+							logger.Puts(LogType.Warning, ex.Message, ex);
+						} else {
+							Debug.WriteLine(ex);
+						}
+						continue;
+					}
+					
+					yield return item;
+				}
+			}
+		}
+
 		/// <summary>
 		/// スクリーンの名前を取得。
 		/// </summary>
@@ -42,6 +64,7 @@ namespace PeMain.Logic
 		/// <returns></returns>
 		public static string GetScreenName(Screen screen, ILogger logger)
 		{
+			/*
 			var id = new string(screen.DeviceName.Trim().SkipWhile(c => !char.IsNumber(c)).ToArray());
 			var query = string.Format("SELECT * FROM Win32_DesktopMonitor where DeviceID like \"DesktopMonitor{0}\"", id);
 			using(var searcher = new ManagementObjectSearcher(query)) {
@@ -62,8 +85,15 @@ namespace PeMain.Logic
 					break;
 				}
 			}
+			 */
+			foreach(var screem in GetScreens(screen.DeviceName, logger)) {
+				if(!string.IsNullOrWhiteSpace(screem.Name)) {
+					var id = NeviceToId(screen.DeviceName);
+					return string.Format("{0}. {1}", id, screem.Name);
+				}
+				break;
+			}
 			
-			//*/
 			var device = new DISPLAY_DEVICE();
 			device.cb = Marshal.SizeOf(device);
 			API.EnumDisplayDevices(screen.DeviceName, 0, ref device, 1);
@@ -71,6 +101,17 @@ namespace PeMain.Logic
 			//return screen.DeviceName;
 			return device.DeviceString;
 		}
+		
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="screen"></param>
+		/// <returns></returns>
+		public static string GetScreenName(Screen screen)
+		{
+			return GetScreenName(screen, null);
+		}
+		
 		public static string GetScreenName(string screenDeviceName, ILogger logger)
 		{
 			var screen = Screen.AllScreens.SingleOrDefault(s => s.DeviceName == screenDeviceName);
