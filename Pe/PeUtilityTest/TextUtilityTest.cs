288f2be6be13360028f79515eb571ed5e6e33b36
sk <content.type.text.net@gmail.com>
2014-11-22 18:22:31 +0900

    nunit test

---
+++ Pe/PeUtilityTest/TextUtilityTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using PeUtility;
+
+namespace PeUtilityTest
+{
+	[TestFixture]
+	public class TextUtilityTest
+	{
+		[TestCase("a", new [] { "a" }, null, "a(2)")]
+		public void ToUnique(string src, IEnumerable<string> list, Func<string, int, string> dg, string result)
+		{
+			Assert.IsTrue(TextUtility.ToUnique(src, list, dg) == result);
+		}
+	}
+}
