8d65870fb825c622381fe60b3ecb19b3fb8c62f9
sk <content.type.text.net@gmail.com>
2014-12-23 15:10:13 +0900

    init rc-0.38.0

--- Pe/PeMain/Properties/AssemblyInfo.cs
+++ Pe/PeMain/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("0.37.0.*")]
+[assembly: AssemblyVersion("0.37.1.*")]
 [assembly: AssemblyInformationalVersion("revision")]
