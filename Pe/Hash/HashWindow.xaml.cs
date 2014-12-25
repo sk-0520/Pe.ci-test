eb9a4a0e134e9cb47674129fe0b78c51b8e9c351
sk <content.type.text.net@gmail.com>
2014-12-25 23:01:56 +0900

    コンポーネント

--- Pe/Hash/HashWindow.xaml.cs
+++ Pe/Hash/HashWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Hash
+namespace ContentTypeTextNet.Pe.Applications.Hash
 {
 	/// <summary>
 	/// MainWindow.xaml の相互作用ロジック
