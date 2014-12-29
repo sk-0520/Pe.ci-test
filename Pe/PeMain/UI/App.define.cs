af5cb06e7c5b1758ae7a13744a2ae8e7de3a0cf0
sk <content.type.text.net@gmail.com>
2014-12-29 22:35:48 +0900

    StartupLogger

--- Pe/PeMain/UI/App.define.cs
+++ Pe/PeMain/UI/App.define.cs
@@ -48,30 +48,6 @@ namespace ContentTypeTextNet.Pe.PeMain.UI
 		const string menuNameSystemEnvClipboard = "menu_systemenv_clipboard";
 		
 
-		class StartupLogger: ILogger
-		{
-			List<LogItem> _logList;
-			
-			public FileLogger FileLogger { get; set; }
-			
-			public StartupLogger(FileLogger fileLogger)
-			{
-				FileLogger = fileLogger;
-				this._logList = new List<LogItem>();
-			}
-			
-			public void Puts(LogType logType, string title, object detail, int frame = 2)
-			{
-				var logItem = new LogItem(logType, title, detail, frame);
-				FileLogger.WiteItem(logItem);
-				this._logList.Add(logItem);
-			}
-			
-			public IEnumerable<LogItem> GetList()
-			{
-				return this._logList;
-			}
-		}
 		
 		class Listener
 		{
