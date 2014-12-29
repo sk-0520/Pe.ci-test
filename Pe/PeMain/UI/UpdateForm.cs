6a77b1647eb139e883bc78c78b53c067b5962148
sk <content.type.text.net@gmail.com>
2014-12-30 00:14:19 +0900

    UpdateForm

--- Pe/PeMain/UI/UpdateForm.cs
+++ Pe/PeMain/UI/UpdateForm.cs
@@ -7,7 +7,13 @@
  * このテンプレートを変更する場合「ツール→オプション→コーディング→標準ヘッダの編集」
  */
 using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Net;
 using System.Windows.Forms;
+using ContentTypeTextNet.Pe.Library.Utility;
+using ContentTypeTextNet.Pe.PeMain.Data;
 using ContentTypeTextNet.Pe.PeMain.IF;
 using ContentTypeTextNet.Pe.PeMain.Logic;
 
@@ -20,6 +26,15 @@ namespace ContentTypeTextNet.Pe.PeMain.UI
 	/// </summary>
 	public partial class UpdateForm : Form, ISetCommonData
 	{
+		#region define
+		#endregion ////////////////////////////////////
+
+		#region static
+		#endregion ////////////////////////////////////
+
+		#region variable
+		#endregion ////////////////////////////////////
+
 		public UpdateForm()
 		{
 			//
@@ -32,7 +47,77 @@ namespace ContentTypeTextNet.Pe.PeMain.UI
 			//
 			Initialize();
 		}
+
+
+		#region property
+		CommonData CommonData { get; set; }
+		public UpdateData UpdateData { get; set; }
+		#endregion ////////////////////////////////////
+
+		#region ISetCommonData
+		public void SetCommonData(CommonData commonData)
+		{
+			CommonData = commonData;
+
+			ApplyLanguage();
+			ApplySetting();
+
+			ApplyUpdate();
+		}
+		#endregion ////////////////////////////////////
+
+		#region override
+		#endregion ////////////////////////////////////
+
+		#region initialize
+		void Initialize()
+		{
+			PointingUtility.AttachmentDefaultButton(this);
+			WebBrowserUtility.AttachmentNewWindow(this.webUpdate);
+		}
+		#endregion ////////////////////////////////////
+
+		#region language
+		void ApplyLanguage()
+		{
+			UIUtility.SetDefaultText(this, CommonData.Language);
+			this.AcceptButton = null;
+
+			var map = new Dictionary<string, string>() {
+				{ AppLanguageName.versionNow,  Literal.ApplicationVersion },
+				{ AppLanguageName.versionNext, UpdateData.Info.Version },
+				{ AppLanguageName.versionType, UpdateData.Info.IsRcVersion ? "${version-rc}": "${version-release}" },
+			};
+			var version = CommonData.Language[this.labelVersion.Text.Substring(1)];
+			version = CommonData.Language[version, map];
+			version = CommonData.Language[version, map];
+			version = CommonData.Language[version, map];
+			this.labelVersion.Text = version;
+		}
+		#endregion ////////////////////////////////////
+
+		#region function
+		void ApplySetting()
+		{
+			if(UpdateData.Info.IsRcVersion) {
+				this.labelVersion.BorderStyle = BorderStyle.FixedSingle;
+				this.labelVersion.ForeColor = Color.Red;
+				this.labelVersion.BackColor = Color.Black;
+			}
+		}
 		
+		void ApplyUpdate()
+		{
+			byte[] httpData = null;
+			using(var web = new WebClient()) {
+				var url = UpdateData.Info.IsRcVersion ? Literal.ChangeLogRcURL: Literal.ChangeLogURL; 
+				httpData = web.DownloadData(url);
+			}
+			this.webUpdate.DocumentStream = new MemoryStream(httpData);
+//			WebBrowserUtility.AttachmentNavigating(this.webUpdate);
+		}
+		#endregion ////////////////////////////////////
+
 		void CommandOk_Click(object sender, EventArgs e)
 		{
 			DialogResult = DialogResult.OK;
