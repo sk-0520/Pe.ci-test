3ae27a3695c1ee8b26a4c180f02c1cde6ba30395
sk <content.type.text.net@gmail.com>
2015-01-02 15:51:18 +0900

    EVDictionary

--- Pe/Common-Applications/EnvironmentVariable.cs
+++ Pe/Common-Applications/EnvironmentVariable.cs
@@ -3,26 +3,52 @@ using System.Collections.Generic;
 
 namespace ContentTypeTextNet.Pe.Applications
 {
-	public static class EnvironmentVariableLiteral
+	/// <summary>
+	/// 環境変数名。
+	/// </summary>
+	public static class EVLiteral
 	{
 		public const string systemExecuteFilePath = "PE_SYS_APP_EXE";
 		public const string systemDirectoryPath = "PE_SYS_APP_DIR";
 		public const string systemSettingDirectoryPath = "PE_SYS_SETTING_DIR";
+		public const string systemLogDirectoryPath = "PE_SYS_LOG_DIR";
 
 		public const string applicationSettingBaseDirectoryPath = "PE_APP_SETTING_BASE_DIR";
 		public const string applicationSettingDirectoryPath = "PE_APP_SETTING_DIR";
+		public const string applicationLogDirectoryPath = "PE_APP_LOG_DIR";
+
+		/// <summary>
+		/// 環境変数名一覧。
+		/// </summary>
+		/// <returns></returns>
+		public static IEnumerable<string> GetEVNames()
+		{
+			return new [] {
+				systemExecuteFilePath,
+				systemDirectoryPath,
+				systemSettingDirectoryPath,
+				systemLogDirectoryPath,
+				// --------------------------------------
+				applicationSettingBaseDirectoryPath,
+				applicationSettingDirectoryPath,
+				applicationLogDirectoryPath,
+			};
+		}
 	}
 
-	public class EnvironmentVariableDictionary: Dictionary<string, string>, IReadOnlyDictionary<string, string>
+	/// <summary>
+	/// Pe環境変数。
+	/// </summary>
+	public class EVDictionary: Dictionary<string, string>
 	{
-		public EnvironmentVariableDictionary()
+		/// <summary>
+		/// 呼び出されるアプリケーション側で実行。
+		/// </summary>
+		public EVDictionary()
 		{
-		}
-
-		public EnvironmentVariableDictionary(IDictionary<string, string> app)
-		{
-			foreach(var pair in app) {
-				Add(pair.Key, pair.Value);
+			foreach(var name in EVLiteral.GetEVNames()) {
+				var value = Environment.GetEnvironmentVariable(name);
+				Add(name, name);
 			}
 		}
 	}
