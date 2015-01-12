621947da5e803949395b3c2f0ca7b357eda95a36
sk <content.type.text.net@gmail.com>
2015-01-12 15:02:28 +0900

    Resource

---
+++ Pe/Skin/SkinResource.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContentTypeTextNet.Pe.Library.Skin
+{
+	public enum SkinImage
+	{
+		Add,
+		AddItem,
+		Applications,
+		Changelog,
+		Clear,
+		Clipboard,
+		ClipboardCopy,
+		ClipboardFile,
+		ClipboardHtml,
+		ClipboardImage,
+		ClipboardRichTextFormat,
+		ClipboardText,
+		Close,
+		Comment,
+		Config,
+		CustomColor,
+		Dir,
+		Disk,
+		Down,
+		Error,
+		File,
+		Filter,
+		Find,
+		Finder,
+		Flag,
+		Font,
+		FontStyle,
+		Group,
+		Help,
+		Information,
+		Kill,
+		Lock,
+		Log,
+		Name,
+		Note,
+		NoteBody,
+		NoteTitle,
+		NotImpl,
+		OpenDir,
+		Pin,
+		Refresh,
+		Remove,
+		Save,
+		SideContract,
+		SideExpand,
+		SystemEnvironment,
+		Tag,
+		Toolbar,
+		Unlock,
+		Up,
+		Update,
+		Warning,
+		WindowList,
+		WindowLoad,
+		Windows,
+		WindowSave,
+	}
+
+	public enum SkinIcon
+	{
+		App,
+		Command,
+		NotFound,
+		ToolbarMain,
+	}
+}
+
