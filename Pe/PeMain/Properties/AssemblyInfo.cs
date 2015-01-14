262c8a8336eb67855abdf36063a7be1565de7eb0
sk <content.type.text.net@gmail.com>
2015-01-15 03:52:14 +0900

    #151 resolved

--- Pe/PeMain/Properties/AssemblyInfo.cs
+++ Pe/PeMain/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("0.39.5.*")]
+[assembly: AssemblyVersion("0.39.6.*")]
 [assembly: AssemblyInformationalVersion("revision")]
