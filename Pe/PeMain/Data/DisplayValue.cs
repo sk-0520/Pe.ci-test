a250d74c92723aa95c5279dfedb1ddd491438748
sk <content.type.text.net@gmail.com>
2015-01-14 19:48:58 +0900

    mv DisplayValue

--- Pe/PeMain/Data/DisplayValue.cs
+++ Pe/PeMain/Data/DisplayValue.cs
@@ -1,13 +1,13 @@
-﻿using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Windows.Forms;
-using ContentTypeTextNet.Pe.Library.Skin;
-using ContentTypeTextNet.Pe.PeMain.Data;
-using ContentTypeTextNet.Pe.PeMain.IF;
-
-namespace ContentTypeTextNet.Pe.PeMain.Logic
+﻿namespace ContentTypeTextNet.Pe.PeMain.Data
 {
+	using System.Collections.Generic;
+	using System.Drawing;
+	using System.Linq;
+	using System.Windows.Forms;
+	using ContentTypeTextNet.Pe.Library.Skin;
+	using ContentTypeTextNet.Pe.PeMain.IF;
+	using ContentTypeTextNet.Pe.PeMain.Logic;
+
 	/// <summary>
 	/// UIに対して項目と名称を設定する。
 	/// </summary>
