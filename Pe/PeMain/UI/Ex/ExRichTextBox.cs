a7d20d8ebed843eb77ab4d81071d2350be167a44
sk <content.type.text.net@gmail.com>
2015-01-17 10:26:55 +0900

    ExRichTextBox

---
+++ Pe/PeMain/UI/Ex/ExRichTextBox.cs
@@ -0,0 +1,27 @@
+﻿namespace ContentTypeTextNet.Pe.PeMain.UI
+{
+	using System.Windows.Forms;
+	using ContentTypeTextNet.Pe.Library.PlatformInvoke.Windows;
+
+	public class ExRichTextBox: RichTextBox
+	{ }
+
+	public class TransparentRichTextBox: ExRichTextBox
+	{
+		#region override
+
+		protected override CreateParams CreateParams
+		{
+			get
+			{
+				CreateParams result = base.CreateParams;
+
+				result.ExStyle |= (int)WS_EX.WS_EX_TRANSPARENT;
+
+				return result;
+			}
+		}
+
+		#endregion //////////////////////////////////////
+	}
+}
