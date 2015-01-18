97fe116be3688537b9fc785cdce368f56ab22a07
sk <content.type.text.net@gmail.com>
2015-01-18 21:24:33 +0900

    あがががが

--- Pe/PlatformInvoke/Windows/CommonControls.cs
+++ Pe/PlatformInvoke/Windows/CommonControls.cs
@@ -216,6 +216,7 @@ namespace ContentTypeTextNet.Pe.Library.PlatformInvoke.Windows
 	partial class NativeMethods
 	{
 		public static Guid IID_IImageList = new Guid("{46EB5926-582E-4017-9FDF-E8998DAA0950}");
+		public static Guid IID_IShellItem = new Guid("{43826d1e-e718-42ee-bc55-a1e261c37bfe}");
 		
 	}
 }
--- Pe/Utility/IconUtility.cs
+++ Pe/Utility/IconUtility.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Runtime.InteropServices;
 using ContentTypeTextNet.Pe.Library.PlatformInvoke.Windows;
 using ContentTypeTextNet.Pe.Library.Skin;
@@ -55,7 +56,7 @@ namespace ContentTypeTextNet.Pe.Library.Utility
 		}
 
 		// BUGS: いろいろ調べてるけどインデックス指定がわけわかめ
-		static Icon LoadLargeIcon(string iconPath, IconScale iconScale, int iconIndex, bool hasIcon)
+		unsafe static Icon LoadLargeIcon(string iconPath, IconScale iconScale, int iconIndex, bool hasIcon)
 		{
 			Debug.Assert(iconScale.IsIn(IconScale.Big, IconScale.Large), iconScale.ToString());
 
@@ -67,26 +68,81 @@ namespace ContentTypeTextNet.Pe.Library.Utility
 
 			var infoFlags = SHGFI.SHGFI_SYSICONINDEX;//| SHGFI.SHGFI_USEFILEATTRIBUTES;
 			//var hImgSmall = NativeMethods.SHGetFileInfo(iconPath, (int)FILE_ATTRIBUTE.FILE_ATTRIBUTE_NORMAL, ref fileInfo, (uint)Marshal.SizeOf(fileInfo), infoFlags);
-			var hImgSmall = NativeMethods.SHGetFileInfo(iconPath, 0, ref fileInfo, (uint)Marshal.SizeOf(fileInfo), infoFlags);
+			var hImgSmall = NativeMethods.SHGetFileInfo(iconPath, (int)FILE_ATTRIBUTE.FILE_ATTRIBUTE_NORMAL, ref fileInfo, (uint)Marshal.SizeOf(fileInfo), infoFlags);
 
 			IImageList imageList = null;
 			var getImageListResult = NativeMethods.SHGetImageList((int)shellImageList, ref NativeMethods.IID_IImageList, out imageList);
 
 			if(getImageListResult == ComResult.S_OK) {
-				IntPtr hIcon = IntPtr.Zero;
+				var hIcon = IntPtr.Zero;
 				if(hasIcon) {
 					int n = 0;
 					imageList.GetImageCount(ref n);
-					Debug.WriteLine("{0}, {1}", n, fileInfo.iIcon);
+					Debug.WriteLine("{0}, {1}, {2}, {3}", iconPath, n, fileInfo.iIcon, hasIcon);
 					//fileInfo.iIcon = iconIndex;
 					var hResult = imageList.GetIcon(fileInfo.iIcon, (int)ImageListDrawItemConstants.ILD_TRANSPARENT, ref hIcon);
-				} else {
-					var hResult = imageList.GetIcon(fileInfo.iIcon, (int)ImageListDrawItemConstants.ILD_TRANSPARENT, ref hIcon);
+				}
+				if(hIcon == IntPtr.Zero) {
+
+					//var hResult = imageList.GetIcon(fileInfo.iIcon, (int)ImageListDrawItemConstants.ILD_TRANSPARENT, ref hIcon);
+					var hBitmap = IntPtr.Zero;
+					IShellItem iShellItem = null;
+					NativeMethods.SHCreateItemFromParsingName(iconPath, IntPtr.Zero, NativeMethods.IID_IShellItem, out iShellItem);
+					var size = iconScale.ToSize();
+					((IShellItemImageFactory)iShellItem).GetImage(new SIZE(size.Width, size.Height), 0x0, out hBitmap);
+
+					var dibsection = new DIBSECTION();
+					NativeMethods.GetObject((IntPtr)hBitmap, Marshal.SizeOf(dibsection), ref dibsection);
+					int width = dibsection.dsBm.bmWidth;
+					int height = dibsection.dsBm.bmHeight;
+					Debug.WriteLine("{0}, {1}, {2}, {3}", iconPath, dibsection.dsBm.bmHeight, dibsection.dsBmih.biHeight, hasIcon);
+					using(var bitmap = new Bitmap(width, height, PixelFormat.Format32bppArgb)) {
+
+						for(int x = 0; x < dibsection.dsBmih.biWidth; x++) {
+							for(int y = 0; y < dibsection.dsBmih.biHeight; y++) {
+								int i = y * dibsection.dsBmih.biWidth + x;
+								IntPtr ptr = dibsection.dsBm.bmBits + (i * Marshal.SizeOf(typeof(RGBQUAD)));
+								var rgbquad = (RGBQUAD)Marshal.PtrToStructure(ptr, typeof(RGBQUAD));
+
+								if(rgbquad.rgbReserved != 0)
+									bitmap.SetPixel(x, y, Color.FromArgb(rgbquad.rgbReserved, rgbquad.rgbRed, rgbquad.rgbGreen, rgbquad.rgbBlue));
+							}
+						}
+						/*
+						{
+							// create the destination Bitmap object
+							Bitmap bmp = new Bitmap(width, height, PixelFormat.Format32bppArgb);
+
+							// get the HDCs and select the HBITMAP
+							Graphics graphics = Graphics.FromImage(bmp);
+
+							IntPtr hdcDest = graphics.GetHdc();
+							IntPtr hdcSrc = NativeMethods.CreateCompatibleDC(hdcDest);
+							IntPtr hobjOriginal = NativeMethods.SelectObject(hdcSrc, (IntPtr)hBitmap);
+
+							// render the bmp using AlphaBlend
+							BLENDFUNCTION blendfunction = new BLENDFUNCTION(AC.AC_SRC_OVER, 0, 0xFF, AC.AC_SRC_ALPHA);
+							NativeMethods.AlphaBlend(hdcDest, 0, 0, width, height, hdcSrc, 0, 0, width, height, blendfunction);
+
+							// clean up
+							NativeMethods.SelectObject(hdcSrc, hobjOriginal);
+							NativeMethods.DeleteDC(hdcSrc);
+							graphics.ReleaseHdc(hdcDest);
+							graphics.Dispose();
+						}
+						*/
+						hIcon = bitmap.GetHicon();
+					}
 				}
 
-				result = (Icon)System.Drawing.Icon.FromHandle(hIcon).Clone();
+				using(var icon = System.Drawing.Icon.FromHandle(hIcon)) {
+					result = (Icon)icon.Clone();
+				}
 				NativeMethods.DestroyIcon(hIcon);
+				NativeMethods.SendMessage(hIcon, WM.WM_CLOSE, IntPtr.Zero, IntPtr.Zero);
 			}
+
+			// -----------------
 
 			return result;
 		}
