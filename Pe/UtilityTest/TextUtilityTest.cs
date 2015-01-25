7e47b5f20493b91543b03f88cdcc6de05855cd45
sk <content.type.text.net@gmail.com>
2015-01-25 11:06:55 +0900

    test ReplaceRangeFromDictionary

--- Pe/UtilityTest/TextUtilityTest.cs
+++ Pe/UtilityTest/TextUtilityTest.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using System.Linq;
-using System.Collections.Generic;
-using NUnit.Framework;
-using ContentTypeTextNet.Pe.Library.Utility;
-
-namespace ContentTypeTextNet.Pe.Test.UtilityTest
+﻿namespace ContentTypeTextNet.Pe.Test.UtilityTest
 {
+	using System;
+	using System.Linq;
+	using System.Collections.Generic;
+	using NUnit.Framework;
+	using ContentTypeTextNet.Pe.Library.Utility;
+
 	[TestFixture]
 	public class TextUtilityTest
 	{
@@ -26,6 +26,33 @@ namespace ContentTypeTextNet.Pe.Test.UtilityTest
 		public void RegexPatternToWildcard(string result, string s)
 		{
 			Assert.IsTrue(TextUtility.RegexPatternToWildcard(s) == result);
+		}
+
+		[TestCase("a", "<", ">", "a")]
+		[TestCase("<a>", "<", ">", "A")]
+		[TestCase("<aa>", "<", ">", "<aa>")]
+		[TestCase("<a><b>", "<", ">", "AB")]
+		[TestCase("<a<a>><b>", "<", ">", "<a<a>>B")]
+		[TestCase("a", "@[", "]", "a")]
+		[TestCase("@[a]", "@[", "]", "A")]
+		[TestCase("@[aa]", "@[", "]", "@[aa]")]
+		[TestCase("@[a]@[b]", "@[", "]", "AB")]
+		[TestCase("@[a@[a]]@[b]", "@[", "]", "@[a@[a]]B")]
+		public void ReplaceRangeFromDictionary(string src, string head, string tail, string result)
+		{
+			var map = new Dictionary<string, string>() {
+				{ "A", "a" },
+				{ "B", "b" },
+				{ "C", "c" },
+				{ "D", "d" },
+				{ "E", "e" },
+				{ "a", "A" },
+				{ "b", "B" },
+				{ "c", "C" },
+				{ "d", "D" },
+				{ "e", "E" },
+			};
+			Assert.IsTrue(TextUtility.ReplaceRangeFromDictionary(src, head, tail, map) == result);
 		}
 
 		[TestCase(false, "a")]
