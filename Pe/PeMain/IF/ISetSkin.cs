f82201b4bde058ead065ff897e415152144e201b
sk <content.type.text.net@gmail.com>
2015-01-25 11:17:05 +0900

    説明

--- Pe/PeMain/IF/ISetSkin.cs
+++ Pe/PeMain/IF/ISetSkin.cs
@@ -2,8 +2,15 @@
 {
 	using ContentTypeTextNet.Pe.Library.Skin;
 
+	/// <summary>
+	/// ISkinを受信可能とする。
+	/// </summary>
 	public interface ISetSkin
 	{
+		/// <summary>
+		/// ISkinの受信。
+		/// </summary>
+		/// <param name="skin"></param>
 		void SetSkin(ISkin skin);
 	}
 }
