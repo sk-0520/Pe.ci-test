4052f434f8c50760c55c37b394cbd064fd7b744a
sk <content.type.text.net@gmail.com>
2015-01-31 13:40:56 +0900

    UnmanagedIcon

---
+++ Pe/Utility/Unmanaged.cs
@@ -0,0 +1,66 @@
+﻿namespace ContentTypeTextNet.Pe.Library.Utility
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+	using ContentTypeTextNet.Pe.Library.PlatformInvoke.Windows;
+
+	public abstract class UnmanagedBase: IDisposable
+	{
+		protected UnmanagedBase()
+		{
+			IsDisposed = false;
+		}
+
+		~UnmanagedBase()
+		{
+			Dispose(false);
+		}
+
+		/// <summary>
+		/// 破棄されたか。
+		/// </summary>
+		public bool IsDisposed { get; protected set; }
+
+		#region IDisposable
+
+		protected virtual void Dispose(bool disposing)
+		{
+			IsDisposed = true;
+		}
+
+		/// <summary>
+		/// 解放。
+		/// </summary>
+		public void Dispose()
+		{
+			Dispose(true);
+		}
+
+		#endregion
+
+	}
+
+	/// <summary>
+	/// アイコンハンドルを管理。
+	/// </summary>
+	public class UnmanagedIcon: UnmanagedBase
+	{
+		public UnmanagedIcon(IntPtr hIcon)
+		{
+			IconHandle = hIcon;
+		}
+
+		public IntPtr IconHandle {get; private set; }
+
+		protected override void Dispose(bool disposing)
+		{
+			NativeMethods.DestroyIcon(IconHandle);
+			IconHandle = IntPtr.Zero;
+
+			base.Dispose(disposing);
+		}
+	}
+}
