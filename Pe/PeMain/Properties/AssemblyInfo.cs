99183fb60eee5a50474da755e9d33615d13593b4
sk <content.type.text.net@gmail.com>
2015-02-04 16:22:15 +0900

    #188 changelog.xml

--- Pe/PeMain/Properties/AssemblyInfo.cs
+++ Pe/PeMain/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("0.44.3.*")]
+[assembly: AssemblyVersion("0.44.4.*")]
 [assembly: AssemblyInformationalVersion("revision")]
