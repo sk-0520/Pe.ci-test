aa053e18165976e285d60e37da6a3d9556cdb165
sk <content.type.text.net@gmail.com>
2015-02-14 18:43:09 +0900

    (；´Д｀)

--- Pe/PeMain/Properties/AssemblyInfo.cs
+++ Pe/PeMain/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("0.49.3.*")]
+[assembly: AssemblyVersion("0.49.4.*")]
 [assembly: AssemblyInformationalVersion("DEVELOPMENT")]
