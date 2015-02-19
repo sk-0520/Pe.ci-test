f41deeac424c634ec674762c28c6b25ad5187be7
sk <content.type.text.net@gmail.com>
2015-02-19 16:15:01 +0900

    データ保持用ボタン

---
+++ Pe/PeMain/UI/Ex/ExButton.cs
@@ -0,0 +1,58 @@
+﻿namespace ContentTypeTextNet.Pe.PeMain.UI.Ex
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Drawing;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+	using System.Windows.Forms;
+	using ContentTypeTextNet.Pe.PeMain.Data;
+	using ContentTypeTextNet.Pe.PeMain.IF;
+
+	public abstract class ExButton: Button
+	{ }
+
+	public class ColorChangedEventArg: EventArgs
+	{
+		public ColorChangedEventArg(Color color)
+		{
+			Color = color;
+		}
+
+		public Color Color { get; private set; }
+	}
+
+	/// <summary>
+	/// 色を保持するボタン。
+	/// </summary>
+	public class ColorButton: ExButton
+	{
+		protected Color _color = Color.Red;
+
+		public event EventHandler<ColorChangedEventArg> ColorChanged = delegate { };
+
+		public ColorButton()
+		{
+			TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+			ImageAlign = ContentAlignment.MiddleCenter;
+		}
+
+		/// <summary>
+		/// 保持する色。
+		/// </summary>
+		public Color Color
+		{
+			get { return this._color; }
+			set
+			{
+				if(this._color != value) {
+					this._color = value;
+					ColorChanged(this, new ColorChangedEventArg(this._color));
+				}
+			}
+		}
+
+
+	}
+}
