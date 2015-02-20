daec205531c15d5287c4994a7e7f099f5f62187e
sk <content.type.text.net@gmail.com>
2015-02-20 22:40:49 +0900

    寒い

--- Pe/Utility/DialogFilterUtility.cs
+++ Pe/Utility/DialogFilterUtility.cs
@@ -14,19 +14,37 @@
 			Wildcard = new List<string>();
 		}
 
+		/// <summary>
+		/// フィルターアイテムの設定。
+		/// </summary>
+		/// <param name="display">表示文字列。</param>
+		/// <param name="wildcard">対象ワイルドカード。</param>
 		public DialogFilterItem(string display, params string[] wildcard)
 		{
 			Display = display;
 			Wildcard = new List<string>(wildcard);
 		}
+
+		/// <summary>
+		/// フィルターアイテムの設定。
+		/// </summary>
+		/// <param name="display">表示文字列。</param>
+		/// <param name="wildcard">対象ワイルドカード。</param>
 		public DialogFilterItem(string display, IEnumerable<string> wildcard)
 		{
 			Display = display;
 			Wildcard = new List<string>(wildcard);
 		}
 
-		public string Display { get; set; }
-		public List<string> Wildcard { get; set; }
+		/// <summary>
+		/// 表示名。
+		/// </summary>
+		public string Display { get; private set; }
+
+		/// <summary>
+		/// 対象ワイルドカード。
+		/// </summary>
+		public IReadOnlyList<string> Wildcard { get; private set; }
 
 		public override string ToString()
 		{
@@ -35,6 +53,10 @@
 	}
 
 
+	/// <summary>
+	/// DialogFilterItemで且つなにかデータを持つ。
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
 	public class DialogFilterValueItem<T>: DialogFilterItem
 	{
 		public DialogFilterValueItem(T value)
@@ -42,11 +64,13 @@
 		{
 			Value = value;
 		}
+
 		public DialogFilterValueItem(T value, string display, params string[] wildcard)
 			: base(display, wildcard)
 		{
 			Value = value;
 		}
+
 		public DialogFilterValueItem(T value, string display, IEnumerable<string> wildcard)
 			: base(display, wildcard)
 		{
@@ -67,6 +91,9 @@
 			Items = new List<DialogFilterItem>();
 		}
 
+		/// <summary>
+		/// 各アイテム。
+		/// </summary>
 		public List<DialogFilterItem> Items { get; private set; }
 
 		public override string ToString()
@@ -75,7 +102,9 @@
 		}
 	}
 
-
+	/// <summary>
+	/// くっつける。
+	/// </summary>
 	public static class DialogFilterUtility
 	{
 		public static void Attachment(this FileDialog dialog, DialogFilter filter)
