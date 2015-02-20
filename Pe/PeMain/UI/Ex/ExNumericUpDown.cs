b8b9020fbb7e6fb630370e5ca0d0a7fdd15bdad4
sk <content.type.text.net@gmail.com>
2015-02-20 15:08:47 +0900

    プロパティごにょごにょ

--- Pe/PeMain/UI/Ex/ExNumericUpDown.cs
+++ Pe/PeMain/UI/Ex/ExNumericUpDown.cs
@@ -14,11 +14,37 @@ using ContentTypeTextNet.Pe.PeMain.IF;
 
 	public class RevertDefaultValueNumericUpDown: DefaultValueNumericUpDown, ISetLanguage, ILanguage
 	{
+		const string MenuItemName_revert = "menuitem_revert";
+
+		protected ContextMenu _contextMenu;
+
+		public RevertDefaultValueNumericUpDown()
+		{
+			this._contextMenu = new ContextMenu();
+
+			var menuItemRevert = new MenuItem() {
+				Name = MenuItemName_revert,
+			};
+			menuItemRevert.Click += MenuItemRevert_Click;
+
+			this._contextMenu.MenuItems.Add(menuItemRevert);
+
+			ContextMenu = this._contextMenu;
+		}
+
 		public Language Language { get; private set; }
 
 		public void SetLanguage(Language language)
 		{
 			Language = language;
+
+			this._contextMenu.MenuItems[MenuItemName_revert].Text = Language["updown/revert"];
 		}
+
+		void MenuItemRevert_Click(object sender, System.EventArgs e)
+		{
+			Value = DefaultValue;
+		}
+
 	}
 }
