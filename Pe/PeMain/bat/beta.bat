60a19d6d0a9c684cf7aa88aa5d302a7f0063af46
sk <content.type.text.net@gmail.com>
2015-02-27 13:53:18 +0900

    copy

--- Pe/PeMain/bat/beta.bat
+++ Pe/PeMain/bat/beta.bat
@@ -9,11 +9,15 @@ set BETA_DIR=%BETA_ROOT_DIR%\Pe
 
 set RELEASE_DIR=%APPDATA%\Pe
 
-rem 既存データをパクってくる
-mkdir "%BETA_DIR%"  2>NUL
-copy  "%RELEASE_DIR%\*.xml"      "%BETA_DIR%" /Y
-copy  "%RELEASE_DIR%\*.sqlite3"  "%BETA_DIR%" /Y
-copy  "%RELEASE_DIR%\*.gz"       "%BETA_DIR%" /Y
+if exist "%BETA_ROOT_DIR%" (
+	rem ベータ用ディレクトリがあるのでコピらない
+) else (
+	rem 既存データをパクってくる
+	mkdir "%BETA_DIR%"  2>NUL
+	copy  "%RELEASE_DIR%\*.xml"      "%BETA_DIR%" /Y
+	copy  "%RELEASE_DIR%\*.sqlite3"  "%BETA_DIR%" /Y
+	copy  "%RELEASE_DIR%\*.gz"       "%BETA_DIR%" /Y
+)
 
-start PeMain.exe /setting-root=%BETA_ROOT_DIR% /mutex=Pe_beta
+start PeMain.exe /setting-root=%BETA_ROOT_DIR% /log=%BETA_ROOT_DIR%\logs /mutex=Pe_beta
 
