26e3220e558308303dcf2130e7e48103a5e31226
sk <content.type.text.net@gmail.com>
2015-05-16 15:38:11 +0900

    やる気でねぇ

---
+++ Pe2/Pe/Library/SharedLibrary/Utility/Serializer.cs
@@ -0,0 +1,13 @@
+﻿namespace ContentTypeTextNet.Pe.Library.SharedLibrary.Utility
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+
+	public static class Serializer
+	{
+
+	}
+}
---
+++ Pe2/Pe/Library/SharedLibrary/ViewModel/ViewModelBase.cs
@@ -0,0 +1,31 @@
+﻿namespace ContentTypeTextNet.Pe.Library.SharedLibrary.ViewModel
+{
+	using System;
+	using System.Collections.Generic;
+	using System.ComponentModel;
+	using System.Linq;
+	using System.Runtime.CompilerServices;
+	using System.Text;
+	using System.Threading.Tasks;
+
+	public abstract class ViewModelBase: INotifyPropertyChanged
+	{
+		#region INotifyPropertyChanged 
+
+		/// <summary>
+		/// プロパティが変更された際に発生。
+		/// </summary>
+		public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+		/// <summary>
+		/// PropertyChanged呼び出し。
+		/// </summary>
+		/// <param name="propertyName"></param>
+		protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+		{
+			this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+		#endregion
+	}
+}
