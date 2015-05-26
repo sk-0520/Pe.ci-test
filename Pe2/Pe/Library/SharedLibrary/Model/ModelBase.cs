ba5f5ab5e521ce3823ca60ab9a461a42c4cf9aa6
sk <content.type.text.net@gmail.com>
2015-05-27 00:52:52 +0900

    メンバ回り

--- Pe2/Pe/Library/SharedLibrary/Model/ModelBase.cs
+++ Pe2/Pe/Library/SharedLibrary/Model/ModelBase.cs
@@ -1,12 +1,58 @@
 ﻿namespace ContentTypeTextNet.Pe.Library.SharedLibrary.Model
 {
 	using System;
-	using System.Collections.Generic;
-	using System.Linq;
-	using System.Text;
-	using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+	using ContentTypeTextNet.Pe.Library.SharedLibrary.IF;
+	using ContentTypeTextNet.Pe.Library.SharedLibrary.Utility;
 
-	public abstract class ModelBase
+	public abstract class ModelBase: IGetMembers
 	{
+		#region variable
+
+		IEnumerable<PropertyInfo> _propertyInfos = null;
+
+		#endregion
+
+		#region override
+
+		public override string ToString()
+		{
+			var name = GetType().Name;
+			var nameValueStrings = GetNameValueList();
+			var joinString = ReflectionUtility.JoinNameValueStrings(nameValueStrings);
+
+			return string.Format("{0} => {1}", name, joinString);
+		}
+
+		#endregion
+
+		#region IGetMembers
+		
+		public IEnumerable<PropertyInfo> PropertyInfos
+		{
+			get
+			{
+				if(this._propertyInfos == null) {
+					this._propertyInfos = GetType().GetProperties();
+				}
+
+				return this._propertyInfos;
+			}
+		}
+
+		public IEnumerable<string> GetNameValueList()
+		{
+			var nameValueMap = ReflectionUtility.GetMembers(this, PropertyInfos);
+			return ReflectionUtility.GetNameValueStrings(nameValueMap);
+		}
+
+		#endregion
+
+		#region function
+		#endregion
 	}
 }
---
+++ Pe2/Pe/Library/SharedLibrary/Utility/ReflectionUtility.cs
@@ -0,0 +1,42 @@
+﻿namespace ContentTypeTextNet.Pe.Library.SharedLibrary.Utility
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Reflection;
+	using System.Text;
+	using System.Threading.Tasks;
+
+	public static class ReflectionUtility
+	{
+		/// <summary>
+		/// メンバ名とその値を取得する。
+		/// </summary>
+		/// <param name="obj"></param>
+		/// <param name="propertyInfos"></param>
+		/// <returns></returns>
+		public static IDictionary<string, object> GetMembers(object obj, IEnumerable<PropertyInfo> propertyInfos)
+		{
+			var result = new Dictionary<string, object>(propertyInfos.Count());
+
+			foreach(var propertyInfo in propertyInfos) {
+				var value = propertyInfo.GetValue(obj, null);
+				result.Add(propertyInfo.Name, obj);
+			}
+
+			return result;
+		}
+
+		public static IEnumerable<string> GetNameValueStrings(IDictionary<string, object> nameValues)
+		{
+			foreach(var pair in nameValues.OrderBy(p => p.Key)) {
+				yield return string.Format("{0}=[{1}]", pair.Key, pair.Value ?? "null");
+			}
+		}
+
+		public static string JoinNameValueStrings(IEnumerable<string> nameValues)
+		{
+			return string.Join(",", nameValues);
+		}
+	}
+}
