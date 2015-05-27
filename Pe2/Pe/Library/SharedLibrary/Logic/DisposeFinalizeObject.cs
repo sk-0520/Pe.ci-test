a48eeb11cdef114d6e6680a64127f5e4a7882449
sk <content.type.text.net@gmail.com>
2015-05-27 22:46:11 +0900

    DisposeFinalizeObject

---
+++ Pe2/Pe/Library/SharedLibrary/Logic/DisposeFinalizeObject.cs
@@ -0,0 +1,42 @@
+﻿namespace ContentTypeTextNet.Pe.Library.SharedLibrary.Logic
+{
+	using System;
+
+	public abstract class DisposeFinalizeObject
+	{
+		protected DisposeFinalizeObject()
+		{
+			IsDisposed = false;
+		}
+
+		~DisposeFinalizeObject()
+		{
+			Dispose(false);
+		}
+
+		/// <summary>
+		/// 破棄されたか。
+		/// </summary>
+		public bool IsDisposed { get; protected set; }
+
+		#region IDisposable
+
+		protected virtual void Dispose(bool disposing)
+		{
+			IsDisposed = true;
+		}
+
+		/// <summary>
+		/// 解放。
+		/// </summary>
+		public void Dispose()
+		{
+			Dispose(true);
+			if(!IsDisposed) {
+				GC.SuppressFinalize(this);
+			}
+		}
+
+		#endregion
+	}
+}
