2b6188882cd7a56a03ffd76885231e7cbb015ee5
sk <content.type.text.net@gmail.com>
2015-05-30 14:14:27 +0900

    DeepClone

--- Pe/PeMain/Logic/Executor.cs
+++ Pe/PeMain/Logic/Executor.cs
@@ -120,7 +120,7 @@
 			Debug.Assert(launcherItem.LauncherType == LauncherType.URI || launcherItem.LauncherType == LauncherType.Command);
 
 			//return RunCommand(launcherItem.Command, launcherItem.Option, commonData);
-			var fileLauncherItem = (LauncherItem)launcherItem.Clone();
+			var fileLauncherItem = (LauncherItem)launcherItem.DeepClone();
 			// ファイルアイテムに変換
 			fileLauncherItem.LauncherType = LauncherType.File;
 			// 管理者権限はどうにも効かなさそう
