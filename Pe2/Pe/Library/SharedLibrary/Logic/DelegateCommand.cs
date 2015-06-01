1b14455d13dc96bd41e4802736c5709791123f3f
sk <content.type.text.net@gmail.com>
2015-06-01 23:51:13 +0900

    DelegateCommandパターン

---
+++ Pe2/Pe/Library/SharedLibrary/Logic/DelegateCommand.cs
@@ -0,0 +1,58 @@
+﻿namespace ContentTypeTextNet.Library.SharedLibrary.Logic
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+	using System.Windows.Input;
+
+    public class DelegateCommand : ICommand
+    {
+		/// <summary>
+		/// コマンド。
+		/// </summary>
+		public Action<object> Command;
+		/// <summary>
+		/// 実行可否。
+		/// </summary>
+		private Func<object, bool> CanExecute;
+
+        // コンストラクタ
+        public DelegateCommand(Action<object> command, Func<object,bool> canExecute)
+        {
+			if(command == null) {
+				throw new ArgumentNullException("command");
+			}
+
+			Command = command;
+			CanExecute = canExecute;
+        }
+
+		#region ICommand
+
+		void ICommand.Execute(object parameter)
+		{
+			Command(parameter);
+		}
+
+		bool ICommand.CanExecute(object parameter)
+		{
+			if(CanExecute != null) {
+				return CanExecute(parameter);
+			} else {
+				return false;
+			}
+		}
+
+		event EventHandler ICommand.CanExecuteChanged
+		{
+			add { CommandManager.RequerySuggested += value; }
+			remove { CommandManager.RequerySuggested -= value; }
+		}
+
+		#endregion
+	}
+
+
+}
