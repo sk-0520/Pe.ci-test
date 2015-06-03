00a0d99ed7e826a366613e6a991d3e19923e36d7
sk <content.type.text.net@gmail.com>
2015-06-03 23:31:52 +0900

    LanguageItemModel

---
+++ Pe2/Pe/Library/SharedLibrary/Model/LanguageItemModel.cs
@@ -0,0 +1,33 @@
+﻿namespace ContentTypeTextNet.Library.SharedLibrary.Model
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+	using ContentTypeTextNet.Library.SharedLibrary.IF;
+
+	/// <summary>
+	/// 言語データの最小データ。
+	/// </summary>
+	public class LanguageItemModel: ModelBase, IName
+	{
+		#region property
+
+		/// <summary>
+		/// 表示用文字列。
+		/// </summary>
+		public string Text { get; set; }
+
+		#endregion
+
+		#region IName
+
+		/// <summary>
+		/// キーとして使用される、
+		/// </summary>
+		public string Name { get; set; }
+
+		#endregion
+	}
+}
