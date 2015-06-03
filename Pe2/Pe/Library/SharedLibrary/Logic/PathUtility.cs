df265a7cac0bc57ee37c08c960dbf6783b2fd59b
sk <content.type.text.net@gmail.com>
2015-06-03 22:28:47 +0900

    ToSafeName

---
+++ Pe2/Pe/Library/SharedLibrary/Logic/PathUtility.cs
@@ -0,0 +1,37 @@
+﻿namespace ContentTypeTextNet.Library.SharedLibrary.Logic
+{
+	using System;
+	using System.Collections.Generic;
+	using System.IO;
+	using System.Linq;
+	using System.Text;
+	using System.Text.RegularExpressions;
+	using System.Threading.Tasks;
+
+	public static class PathUtility
+	{
+		/// <summary>
+		/// パスに拡張子を追加する。
+		/// </summary>
+		/// <param name="path"></param>
+		/// <param name="ext"></param>
+		/// <returns></returns>
+		public static string AppendExtension(string path, string ext)
+		{
+			return path + "." + ext;
+		}
+
+		/// <summary>
+		/// ファイル名をそれとなく安全な名称に変更する。
+		/// </summary>
+		/// <param name="name"></param>
+		/// <returns></returns>
+		public static string ToSafeName(string name, Func<char, char> fn)
+		{
+			var pattern = Regex.Escape(string.Concat(Path.GetInvalidPathChars().Concat(Path.GetInvalidFileNameChars())));
+			var reg = new Regex("([" + pattern + "])");
+			return reg.Replace(name.Trim(), m => new string(fn(m.Groups[0].Value[0]), 1));
+		}
+
+	}
+}
---
+++ Pe2/Pe/Test/Library/SharedLibraryTest/Logic/PathUtilityTest.cs
@@ -0,0 +1,34 @@
+﻿namespace ContentTypeTextNet.Test.Library.SharedLibraryTest.Logic
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+	using ContentTypeTextNet.Library.SharedLibrary.Logic;
+	using NUnit.Framework;
+
+	[TestFixture]
+	class PathUtilityTest
+	{
+		[TestCase("a.txt", "a", "txt")]
+		[TestCase("a.txt.txt", "a.txt", "txt")]
+		[TestCase("a..txt", "a.", "txt")]
+		[TestCase("a..txt", "a", ".txt")]
+		public void AppendExtensionTest(string test, string path, string ext)
+		{
+			Assert.AreEqual(test, PathUtility.AppendExtension(path, ext));
+		}
+
+		[TestCase("", "", '!')]
+		[TestCase("", " ", '!')]
+		[TestCase("a", "a", '!')]
+		[TestCase("a!", "a?", '!')]
+		[TestCase("a?", "a?", '?')]
+		[TestCase("a@b@c@d", "a?b\\c*d", '@')]
+		public void ToSafeNameTest(string test, string s, char c)
+		{
+			Assert.AreEqual(test, PathUtility.ToSafeName(s, v => c));
+		}
+	}
+}
