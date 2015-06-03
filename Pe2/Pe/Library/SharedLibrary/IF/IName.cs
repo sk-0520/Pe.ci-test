5ecf7e720f35aca2f0b753be94ed511c5cc0cf42
sk <content.type.text.net@gmail.com>
2015-06-03 22:41:05 +0900

    IName

---
+++ Pe2/Pe/Library/SharedLibrary/IF/IName.cs
@@ -0,0 +1,19 @@
+﻿namespace ContentTypeTextNet.Library.SharedLibrary.IF
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+
+	/// <summary>
+	/// 名前を持つ。
+	/// </summary>
+	interface IName
+	{
+		/// <summary>
+		/// 名前。
+		/// </summary>
+		string Name { get; set; }
+	}
+}
