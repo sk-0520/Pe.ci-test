737d96375e96c319714a308b95e406cfa61a80a3
sk <content.type.text.net@gmail.com>
2015-06-04 11:28:16 +0900

    SettingModelBase

---
+++ Pe2/Pe/Library/PeModel/SettingModelBase.cs
@@ -0,0 +1,18 @@
+﻿namespace ContentTypeTextNet.Pe.Library.PeModel
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+	using ContentTypeTextNet.Library.SharedLibrary.Model;
+
+	/// <summary>
+	/// 設定データとして使用する基底モデル。
+	/// </summary>
+	public abstract class SettingModelBase : DisposeFinalizeModelBase
+	{
+		public SettingModelBase():base()
+		{ }
+	}
+}
