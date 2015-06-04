96bd56caf1731b36b689eabf1f247e3149b35369
sk <content.type.text.net@gmail.com>
2015-06-04 23:25:13 +0900

    HistoryItemModel

---
+++ Pe2/Pe/Library/PeModel/Item/HistoryItemModel.cs
@@ -0,0 +1,40 @@
+﻿namespace ContentTypeTextNet.Pe.Library.PeModel.Item
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Runtime.Serialization;
+	using System.Text;
+	using System.Threading.Tasks;
+	using System.Xml.Serialization;
+
+	/// <summary>
+	/// 履歴保持アイテム。
+	/// </summary>
+	[DataContract, Serializable]
+	public class HistoryItemModel: ItemModelBase
+	{
+		public HistoryItemModel() 
+			: base()
+		{
+			UpdateDateTime = CreateDateTime = DateTime.Now;
+			UpdateCount = 0;
+		}
+
+		/// <summary>
+		/// 作成日。
+		/// </summary>
+		[DataMember, XmlAttribute]
+		public DateTime CreateDateTime { get; set; }
+		/// <summary>
+		/// 更新日。
+		/// </summary>
+		[DataMember, XmlAttribute]
+		public DateTime UpdateDateTime { get; set; }
+		/// <summary>
+		/// 更新回数。
+		/// </summary>
+		[DataMember, XmlAttribute]
+		public int UpdateCount { get; set; }
+	}
+}
