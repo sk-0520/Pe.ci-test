db9ae9e843cda8b813ee56c993defca3cd09ef24
sk <content.type.text.net@gmail.com>
2015-06-04 23:32:38 +0900

    TagsItemModel

---
+++ Pe2/Pe/Library/PeModel/Item/TagsItemModel.cs
@@ -0,0 +1,29 @@
+﻿namespace ContentTypeTextNet.Pe.Library.PeModel.Item
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Runtime.Serialization;
+	using System.Text;
+	using System.Threading.Tasks;
+	using System.Xml.Serialization;
+
+	/// <summary>
+	/// タグを管理。
+	/// </summary>
+	[DataContract, Serializable]
+	public class TagsItemModel: PeModelBase
+	{
+		public TagsItemModel() 
+			: base() 
+		{
+			Items = new List<string>();
+		}
+
+		/// <summary>
+		/// タグ。
+		/// </summary>
+		[DataMember, XmlArray("Items"), XmlArrayItem("Item")]
+		IList<string> Items { get; set; }
+	}
+}
