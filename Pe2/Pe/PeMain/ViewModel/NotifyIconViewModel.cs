1aa3c1fc3fc1115afb905ac4ff5b2f274aaf2dda
sk <content.type.text.net@gmail.com>
2015-06-04 12:54:50 +0900

    なれんのう

--- Pe2/Pe/PeMain/ViewModel/NotifyIconViewModel.cs
+++ Pe2/Pe/PeMain/ViewModel/NotifyIconViewModel.cs
@@ -2,16 +2,24 @@
 {
 using System;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
-	using ContentTypeTextNet.Library.SharedLibrary.Logic;
+using ContentTypeTextNet.Library.SharedLibrary.Logic;
+	using ContentTypeTextNet.Library.SharedLibrary.ViewModel;
 
 	/// <summary>
 	/// Provides bindable properties and commands for the NotifyIcon. In this sample, the
 	/// view model is assigned to the NotifyIcon in XAML. Alternatively, the startup routing
 	/// in App.xaml.cs could have created this view model, and assigned it to the NotifyIcon.
 	/// </summary>
-	public class NotifyIconViewModel
+	public class NotifyIconViewModel: ViewModelBase
 	{
+		#region property
+
+		#endregion
+
+		#region command
+
 		/// <summary>
 		/// Hides the main window. This command is only enabled if a window is open.
 		/// </summary>
@@ -37,5 +45,7 @@ using System.Windows.Input;
 				return new DelegateCommand { Command = o => Application.Current.Shutdown() };
 			}
 		}
+
+		#endregion
 	}
 }
