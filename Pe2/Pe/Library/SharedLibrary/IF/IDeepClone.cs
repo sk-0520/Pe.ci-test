20e466f56bf2b87796dc7d36aed34d359d3f528b
sk <content.type.text.net@gmail.com>
2015-06-04 22:35:34 +0900

    IDeepClone

---
+++ Pe2/Pe/Library/SharedLibrary/IF/IDeepClone.cs
@@ -0,0 +1,22 @@
+﻿namespace ContentTypeTextNet.Library.SharedLibrary.IF
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+
+	/// <summary>
+	/// ディープコピー。
+	/// <para>ICloneableはシャローなのかディープなのかようわからん。</para>
+	/// </summary>
+	public interface IDeepClone
+	{
+		/// <summary>
+		/// 全データを完全複製。
+		/// <para>状態までは面倒見ない。DBへのコネクションとかね。</para>
+		/// </summary>
+		/// <returns></returns>
+		IDeepClone DeepClone();
+	}
+}
