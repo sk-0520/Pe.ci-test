77dbfbcb577da049668e5bbfb286bdce1c435018
sk <content.type.text.net@gmail.com>
2015-06-04 10:32:09 +0900

    ShortcutFile

---
+++ Pe2/Pe/Library/SharedLibrary/Model/IconPathModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContentTypeTextNet.Library.SharedLibrary.Model
+{
+	/// <summary>
+	/// アイコンのパスを保持。
+	/// </summary>
+	public class IconPathModel: ModelBase
+	{
+		/// <summary>
+		/// パス。
+		/// </summary>
+		public string Path { get; set; }
+		/// <summary>
+		/// アイコンインデックス。
+		/// </summary>
+		public int Index { get; set; }
+	}
+}
