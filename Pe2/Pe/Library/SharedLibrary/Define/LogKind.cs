66ae600383e9331c8c4de2b4bac224bb2affe18f
sk <content.type.text.net@gmail.com>
2015-06-05 11:33:57 +0900

    LogItemModel

---
+++ Pe2/Pe/Library/SharedLibrary/Define/LogKind.cs
@@ -0,0 +1,39 @@
+﻿namespace ContentTypeTextNet.Library.SharedLibrary.Define
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+
+	/// <summary>
+	/// ログ種別。
+	/// </summary>
+	public enum LogKind
+	{
+		/// <summary>
+		/// デバッグ情報。
+		/// </summary>
+		Debug,
+		/// <summary>
+		/// トレース情報。
+		/// </summary>
+		Trace,
+		/// <summary>
+		/// 操作情報。
+		/// </summary>
+		Information,
+		/// <summary>
+		/// 注意。
+		/// </summary>
+		Warning,
+		/// <summary>
+		/// だめ。
+		/// </summary>
+		Error,
+		/// <summary>
+		/// しぬ。
+		/// </summary>
+		Fatal
+	}
+}
---
+++ Pe2/Pe/Library/SharedLibrary/Model/LogItemModel.cs
@@ -0,0 +1,44 @@
+﻿namespace ContentTypeTextNet.Library.SharedLibrary.Model
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Diagnostics;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+	using ContentTypeTextNet.Library.SharedLibrary.Define;
+
+	/// <summary>
+	/// ログとして出力するデータ。
+	/// </summary>
+	public class LogItemModel: ModelBase
+	{
+		/// <summary>
+		/// 発生日。
+		/// </summary>
+		public DateTime DateTime { get; set; }
+		/// <summary>
+		/// ログ種別。
+		/// </summary>
+		public LogKind LogKind { get; set; }
+		/// <summary>
+		/// ログメッセージ。
+		/// </summary>
+		public string Message { get; set; }
+		/// <summary>
+		/// 詳細。
+		/// <para>nullで何もなし。</para>
+		/// <para>判定にはHasDetailを使用。</para>
+		/// </summary>
+		public object Detail { get; set; }
+		/// <summary>
+		/// 詳細はあるか。
+		/// </summary>
+		public bool HasDetail { get { return Detail == null; } }
+		/// <summary>
+		/// スタックトレース情報。
+		/// </summary>
+		public StackTrace StackTrace { get; private set; }
+
+	}
+}
