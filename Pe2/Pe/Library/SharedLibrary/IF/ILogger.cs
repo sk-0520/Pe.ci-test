c488cfec1ad4d60fd75fb3474b4b109ba5d299f8
sk <content.type.text.net@gmail.com>
2015-06-05 11:50:23 +0900

    ILogger

---
+++ Pe2/Pe/Library/SharedLibrary/IF/ILogger.cs
@@ -0,0 +1,29 @@
+﻿namespace ContentTypeTextNet.Library.SharedLibrary.IF
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Runtime.CompilerServices;
+	using System.Text;
+	using System.Threading.Tasks;
+	using ContentTypeTextNet.Library.SharedLibrary.Model;
+
+	/// <summary>
+	/// LogItemModelを用いたログ出力用IF。
+	/// </summary>
+	public interface ILogger
+	{
+		/// <summary>
+		/// 出力担当。
+		/// </summary>
+		/// <param name="item"></param>
+		void Puts(LogItemModel item);
+
+		void Debug(string message, object detail = null, int frame = 1, [CallerFilePath] string file = "", [CallerLineNumber] int line = -1, [CallerMemberName] string member = "");
+		void Trace(string message, object detail = null, int frame = 1, [CallerFilePath] string file = "", [CallerLineNumber] int line = -1, [CallerMemberName] string member = "");
+		void Information(string message, object detail = null, int frame = 1, [CallerFilePath] string file = "", [CallerLineNumber] int line = -1, [CallerMemberName] string member = "");
+		void Warning(string message, object detail = null, int frame = 1, [CallerFilePath] string file = "", [CallerLineNumber] int line = -1, [CallerMemberName] string member = "");
+		void Error(string message, object detail = null, int frame = 1, [CallerFilePath] string file = "", [CallerLineNumber] int line = -1, [CallerMemberName] string member = "");
+		void Fatal(string message, object detail = null, int frame = 1, [CallerFilePath] string file = "", [CallerLineNumber] int line = -1, [CallerMemberName] string member = "");
+	}
+}
