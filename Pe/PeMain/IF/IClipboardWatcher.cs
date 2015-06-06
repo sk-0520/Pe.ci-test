b454d54e369a13b794175610a4978e3b8ab2429c
sk <content.type.text.net@gmail.com>
2015-06-06 19:16:25 +0900

    rc*

--- Pe/PeMain/IF/IClipboardWatcher.cs
+++ Pe/PeMain/IF/IClipboardWatcher.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ContentTypeTextNet.Pe.PeMain.IF
+﻿namespace ContentTypeTextNet.Pe.PeMain.IF
 {
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+
 	public interface IClipboardWatcher
 	{
 		/// <summary>
