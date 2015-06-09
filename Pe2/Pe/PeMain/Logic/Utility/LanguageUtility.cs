e5ca13b8c5d0d58aa16c76eafbf2a87fdee30e1b
sk <content.type.text.net@gmail.com>
2015-06-10 06:48:08 +0900

    いれわすれ

---
+++ Pe2/Pe/PeMain/Logic/Utility/LanguageUtility.cs
@@ -0,0 +1,19 @@
+﻿namespace ContentTypeTextNet.Pe.PeMain.Logic.Utility
+{
+	using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using ContentTypeTextNet.Library.SharedLibrary.Logic;
+using ContentTypeTextNet.Library.SharedLibrary.ViewModel;
+using ContentTypeTextNet.Pe.PeMain.Data;
+using ContentTypeTextNet.Pe.PeMain.View.Parts;
+
+	public static class LanguageUtility
+	{
+	}
+}
