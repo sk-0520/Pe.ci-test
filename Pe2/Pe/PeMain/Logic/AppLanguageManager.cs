a04b762d623f5c1370f466fae66aa7cfe0217811
sk <content.type.text.net@gmail.com>
2015-06-10 09:01:46 +0900

    仕事するきでねぇ

--- Pe2/Pe/PeMain/Logic/AppLanguageManager.cs
+++ Pe2/Pe/PeMain/Logic/AppLanguageManager.cs
@@ -18,8 +18,25 @@
 			BaseDirectoryPath = Path.GetDirectoryName(LanguageFilePath);
 		}
 
+		#region property
+
 		public string BaseFileName { get; private set; }
 		public string BaseDirectoryPath { get; private set; }
 		public string AcceptDocumentFilePath { get { return Path.Combine(BaseDirectoryPath, BaseFileName + Constants.languageAcceptDocumentFileName); } }
+
+		#endregion 
+
+		#region LanguageManager
+
+		protected override IDictionary<string, string> GetSystemMap(DateTime dateTime)
+		{
+			var baseMap = base.GetSystemMap(dateTime);
+			baseMap.Add("APPLICATION", Constants.programName);
+			baseMap.Add("APPLICATION:VERSION", Constants.applicationVersion.ToString());
+			baseMap.Add("APPLICATION:REVISION", Constants.applicationRevision);
+			return baseMap;
+		}
+
+		#endregion
 	}
 }
