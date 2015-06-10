f70954a6adc7ed5f4eb6987596f2a7e4abb33aeb
sk <content.type.text.net@gmail.com>
2015-06-10 11:19:55 +0900

    値は設定した

--- Pe2/Pe/PeMain/ViewModel/AcceptViewModel.cs
+++ Pe2/Pe/PeMain/ViewModel/AcceptViewModel.cs
@@ -5,6 +5,8 @@
 	using System.Linq;
 	using System.Text;
 	using System.Threading.Tasks;
+	using System.Windows.Input;
+	using ContentTypeTextNet.Library.SharedLibrary.Logic;
 	using ContentTypeTextNet.Library.SharedLibrary.ViewModel;
 	using ContentTypeTextNet.Pe.Library.PeData.Item;
 
@@ -32,6 +34,34 @@
 		{
 			get { return Model.Accept; }
 			set { Model.Accept = value; }
+		}
+
+		#endregion
+
+		#region command
+
+		public ICommand OkCommand
+		{
+			get
+			{
+				return new DelegateCommand(
+					o => {
+						Model.Accept = true;
+					}
+				);
+			}
+		}
+
+		public ICommand NgCommand
+		{
+			get
+			{
+				return new DelegateCommand(
+					o => {
+						Model.Accept = false;
+					}
+				);
+			}
 		}
 
 		#endregion
