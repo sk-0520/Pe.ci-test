300202efc2d6960eb4c6898d5bc13ef7abcff4f7
sk <content.type.text.net@gmail.com>
2015-06-11 23:01:35 +0900

    EventUtility

---
+++ Pe2/Pe/Library/SharedLibrary/Logic/EventDisposer.cs
@@ -0,0 +1,159 @@
+﻿namespace ContentTypeTextNet.Library.SharedLibrary.Logic
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+
+	public class EventDisposer: DisposeFinalizeBase
+	{
+		public EventDisposer()
+			: base()
+		{ }
+
+		#region property
+
+		protected Delegate EventHandler { get; private set; }
+		protected Action<Delegate> ReleaseEvent { get; private set; }
+
+		#endregion
+
+		#region DisposeFinalizeBase
+
+		protected override void Dispose(bool disposing)
+		{
+			if(!IsDisposed) {
+				ReleaseEvent(EventHandler);
+			}
+
+			base.Dispose(disposing);
+		}
+		#endregion
+
+		#region function
+
+		public Delegate Handle(Delegate eventHandler, Action<Delegate> releaseEvent)
+		{
+			if(EventHandler != null) {
+				throw new InvalidOperationException("EventHandler");
+			}
+			if(ReleaseEvent != null) {
+				throw new InvalidOperationException("ReleaseEvent");
+			}
+
+			if(eventHandler == null) {
+				throw new ArgumentNullException("eventHandler");
+			}
+			if(releaseEvent == null) {
+				throw new ArgumentNullException("releaseEvent");
+			}
+
+			ReleaseEvent = releaseEvent;
+			return EventHandler = eventHandler;
+		}
+
+		#endregion
+	}
+
+	public class EventHandlerDisposer: DisposeFinalizeBase
+	{
+		public EventHandlerDisposer()
+			: base()
+		{ }
+
+		#region property
+
+		protected EventHandler EventHandler {get; private set;}
+		protected Action<EventHandler> ReleaseEvent { get; private set; }
+
+		#endregion
+
+		#region DisposeFinalizeBase
+
+		protected override void Dispose(bool disposing)
+		{
+			if(!IsDisposed) {
+				ReleaseEvent(EventHandler);
+			}
+
+			base.Dispose(disposing);
+		}
+		#endregion
+
+		#region function
+
+		public EventHandler Handle(EventHandler eventHandler, Action<EventHandler> releaseEvent)
+		{
+			if(EventHandler != null) {
+				throw new InvalidOperationException("EventHandler");
+			}
+			if(ReleaseEvent != null) {
+				throw new InvalidOperationException("ReleaseEvent");
+			}
+
+			if(eventHandler == null) {
+				throw new ArgumentNullException("eventHandler");
+			}
+			if(releaseEvent == null) {
+				throw new ArgumentNullException("releaseEvent");
+			}
+
+			ReleaseEvent = releaseEvent;
+			return EventHandler = eventHandler;
+		}
+
+		#endregion
+	}
+
+	public class EventHandlerDisposer<TEventArgs>: DisposeFinalizeBase
+	{
+		public EventHandlerDisposer()
+			: base()
+		{ }
+
+		#region property
+
+		protected EventHandler<TEventArgs> EventHandler { get; private set; }
+		protected Action<EventHandler<TEventArgs>> ReleaseEvent { get; private set; }
+
+		#endregion
+
+		#region DisposeFinalizeBase
+
+		protected override void Dispose(bool disposing)
+		{
+			if(!IsDisposed) {
+				ReleaseEvent(EventHandler);
+			}
+
+			base.Dispose(disposing);
+		}
+		#endregion
+
+		#region function
+
+		public EventHandler<TEventArgs> Handle(EventHandler<TEventArgs> eventHandler, Action<EventHandler<TEventArgs>> releaseEvent)
+		{
+			if(EventHandler != null) {
+				throw new InvalidOperationException("EventHandler");
+			}
+			if(ReleaseEvent != null) {
+				throw new InvalidOperationException("ReleaseEvent");
+			}
+
+			if(eventHandler == null) {
+				throw new ArgumentNullException("eventHandler");
+			}
+			if(releaseEvent == null) {
+				throw new ArgumentNullException("releaseEvent");
+			}
+
+			ReleaseEvent = releaseEvent;
+			return EventHandler = eventHandler;
+		}
+
+		#endregion
+	}
+
+}
---
+++ Pe2/Pe/Library/SharedLibrary/Logic/Utility/EventUtility.cs
@@ -0,0 +1,44 @@
+﻿namespace ContentTypeTextNet.Library.SharedLibrary.Logic.Utility
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+
+	public static class EventUtility
+	{
+		public static Delegate Create(Delegate handler, Action<Delegate> releaseEvent, out EventDisposer eventDisposer)
+		{
+			eventDisposer = new EventDisposer();
+			return eventDisposer.Handle(handler, releaseEvent);
+		}
+		public static Delegate Auto(Delegate handler, Action<Delegate> releaseEvent)
+		{
+			EventDisposer eventDisposer;
+			return Create(handler, releaseEvent, out eventDisposer);
+		}
+
+		public static EventHandler Create(EventHandler handler, Action<EventHandler> releaseEvent, out EventHandlerDisposer eventDisposer)
+		{
+			eventDisposer = new EventHandlerDisposer();
+			return eventDisposer.Handle(handler, releaseEvent);
+		}
+		public static EventHandler Auto(EventHandler handler, Action<EventHandler> releaseEvent)
+		{
+			EventHandlerDisposer eventDisposer;
+			return Create(handler, releaseEvent, out eventDisposer);
+		}
+
+		public static EventHandler<TEventArgs> Create<TEventArgs>(EventHandler<TEventArgs> handler, Action<EventHandler<TEventArgs>> releaseEvent, out EventHandlerDisposer<TEventArgs> eventDisposer)
+		{
+			eventDisposer = new EventHandlerDisposer<TEventArgs>();
+			return eventDisposer.Handle(handler, releaseEvent);
+		}
+		public static EventHandler<TEventArgs> Auto<TEventArgs>(EventHandler<TEventArgs> handler, Action<EventHandler<TEventArgs>> releaseEvent)
+		{
+			EventHandlerDisposer<TEventArgs> eventDisposer;
+			return Create(handler, releaseEvent, out eventDisposer);
+		}
+	}
+}
