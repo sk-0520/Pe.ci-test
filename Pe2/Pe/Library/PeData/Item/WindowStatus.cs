da6929eeced115b85701a95db3cd9a77d4f6f15e
sk <content.type.text.net@gmail.com>
2015-06-12 09:43:39 +0900

    ウィンドウ状態も保持するように変更

--- Pe2/Pe/Library/PeData/Item/WindowStatus.cs
+++ Pe2/Pe/Library/PeData/Item/WindowStatus.cs
@@ -3,6 +3,7 @@
 	using System;
 	using System.Collections.Generic;
 	using System.Linq;
+	using System.Runtime.Serialization;
 	using System.Text;
 	using System.Threading.Tasks;
 
@@ -16,18 +17,25 @@
 		/// <summary>
 		/// 論理X座標。
 		/// </summary>
+		[DataMember]
 		public double X { get; set; }
 		/// <summary>
 		/// 論理Y座標。
 		/// </summary>
+		[DataMember]
 		public double Y { get; set; }
 		/// <summary>
 		/// 論理横幅。
 		/// </summary>
+		[DataMember]
 		public double Width { get; set; }
 		/// <summary>
 		/// 論理高さ。
 		/// </summary>
+		[DataMember]
 		public double Height { get; set; }
+
+		[DataMember]
+		public WindowStatus State { get; set; }
 	}
 }
