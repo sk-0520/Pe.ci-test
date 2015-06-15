6b4f61cfde9f96201e5c314f221140e72a68c4b8
sk <content.type.text.net@gmail.com>
2015-06-15 18:09:04 +0900

    忘れ物

---
+++ Pe2/Pe/Library/SharedLibrary/Model/Unmanaged/Gdi/GdiObjectModelBase.cs
@@ -0,0 +1,58 @@
+﻿namespace ContentTypeTextNet.Library.SharedLibrary.Model.Unmanaged.Gdi
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+	using System.Windows.Interop;
+	using System.Windows.Media;
+	using System.Windows.Media.Imaging;
+	using ContentTypeTextNet.Library.PInvoke.Windows;
+	using ContentTypeTextNet.Library.SharedLibrary.Logic.Utility;
+
+	/// <summary>
+	/// アンマネージドなGDIオブジェクトを管理。
+	/// </summary>
+	public abstract class GdiObjectModelBase: UnmanagedHandleModelBase
+	{
+		public GdiObjectModelBase(IntPtr hHandle)
+			: base(hHandle)
+		{ }
+
+		#region property
+
+		public virtual bool CanMakeImageSource { get { return false; } }
+
+		#endregion
+
+		#region UnmanagedHandleModelBase
+
+		protected override void ReleaseHandle()
+		{
+			NativeMethods.DeleteObject(Handle);
+		}
+
+		#endregion
+
+		#region function
+
+		protected virtual BitmapSource MakeImageSourceImpl()
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// GDIオブジェクトからImageSource作成。
+		/// </summary>
+		/// <returns></returns>
+		/// <exception cref="NotImplementedException">CanMakeImageSource</exception>
+		public BitmapSource MakeImageSource()
+		{
+			CheckUtility.Enforce<NotImplementedException>(CanMakeImageSource);
+			return MakeImageSourceImpl();
+		}
+
+		#endregion
+	}
+}
