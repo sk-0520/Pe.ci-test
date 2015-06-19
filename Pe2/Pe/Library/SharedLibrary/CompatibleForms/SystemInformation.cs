b3e16662e3007aa0ac65b4d38850175383548afa
sk <content.type.text.net@gmail.com>
2015-06-19 23:15:09 +0900

    サブメニューを持たせるのはどうにも得策じゃない

---
+++ Pe2/Pe/Library/SharedLibrary/CompatibleForms/SystemInformation.cs
@@ -0,0 +1,14 @@
+﻿namespace ContentTypeTextNet.Library.SharedLibrary.CompatibleForms
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using Forms = System.Windows.Forms;
+	using System.Threading.Tasks;
+
+	public static class SystemInformation
+	{
+		public static TimeSpan MouseHoverTime { get { return TimeSpan.FromMilliseconds(Forms.SystemInformation.MouseHoverTime); } }
+	}
+}
---
+++ Pe2/Pe/PeMain/View/Parts/Control/LauncherToolbar/FastToggleButton.cs
@@ -0,0 +1,21 @@
+﻿namespace ContentTypeTextNet.Pe.PeMain.View.Parts.Control.LauncherToolbar
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+	using System.Windows.Controls.Primitives;
+	using System.Windows.Input;
+
+	public class FastToggleButton: ToggleButton
+	{
+		public FastToggleButton()
+			: base()
+		{ }
+
+		#region ToggleButton
+
+		#endregion
+	}
+}
