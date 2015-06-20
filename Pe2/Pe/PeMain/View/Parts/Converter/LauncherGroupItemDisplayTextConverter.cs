0b18788e5f04b82b1688256fb7f541af97ecfd6d
sk <content.type.text.net@gmail.com>
2015-06-20 23:11:45 +0900

    LauncherGroupItemDisplayTextConverter

---
+++ Pe2/Pe/PeMain/View/Parts/Converter/LauncherGroupItemDisplayTextConverter.cs
@@ -0,0 +1,26 @@
+﻿namespace ContentTypeTextNet.Pe.PeMain.View.Parts.Converter
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+	using System.Windows.Data;
+	using ContentTypeTextNet.Library.SharedLibrary.IF;
+	using ContentTypeTextNet.Pe.Library.PeData.Item;
+	using ContentTypeTextNet.Pe.PeMain.Logic.Utility;
+
+	public class LauncherGroupItemDisplayTextConverter: IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			var model = (LauncherGroupItemModel)value;
+			return DisplayTextUtility.GetDisplayName(model);
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			return null;
+		}
+	}
+}
