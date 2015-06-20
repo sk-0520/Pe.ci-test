ed82ee902b215b4d535cc689f4690cb029942efb
sk <content.type.text.net@gmail.com>
2015-06-20 21:55:40 +0900

    VSのgit add条件が全く分からん

---
+++ Pe2/Pe/PeMain/Logic/Utility/LauncherItemUtility.cs
@@ -0,0 +1,51 @@
+﻿namespace ContentTypeTextNet.Pe.PeMain.Logic.Utility
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+	using System.Windows.Media.Imaging;
+	using ContentTypeTextNet.Library.SharedLibrary.Define;
+	using ContentTypeTextNet.Library.SharedLibrary.IF;
+	using ContentTypeTextNet.Library.SharedLibrary.Logic.Utility;
+	using ContentTypeTextNet.Library.SharedLibrary.Model;
+	using ContentTypeTextNet.Pe.Library.PeData.Item;
+
+	public static class LauncherItemUtility
+	{
+		public static BitmapSource GetIcon(LauncherItemModel model, IconScale iconScale, ILogger logger = null)
+		{
+			CheckUtility.DebugEnforceNotNull(model);
+			CheckUtility.DebugEnforceNotNull(model.Icon);
+
+			var hasIcon = false;
+			var useIcon = new IconPathModel();
+
+			if(!string.IsNullOrWhiteSpace(model.Icon.Path)) {
+				var expandIconPath = Environment.ExpandEnvironmentVariables(model.Icon.Path);
+				hasIcon = FileUtility.Exists(expandIconPath);
+				if(hasIcon) {
+					useIcon.Path = expandIconPath;
+					useIcon.Index = model.Icon.Index;
+				}
+			}
+			if(!hasIcon) {
+				if(!string.IsNullOrWhiteSpace(model.Command)) {
+					var expandCommandPath = Environment.ExpandEnvironmentVariables(model.Command);
+					hasIcon = FileUtility.Exists(expandCommandPath);
+					if(hasIcon) {
+						useIcon.Path = expandCommandPath;
+						useIcon.Index = 0;
+					}
+				}
+			}
+
+			if(hasIcon) {
+				return AppUtility.LoadIconDefault(useIcon, iconScale, logger);
+			} else {
+				return null;
+			}
+		}
+	}
+}
