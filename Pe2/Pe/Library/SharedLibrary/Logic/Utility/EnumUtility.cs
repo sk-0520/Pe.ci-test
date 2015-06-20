94b7c3340156914d40a86f597f0a430499dd707f
sk <content.type.text.net@gmail.com>
2015-06-20 17:16:34 +0900

    enumの共通処理作るまいか悩んだけどキャストだるいので作った

---
+++ Pe2/Pe/Library/SharedLibrary/Logic/Utility/EnumUtility.cs
@@ -0,0 +1,20 @@
+﻿namespace ContentTypeTextNet.Library.SharedLibrary.Logic.Utility
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+
+	public static class EnumUtility
+	{
+		public static IEnumerable<TEnum> GetMembers<TEnum>(Type type)
+		{
+			return Enum.GetValues(type).Cast<TEnum>();
+		}
+		public static IEnumerable<TEnum> GetMembers<TEnum>()
+		{
+			return GetMembers<TEnum>(typeof(TEnum));
+		}
+	}
+}
