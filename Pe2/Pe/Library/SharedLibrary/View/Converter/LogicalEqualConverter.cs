52d8389ab8f2ee86bcabf02b1d3bd92653d0ff0d
sk <content.type.text.net@gmail.com>
2015-06-21 15:32:23 +0900

    チェックつけてみたけど前はこれラジオだったなぁ。。。

---
+++ Pe2/Pe/Library/SharedLibrary/View/Converter/LogicalEqualConverter.cs
@@ -0,0 +1,22 @@
+﻿namespace ContentTypeTextNet.Library.SharedLibrary.View.Converter
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+	using System.Windows.Data;
+
+	public class LogicalEqualConverter: IMultiValueConverter
+	{
+		public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			return values[0] == values[1];
+		}
+
+		public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+		{
+			return null;
+		}
+	}
+}
