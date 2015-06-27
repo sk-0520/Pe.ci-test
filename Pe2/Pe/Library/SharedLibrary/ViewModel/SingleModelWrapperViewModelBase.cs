c5640665fe1a8525c932fbb519cdae659a99b42a
sk <content.type.text.net@gmail.com>
2015-06-27 17:13:10 +0900

    SetModelValue

--- Pe2/Pe/Library/SharedLibrary/ViewModel/SingleModelWrapperViewModelBase.cs
+++ Pe2/Pe/Library/SharedLibrary/ViewModel/SingleModelWrapperViewModelBase.cs
@@ -3,6 +3,7 @@
 	using System;
 	using System.Collections.Generic;
 	using System.Linq;
+	using System.Runtime.CompilerServices;
 	using System.Text;
 	using System.Threading.Tasks;
 	using ContentTypeTextNet.Library.SharedLibrary.Model;
@@ -38,6 +39,19 @@
 
 		protected virtual void InitializeModel()
 		{ }
+
+		/// <summary>
+		/// モデル変更用ヘルパ。
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="value">変更データ。</param>
+		/// <param name="memberName">対象オブジェクトのメンバ名。</param>
+		/// <param name="propertyName"></param>
+		/// <returns>変更があった場合は真を返す。</returns>
+		protected bool SetModelValue<T>(T value, [CallerMemberName] string memberName = "", [CallerMemberName] string propertyName = "")
+		{
+			return SetPropertyValue(Model, value, memberName, propertyName);
+		}
 
 		#endregion
 
