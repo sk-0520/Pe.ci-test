e7e88a88e6887c4e5d492ca6a66097ac72a23f7d
sk <content.type.text.net@gmail.com>
2015-06-27 15:23:27 +0900

    あー、設定だるい。。。Formsでいいじゃん・・・

---
+++ Pe2/Pe/PeMain/View/Parts/Control/LauncherItemsListControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace ContentTypeTextNet.Pe.PeMain.View.Parts.Control
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+	using System.Windows;
+	using System.Windows.Controls;
+	using System.Windows.Data;
+	using System.Windows.Documents;
+	using System.Windows.Input;
+	using System.Windows.Media;
+	using System.Windows.Media.Imaging;
+	using System.Windows.Navigation;
+	using System.Windows.Shapes;
+
+	/// <summary>
+	/// LauncherItemsListControl.xaml の相互作用ロジック
+	/// </summary>
+	public partial class LauncherItemsListControl: CommonDataUserControl
+	{
+		public LauncherItemsListControl()
+		{
+			InitializeComponent();
+		}
+	}
+}
