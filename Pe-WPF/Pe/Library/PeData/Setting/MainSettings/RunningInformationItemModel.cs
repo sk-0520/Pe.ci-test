b8796408a11ec71e754aef6fb80ab4381af9f039
sk <content.type.text.net@gmail.com>
2015-07-07 14:46:01 +0900

    違和感を感じてたので移動

---
+++ Pe-WPF/Pe/Library/PeData/Setting/MainSettings/RunningInformationItemModel.cs
@@ -0,0 +1,48 @@
+﻿namespace ContentTypeTextNet.Pe.Library.PeData.Item
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Runtime.Serialization;
+	using System.Text;
+	using System.Threading.Tasks;
+
+	[Serializable]
+	public class RunningInformationItemModel: ItemModelBase
+	{
+		public RunningInformationItemModel()
+			: base()
+		{ }
+
+		/// <summary>
+		/// 実行が許可されているか。
+		/// </summary>
+		[DataMember]
+		public bool Accept { get; set; }
+		/// <summary>
+		/// 前回終了時のバージョン。
+		/// </summary>
+		[DataMember]
+		public Version LastExecuteVersion { get; set; }
+		/// <summary>
+		/// アップデートチェックを行うか。
+		/// </summary>
+		[DataMember]
+		public bool CheckUpdateRelease { get; set; }
+		/// <summary>
+		/// RCアップデートチェックを行うか。
+		/// </summary>
+		[DataMember]
+		public bool CheckUpdateRC { get; set; }
+		/// <summary>
+		/// アップデートチェックで無視するバージョン。
+		/// </summary>
+		[DataMember]
+		public Version IgnoreUpdateVersion { get; set; }
+		/// <summary>
+		/// プログラム実行回数。
+		/// </summary>
+		[DataMember]
+		public int ExecuteCount { get; set; }
+	}
+}
