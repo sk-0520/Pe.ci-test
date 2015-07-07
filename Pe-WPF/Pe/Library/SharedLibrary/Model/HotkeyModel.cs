8fe2b1f1115c07e5495b41b97ec2f3f509575ffd
sk <content.type.text.net@gmail.com>
2015-07-07 15:04:35 +0900

    HotkeyModel.DeepClone

--- Pe-WPF/Pe/Library/SharedLibrary/Model/HotkeyModel.cs
+++ Pe-WPF/Pe/Library/SharedLibrary/Model/HotkeyModel.cs
@@ -7,9 +7,10 @@
 	using System.Text;
 	using System.Threading.Tasks;
 	using System.Windows.Input;
+using ContentTypeTextNet.Library.SharedLibrary.IF;
 
 	[Serializable]
-	public class HotkeyModel: ModelBase
+	public class HotkeyModel: ModelBase, IDeepClone
 	{
 		public HotkeyModel()
 			: base()
@@ -19,5 +20,19 @@
 		public ModifierKeys ModifierKeys { get; set; }
 		[DataMember]
 		public Key Key { get; set; }
+
+		#region IDeepClone
+
+		public virtual IDeepClone DeepClone()
+		{
+			var result = new HotkeyModel() {
+				Key = this.Key,
+				ModifierKeys = this.ModifierKeys,
+			};
+
+			return result;
+		}
+
+		#endregion
 	}
 }
