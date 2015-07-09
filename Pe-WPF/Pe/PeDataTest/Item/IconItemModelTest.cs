b7e4d0471727ead2ed22a28bcfe0df8303778e1f
sk <content.type.text.net@gmail.com>
2015-07-09 22:17:30 +0900

    IconItemModelTest

---
+++ Pe-WPF/Pe/PeDataTest/Item/IconItemModelTest.cs
@@ -0,0 +1,33 @@
+﻿namespace PeDataTest.Item
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+	using ContentTypeTextNet.Pe.Library.PeData.Item;
+	using NUnit.Framework;
+
+	[TestFixture]
+	class IconItemModelTest
+	{
+		public void DeepCloneTest()
+		{
+			var src = new IconItemModel() {
+				Path = "path",
+				Index = 1,
+			};
+
+			var cp = (IconItemModel)src.DeepClone();
+
+			Assert.AreEqual(cp.Path, src.Path);
+			Assert.AreEqual(cp.Index, src.Index);
+
+			src.Path = "test";
+			src.Index = 2
+
+			Assert.AreNotEqual(cp.Path, src.Path);
+			Assert.AreNotEqual(cp.Index, src.Index);
+		}
+	}
+}
