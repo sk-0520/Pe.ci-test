c864397325ce6791a8e8a0914da6f301f27d1294
sk <content.type.text.net@gmail.com>
2015-07-10 16:32:43 +0900

    EnabledClipboardTypes/SaveClipboardTypes

--- Pe-WPF/Pe/Library/PeData/Define/ClipboardType.cs
+++ Pe-WPF/Pe/Library/PeData/Define/ClipboardType.cs
@@ -7,13 +7,13 @@
 	using System.Threading.Tasks;
 
 	[Flags]
-	public enum ClipboardType
+	public enum ClipboardType:int
 	{
-		None,
-		Text,
-		Rtf,
-		Html,
-		Image,
-		File,
+		None = 0x00,
+		Text = 0x01,
+		Rtf = 0x02,
+		Html = 0x04,
+		Image = 0x08,
+		File = 0x10,
 	}
 }
--- Pe-WPF/Pe/PeMain/ViewModel/Control/SettingPage/ClipboardSettingViewModel.cs
+++ Pe-WPF/Pe/PeMain/ViewModel/Control/SettingPage/ClipboardSettingViewModel.cs
@@ -3,15 +3,25 @@
 	using System;
 	using System.Collections.Generic;
 	using System.Linq;
+	using System.Runtime.CompilerServices;
 	using System.Text;
 	using System.Threading.Tasks;
 	using ContentTypeTextNet.Library.SharedLibrary.IF;
+	using ContentTypeTextNet.Library.SharedLibrary.Model;
+	using ContentTypeTextNet.Pe.Library.PeData.Define;
 	using ContentTypeTextNet.Pe.Library.PeData.Setting.MainSettings;
 	using ContentTypeTextNet.Pe.PeMain.Data;
 	using ContentTypeTextNet.Pe.PeMain.Data.Temporary;
 
 	public class ClipboardSettingViewModel : SettingPageViewModelBase
 	{
+		#region variable
+
+		const string defineEnabled = "EnabledClipboardTypes";
+		const string defineSave = "SaveClipboardTypes";
+
+		#endregion
+
 		public ClipboardSettingViewModel(ClipboardSettingModel clipboard, INonProcess nonProcess, VariableConstants variableConstants, SettingNotifiyItem settingNotifiyItem)
 			: base(nonProcess, variableConstants, settingNotifiyItem)
 		{
@@ -22,6 +32,138 @@
 
 		ClipboardSettingModel Clipboard { get; set; }
 
+		public bool Enabled
+		{
+			get { return Clipboard.Enabled; }
+			set { SetPropertyValue(Clipboard, value); }
+		}
+
+		public bool EnabledApplicationCopy
+		{
+			get { return Clipboard.EnabledApplicationCopy; }
+			set { SetPropertyValue(Clipboard, value); }
+		}
+
+		public bool SaveClipboard
+		{
+			get { return Clipboard.SaveClipboard; }
+			set { SetPropertyValue(Clipboard, value); }
+		}
+
+		public HotKeyModel ToggleHotKey
+		{
+			get { return Clipboard.ToggleHotKey; }
+			set { SetPropertyValue(Clipboard, value); }
+		}
+
+		#region EnabledClipboardTypes
+
+		public bool EnabledClipboardTypesText
+		{
+			get { return GetBoolean(Clipboard.EnabledClipboardTypes, ClipboardType.Text); }
+			set { SetClipboardType(Clipboard, Clipboard.EnabledClipboardTypes, ClipboardType.Text, defineEnabled); }
+		}
+		public bool EnabledClipboardTypesRtf
+		{
+			get { return GetBoolean(Clipboard.EnabledClipboardTypes, ClipboardType.Rtf); }
+			set { SetClipboardType(Clipboard, Clipboard.EnabledClipboardTypes, ClipboardType.Rtf, defineEnabled); }
+		}
+		public bool EnabledClipboardTypesHtml
+		{
+			get { return GetBoolean(Clipboard.EnabledClipboardTypes, ClipboardType.Html); }
+			set { SetClipboardType(Clipboard, Clipboard.EnabledClipboardTypes, ClipboardType.Html, defineEnabled); }
+		}
+		public bool EnabledClipboardTypesImage
+		{
+			get { return GetBoolean(Clipboard.EnabledClipboardTypes, ClipboardType.Image); }
+			set { SetClipboardType(Clipboard, Clipboard.EnabledClipboardTypes, ClipboardType.Image, defineEnabled); }
+		}
+		public bool EnabledClipboardTypesFile
+		{
+			get { return GetBoolean(Clipboard.EnabledClipboardTypes, ClipboardType.File); }
+			set { SetClipboardType(Clipboard, Clipboard.EnabledClipboardTypes, ClipboardType.File, defineEnabled); }
+		}
+
+		#endregion
+
+		#region SaveClipboardTypes
+
+		public bool SaveClipboardTypesText
+		{
+			get { return GetBoolean(Clipboard.SaveClipboardTypes, ClipboardType.Text); }
+			set { SetClipboardType(Clipboard, Clipboard.SaveClipboardTypes, ClipboardType.Text, defineSave); }
+		}
+		public bool SaveClipboardTypesRtf
+		{
+			get { return GetBoolean(Clipboard.SaveClipboardTypes, ClipboardType.Rtf); }
+			set { SetClipboardType(Clipboard, Clipboard.SaveClipboardTypes, ClipboardType.Rtf, defineSave); }
+		}
+		public bool SaveClipboardTypesHtml
+		{
+			get { return GetBoolean(Clipboard.SaveClipboardTypes, ClipboardType.Html); }
+			set { SetClipboardType(Clipboard, Clipboard.SaveClipboardTypes, ClipboardType.Html, defineSave); }
+		}
+		public bool SaveClipboardTypesImage
+		{
+			get { return GetBoolean(Clipboard.SaveClipboardTypes, ClipboardType.Image); }
+			set { SetClipboardType(Clipboard, Clipboard.SaveClipboardTypes, ClipboardType.Image, defineSave); }
+		}
+		public bool SaveClipboardTypesFile
+		{
+			get { return GetBoolean(Clipboard.SaveClipboardTypes, ClipboardType.File); }
+			set { SetClipboardType(Clipboard, Clipboard.SaveClipboardTypes, ClipboardType.File, defineSave); }
+		}
+
+		#endregion
+
+		public int SaveCount
+		{
+			get { return Clipboard.SaveCount; }
+			set { SetPropertyValue(Clipboard, value); }
+		}
+
+		public double WaitTimeMs
+		{
+			get { return Clipboard.WaitTime.TotalMilliseconds; }
+			set { SetPropertyValue(Clipboard, TimeSpan.FromMilliseconds(value), "WaitTime"); }
+		}
+
+		public int DuplicationCount
+		{
+			get { return Clipboard.DuplicationCount; }
+			set { SetPropertyValue(Clipboard, value); }
+		}
+
+		public bool UsingClipboard
+		{
+			get { return Clipboard.UsingClipboard; }
+			set { SetPropertyValue(Clipboard, value); }
+		}
+
+		public bool TopMost
+		{
+			get { return Clipboard.TopMost; }
+			set { SetPropertyValue(Clipboard, value); }
+		}
+
+		public bool Visible
+		{
+			get { return Clipboard.Visible; }
+			set { SetPropertyValue(Clipboard, value); }
+		}
+
+		#endregion
+
+		#region function
+
+		bool GetBoolean(ClipboardType clipboardType, ClipboardType value)
+		{
+			return clipboardType.HasFlag(value);
+		}
+		void SetClipboardType(object obj, ClipboardType nowValue, ClipboardType clipboardType, string memberName, [CallerMemberName]string propertyName = "")
+		{
+			SetPropertyValue(obj, nowValue ^ clipboardType, memberName, propertyName);
+		}
 		#endregion
 	}
 }
