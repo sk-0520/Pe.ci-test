f3ffaa3159659bace182315c8f7f5a9eed13ed2b
sk <content.type.text.net@gmail.com>
2015-07-11 00:18:06 +0900

    NoteItemCollectionModel

---
+++ Pe-WPF/Pe/Library/PeData/Item/NoteItemCollectionModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ContentTypeTextNet.Library.SharedLibrary.Model;
+
+namespace ContentTypeTextNet.Pe.Library.PeData.Item
+{
+	[Serializable]
+	public class NoteItemCollectionModel: GuidCollectionBase<NoteItemModel>
+	{
+		public NoteItemCollectionModel()
+			: base()
+		{ }
+	}
+}
