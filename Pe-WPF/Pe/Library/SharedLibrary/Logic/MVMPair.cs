2dbd09565e793f24db40fe583d1ef4ddbe1cdd92
sk <content.type.text.net@gmail.com>
2015-07-17 10:39:59 +0900

    MVMPairCollectionBase

---
+++ Pe-WPF/Pe/Library/SharedLibrary/Logic/MVMPair.cs
@@ -0,0 +1,63 @@
+﻿namespace ContentTypeTextNet.Library.SharedLibrary.Logic
+{
+	using System;
+	using System.Collections;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+	using ContentTypeTextNet.Library.SharedLibrary.Model;
+	using ContentTypeTextNet.Library.SharedLibrary.ViewModel;
+
+	public struct MVMPair<TModel, TViewModel>
+		where TModel : ModelBase
+		where TViewModel : ViewModelBase
+	{
+		#region variable
+
+		TModel _model;
+		TViewModel _viewModel;
+
+		#endregion
+
+		public MVMPair(TModel model, TViewModel viewModel)
+		{
+			this._model = model;
+			this._viewModel = viewModel;
+		}
+
+		#region property
+
+		public TModel Model { get { return this._model; } }
+		public TViewModel ViewModel { get { return this._viewModel; } }
+
+		#endregion
+
+		#region object
+
+		public override string ToString()
+		{
+			return string.Format(
+				"Model=>{0},ViewModel=>{1}",
+				Model,
+				ViewModel
+			);
+		}
+
+		#endregion
+	}
+
+	/// <summary>
+	/// MVMPair生成のヘルパ。
+	/// </summary>
+	public static class MVMPair
+	{
+		public static MVMPair<TModel, TViewModel> Create<TModel, TViewModel>(TModel model, TViewModel viewModel)
+			where TModel : ModelBase
+			where TViewModel : ViewModelBase
+		{
+			return new MVMPair<TModel, TViewModel>(model, viewModel);
+		}
+
+	}
+}
---
+++ Pe-WPF/Pe/Library/SharedLibrary/Logic/MVMPairCollectionBase.cs
@@ -0,0 +1,145 @@
+﻿namespace ContentTypeTextNet.Library.SharedLibrary.Logic
+{
+	using System;
+	using System.Collections;
+	using System.Collections.Generic;
+	using System.Collections.ObjectModel;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+	using ContentTypeTextNet.Library.SharedLibrary.Model;
+	using ContentTypeTextNet.Library.SharedLibrary.ViewModel;
+
+	/// <summary>
+	/// ModelとViewModelの対となるコレクションを保持する。
+	/// </summary>
+	/// <typeparam name="TModel"></typeparam>
+	/// <typeparam name="TViewModel"></typeparam>
+	public abstract class MVMPairCollectionBase<TModel, TViewModel> : ICollection<MVMPair<TModel, TViewModel>>
+		where TModel : ModelBase
+		where TViewModel : ViewModelBase
+	{
+		public MVMPairCollectionBase(ObservableCollection<TModel> modelList, ObservableCollection<TViewModel> viewModelList)
+		{
+			ModelList = modelList;
+			ViewModelList = viewModelList;
+		}
+
+		public MVMPairCollectionBase(ObservableCollection<TModel> modelList, IEnumerable<object> dataList)
+		{
+			ModelList = modelList;
+			ViewModelList = new ObservableCollection<TViewModel>(CreateViewModelList(modelList, dataList));
+		}
+
+		public MVMPairCollectionBase(ObservableCollection<TModel> modelList, object data)
+		{
+			ModelList = modelList;
+			ViewModelList = new ObservableCollection<TViewModel>(CreateViewModelList(modelList, Enumerable.Repeat(data, ModelList.Count)));
+		}
+
+		#region property
+
+		public ObservableCollection<TModel> ModelList { get; private set; }
+		public ObservableCollection<TViewModel> ViewModelList { get; private set; } 
+
+		#endregion
+
+		#region function
+
+		/// <summary>
+		/// ViewModelを作成。
+		/// </summary>
+		/// <param name="model">元になるModel。</param>
+		/// <param name="data">ViewModel生成に必要なModel以外のデータ。</param>
+		/// <returns></returns>
+		public abstract TViewModel CreateViewModel(TModel model, object data);
+
+		protected IEnumerable<TViewModel> CreateViewModelList(IEnumerable<TModel> model, IEnumerable<object> data)
+		{
+			foreach (var pair in model.Zip(data, (f, s) => new { Model = f, Data = s })) {
+				yield return CreateViewModel(pair.Model, pair.Data);
+			}
+		}
+
+		public int IndexOf(MVMPair<TModel, TViewModel> item)
+		{
+			var index = ModelList.IndexOf(item.Model);
+			if (index == -1) {
+				return -1;
+			}
+			
+			if (ViewModelList[index] != item.ViewModel) {
+				return -1;
+			}
+
+			return index;
+		}
+
+		public void Add(TModel model, object data)
+		{
+			var viewModel = CreateViewModel(model, data);
+
+			Add(MVMPair.Create(model, viewModel));
+		}
+
+		public void RemoveAt(int index)
+		{
+			ModelList.RemoveAt(index);
+			ViewModelList.RemoveAt(index);
+		}
+
+		#endregion
+
+		#region ICollection
+
+		public void Add(MVMPair<TModel, TViewModel> item)
+		{
+			ModelList.Add(item.Model);
+			ViewModelList.Add(item.ViewModel);
+		}
+
+		public void Clear()
+		{
+			ModelList.Clear();
+			ViewModelList.Clear();
+		}
+
+		public bool Contains(MVMPair<TModel, TViewModel> item)
+		{
+			var index = IndexOf(item);
+			return index != -1;
+		}
+
+
+		public void CopyTo(MVMPair<TModel, TViewModel>[] array, int index)
+		{
+			var i = index;
+			foreach (var item in array) {
+				array[i++] = item;
+			}
+		}
+
+		public IEnumerator<MVMPair<TModel, TViewModel>> GetEnumerator()
+		{
+			foreach (var pair in ModelList.Zip(ViewModelList, (m, v) => MVMPair.Create(m, v))) {
+				yield return pair;
+			}
+		}
+
+		IEnumerator IEnumerable.GetEnumerator() { return this.GetEnumerator(); }
+
+		public bool Remove(MVMPair<TModel, TViewModel> item)
+		{
+			var index = IndexOf(item);
+			if (index == -1) {
+				return false;
+			}
+
+			RemoveAt(index);
+
+			return true;
+		}
+
+		#endregion
+	}
+}
