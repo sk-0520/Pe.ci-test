f6c6d81d1e77c2b223d00cfdbe90769153909234
sk <content.type.text.net@gmail.com>
2015-07-25 00:14:39 +0900

    色々分割

---
+++ Pe-WPF/Pe/PeMain/AppResource/AppResource-const.cs
@@ -0,0 +1,17 @@
+﻿namespace ContentTypeTextNet.Pe.PeMain
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+
+	partial class AppResourceT4
+	{
+		[AppResource(AppResourceType.Image)]
+		const string commonFiltering = "/Resources/Image/Common/Filtering.png";
+
+		[AppResource(AppResourceType.Icon)]
+		const string applicationIcon = "/Resources/Icon/App.ico";
+	}
+}
---
+++ Pe-WPF/Pe/PeMain/AppResource/AppResource-main.cs
@@ -0,0 +1,49 @@
+﻿namespace ContentTypeTextNet.Pe.PeMain
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+	using System.Windows.Media.Imaging;
+	using ContentTypeTextNet.Library.SharedLibrary.CompatibleWindows;
+	using ContentTypeTextNet.Library.SharedLibrary.Define;
+	using ContentTypeTextNet.Library.SharedLibrary.IF;
+	using ContentTypeTextNet.Library.SharedLibrary.Logic;
+	using ContentTypeTextNet.Pe.PeMain.Logic;
+
+	/// <summary>
+	/// Peで使用するリソース関係。
+	/// <para>自分の持ち物くらい好きにさわらせてくれ。</para>
+	/// </summary>
+	public static partial class AppResourceT4
+	{
+		#region static
+
+		static IconCaching<string> _iconCaching = new IconCaching<string>();
+		static Caching<string,BitmapSource> _imageCaching = new Caching<string,BitmapSource>();
+
+		#endregion
+
+		#region function
+
+		static BitmapSource GetImage(string path)
+		{
+			return _imageCaching.Get(path, () => {
+				var uri = SharedConstants.GetEntryUri(path);
+				return new BitmapImage(uri);
+			});
+		}
+
+		static BitmapSource GetIcon(string path, IconScale iconScale, ILogger logger = null)
+		{
+			return _iconCaching[iconScale].Get(path, () => {
+				using(var icon = new IconWrapper(path, iconScale)) {
+					return icon.MakeBitmapSource();
+				}
+			});
+		}
+
+		#endregion
+	}
+}
