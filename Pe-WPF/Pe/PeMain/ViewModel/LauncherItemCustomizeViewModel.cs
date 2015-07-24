0bb5a7e563902a533b00ec170c0de5c2f7200f48
sk <content.type.text.net@gmail.com>
2015-07-25 04:49:12 +0900

    LauncherItemCustomizeWindow初期実装しただけ

--- Pe-WPF/Pe/PeMain/ViewModel/LauncherItemCustomizeViewModel.cs
+++ Pe-WPF/Pe/PeMain/ViewModel/LauncherItemCustomizeViewModel.cs
@@ -1,15 +1,16 @@
 ﻿namespace ContentTypeTextNet.Pe.PeMain.ViewModel
 {
 	using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ContentTypeTextNet.Library.SharedLibrary.IF;
-using ContentTypeTextNet.Pe.Library.PeData.Item;
-using ContentTypeTextNet.Pe.PeMain.Data;
-using ContentTypeTextNet.Pe.PeMain.View;
-using ContentTypeTextNet.Pe.PeMain.ViewModel.Control;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+	using System.Windows.Input;
+	using ContentTypeTextNet.Library.SharedLibrary.IF;
+	using ContentTypeTextNet.Pe.Library.PeData.Item;
+	using ContentTypeTextNet.Pe.PeMain.Data;
+	using ContentTypeTextNet.Pe.PeMain.View;
+	using ContentTypeTextNet.Pe.PeMain.ViewModel.Control;
 
 	public class LauncherItemCustomizeViewModel: LauncherItemEditViewModel, IHavingView<LauncherItemCustomizeWindow>
 	{
@@ -18,6 +19,42 @@ using ContentTypeTextNet.Pe.PeMain.ViewModel.Control;
 		{
 			View = view;
 		}
+
+		#region property
+		#endregion
+
+		#region command
+
+		public ICommand SaveCommand
+		{
+			get
+			{
+				var result = CreateCommand(
+					o => {
+					}
+				);
+
+				return result;
+			}
+		}
+
+		public ICommand CancelCommand
+		{
+			get
+			{
+				var result = CreateCommand(
+					o => {
+						if(HasView) {
+							View.Close();
+						}
+					}
+				);
+
+				return result;
+			}
+		}
+
+		#endregion
 
 		#region LauncherItemCustomizeWindow
 
