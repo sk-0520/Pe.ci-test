78e47f1edf551cf3d2626dd0001b2dec2ed16ed0
sk <content.type.text.net@gmail.com>
2015-07-29 23:00:21 +0900

    FontModel

---
+++ Pe-WPF/Pe/Library/SharedLibrary/Model/FontModel.cs
@@ -0,0 +1,23 @@
+﻿namespace ContentTypeTextNet.Library.SharedLibrary.Model
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Runtime.Serialization;
+	using System.Text;
+	using System.Threading.Tasks;
+
+
+	[Serializable]
+	public class FontModel: ModelBase
+	{
+		[DataMember]
+		public string Family { get; set; }
+		[DataMember]
+		public double Size { get; set; }
+		[DataMember]
+		public bool Bold { get; set; }
+		[DataMember]
+		public bool Italic { get; set; }
+	}
+}
