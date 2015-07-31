895555a65a61c53f4b60f95713c2403e16e81931
sk <content.type.text.net@gmail.com>
2015-07-31 23:13:53 +0900

    StreamSettingModel

--- Pe-WPF/Pe/Library/PeData/Setting/MainSettings/StreamSettingModel.cs
+++ Pe-WPF/Pe/Library/PeData/Setting/MainSettings/StreamSettingModel.cs
@@ -5,12 +5,33 @@
 	using System.Linq;
 	using System.Text;
 	using System.Threading.Tasks;
+	using ContentTypeTextNet.Library.SharedLibrary.IF;
 
 	[Serializable]
-	public class StreamSettingModel : SettingModelBase
+	public class StreamSettingModel : SettingModelBase, IDeepClone
 	{
 		public StreamSettingModel()
 			: base()
 		{ }
+
+		#region IDeepClone
+
+		public void DeepCloneTo(IDeepClone target)
+		{
+			var obj = (StreamSettingModel)target;
+
+
+		}
+
+		public IDeepClone DeepClone()
+		{
+			var result = new StreamSettingModel();
+
+			DeepCloneTo(result);
+
+			return result;
+		}
+
+		#endregion
 	}
 }
