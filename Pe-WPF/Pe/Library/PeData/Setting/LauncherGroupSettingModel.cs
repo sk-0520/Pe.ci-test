8316905215dfd24af4c15f8a98add80d6baad90e
sk <content.type.text.net@gmail.com>
2015-07-31 23:45:01 +0900

    LauncherGroupSettingModel

--- Pe-WPF/Pe/Library/PeData/Setting/LauncherGroupSettingModel.cs
+++ Pe-WPF/Pe/Library/PeData/Setting/LauncherGroupSettingModel.cs
@@ -6,13 +6,14 @@
 	using System.Runtime.Serialization;
 	using System.Text;
 	using System.Threading.Tasks;
+	using ContentTypeTextNet.Library.SharedLibrary.IF;
 	using ContentTypeTextNet.Pe.Library.PeData.Item;
 
 	/// <summary>
 	/// ランチャグループ。
 	/// </summary>
 	[DataContract, Serializable]
-	public class LauncherGroupSettingModel: SettingModelBase
+	public class LauncherGroupSettingModel: SettingModelBase, IDeepClone
 	{
 		public LauncherGroupSettingModel()
 			: base()
@@ -20,7 +21,31 @@
 			Groups = new LauncherGroupItemCollectionModel();
 		}
 
+		#region property
+
 		[DataMember]
 		public LauncherGroupItemCollectionModel Groups { get; set; }
+
+		#endregion
+
+		#region IDeepClone
+
+		public void DeepCloneTo(IDeepClone target)
+		{
+			var obj = (LauncherGroupSettingModel)target;
+
+			obj.Groups.AddRange(Groups.Select(i => (LauncherGroupItemModel)i.DeepClone()));
+		}
+
+		public IDeepClone DeepClone()
+		{
+			var result = new LauncherGroupSettingModel();
+
+			DeepCloneTo(result);
+
+			return result;
+		}
+
+		#endregion
 	}
 }
