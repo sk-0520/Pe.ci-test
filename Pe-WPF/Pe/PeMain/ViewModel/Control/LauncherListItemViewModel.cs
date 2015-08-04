dbf47a15ef29384253a74772acc1e2485e3dcec5
sk <content.type.text.net@gmail.com>
2015-08-04 17:23:55 +0900

    LauncherListItemViewModel

---
+++ Pe-WPF/Pe/PeMain/ViewModel/Control/LauncherListItemViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace ContentTypeTextNet.Pe.PeMain.ViewModel.Control
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+	using ContentTypeTextNet.Pe.Library.PeData.Item;
+	using ContentTypeTextNet.Pe.PeMain.IF;
+
+	public class LauncherListItemViewModel : LauncherItemViewModelBase
+	{
+		public LauncherListItemViewModel(LauncherItemModel model, IAppNonProcess nonPorocess, IAppSender appSender)
+			: base(model, nonPorocess, appSender)
+		{ }
+	}
+}
