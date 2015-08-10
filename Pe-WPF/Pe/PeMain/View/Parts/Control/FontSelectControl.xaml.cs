28280d06fca43c4a16f18f1d38f7e7bb76afc9a5
sk <content.type.text.net@gmail.com>
2015-08-10 13:09:05 +0900

    control暫定作成

---
+++ Pe-WPF/Pe/PeMain/View/Parts/Control/FontSelectControl.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace ContentTypeTextNet.Pe.PeMain.View.Parts.Control
+{
+	using System.Windows.Controls;
+	using System.Windows.Data;
+	using System.Windows.Documents;
+	using System.Windows.Input;
+	using System.Windows.Media;
+	using System.Windows.Media.Imaging;
+	using System.Windows.Navigation;
+	using System.Windows.Shapes;
+
+	/// <summary>
+	/// FontSelectControl.xaml の相互作用ロジック
+	/// </summary>
+	public partial class FontSelectControl : CommonDataUserControl
+	{
+		public FontSelectControl()
+		{
+			InitializeComponent();
+		}
+	}
+}
