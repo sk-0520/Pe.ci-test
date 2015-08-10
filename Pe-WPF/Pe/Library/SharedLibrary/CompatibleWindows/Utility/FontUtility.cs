442e6b8eb1136ec645176e8bc2cef48f1a862d88
sk <content.type.text.net@gmail.com>
2015-08-10 11:45:08 +0900

    fontdialog実装しようと思ったけどコントロールの方がいいかも

---
+++ Pe-WPF/Pe/Library/SharedLibrary/CompatibleWindows/Utility/FontUtility.cs
@@ -0,0 +1,33 @@
+﻿namespace ContentTypeTextNet.Library.SharedLibrary.CompatibleWindows.Utility
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+	using Drawing = System.Drawing;
+
+	public static class FontUtility
+	{
+		/// <summary>
+		/// FormsフォントサイズをWOFフォントサイズに変換。
+		/// </summary>
+		/// <param name="size"></param>
+		/// <returns></returns>
+		public static double ConvertSizeFromDrawing(float size)
+		{
+			return size / 72.0 * 96.0;
+		}
+
+		/// <summary>
+		/// WPFフォントサイズをFormsフォントサイズに変換。
+		/// </summary>
+		/// <param name="size"></param>
+		/// <returns></returns>
+		public static float ConvertSizeFromWpf(double size)
+		{
+			return (float)(size * 72.0 / 96.0);
+		}
+
+	}
+}
