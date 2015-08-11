cb20d5614f88e57661e30f6cad8fa37029e3fb74
sk <content.type.text.net@gmail.com>
2015-08-11 10:07:58 +0900

    GetNormalization

--- Pe-WPF/Pe/Library/SharedLibrary/Logic/Utility/EnumUtility.cs
+++ Pe-WPF/Pe/Library/SharedLibrary/Logic/Utility/EnumUtility.cs
@@ -5,6 +5,7 @@
 	using System.Linq;
 	using System.Text;
 	using System.Threading.Tasks;
+	using ContentTypeTextNet.Library.SharedLibrary.Define;
 
 	public static class EnumUtility
 	{
@@ -16,6 +17,22 @@
 		public static IEnumerable<TEnum> GetMembers<TEnum>()
 		{
 			return GetMembers<TEnum>(typeof(TEnum));
+		}
+
+		/// <summary>
+		/// 指定値を正規化された enum の値に変換する。
+		/// </summary>
+		/// <typeparam name="TEnum"></typeparam>
+		/// <param name="value"></param>
+		/// <param name="defaultValue"></param>
+		/// <returns>指定値がEnumに存在すれば指定値、存在しなければ<paramref name="defaultValue"/>を返す。</returns>
+		public static TEnum GetNormalization<TEnum>(object value, TEnum defaultValue)
+		{
+			if(Enum.IsDefined(typeof(TEnum), value)) {
+				return (TEnum)value;
+			} else {
+				return defaultValue;
+			}
 		}
 	}
 }
