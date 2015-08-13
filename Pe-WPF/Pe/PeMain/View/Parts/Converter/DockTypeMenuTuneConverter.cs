9fa937d8748f0639a30b680b17b55636641da505
sk <content.type.text.net@gmail.com>
2015-08-13 11:10:19 +0900

    ツールバーが左の際に右側▽の強調を自然にする

---
+++ Pe-WPF/Pe/PeMain/View/Parts/Converter/DockTypeMenuTuneConverter.cs
@@ -0,0 +1,29 @@
+﻿namespace ContentTypeTextNet.Pe.PeMain.View.Parts.Converter
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Diagnostics;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+	using System.Windows.Data;
+	using ContentTypeTextNet.Library.SharedLibrary.Define;
+
+	public class DockTypeMenuTuneConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			var dockType = (DockType)value;
+			if (dockType == DockType.Left) {
+				Debug.WriteLine(value);
+				return DockType.Right;
+			}
+			return dockType;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
