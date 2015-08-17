76de021c8fc61308b0eba7930c89f0158d59afd2
sk <content.type.text.net@gmail.com>
2015-08-17 23:11:57 +0900

    InitializeToolbarItem.V_Firstの廃止

--- Pe-WPF/Pe/PeMain/Logic/Utility/SettingUtilityImplement/InitializeToolbarItem.cs
+++ Pe-WPF/Pe/PeMain/Logic/Utility/SettingUtilityImplement/InitializeToolbarItem.cs
@@ -2,11 +2,13 @@
 {
 	using System;
 	using System.Collections.Generic;
+	using System.Diagnostics;
 	using System.Linq;
 	using System.Text;
 	using System.Threading.Tasks;
 	using ContentTypeTextNet.Library.SharedLibrary.Define;
 	using ContentTypeTextNet.Library.SharedLibrary.IF;
+	using ContentTypeTextNet.Library.SharedLibrary.Logic.Utility;
 	using ContentTypeTextNet.Pe.Library.PeData.Item;
 	using ContentTypeTextNet.Pe.Library.PeData.Setting;
 
@@ -14,11 +16,14 @@
 	{
 		public static void Correction(ToolbarItemModel setting, Version previousVersion, INonProcess nonProcess)
 		{
-			V_First(setting, previousVersion, nonProcess);
+			//V_First(setting, previousVersion, nonProcess);
+			// ロジック的に previousVersion が null(初回) で到達することはなさそう
+			Debug.Assert(previousVersion != null);
 
 			setting.HideWaitTime = Constants.toolbarHideWaitTime.GetClamp(setting.HideWaitTime);
 			setting.HideAnimateTime = Constants.toolbarHideAnimateTime.GetClamp(setting.HideAnimateTime);
 			setting.Font.Size = Constants.toolbarFontSize.GetClamp(setting.Font.Size);
+			setting.IconScale = EnumUtility.GetNormalization(setting.IconScale, IconScale.Normal);
 
 			if(SettingUtility.IsIllegalPlusNumber(setting.FloatToolbar.WidthButtonCount)) {
 				setting.FloatToolbar.WidthButtonCount = 1;
@@ -26,18 +31,6 @@
 			if(SettingUtility.IsIllegalPlusNumber(setting.FloatToolbar.HeightButtonCount)) {
 				setting.FloatToolbar.HeightButtonCount = 1;
 			}
-		}
-
-		static void V_First(ToolbarItemModel setting, Version previousVersion, INonProcess nonProcess)
-		{
-			if(previousVersion != null) {
-				return;
-			}
-
-			setting.IconScale = IconScale.Normal;
-			setting.HideWaitTime = Constants.toolbarHideWaitTime.median;
-			setting.HideAnimateTime = Constants.toolbarHideAnimateTime.median;
-			setting.Font.Size = Constants.toolbarFontSize.median;
 		}
 	}
 }
