8500ab1603c3d81f561a4a8eeeff57d2d92275ce
sk <content.type.text.net@gmail.com>
2015-08-27 00:19:52 +0900

    Attribute

--- Pe-WPF/Pe/Library/SharedLibrary/Attribute/EntityMappingAttribute.cs
+++ Pe-WPF/Pe/Library/SharedLibrary/Attribute/EntityMappingAttribute.cs
@@ -18,22 +18,32 @@
 	)]
 	public sealed class EntityMappingAttribute: System.Attribute
 	{
+		/// <summary>
+		/// 物理名及び主キーを指定。
+		/// </summary>
+		/// <param name="physicalName"></param>
+		/// <param name="primaryKey"></param>
 		public EntityMappingAttribute(string physicalName, bool primaryKey)
 		{
 			PhysicalName = physicalName;
 			PrimaryKey = primaryKey;
 		}
 
+		/// <summary>
+		/// 物理名を指定。
+		/// <para>主キーはfalseとなる。</para>
+		/// </summary>
+		/// <param name="physicalName"></param>
 		public EntityMappingAttribute(string physicalName)
 			: this(physicalName, false)
 		{ }
 
 		/// <summary>
-		/// 物理名
+		/// 物理名。
 		/// </summary>
 		public string PhysicalName { get; private set; }
 		/// <summary>
-		/// 主キー
+		/// 主キー。
 		/// </summary>
 		public bool PrimaryKey { get; private set; }
 	}
--- Pe-WPF/Pe/Library/SharedLibrary/Attribute/PixelKindAttribute.cs
+++ Pe-WPF/Pe/Library/SharedLibrary/Attribute/PixelKindAttribute.cs
@@ -7,9 +7,16 @@
 	using System.Threading.Tasks;
 	using ContentTypeTextNet.Library.SharedLibrary.Define;
 
+	/// <summary>
+	/// ピクセル情報を指定。
+	/// </summary>
 	[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.ReturnValue, AllowMultiple=false)]
 	public class PixelKindAttribute : System.Attribute
 	{
+		/// <summary>
+		/// ピクセル情報を指定。
+		/// </summary>
+		/// <param name="px"></param>
 		public PixelKindAttribute(Px px)
 		{
 			Px = px;
@@ -17,6 +24,9 @@
 
 		#region property
 
+		/// <summary>
+		/// ピクセル情報。
+		/// </summary>
 		public Px Px { get; private set; }
 
 		#endregion
