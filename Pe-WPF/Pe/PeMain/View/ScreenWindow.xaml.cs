f6d18ae010cb9960142786799655f4d444350f94
sk <content.type.text.net@gmail.com>
2015-08-28 18:39:59 +0900

    ScreenWindow

---
+++ Pe-WPF/Pe/PeMain/View/ScreenWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿namespace ContentTypeTextNet.Pe.PeMain.View
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+	using System.Windows;
+	using System.Windows.Controls;
+	using System.Windows.Data;
+	using System.Windows.Documents;
+	using System.Windows.Input;
+	using System.Windows.Media;
+	using System.Windows.Media.Imaging;
+	using System.Windows.Shapes;
+	using ContentTypeTextNet.Library.SharedLibrary.IF;
+	using ContentTypeTextNet.Pe.PeMain.Data.Temporary;
+	using ContentTypeTextNet.Pe.PeMain.View.Parts.Window;
+	using ContentTypeTextNet.Pe.PeMain.ViewModel;
+
+	/// <summary>
+	/// ScreenWindow.xaml の相互作用ロジック
+	/// </summary>
+	public partial class ScreenWindow : ViewModelCommonDataWindow<ScreenViewModel>
+	{
+		public ScreenWindow()
+		{
+			InitializeComponent();
+		}
+
+		#region ViewModelCommonDataWindow
+
+		protected override void CreateViewModel()
+		{
+			var data = (ItemWithScreen<IModel>)ExtensionData;
+
+			ViewModel = new ScreenViewModel(this, data.Screen, CommonData.NonProcess);
+		}
+
+		protected override void ApplyViewModel()
+		{
+			base.ApplyViewModel();
+
+			DataContext = ViewModel;
+		}
+
+		#endregion
+	}
+}
---
+++ Pe-WPF/Pe/PeMain/ViewModel/ScreenViewModel.cs
@@ -0,0 +1,37 @@
+﻿namespace ContentTypeTextNet.Pe.PeMain.ViewModel
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+	using ContentTypeTextNet.Library.SharedLibrary.Model;
+	using ContentTypeTextNet.Library.SharedLibrary.ViewModel;
+	using ContentTypeTextNet.Pe.PeMain.IF;
+	using ContentTypeTextNet.Pe.PeMain.View;
+
+	public class ScreenViewModel : HavingViewModelBase<ScreenWindow>, IHavingAppNonProcess
+	{
+		#region variable
+		#endregion
+
+		public ScreenViewModel(ScreenWindow view, ScreenModel screen, IAppNonProcess appNonProcess)
+			: base(view)
+		{
+			Screen = screen;
+			AppNonProcess = appNonProcess;
+		}
+
+		#region property
+
+		ScreenModel Screen { get; set; }
+
+		#endregion
+
+		#region IHavingAppNonProcess
+
+		public IAppNonProcess AppNonProcess { get; private set; }
+
+		#endregion
+	}
+}
