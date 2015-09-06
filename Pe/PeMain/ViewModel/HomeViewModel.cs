5164ddd30c1a74e0156d240005bedd2d47a78840
sk <content.type.text.net@gmail.com>
2015-09-06 14:45:48 +0900

    これめんどいなぁ

--- Pe/PeMain/ViewModel/HomeViewModel.cs
+++ Pe/PeMain/ViewModel/HomeViewModel.cs
@@ -1,13 +1,14 @@
 ﻿namespace ContentTypeTextNet.Pe.PeMain.ViewModel
 {
 	using System;
-	using System.Collections.Generic;
-	using System.Linq;
-	using System.Text;
-	using System.Threading.Tasks;
-	using ContentTypeTextNet.Library.SharedLibrary.ViewModel;
-	using ContentTypeTextNet.Pe.PeMain.View;
-	using ContentTypeTextNet.Pe.PeMain.View.Parts;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using ContentTypeTextNet.Library.SharedLibrary.ViewModel;
+using ContentTypeTextNet.Pe.PeMain.View;
+using ContentTypeTextNet.Pe.PeMain.View.Parts;
 
 	public class HomeViewModel: HavingViewModelBase<HomeWindow>
 	{
@@ -17,6 +18,68 @@
 		}
 
 		#region property
+		#endregion
+
+		#region command
+
+		public ICommand CloseCommand
+		{
+			get
+			{
+				var result = CreateCommand(
+					o => {
+						if(HasView) {
+							View.Close();
+						}
+					}
+				);
+
+				return result;
+			}
+		}
+
+		public ICommand ShowNotifyAreaCommand
+		{
+			get
+			{
+				var result = CreateCommand(
+					o => {
+
+					}
+				);
+
+				return result;
+			}
+		}
+
+		public ICommand ResistStartupCommand
+		{
+			get
+			{
+				var result = CreateCommand(
+					o => {
+
+					}
+				);
+
+				return result;
+			}
+		}
+
+		public ICommand ResistItemsCommand
+		{
+			get
+			{
+				var result = CreateCommand(
+					o => {
+
+					}
+				);
+
+				return result;
+			}
+		}
+
 		#endregion
 	}
 }
