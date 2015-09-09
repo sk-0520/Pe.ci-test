f37fb35c7e57a7fc6459fc1522391825efff9c1e
sk <content.type.text.net@gmail.com>
2015-09-09 14:22:03 +0900

    comment

--- Pe/Library/PeData/Define/ComponentKind.cs
+++ Pe/Library/PeData/Define/ComponentKind.cs
@@ -6,10 +6,22 @@
 	using System.Text;
 	using System.Threading.Tasks;
 
+	/// <summary>
+	/// コンポーネント種別。
+	/// </summary>
 	public enum ComponentKind
 	{
+		/// <summary>
+		/// ライブラリ。
+		/// </summary>
 		Library,
+		/// <summary>
+		/// 画像関係。
+		/// </summary>
 		Image,
+		/// <summary>
+		/// ソース。
+		/// </summary>
 		Source,
 	}
 }
