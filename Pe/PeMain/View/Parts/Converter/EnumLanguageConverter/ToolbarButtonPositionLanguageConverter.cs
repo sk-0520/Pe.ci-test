76517d3b8e45dc565a047b8fba9e5405e7d2c577
sk <content.type.text.net@gmail.com>
2015-09-15 22:16:22 +0900

    ボタン開始位置の設定項目追加

---
+++ Pe/PeMain/View/Parts/Converter/EnumLanguageConverter/ToolbarButtonPositionLanguageConverter.cs
@@ -0,0 +1,12 @@
+﻿namespace ContentTypeTextNet.Pe.PeMain.View.Parts.Converter.EnumLanguageConverter
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+	using ContentTypeTextNet.Pe.Library.PeData.Define;
+
+	public class ToolbarButtonPositionLanguageConverter: EnumLanguageConverterBase<ToolbarButtonPosition>
+	{ }
+}
