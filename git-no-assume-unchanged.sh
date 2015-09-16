f2d1f29e9ef4c5aad140b8cd1c0b9d23d97f3954
sk <content.type.text.net@gmail.com>
2015-09-16 22:39:14 +0900

    msg

--- git-no-assume-unchanged.sh
+++ git-no-assume-unchanged.sh
@@ -1,10 +1,12 @@
-
 git ls-files -v | grep '^[[:lower:]]'
-for FILEPATH in `git ls-files -v | grep '^[[:lower:]]' | cut -d ' ' -f 2` ; do
-	echo $FILEPATH
-	git update-index --no-assume-unchanged ${FILEPATH}
-done
-
-echo "pause..."
-read Wait
+echo "exec? (y/n)"
+read isExec
+if [ $isExec = 'y' ] ; then
+	for FILEPATH in `git ls-files -v | grep '^[[:lower:]]' | cut -d ' ' -f 2` ; do
+		echo $FILEPATH
+		#git update-index --no-assume-unchanged ${FILEPATH}
+	done
+	echo "pause..."
+	read _
+fi
 
