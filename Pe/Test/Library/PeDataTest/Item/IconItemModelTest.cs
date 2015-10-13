17c490d958aaa494bd3c7f685dc5f908d1501fca
sk <content.type.text.net@gmail.com>
2015-10-13 20:08:06 +0900

    PeDataTest

--- Pe/Test/Library/PeDataTest/Item/IconItemModelTest.cs
+++ Pe/Test/Library/PeDataTest/Item/IconItemModelTest.cs
@@ -1,34 +1,50 @@
-﻿namespace PeDataTest.Item
+﻿/**
+This file is part of Pe.
+
+Pe is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Pe is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Pe.  If not, see <http://www.gnu.org/licenses/>.
+*/
+namespace PeDataTest.Item
 {
-	using System;
-	using System.Collections.Generic;
-	using System.Linq;
-	using System.Text;
-	using System.Threading.Tasks;
-	using ContentTypeTextNet.Pe.Library.PeData.Item;
-	using NUnit.Framework;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using ContentTypeTextNet.Pe.Library.PeData.Item;
+    using NUnit.Framework;
 
-	[TestFixture]
-	class IconItemModelTest
-	{
-		[Test]
-		public void DeepCloneTest()
-		{
-			var src = new IconItemModel() {
-				Path = "path",
-				Index = 1,
-			};
+    [TestFixture]
+    class IconItemModelTest
+    {
+        [Test]
+        public void DeepCloneTest()
+        {
+            var src = new IconItemModel() {
+                Path = "path",
+                Index = 1,
+            };
 
-			var cp = (IconItemModel)src.DeepClone();
+            var cp = (IconItemModel)src.DeepClone();
 
-			Assert.AreEqual(cp.Path, src.Path);
-			Assert.AreEqual(cp.Index, src.Index);
+            Assert.AreEqual(cp.Path, src.Path);
+            Assert.AreEqual(cp.Index, src.Index);
 
-			src.Path = "test";
-			src.Index = 2;
+            src.Path = "test";
+            src.Index = 2;
 
-			Assert.AreNotEqual(cp.Path, src.Path);
-			Assert.AreNotEqual(cp.Index, src.Index);
-		}
-	}
+            Assert.AreNotEqual(cp.Path, src.Path);
+            Assert.AreNotEqual(cp.Index, src.Index);
+        }
+    }
 }
