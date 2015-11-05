2497a68a8d230e75b0f18022ff5dab239b6bfe96
sk <content.type.text.net@gmail.com>
2015-11-06 00:19:25 +0900

    構造体の一応コピー

---
+++ Pe/Library/SharedLibrary/Logic/Utility/DeepCloneUtility.cs
@@ -0,0 +1,63 @@
+﻿/**
+This file is part of SharedLibrary.
+
+SharedLibrary is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+SharedLibrary is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SharedLibrary.  If not, see <http://www.gnu.org/licenses/>.
+*/
+namespace ContentTypeTextNet.Library.SharedLibrary.Logic.Utility
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Attribute;
+    using IF;
+
+    public static class DeepCloneUtility
+    {
+        public static IEnumerable<MemberInfo> GetMembers(IDeepClone deepCone)
+        {
+            return deepCone.GetType().GetMembers(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public)
+                .Where(p => p.GetCustomAttributes(typeof(IsDeepCloneAttribute)).Any())
+            ;
+        }
+
+        public static void DeepCopy(IDeepClone dst, IDeepClone src)
+        {
+            if(dst.GetType() != src.GetType()) {
+                throw new ArgumentException(string.Format("dst[{0}] != src[{1}]", dst.GetType(), src.GetType()));
+            }
+
+            var properties = GetMembers(src);
+            
+            foreach(var property in properties.OfType<FieldInfo>()) {
+                var srcValue = property.GetValue(src);
+                var srcClone = srcValue as IDeepClone;
+                if(srcClone != null) {
+                    var dstClone = (IDeepClone)property.GetValue(dst);
+                    srcClone.DeepCloneTo(dstClone);
+                    if(dstClone.GetType().IsValueType) {
+                        // 構造体は値の再設定が必要(多分)
+                        property.SetValue(dst, dstClone);
+                    }
+                } else {
+                    property.SetValue(dst, srcValue);
+                }
+            }
+        }
+
+    }
+}
---
+++ Pe/Test/Library/SharedLibraryTest/Logic/Utility/DeepCloneUtilityTest.cs
@@ -0,0 +1,65 @@
+﻿/**
+This file is part of SharedLibrary.
+
+SharedLibrary is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+SharedLibrary is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SharedLibrary.  If not, see <http://www.gnu.org/licenses/>.
+*/
+namespace ContentTypeTextNet.Test.Library.SharedLibraryTest.Logic.Utility
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using ContentTypeTextNet.Library.SharedLibrary.Attribute;
+    using ContentTypeTextNet.Library.SharedLibrary.IF;
+    using ContentTypeTextNet.Library.SharedLibrary.Logic.Utility;
+    using NUnit.Framework;
+
+    [TestFixture]
+    class DeepCloneUtilityTest
+    {
+        struct PlainStruct:IDeepClone
+        {
+#pragma warning disable 169
+            [IsDeepClone]
+            int private_field_target;
+            [IsDeepClone]
+            public int public_field_target;
+            int private_field_untarget;
+            public int public_field_untarget;
+#pragma warning restore 169
+
+            public void DeepCloneTo(IDeepClone target)
+            {
+                DeepCloneUtility.DeepCopy(target, this);
+            }
+
+            public IDeepClone DeepClone()
+            {
+                var result = new PlainStruct();
+                DeepCloneTo(result);
+                return result;
+            }
+        }
+        [Test]
+        public void DeepCopy_PlainStruct_Test()
+        {
+            var s = new PlainStruct();
+            s.public_field_target = 10;
+            s.public_field_untarget = 20;
+            var d = s.DeepClone();
+            Assert.AreEqual(s, d);
+        }
+    }
+}
