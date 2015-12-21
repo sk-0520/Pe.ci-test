91855c724e6cc99af39debc5460c76c6f5811b3d
sk <content.type.text.net@gmail.com>
2015-12-21 20:02:32 +0900

    ?

--- git-no-assume-unchanged.sh
+++ git-no-assume-unchanged.sh
@@ -4,7 +4,7 @@ read isExec
 if [ $isExec = 'y' ] ; then
 	for FILEPATH in `git ls-files -v | grep '^[[:lower:]]' | cut -d ' ' -f 2` ; do
 		echo $FILEPATH
-		#git update-index --no-assume-unchanged ${FILEPATH}
+		git update-index --no-assume-unchanged ${FILEPATH}
 	done
 	echo "pause..."
 	read _
