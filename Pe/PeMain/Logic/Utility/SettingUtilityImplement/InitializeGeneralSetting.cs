8ca968236494282d2822739cb17acefdbfc1e928
sk <content.type.text.net@gmail.com>
2015-12-21 22:16:29 +0900

    通知方法をデータ化

---
+++ Pe/PeMain/Logic/Utility/SettingUtilityImplement/InitializeGeneralSetting.cs
@@ -0,0 +1,50 @@
+﻿/**
+This file is part of Pe.
+
+Pe is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Pe is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Pe.  If not, see <http://www.gnu.org/licenses/>.
+*/
+namespace ContentTypeTextNet.Pe.PeMain.Logic.Utility.SettingUtilityImplement
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using ContentTypeTextNet.Library.SharedLibrary.IF;
+    using Library.PeData.Item;
+    using ContentTypeTextNet.Pe.Library.PeData.Setting.MainSettings;
+    using ContentTypeTextNet.Pe.Library.PeData.Define;
+
+    internal static class InitializeGeneralSetting
+    {
+        public static void Correction(GeneralSettingModel setting, Version previousVersion, INonProcess nonProcess)
+        {
+            V_First(setting, previousVersion, nonProcess);
+            V_Last(setting, previousVersion, nonProcess);
+        }
+
+        static void V_Last(GeneralSettingModel setting, Version previousVersion, INonProcess nonProcess)
+        {
+        }
+
+        static void V_First(GeneralSettingModel setting, Version previousVersion, INonProcess nonProcess)
+        {
+            if(previousVersion != null) {
+                return;
+            }
+
+            setting.Notification = Notification.System;
+        }
+    }
+}
