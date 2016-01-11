c0dd2ecb950646167fcc713849e51d3e52974003
sk <content.type.text.net@gmail.com>
2016-01-11 17:10:28 +0900

    読込と圧縮を分離

--- Pe/PeMain/Logic/Utility/ArchiveUtility.cs
+++ Pe/PeMain/Logic/Utility/ArchiveUtility.cs
@@ -18,6 +18,7 @@ namespace ContentTypeTextNet.Pe.PeMain.Logic.Utility
 {
     using System;
     using System.Collections.Generic;
+    using System.Diagnostics;
     using System.IO;
     using System.IO.Compression;
     using System.Linq;
@@ -27,39 +28,73 @@ namespace ContentTypeTextNet.Pe.PeMain.Logic.Utility
 
     internal static class ArchiveUtility
     {
-        static ZipArchiveEntry WriteArchive(ZipArchive archive, string path, string baseDirPath)
+        struct FilePathAndBuffer
         {
-            var entryPath = path.Substring(baseDirPath.Length);
+            public FilePathAndBuffer(string path, byte[] buffer)
+            {
+                Path = path;
+                Buffer = buffer;
+            }
+
+            public string Path { get; private set; }
+            public byte[] Buffer { get; private set; }
+        }
+
+        static FilePathAndBuffer GetFilePathAndBuffer(string path)
+        {
+            var buffer = FileUtility.ToBinary(path);
+            return new FilePathAndBuffer(path, buffer);
+        }
+
+        static ZipArchiveEntry WriteArchive(ZipArchive archive, string entryPath, byte[] buffer, CompressionLevel compressionLevel)
+        {
+            var entry = archive.CreateEntry(entryPath, compressionLevel);
+            using(var entryStream = new BinaryWriter(entry.Open())) {
+                entryStream.Write(buffer);
+            }
+            return entry;
+        }
+
+        static ZipArchiveEntry WriteArchive(ZipArchive archive, string path, string baseDirectoryPath, CompressionLevel compressionLevel)
+        {
+            var entryPath = GetArchiveEntryPath(path, baseDirectoryPath);
             while(entryPath.First() == Path.DirectorySeparatorChar) {
                 entryPath = entryPath.Substring(1);
             }
 
-            var entry = archive.CreateEntry(entryPath);
+            var buffer = FileUtility.ToBinary(path);
+            return WriteArchive(archive, entryPath, buffer, compressionLevel);
+        }
 
-            using(var entryStream = new BinaryWriter(entry.Open())) {
-                var buffer = FileUtility.ToBinary(path);
-                entryStream.Write(buffer);
+        static string GetArchiveEntryPath(string path, string baseDirectoryPath)
+        {
+            var entryPath = path.Substring(baseDirectoryPath.Length);
+            while(entryPath.First() == Path.DirectorySeparatorChar) {
+                entryPath = entryPath.Substring(1);
             }
 
-            return entry;
+            return entryPath;
         }
 
         /// <summary>
         /// 指定パスにZIP形式でアーカイブを作成。
         /// </summary>
         /// <param name="saveFilePath">保存先パス。</param>
-        /// <param name="basePath">基準とするディレクトリパス。</param>
+        /// <param name="baseDirectoryPath">基準とするディレクトリパス。</param>
         /// <param name="targetFiles">取り込み対象パス。ディレクトリを指定した場合は、以下のファイル・ディレクトリをすべてその対象とする</param>
-        public static void CreateZipFile(string saveFilePath, string basePath, IEnumerable<string> targetFiles)
+        public static void CreateZipFile(string saveFilePath, string baseDirectoryPath, IEnumerable<string> targetFiles)
         {
             using(var zip = new ZipArchive(new FileStream(saveFilePath, FileMode.Create), ZipArchiveMode.Create)) {
                 foreach(var filePath in targetFiles) {
                     if(File.Exists(filePath)) {
-                        WriteArchive(zip, filePath, basePath);
+                        WriteArchive(zip, filePath, baseDirectoryPath, CompressionLevel.Optimal);
                     } else if(Directory.Exists(filePath)) {
-                        var list = Directory.EnumerateFiles(filePath, "*", SearchOption.AllDirectories);
-                        foreach(var f in list) {
-                            WriteArchive(zip, f, basePath);
+                        var list = Directory.EnumerateFiles(filePath, "*", SearchOption.AllDirectories)
+                            .AsParallel()
+                            .Select(f => new FilePathAndBuffer(GetArchiveEntryPath(f, baseDirectoryPath), FileUtility.ToBinary(f)))
+                        ;
+                        foreach(var pb in list) {
+                            WriteArchive(zip, pb.Path, pb.Buffer, CompressionLevel.NoCompression);
                         }
                     }
                 }
