8a424e858cbf69c147ce65ab01fd3817b3fd560f
sk <content.type.text.net@gmail.com>
2016-03-28 16:58:08 +0900

    ファイルだけ移植(整合性は無視)

---
+++ Pe/PeMain/IF/IRuntimeTextTemplate.cs
@@ -0,0 +1,39 @@
+﻿/*
+This file is part of SharedLibrary.
+
+SharedLibrary is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+SharedLibrary is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with SharedLibrary.  If not, see <http://www.gnu.org/licenses/>.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TextTemplating;
+
+namespace ContentTypeTextNet.Library.SharedLibrary.IF
+{
+    /// <summary>
+    /// 実行時テンプレートのインターフェイス
+    /// </summary>
+    public interface IRuntimeTextTemplate: IDisposable
+    {
+        ITextTemplatingEngineHost Host { get; set; }
+
+        /// <summary>
+        /// テンプレート変換を実施する.
+        /// </summary>
+        /// <returns></returns>
+        string TransformText();
+    }
+}
