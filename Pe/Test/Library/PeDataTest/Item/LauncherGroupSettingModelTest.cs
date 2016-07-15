b9041f0e698d1820261d4c9f17504a73778f2978
sk <content.type.text.net@gmail.com>
2016-07-15 21:38:37 +0900

    LauncherGroupSettingModel

---
+++ Pe/Test/Library/PeDataTest/Item/LauncherGroupSettingModelTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ContentTypeTextNet.Library.SharedLibrary.Logic.Utility;
+using ContentTypeTextNet.Pe.Library.PeData.Setting;
+using NUnit.Framework;
+
+namespace ContentTypeTextNet.Test.Library.PeDataTest.Item
+{
+    [TestFixture]
+    class LauncherGroupSettingModelTest
+    {
+        [Test]
+        public void DeepCloneTest()
+        {
+            var src = new LauncherGroupSettingModel();
+            src.Groups.Add(new Pe.Library.PeData.Item.LauncherGroupItemModel() { Name = "name", });
+
+            var dst = (LauncherGroupSettingModel)src.DeepClone();
+
+            Assert.IsTrue(src.Groups != dst.Groups);
+            for(var i = 0; i < src.Groups.Count; i++) {
+                var s = src.Groups[i];
+                var d = dst.Groups[i];
+                Assert.True(s.GroupIconColor == d.GroupIconColor);
+            }
+        }
+    }
+}
