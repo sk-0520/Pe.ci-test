275cb65116050f655761e7fbcc903e144846858e
sk <content.type.text.net@gmail.com>
2016-12-31 15:59:34 +0900

    まずは透明になっちゃう問題対応

--- Source/PeMain/View/Parts/ViewExtend/ThemeStyle.cs
+++ Source/PeMain/View/Parts/ViewExtend/ThemeStyle.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,12 +22,14 @@ namespace ContentTypeTextNet.Pe.PeMain.View.Parts.ViewExtend
         const string personalizeKey = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize";
         const string personalizeColorPrevalenceName = @"ColorPrevalence";
         const string personalizeSpecialColorName = @"SpecialColor";
+        const string personalizeEnableTransparencyName = @"EnableTransparency";
 
         const string dwmKey = @"SOFTWARE\Microsoft\Windows\DWM";
-        const string dwmAccentColor = "ColorizationColor";
+        const string dwmColorizationColor = "ColorizationColor";
+        const string dwmAccentColor = "AccentColor";
 
         const string accentKey = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Accent";
-        const string accentColorMenuName = @"StartColorMenu";
+        const string accentStartColorMenuName = @"StartColorMenu";
 
         #endregion
 
@@ -45,53 +48,63 @@ namespace ContentTypeTextNet.Pe.PeMain.View.Parts.ViewExtend
 
         #region function
 
-        //Color ToColor(uint rawColor)
-        //{
-        //    var a = (byte)((rawColor & 0xff000000) >> 24);
-        //    var r = (byte)((rawColor & 0x00ff0000) >> 16);
-        //    var g = (byte)((rawColor & 0x0000ff00) >> 8);
-        //    var b = (byte)((rawColor & 0x000000ff) >> 0);
+        Color ConvertFromABGR(uint rawColor)
+        {
+            var a = (byte)((rawColor & 0xff000000) >> 24);
+            var b = (byte)((rawColor & 0x00ff0000) >> 16);
+            var g = (byte)((rawColor & 0x0000ff00) >> 8);
+            var r = (byte)((rawColor & 0x000000ff) >> 0);
 
-        //    return Color.FromArgb(a, r, g, b);
-        //}
+            return Color.FromArgb(a, r, g, b);
+        }
+
+        Color GetColor(RegistryKey key, string name, Func<uint, Color> converter)
+        {
+            var rawValue = key.GetValue(name);
+            if(rawValue != null) {
+                var rawColor = (uint)(int)rawValue;
+                var color= converter(rawColor);
+                NonProcess.Logger.Trace($"{name}: {color}");
+                return color;
+            }
+
+            return Colors.Transparent;
+        }
+
+        private void SetColor(Color color)
+        {
+            RestrictionViewModel.VisualAlphaColor = color;
+            RestrictionViewModel.VisualPlainColor = Color.FromRgb(color.R, color.G, color.B);
+        }
 
         bool SetWindowColorWindows10()
         {
-            using(var dwm = Registry.CurrentUser.OpenSubKey(dwmKey))
             using(var personalize = Registry.CurrentUser.OpenSubKey(personalizeKey))
-            using(var accent = Registry.CurrentUser.OpenSubKey(accentKey)) {
-                var rawColorPrevalence = personalize.GetValue(personalizeColorPrevalenceName);
-                if(rawColorPrevalence == null) {
+            //using(var accent = Registry.CurrentUser.OpenSubKey(accentKey))
+            using(var dwm = Registry.CurrentUser.OpenSubKey(dwmKey)) {
+
+                var rawPersonalizeColorPrevalence = personalize.GetValue(personalizeColorPrevalenceName);
+                if(rawPersonalizeColorPrevalence == null) {
                     return false;
                 }
-                var colorPrevalence = (uint)(int)rawColorPrevalence;
-                if(colorPrevalence == 1) {
-                    // タスクバーに色を付ける
-                    var rawSpecialColor = personalize.GetValue(personalizeSpecialColorName);
-                    if(rawSpecialColor != null) {
-                    var specialColor = (uint)(int)rawSpecialColor;
-                        RestrictionViewModel.VisualAlphaColor = MediaUtility.ConvertColorFromRawColor((uint)specialColor);
-                        RestrictionViewModel.VisualPlainColor = MediaUtility.ConvertColorFromRawColor((uint)(specialColor & 0x00ffffff));
+                var personalizeColorPrevalence = (uint)(int)rawPersonalizeColorPrevalence;
+                if(personalizeColorPrevalence == 1 || true) { // 強制遷移
+                    //// タスクバーに色を付ける
+                    //var specialColor = GetColor(personalize, personalizeSpecialColorName, ConvertFromABGR);
+
+                    // DWMアクセントカラー
+                    var accentColor = GetColor(dwm, dwmAccentColor, ConvertFromABGR);
+                    var colorizationColor = GetColor(dwm, dwmColorizationColor, MediaUtility.ConvertColorFromRawColor);
+
+                    if(colorizationColor.A == 0) {
+                        SetColor(accentColor);
                         return true;
                     }
 
-                    var rawDwmAccentColor = dwm.GetValue(dwmAccentColor);
-                    if(rawDwmAccentColor != null) {
-                        var dwmAccentColorMenu = (uint)(int)rawDwmAccentColor;
-                        RestrictionViewModel.VisualAlphaColor = MediaUtility.ConvertColorFromRawColor((uint)dwmAccentColorMenu);
-                        RestrictionViewModel.VisualPlainColor = MediaUtility.ConvertColorFromRawColor((uint)(dwmAccentColorMenu & 0x00ffffff));
-                        return true;
-                    }
+                    //// エクスプローラアクセントカラー
+                    //var accentStartColorMenu = GetColor(accent, accentStartColorMenuName, MediaUtility.ConvertColorFromRawColor);
 
-                    // エクスプローラアクセントカラー
-                    var rawAccentColorMenu = accent.GetValue(accentColorMenuName);
-                    if(rawAccentColorMenu != null) {
-                        var accentColorMenu = (uint)(int)rawAccentColorMenu;
-                        RestrictionViewModel.VisualAlphaColor = MediaUtility.ConvertColorFromRawColor((uint)accentColorMenu);
-                        RestrictionViewModel.VisualPlainColor = MediaUtility.ConvertColorFromRawColor((uint)(accentColorMenu & 0x00ffffff));
-                        return true;
-                    }
-
+                    SetColor(colorizationColor);
 
                     return true;
                 } else {
@@ -102,6 +115,11 @@ namespace ContentTypeTextNet.Pe.PeMain.View.Parts.ViewExtend
             return false;
         }
 
+        void SetBrush()
+        {
+            View.Background = new SolidColorBrush(RestrictionViewModel.VisualAlphaColor);
+            //View.Background = SystemParameters.WindowGlassBrush;
+        }
 
         #endregion
 
@@ -111,8 +129,7 @@ namespace ContentTypeTextNet.Pe.PeMain.View.Parts.ViewExtend
         {
             if(IsWindows10) {
                 if(SetWindowColorWindows10()) {
-                    //View.Background = new SolidColorBrush(RestrictionViewModel.VisualAlphaColor);
-                    View.Background = SystemParameters.WindowGlassBrush;
+                    SetBrush();
                     return;
                 }
             }
@@ -123,10 +140,21 @@ namespace ContentTypeTextNet.Pe.PeMain.View.Parts.ViewExtend
         public override IntPtr WndProc(IntPtr hWnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
         {
             if(RestrictionViewModel.UsingVisualStyle) {
-                if(msg == (int)WM.WM_SETTINGCHANGE) {
-                    SetWindowColor();
+                switch(msg) {
+                    case (int)WM.WM_DWMCOLORIZATIONCOLORCHANGED:
+                        if(IsWindows10) {
+                            var color = MediaUtility.ConvertColorFromRawColor((uint)wParam);
+                            SetColor(color);
+                            SetBrush();
+                        } else {
+                            base.SetWindowColor();
+                        }
+                        break;
+                    case (int)WM.WM_SETTINGCHANGE:
+                    case (int)WM.WM_DWMCOMPOSITIONCHANGED:
+                        SetWindowColor();
+                        break;
                 }
-                
             }
 
             return base.WndProc(hWnd, msg, wParam, lParam, ref handled);
