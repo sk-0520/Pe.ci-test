2e954a805b6c812d60789de535adf8a9ffcc3f48
sk <content.type.text.net@gmail.com>
2017-12-19 22:49:29 +0900

    もっかいやり直すのは心がしんどいなぁ

--- Source2/PeMain/App.xaml.cs
+++ Source2/PeMain/App.xaml.cs
@@ -6,12 +6,26 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace PeMain
+namespace ContentTypeTextNet.Pe.PeMain
 {
     /// <summary>
     /// App.xaml の相互作用ロジック
     /// </summary>
     public partial class App : Application
     {
+        #region property
+
+
+
+        #endregion
+
+        #region Application
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+        }
+
+        #endregion
     }
 }
