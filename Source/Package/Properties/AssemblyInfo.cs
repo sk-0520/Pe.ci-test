c30ab84582ffb429a8b6543880c819e8caf01c42
sk <content.type.text.net@gmail.com>
2018-11-23 00:04:00 +0900

    sqlite+dapper

--- Source/Package/Properties/AssemblyInfo.cs
+++ Source/Package/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("PePackage")]
+[assembly: AssemblyTitle("Package")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PePackage")]
+[assembly: AssemblyProduct("Package")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
