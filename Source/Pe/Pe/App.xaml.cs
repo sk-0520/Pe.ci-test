211488512a876a64703fa867965b0beb35352d18
sk <content.type.text.net@gmail.com>
2018-11-28 19:30:17 +0900

    FormsとWPFの共存諦め
    だるい

---
+++ Source/Pe/Pe/App.xaml.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ContentTypeTextNet.Pe.Main
+{
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// </summary>
+    public partial class App : Application
+    {
+        #region Application
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+        }
+
+        #endregion
+    }
+}
