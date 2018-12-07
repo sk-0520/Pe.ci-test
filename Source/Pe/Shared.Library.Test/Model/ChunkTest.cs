80a874cdb6fbf1b3070d8a872a580166463e15e0
sk <content.type.text.net@gmail.com>
2018-12-08 03:34:10 +0900

    ChunkedList

--- Source/Pe/Shared.Library.Test/Model/ChunkTest.cs
+++ Source/Pe/Shared.Library.Test/Model/ChunkTest.cs
@@ -114,36 +114,169 @@ namespace Shared.Library.Test.Model
         [TestMethod]
         public void AddTest()
         {
-            var item = new ChunkedList<int>(2, 3);
-            item.Add(10);
-            Assert.AreEqual(1, item.Count);
+            var list = new ChunkedList<int>(2, 3);
+            list.Add(10);
+            Assert.AreEqual(1, list.Count);
 
-            item.Add(20);
-            item.Add(30);
+            list.Add(20);
+            list.Add(30);
 
-            item.Add(40);
-            item.Add(50);
-            item.Add(60);
-            Assert.ThrowsException<OutOfMemoryException>(() => item.Add(70));
+            list.Add(40);
+            list.Add(50);
+            list.Add(60);
+            Assert.ThrowsException<OutOfMemoryException>(() => list.Add(70));
         }
 
         [TestMethod]
         public void ClearTest()
         {
-            var item = new ChunkedList<int>(5, 1);
+            var list = new ChunkedList<int>(5, 1);
 
-            item.Add(10);
-            item.Add(20);
-            item.Add(30);
-            item.Add(40);
-            item.Add(50);
-            item.Clear();
-            Assert.AreEqual(0, item.Count);
+            list.Add(10);
+            list.Add(20);
+            list.Add(30);
+            list.Add(40);
+            list.Add(50);
+            list.Clear();
+            Assert.AreEqual(0, list.Count);
 
-            item.Add(10);
-            item.Add(20);
-            item.Add(30);
-            Assert.AreEqual(3, item.Count);
+            list.Add(10);
+            list.Add(20);
+            list.Add(30);
+            Assert.AreEqual(3, list.Count);
+        }
+
+        [TestMethod]
+        public void ContainsTest()
+        {
+            var list = new ChunkedList<int>(5, 1);
+
+            list.Add(10);
+            list.Add(20);
+            list.Add(30);
+            list.Add(40);
+            list.Add(50);
+
+            Assert.IsFalse(list.Contains(0));
+            Assert.IsTrue(list.Contains(10));
+            Assert.IsTrue(list.Contains(20));
+            Assert.IsTrue(list.Contains(30));
+            Assert.IsTrue(list.Contains(40));
+            Assert.IsTrue(list.Contains(50));
+            Assert.IsFalse(list.Contains(51));
+        }
+
+        [TestMethod]
+        public void CopyToTest()
+        {
+            var list = new ChunkedList<int>(5, 2);
+            foreach(var n in Enumerable.Range(0, 5*2)) {
+                list.Add(n);
+            }
+
+            var array1 = new int[list.Count];
+            list.CopyTo(array1, 0);
+            CollectionAssert.AreEqual(array1, Enumerable.Range(0, list.Count).ToArray());
+
+            var array2 = new int[list.Count];
+            list.CopyTo(array2, 5);
+            var a = array2.Take(5).ToArray();
+            var b = Enumerable.Range(0, list.Count).Skip(5).ToArray();
+            CollectionAssert.AreEqual(a,b);
+        }
+
+
+        [TestMethod]
+        [DataRow(3, 4, 3 * 4 - 0)]
+        [DataRow(3, 4, 3 * 4 - 1)]
+        [DataRow(3, 4, 3 * 4 - 2)]
+        [DataRow(3, 4, 3 * 4 - 3)]
+        [DataRow(3, 4, 3 * 4 - 4)]
+        [DataRow(3, 4, 3 * 4 - 5)]
+        [DataRow(3, 4, 3 * 4 - 11)]
+        [DataRow(1, 1, 1*1)]
+        [DataRow(100, 1, 100 * 1)]
+        [DataRow(1, 100, 1 * 100)]
+        [DataRow(100, 1, 50)]
+        [DataRow(1, 100, 50)]
+        public void GetEnumeratorTest(int capacity, int itemCapacity, int size)
+        {
+            var rnd = new Random();
+            var items = Enumerable.Repeat(0, size).Select(i => rnd.Next()).ToArray();
+            var list = new ChunkedList<int>(capacity, itemCapacity);
+            foreach(var item in items) {
+                list.Add(item);
+            }
+            Assert.IsTrue(list.SequenceEqual(items));
+
+        }
+
+        [TestMethod]
+        [DataRow(3, 4, 3 * 4 - 0)]
+        [DataRow(3, 4, 3 * 4 - 1)]
+        [DataRow(3, 4, 3 * 4 - 2)]
+        [DataRow(3, 4, 3 * 4 - 3)]
+        [DataRow(3, 4, 3 * 4 - 4)]
+        [DataRow(3, 4, 3 * 4 - 5)]
+        [DataRow(3, 4, 3 * 4 - 11)]
+        [DataRow(1, 1, 1 * 1)]
+        [DataRow(100, 1, 100 * 1)]
+        [DataRow(1, 100, 1 * 100)]
+        [DataRow(100, 1, 50)]
+        [DataRow(1, 100, 50)]
+        public void RemoveTest(int capacity, int itemCapacity, int size)
+        {
+            var items = Enumerable.Range(0, size).ToArray();
+            var list1 = new ChunkedList<int>(capacity, itemCapacity);
+            var list2 = new ChunkedList<int>(capacity, itemCapacity);
+            var list3 = new ChunkedList<int>(capacity, itemCapacity);
+            foreach(var item in items) {
+                list1.Add(item);
+                list2.Add(item);
+                list3.Add(item);
+            }
+
+            var count1 = list1.Count;
+            list1.Remove(items[0]);
+            Assert.AreEqual(count1, list1.Count + 1);
+            Assert.IsTrue(list1.SequenceEqual(items.Skip(1)));
+
+            var count2 = list2.Count;
+            list2.Remove(items.Last());
+            Assert.AreEqual(count2, list2.Count + 1);
+            Assert.IsTrue(list2.SequenceEqual(items.Take(items.Length - 1)));
+
+            var count3 = list3.Count;
+            var removeIndex = count3 / 2;
+            list3.Remove(items[removeIndex]);
+            Assert.AreEqual(count3, list3.Count + 1);
+            Assert.IsTrue(list3.SequenceEqual(items.Take(removeIndex).Concat(items.Skip(removeIndex + 1))));
+        }
+
+        [TestMethod]
+        [DataRow(3, 4, 3 * 4 - 0)]
+        [DataRow(3, 4, 3 * 4 - 1)]
+        [DataRow(3, 4, 3 * 4 - 2)]
+        [DataRow(3, 4, 3 * 4 - 3)]
+        [DataRow(3, 4, 3 * 4 - 4)]
+        [DataRow(3, 4, 3 * 4 - 5)]
+        [DataRow(3, 4, 3 * 4 - 11)]
+        [DataRow(1, 1, 1 * 1)]
+        [DataRow(100, 1, 100 * 1)]
+        [DataRow(1, 100, 1 * 100)]
+        [DataRow(100, 1, 50)]
+        [DataRow(1, 100, 50)]
+        public void IndexTest(int capacity, int itemCapacity, int size)
+        {
+            var items = Enumerable.Range(0, size).ToArray();
+            var list = new ChunkedList<int>(capacity, itemCapacity);
+            foreach(var item in items) {
+                list.Add(item);
+            }
+
+            for(var i = 0; i < items.Length; i++) {
+                Assert.AreEqual(items[i], list[i]);
+            }
         }
     }
 }
--- Source/Pe/Shared.Library/Model/Chunk.cs
+++ Source/Pe/Shared.Library/Model/Chunk.cs
@@ -23,6 +23,24 @@ namespace ContentTypeTextNet.Pe.Library.Shared.Library.Model
 
         #endregion
 
+        #region function
+
+        void CopyTo(int sourceIndex, Array destinationArray, int destinationIndex, int destinationLength)
+        {
+            Array.Copy(Items, sourceIndex, destinationArray, destinationIndex, destinationLength);
+        }
+
+        public void CopyTo(T[] destinationArray, int sourceIndex, int destinationIndex)
+        {
+            CopyTo(sourceIndex, destinationArray, destinationIndex, Count - sourceIndex);
+        }
+        public void CopyTo(Array destinationArray, int sourceIndex, int destinationIndex)
+        {
+            CopyTo(sourceIndex, destinationArray, destinationIndex, Count - sourceIndex);
+        }
+
+        #endregion
+
         #region IReadOnlyList
 
         public T this[int index]
@@ -76,13 +94,13 @@ namespace ContentTypeTextNet.Pe.Library.Shared.Library.Model
             return Array.IndexOf(Items, item, 0, Count) != -1;
         }
 
-        public void CopyTo(T[] array, int arrayIndex)
+        public void CopyTo(T[] destinationArray, int sourceIndex)
         {
-            Array.Copy(Items, 0, array, arrayIndex, Count);
+            CopyTo(sourceIndex, destinationArray, 0, Count - sourceIndex);
         }
-        public void CopyTo(Array array, int index)
+        public void CopyTo(Array destinationArray, int sourceIndex)
         {
-            Array.Copy(Items, 0, array, index, Count);
+            CopyTo(sourceIndex, destinationArray, 0, Count - sourceIndex);
         }
 
         public IEnumerator<T> GetEnumerator()
@@ -114,7 +132,7 @@ namespace ContentTypeTextNet.Pe.Library.Shared.Library.Model
 
     }
 
-    public class ChunkedList<T> : ICollection<T>, ICollection
+    public class ChunkedList<T> : ICollection<T>, ICollection, IReadOnlyList<T>
     {
         public ChunkedList(int capacity, int itemCapacity)
         {
@@ -131,9 +149,6 @@ namespace ContentTypeTextNet.Pe.Library.Shared.Library.Model
 
         public int Capacity { get; }
         public int ChunkItemCapacity { get; }
-
-        ChunkItem<T> CurrentChunkItem { get; set; }
-        int CurrentChunkItemIndex { get; set; }
 
         #endregion
 
@@ -155,6 +170,11 @@ namespace ContentTypeTextNet.Pe.Library.Shared.Library.Model
             return Count / ChunkItemCapacity;
         }
 
+        protected virtual ChunkItem<T> CreateChunkItem()
+        {
+            return new ChunkItem<T>(ChunkItemCapacity);
+        }
+
         ChunkItem<T> GetOrCreateChunkItem(int chunkItemIndex)
         {
             if(ChunkItemCount - 1 < chunkItemIndex) {
@@ -163,7 +183,7 @@ namespace ContentTypeTextNet.Pe.Library.Shared.Library.Model
                 }
 
                 if(ChunkItems[chunkItemIndex] == null) {
-                    var item = new ChunkItem<T>(ChunkItemCapacity);
+                    var item = CreateChunkItem();
                     ChunkItems[chunkItemIndex] = item;
                 }
                 ChunkItemCount += 1;
@@ -177,8 +197,36 @@ namespace ContentTypeTextNet.Pe.Library.Shared.Library.Model
             chunkItem.Add(item);
 
             Count += 1;
-            CurrentChunkItem = chunkItem;
-            CurrentChunkItemIndex = chunkItemIndex;
+        }
+
+        #endregion
+
+        #region IReadOnlyList
+
+        public T this[int index]
+        {
+            get
+            {
+                if(Count - 1 < index) {
+                    throw new IndexOutOfRangeException();
+                }
+
+                var chunkItemIndex = index / ChunkItemCapacity;
+                var workIndex = index - chunkItemIndex * ChunkItemCapacity;
+
+                return ChunkItems[chunkItemIndex][workIndex];
+            }
+            set
+            {
+                if(Count - 1 < index) {
+                    throw new IndexOutOfRangeException();
+                }
+
+                var chunkItemIndex = index / ChunkItemCapacity;
+                var workIndex = index - chunkItemIndex * ChunkItemCapacity;
+
+                ChunkItems[chunkItemIndex][workIndex] = value;
+            }
         }
 
         #endregion
@@ -209,39 +257,82 @@ namespace ContentTypeTextNet.Pe.Library.Shared.Library.Model
                 ChunkItems[i].Clear();
             }
 
-            CurrentChunkItem = null;
-            CurrentChunkItemIndex = 0;
-
             ChunkItemCount = 0;
             Count = 0;
         }
 
         public bool Contains(T item)
         {
-            throw new NotImplementedException();
+            for(var i = 0; i < ChunkItems.Length; i++) {
+                if(i == ChunkItemCount) {
+                    return false;
+                }
+
+                if(ChunkItems[i].Contains(item)) {
+                    return true;
+                }
+            }
+
+            return false;
         }
 
-        public void CopyTo(T[] array, int arrayIndex)
+        public void CopyTo(T[] array, int sourceIndex)
         {
-            throw new NotImplementedException();
+            CopyTo((Array)array, sourceIndex);
         }
-        public void CopyTo(Array array, int index)
+        public void CopyTo(Array array, int sourceIndex)
         {
-            throw new NotImplementedException();
+            var itemIndex = sourceIndex / ChunkItemCapacity;
+            var startIndex = sourceIndex % ChunkItemCapacity;
+            var destAddIndex = 0;
+            for(int i = 0, j = 0; i < ChunkItemCount; i++) {
+                if(i < itemIndex) {
+                    continue;
+                }
+                var item = ChunkItems[i];
+                item.CopyTo(array, startIndex, j * ChunkItemCapacity - destAddIndex);
+                if(j == 0) {
+                    destAddIndex = startIndex;
+                    startIndex = 0;
+                }
+                j += 1;
+            }
         }
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            for(var i = 0; i < ChunkItemCount; i++) {
+                var item = ChunkItems[i];
+                for(var j = 0; j < item.Count; j++) {
+                    yield return item[j];
+                }
+            }
         }
         IEnumerator IEnumerable.GetEnumerator()
         {
-            throw new NotImplementedException();
+            return GetEnumerator();
         }
 
         public bool Remove(T item)
         {
-            throw new NotImplementedException();
+            for(var i = 0; i < ChunkItemCount; i++) {
+                var chunkItem = ChunkItems[i];
+                var chunkCount = chunkItem.Count;
+                if(chunkItem.Remove(item)) {
+                    // 後ろの子らをずらす
+                    var prevChunkItem = chunkItem;
+                    for(var j = i + 1; j < ChunkItemCount; j++) {
+                        var nextChunkItem = ChunkItems[j];
+                        prevChunkItem.Add(nextChunkItem[0]);
+                        nextChunkItem.Remove(nextChunkItem[0]);
+                        prevChunkItem = nextChunkItem;
+                    }
+                    Count -= 1;
+                    return true;
+                }
+            }
+
+            return false;
         }
 
 
