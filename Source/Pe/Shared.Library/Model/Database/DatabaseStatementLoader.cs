de4ba38982bd8f96232ae359898b4879d23adcd0
sk <content.type.text.net@gmail.com>
2018-12-09 02:10:39 +0900

    なんかのこってた

--- Source/Pe/Shared.Library/Model/Database/DatabaseStatementLoader.cs
+++ Source/Pe/Shared.Library/Model/Database/DatabaseStatementLoader.cs
@@ -82,6 +82,9 @@ namespace ContentTypeTextNet.Pe.Library.Shared.Library.Model.Database
             : this(logFactory.CreateCurrentClass())
         { }
 
+        #region function
+        #endregion
+
         #region DatabaseStatementLoaderBase
 
         public override string LoadStatement(string key)
