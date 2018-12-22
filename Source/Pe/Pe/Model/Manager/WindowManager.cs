1947b3a921ee9601a504c617e5f4ede8c16bce4f
sk <content.type.text.net@gmail.com>
2018-12-22 16:50:01 +0900

    かたこり

--- Source/Pe/Pe/Model/Manager/WindowManager.cs
+++ Source/Pe/Pe/Model/Manager/WindowManager.cs
@@ -3,16 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using ContentTypeTextNet.Pe.Library.Shared.Library.Model;
 using ContentTypeTextNet.Pe.Library.Shared.Link.Model;
 
 namespace ContentTypeTextNet.Pe.Main.Model.Manager
 {
+    public class WindowItem
+    {
+        public WindowItem(Window window)
+        {
+            Window = window;
+        }
+
+        #region property
+
+        Window Window { get; }
+
+        #endregion
+    }
+
     public class WindowManager : ManagerBase
     {
         public WindowManager(IDiContainer diContainer, ILoggerFactory loggerFactory)
             : base(diContainer, loggerFactory)
-        {
-        }
+        { }
+
+        #region property
+        #endregion
+
+        #region function
+        #endregion
     }
 }
