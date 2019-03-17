92ba25b49ccd2717b6d0287cc1b358f61887e376
sk <content.type.text.net@gmail.com>
2019-03-17 13:30:00 +0900

    画面表示用にVM作成

---
+++ Source/Pe/Pe/ViewModel/LauncherItem/LauncherItemViewModel.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ContentTypeTextNet.Pe.Library.Shared.Library.ViewModel;
+using ContentTypeTextNet.Pe.Library.Shared.Link.Model;
+using ContentTypeTextNet.Pe.Main.Model.Element.LauncherItem;
+
+namespace ContentTypeTextNet.Pe.Main.ViewModel.LauncherItem
+{
+    public class LauncherItemViewModel : SingleModelViewModelBase<LauncherItemElement>
+    {
+        public LauncherItemViewModel(LauncherItemElement model, ILoggerFactory loggerFactory)
+            : base(model, loggerFactory)
+        { }
+
+        #region property
+        #endregion
+
+        #region command
+        #endregion
+
+        #region function
+        #endregion
+
+        #region SingleModelViewModelBase
+        #endregion
+    }
+}
