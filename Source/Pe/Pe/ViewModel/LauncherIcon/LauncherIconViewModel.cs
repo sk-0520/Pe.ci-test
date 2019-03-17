1bd54682bd2ae255a8a0964d627b122bdf933377
sk <content.type.text.net@gmail.com>
2019-03-17 23:20:45 +0900

    ランチャーのアイコン用各種サイズを構築

--- Source/Pe/Pe/ViewModel/LauncherIcon/LauncherIconViewModel.cs
+++ Source/Pe/Pe/ViewModel/LauncherIcon/LauncherIconViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ContentTypeTextNet.Pe.Library.Shared.Library.Model;
 using ContentTypeTextNet.Pe.Library.Shared.Library.ViewModel;
 using ContentTypeTextNet.Pe.Library.Shared.Link.Model;
 using ContentTypeTextNet.Pe.Main.Model.Element.LauncherIcon;
@@ -15,11 +16,18 @@ namespace ContentTypeTextNet.Pe.Main.ViewModel.LauncherIcon
         public LauncherIconViewModel(LauncherIconElement model, ILoggerFactory loggerFactory)
             : base(model, loggerFactory)
         {
+            Small = new IconViewerViewModel(Model.IconImageLoaders[IconScale.Small], Logger.Factory);
+            Normal = new IconViewerViewModel(Model.IconImageLoaders[IconScale.Normal], Logger.Factory);
+            Big = new IconViewerViewModel(Model.IconImageLoaders[IconScale.Big], Logger.Factory);
+            Large = new IconViewerViewModel(Model.IconImageLoaders[IconScale.Large], Logger.Factory);
         }
 
         #region property
 
         public IconViewerViewModel Small { get; }
+        public IconViewerViewModel Normal { get; }
+        public IconViewerViewModel Big { get; }
+        public IconViewerViewModel Large { get; }
 
         #endregion
 
