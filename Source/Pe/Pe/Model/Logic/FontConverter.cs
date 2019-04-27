f021d3e4581bd8cc1fe4131e99bfcd21f0bef60d
sk <content.type.text.net@gmail.com>
2019-04-27 22:14:29 +0900

    FontUtility を移植

--- Source/Pe/Pe/Model/Logic/FontConverter.cs
+++ Source/Pe/Pe/Model/Logic/FontConverter.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
 using ContentTypeTextNet.Pe.Library.Shared.Link.Model;
 
 namespace ContentTypeTextNet.Pe.Main.Model.Logic
@@ -21,6 +22,46 @@ namespace ContentTypeTextNet.Pe.Main.Model.Logic
         #region property
 
         ILogger Logger { get; }
+
+        #endregion
+
+        #region function
+
+        /// <summary>
+        /// 指定フォントファミリ名からFontFamily作成。
+        /// </summary>
+        /// <param name="fontFamily">フォントファミリ名</param>
+        /// <param name="defaultFontFamily">指定名から生成できなかった場合に代替として使用されるFontFamily。</param>
+        /// <returns></returns>
+        public FontFamily MakeFontFamily(string fontFamily, FontFamily defaultFontFamily)
+        {
+            if(!string.IsNullOrWhiteSpace(fontFamily)) {
+                if(Fonts.SystemFontFamilies.Any(f => f.FamilyNames.Any(n => n.Value == fontFamily))) {
+                    var result = new FontFamily(fontFamily);
+                    return result;
+                }
+            }
+
+            return defaultFontFamily;
+        }
+
+        /// <summary>
+        /// <para>TODO: この環境で再現できないのでスタブのみ作成</para>
+        /// </summary>
+        /// <param name="fontFamily"></param>
+        /// <returns></returns>
+        public string GetOriginalFontFamilyName(FontFamily fontFamily)
+        {
+            if(fontFamily == null) {
+                throw new ArgumentNullException(nameof(fontFamily));
+            }
+
+            if(fontFamily.FamilyNames.Any()) {
+                return fontFamily.FamilyNames.First().Value;
+            }
+
+            return fontFamily.Source;
+        }
 
         #endregion
     }
