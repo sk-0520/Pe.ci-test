1fcda67e3386c54d18523aa098e119ae01ac2931
sk <content.type.text.net@gmail.com>
2019-04-29 22:54:20 +0900

    書式どうすっかねぇ

--- Source/Pe/Pe/View/Note/NoteWindow.xaml.cs
+++ Source/Pe/Pe/View/Note/NoteWindow.xaml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
@@ -26,6 +28,8 @@ namespace ContentTypeTextNet.Pe.Main.View.Note
         public NoteWindow()
         {
             InitializeComponent();
+
+            Language = XmlLanguage.GetLanguage(CultureInfo.CurrentUICulture.Name);
         }
 
         #region property
