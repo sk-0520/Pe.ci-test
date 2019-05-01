a86e64d0ce93f08bc20cf5954184fb7ee2e43efc
sk <content.type.text.net@gmail.com>
2019-05-01 23:31:23 +0900

    コメント追加

--- Source/Pe/Pe/ViewModel/Manager/INotifyArea.cs
+++ Source/Pe/Pe/ViewModel/Manager/INotifyArea.cs
@@ -7,13 +7,39 @@ using System.Windows;
 
 namespace ContentTypeTextNet.Pe.Main.ViewModel.Manager
 {
+    /// <summary>
+    /// 通知領域の表示用。
+    /// </summary>
     public interface INotifyArea
     {
         #region property
 
+        /// <summary>
+        /// 通知領域 メニューヘッダ。
+        /// </summary>
         string MenuHeader { get; }
+        /// <summary>
+        /// 通知領域 メニュー キー。
+        /// <para>どうしてたっけなぁ。</para>
+        /// </summary>
+        string MenuKeys { get; }
+        /// <summary>
+        /// 通知領域 メニューアイコン。
+        /// </summary>
         DependencyObject MenuIcon { get; }
+        /// <summary>
+        /// 通知領域 メニューアイコンが有効か。
+        /// <para>これが真の場合にのみ <see cref="MenuIcon"/> が使用される。</para>
+        /// </summary>
+        bool MenuHasIcon {get;}
+        /// <summary>
+        /// 通知領域 メニュー 有効状態。
+        /// </summary>
         bool MenuIsEnabled { get; }
+        /// <summary>
+        /// 通知領域 メニュー チェック状態。
+        /// </summary>
+        bool MenuIsChecked { get; }
 
         #endregion
     }
