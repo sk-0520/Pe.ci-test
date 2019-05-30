8ffb7bdb959d67c3ee2918902f70551152f1058a
sk <content.type.text.net@gmail.com>
2019-05-30 21:51:07 +0900

    業務でいっつもこんなわけわからん変換作ってんなぁ

--- Source/Pe/Shared.Library.Test/Model/TextMatcherTest.cs
+++ Source/Pe/Shared.Library.Test/Model/TextMatcherTest.cs
@@ -1,14 +1,68 @@
 using System;
+using ContentTypeTextNet.Pe.Library.Shared.Library.Model;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Shared.Library.Test.Model
 {
     [TestClass]
-    public class TextMatcherTest
+    public class TextMatcherTestTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ConvertHiraganaToKatakaTest_Test()
         {
+            var textMatcher = new TextMatcher();
+            Assert.ThrowsException<ArgumentNullException>(() => textMatcher.ConvertHiraganaToKataka(null));
         }
+
+        [TestMethod]
+        [DataRow("", "")]
+        [DataRow("a", "a")]
+        [DataRow("ア", "あ")]
+        [DataRow("アア亜", "あア亜")]
+        [DataRow("アイウエオ", "あいうえお")]
+        [DataRow("カキクケコ", "かきくけこ")]
+        [DataRow("サシスセソ", "さしすせそ")]
+        [DataRow("タチツテト", "たちつてと")]
+        [DataRow("ナニヌネノ", "なにぬねの")]
+        [DataRow("ハヒフヘホ", "はひふへほ")]
+        [DataRow("マミムメモ", "まみむめも")]
+        [DataRow("ヤユヨ", "やゆよ")]
+        [DataRow("ラリルレロ", "らりるれろ")]
+        public void ConvertHiraganaToKatakaTest_Normal(string test, string input)
+        {
+            var textMatcher = new TextMatcher();
+            var result = textMatcher.ConvertHiraganaToKataka(input);
+            Assert.AreEqual(test, result);
+        }
+
+
+        [TestMethod]
+        public void ConvertKatakaToHiragana_Test_Null()
+        {
+            var textMatcher = new TextMatcher();
+            Assert.ThrowsException<ArgumentNullException>(() => textMatcher.ConvertKatakaToHiragana(null));
+        }
+
+        [TestMethod]
+        [DataRow("", "")]
+        [DataRow("a", "a")]
+        [DataRow("あ", "ア")]
+        [DataRow("ああ亜", "あア亜")]
+        [DataRow("あいうえお", "アイウエオ")]
+        [DataRow("かきくけこ", "カキクケコ")]
+        [DataRow("さしすせそ", "サシスセソ")]
+        [DataRow("たちつてと", "タチツテト")]
+        [DataRow("なにぬねの", "ナニヌネノ")]
+        [DataRow("はひふへほ", "ハヒフヘホ")]
+        [DataRow("まみむめも", "マミムメモ")]
+        [DataRow("やゆよ", "ヤユヨ")]
+        [DataRow("らりるれろ", "ラリルレロ")]
+        public void ConvertKatakaToHiragana_Normal(string test, string input)
+        {
+            var textMatcher = new TextMatcher();
+            var result = textMatcher.ConvertKatakaToHiragana(input);
+            Assert.AreEqual(test, result);
+        }
+
     }
 }
--- Source/Pe/Shared.Library/Model/TextMatcher.cs
+++ Source/Pe/Shared.Library/Model/TextMatcher.cs
@@ -8,17 +8,68 @@ using ContentTypeTextNet.Pe.Library.Shared.Embedded.Model;
 
 namespace ContentTypeTextNet.Pe.Library.Shared.Library.Model
 {
+    /// <summary>
+    ///
+    /// </summary>
+    /// <remarks>http://www.unicode.org/Public/UNIDATA/Blocks.txt</remarks>
     public class TextMatcher
     {
         #region property
+
+        /// <summary>
+        /// 3040..309F; Hiragana
+        /// </summary>
+        protected virtual Range<char> HiraganaRange { get; } = Range.Create('\u3040', '\u309F');
+        /// <summary>
+        /// 30A0..30FF; Katakana
+        /// </summary>
+        protected virtual Range<char> KatakanaRange { get; } = Range.Create('\u30A0', '\u30FF');
 
         #endregion
 
         #region function
 
-        public virtual string ConvertKatakaToHiragana(string input)
+        protected virtual bool IsHiragana(char c) => HiraganaRange.IsIn(c);
+        protected virtual bool IsKatakana(char c) => KatakanaRange.IsIn(c);
+
+        string ConvertCore(string input, Func<string, bool> checker, Action<StringBuilder, string> appender)
         {
-            throw new NotImplementedException();
+            var sb = new StringBuilder(input.Length);
+            foreach(var s in TextUtility.GetCharacters(input)) {
+                if(checker(s)) {
+                    appender(sb, s);
+                } else {
+                    sb.Append(s);
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        string ConvertCore(string input, Func<string, bool> checker, Func<string, char> converter)
+        {
+            return ConvertCore(input, checker, (sb, s) => sb.Append(converter(s)));
+        }
+        string ConvertCore(string input, Func<string, bool> checker, Func<string, string> converter)
+        {
+            return ConvertCore(input, checker, (sb, s) => sb.Append(converter(s)));
+        }
+
+        public string ConvertHiraganaToKataka(string input)
+        {
+            if(input == null) {
+                throw new ArgumentNullException(nameof(input));
+            }
+
+            return ConvertCore(input, s => s.Length == 1 && IsHiragana(s[0]), s => (char)(s[0] + 'ァ' - 'ぁ'));
+        }
+        public string ConvertKatakaToHiragana(string input)
+        {
+            if(input == null) {
+                throw new ArgumentNullException(nameof(input));
+            }
+
+            return ConvertCore(input, s => s.Length == 1 && IsKatakana(s[0]), s => (char)(s[0] + 'ぁ' - 'ァ'));
         }
 
         #endregion
