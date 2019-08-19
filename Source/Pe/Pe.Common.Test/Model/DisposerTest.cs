44fa3fa4e1a9b48c893a10855ff4e24702d3410e
sk <content.type.text.net@gmail.com>
2019-08-20 04:25:34 +0900

    共有プロジェクトをPe.Commonに移植

---
+++ Source/Pe/Pe.Common.Test/Model/DisposerTest.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ContentTypeTextNet.Pe.Common.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ContentTypeTextNet.Pe.Common.Test.Model
+{
+    [TestClass]
+    public class ActionDisposerTest
+    {
+        [TestMethod]
+        public void UsingTest()
+        {
+            using(var dispoer = new ActionDisposer(disposing => {
+                Assert.IsTrue(disposing);
+            })) {
+                Assert.IsTrue(true);
+            }
+        }
+
+        [TestMethod]
+        public void FinalizeTest()
+        {
+            var dispoer = new ActionDisposer(disposing => {
+                Assert.IsFalse(disposing);
+            });
+        }
+    }
+}
