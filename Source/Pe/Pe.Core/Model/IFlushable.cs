ce8ec41c2a77a3a826bf0352cc19b282e4499c0b
sk <content.type.text.net@gmail.com>
2019-08-20 04:32:09 +0900

    IFlushable

---
+++ Source/Pe/Pe.Core/Model/IFlushable.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ContentTypeTextNet.Pe.Common.Model;
+
+namespace ContentTypeTextNet.Pe.Core.Model
+{
+    /// <summary>
+    /// 保留中の処理を実施。
+    /// </summary>
+    public interface IFlushable
+    {
+        #region function
+
+        /// <summary>
+        /// 現在保留中の処理を実施する。
+        /// <para>実施後、<see cref="IDisposable.Dispose"/>と異なりオブジェクト自体は生きている。</para>
+        /// <para><see cref="IDisposable.Dispose(false)"/>でも実施できるように実装するのが努力目標。</para>
+        /// </summary>
+        void Flush();
+
+        #endregion
+    }
+
+    public static class IFlushableExtensions
+    {
+        #region function
+
+        public static bool SafeFlush<T>(this T @this)
+            where T : IDisposer, IFlushable
+        {
+            if(@this == null) {
+                return false;
+            }
+
+            if(@this.IsDisposed) {
+                return false;
+            }
+
+            @this.Flush();
+            return true;
+        }
+
+        #endregion
+    }
+}
