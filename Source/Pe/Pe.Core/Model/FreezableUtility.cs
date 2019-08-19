0a4f8ece62524ad04e988bf72da0607134a16f3a
sk <content.type.text.net@gmail.com>
2019-08-20 04:34:23 +0900

    EncodingUtility

---
+++ Source/Pe/Pe.Core/Model/FreezableUtility.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace ContentTypeTextNet.Pe.Core.Model
+{
+    /// <summary>
+    /// <see cref="Freezable"/>に対するあれ。
+    /// </summary>
+    public static class FreezableUtility
+    {
+        /// <summary>
+        /// 安全に<see cref="Freezable.Freeze()"/>する。
+        /// <para>どんな時にできないのかは知らん。</para>
+        /// </summary>
+        /// <param name="freezable"></param>
+        /// <returns></returns>
+        public static bool SafeFreeze(Freezable freezable)
+        {
+            var result = freezable.CanFreeze;
+            if(result) {
+                freezable.Freeze();
+            }
+
+            return result;
+        }
+
+        public static TFreezable GetSafeFreeze<TFreezable>(TFreezable freezable)
+            where TFreezable : Freezable
+        {
+            SafeFreeze(freezable);
+            return freezable;
+        }
+    }
+}
