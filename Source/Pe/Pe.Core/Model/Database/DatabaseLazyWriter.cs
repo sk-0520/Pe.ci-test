0d6b5419098c1debfd57ad54c4bd7c15f5c4f238
sk <content.type.text.net@gmail.com>
2019-09-13 22:21:21 +0900

    わっけわからん

--- Source/Pe/Pe.Core/Model/Database/DatabaseLazyWriter.cs
+++ Source/Pe/Pe.Core/Model/Database/DatabaseLazyWriter.cs
@@ -29,33 +29,6 @@ namespace ContentTypeTextNet.Pe.Core.Model.Database
         #endregion
     }
 
-    public class UniqueKeyPool
-    {
-        public UniqueKeyPool()
-        { }
-
-        #region property
-
-        ConcurrentDictionary<string, object> Pool { get; } = new ConcurrentDictionary<string, object>();
-
-        #endregion
-
-        #region function
-
-        public object Get([CallerMemberName] string callerMemberName = "", [CallerLineNumber] int callerLineNumber = -1)
-        {
-            var sb = new StringBuilder(callerMemberName.Length + 1 + callerLineNumber);
-            sb.Append(callerMemberName);
-            sb.Append('.');
-            sb.Append(callerLineNumber);
-
-            var result = Pool.GetOrAdd(sb.ToString(), k => new object());
-            return result;
-        }
-
-        #endregion
-    }
-
     public class DatabaseLazyWriter : DisposerBase, IFlushable
     {
         #region variable
