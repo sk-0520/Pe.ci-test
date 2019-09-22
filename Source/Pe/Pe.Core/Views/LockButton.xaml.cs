d7cac3416acc5dfdb190dd951c203e4627fd9e33
sk <content.type.text.net@gmail.com>
2019-09-22 21:03:46 +0900

    LockButton

---
+++ Source/Pe/Pe.Core/Views/LockButton.xaml.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ContentTypeTextNet.Pe.Core.Views
+{
+    /// <summary>
+    /// LockButton.xaml の相互作用ロジック
+    /// </summary>
+    public partial class LockButton : UserControl
+    {
+        public LockButton()
+        {
+            InitializeComponent();
+        }
+
+        #region LockContentProperty
+
+        public static readonly DependencyProperty LockContentProperty = DependencyProperty.Register(
+            nameof(LockContent),
+            typeof(object),
+            typeof(LockButton),
+            new FrameworkPropertyMetadata(
+                null,
+                new PropertyChangedCallback(OnContentChanged)
+            )
+        );
+
+        public object LockContent
+        {
+            get { return (object)GetValue(LockContentProperty); }
+            set { SetValue(LockContentProperty, value); }
+        }
+
+        static void OnContentChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var ctrl = d as LockButton;
+            if(ctrl != null) {
+                ctrl.LockContent = (object)e.NewValue;
+            }
+        }
+
+        #endregion
+
+        #region RestoreTimeProperty
+
+        public static readonly DependencyProperty RestoreTimeProperty = DependencyProperty.Register(
+            nameof(RestoreTime),
+            typeof(TimeSpan),
+            typeof(LockButton),
+            new FrameworkPropertyMetadata(
+                TimeSpan.FromSeconds(10),
+                new PropertyChangedCallback(OnRestoreTimeChanged)
+            )
+        );
+
+        public object RestoreTime
+        {
+            get { return (object)GetValue(RestoreTimeProperty); }
+            set { SetValue(RestoreTimeProperty, value); }
+        }
+
+        static void OnRestoreTimeChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var ctrl = d as LockButton;
+            if(ctrl != null) {
+                ctrl.RestoreTime = (TimeSpan)e.NewValue;
+            }
+        }
+
+        #endregion
+
+    }
+}
