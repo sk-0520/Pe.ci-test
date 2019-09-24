131641f8a9e6997e58d9beaf5558e405a0c9e683
sk <content.type.text.net@gmail.com>
2019-09-24 16:04:09 +0900

    適す変更タイミングを抑制

--- Source/Pe/Pe/ViewModels/Note/NoteRichTextContentViewModel.cs
+++ Source/Pe/Pe/ViewModels/Note/NoteRichTextContentViewModel.cs
@@ -1,12 +1,15 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Documents;
 using System.Windows.Input;
 using ContentTypeTextNet.Pe.Bridge.Models;
+using ContentTypeTextNet.Pe.Core.Models;
 using ContentTypeTextNet.Pe.Main.Models.Element.Note;
 using ContentTypeTextNet.Pe.Main.Models.Manager;
 using Microsoft.Extensions.Logging;
@@ -24,14 +27,17 @@ namespace ContentTypeTextNet.Pe.Main.ViewModels.Note
 
         public NoteRichTextContentViewModel(NoteContentElement model, IClipboardManager clipboardManager, IDispatcherWapper dispatcherWapper, ILoggerFactory loggerFactory)
             : base(model, clipboardManager, dispatcherWapper, loggerFactory)
-        { }
+        {
+            TextChangeLazyAction = new LazyAction("RTF変更抑制", TimeSpan.FromSeconds(2), LoggerFactory);
+        }
 
         #region property
 
         //Xceed.Wpf.Toolkit.RichTextBox Control { get; set; }
         RichTextBox? Control { get; set; }
+        LazyAction TextChangeLazyAction { get; }
 
-    public string? RtfContent
+        public string? RtfContent
         {
             get => this._rtfContent;
             set
@@ -61,14 +67,31 @@ namespace ContentTypeTextNet.Pe.Main.ViewModels.Note
             //Control = (Xceed.Wpf.Toolkit.RichTextBox)control;
             Control = (RichTextBox)control;
 
+            Control.TextChanged += Control_TextChanged;
+
             return Task.Run(() => {
                 var content = Model.LoadRichTextContent();
-                RtfContent = content;
+                //RtfContent = content;
+
+                var doc = Control!.Document;
+                var range = new TextRange(doc.ContentStart, doc.ContentEnd);
+                var rtf = Encoding.UTF8.GetBytes(content);
+                using(var stream = new MemoryStream(rtf)) {
+                    DispatcherWapper.Invoke(() => {
+                        range.Load(stream, DataFormats.Rtf);
+                    });
+                }
             });
         }
 
         protected override void UnloadContent()
-        { }
+        {
+            if(Control == null) {
+                return;
+            }
+
+            Control.TextChanged -= Control_TextChanged;
+        }
 
         protected override IDataObject GetContentData()
         {
@@ -83,6 +106,24 @@ namespace ContentTypeTextNet.Pe.Main.ViewModels.Note
             return data;
         }
 
+        void ChangedText()
+        {
+            var doc = Control!.Document;
+            var range = new TextRange(doc.ContentStart, doc.ContentEnd);
+            using(var stream = new MemoryStream()) {
+                DispatcherWapper.Invoke(() => range.Save(stream, DataFormats.Rtf));
+
+                var content = Encoding.UTF8.GetString(stream.ToArray());
+                Model.ChangeRichTextContent(content);
+            }
+        }
+
         #endregion
+
+        private void Control_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextChangeLazyAction.DelayAction(ChangedText);
+        }
+
     }
 }
