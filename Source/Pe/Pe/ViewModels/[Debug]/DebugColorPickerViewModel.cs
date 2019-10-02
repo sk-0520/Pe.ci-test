0f9b147daab4f6298f78cb87d74ff42bf864c5bd
sk <content.type.text.net@gmail.com>
2019-10-02 23:49:20 +0900

    あぁしんど

--- Source/Pe/Pe/ViewModels/[Debug]/DebugColorPickerViewModel.cs
+++ Source/Pe/Pe/ViewModels/[Debug]/DebugColorPickerViewModel.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Media;
 using ContentTypeTextNet.Pe.Core.ViewModels;
 using ContentTypeTextNet.Pe.Main.Models.Element._Debug_;
 using Microsoft.Extensions.Logging;
@@ -9,11 +10,25 @@ namespace ContentTypeTextNet.Pe.Main.ViewModels._Debug_
 {
     public class DebugColorPickerViewModel : DebugViewModelBase<DebugColorPickerElement>
     {
+        #region variable
+
+        Color _color = Colors.Red;
+
+        #endregion
+
         public DebugColorPickerViewModel(DebugColorPickerElement model, ILoggerFactory loggerFactory)
             : base(model, loggerFactory)
         { }
 
         #region property
+
+        public Color Color
+        {
+            get => this._color;
+            set => SetProperty(ref this._color, value);
+        }
+
+
         #endregion
 
         #region command
