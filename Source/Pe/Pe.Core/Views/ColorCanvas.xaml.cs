e00d397f04d1e92b0981410c6fe25fe8ffb3ef24
sk <content.type.text.net@gmail.com>
2019-10-02 22:00:02 +0900

    うーん、本筋じゃないところでこれ以上時間使うのはちときついぞ

--- Source/Pe/Pe.Core/Views/ColorCanvas.xaml.cs
+++ Source/Pe/Pe.Core/Views/ColorCanvas.xaml.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -22,6 +24,33 @@ namespace ContentTypeTextNet.Pe.Core.Views
         {
             InitializeComponent();
         }
+
+        #region property
+
+        #endregion
+
+        #region BaseColorProperty
+
+        public static readonly DependencyProperty BaseColorProperty = DependencyProperty.Register(
+            nameof(BaseColor),
+            typeof(Color),
+            typeof(ColorCanvas),
+            new PropertyMetadata(Colors.Red, OnBaseColorPropertyChanged)
+        );
+
+        public Color BaseColor
+        {
+            get { return (Color)GetValue(BaseColorProperty); }
+            set { SetValue(BaseColorProperty, value); }
+        }
+
+        private static void OnBaseColorPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var ctrl = (ColorCanvas)d;
+            ctrl.BaseColor = (Color)e.NewValue;
+        }
+
+        #endregion
 
         #region HasAlphaProperty
 
@@ -46,33 +75,113 @@ namespace ContentTypeTextNet.Pe.Core.Views
 
         #endregion
 
-        #region ValueProperty
+        #region SelectedColorProperty
 
-        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(
-            nameof(Value),
+        public static readonly DependencyProperty SelectedColorProperty = DependencyProperty.Register(
+            nameof(SelectedColor),
             typeof(Color),
             typeof(ColorCanvas),
-            new PropertyMetadata(Colors.Red, OnValuePropertyChanged)
+            new PropertyMetadata(Colors.Red, OnSelectedColorPropertyChanged)
         );
 
-        public Color Value
+        public Color SelectedColor
         {
-            get { return (Color)GetValue(ValueProperty); }
-            set { SetValue(ValueProperty, value); }
+            get { return (Color)GetValue(SelectedColorProperty); }
+            set { SetValue(SelectedColorProperty, value); }
         }
 
-        private static void OnValuePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        private static void OnSelectedColorPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             var ctrl = (ColorCanvas)d;
             var color = (Color)e.NewValue;
             if(ctrl.HasAlpha) {
-                ctrl.Value = color;
+                ctrl.SelectedColor = color;
             } else {
-                ctrl.Value = Color.FromRgb(color.R, color.G, color.B);
+                ctrl.SelectedColor = Color.FromRgb(color.R, color.G, color.B);
             }
         }
 
         #endregion
 
+        #region HueGradationsProperty
+
+        public static readonly DependencyProperty HueGradationsProperty = DependencyProperty.Register(
+            nameof(HueGradations),
+            typeof(GradientStopCollection),
+            typeof(ColorCanvas),
+            new PropertyMetadata(
+                new GradientStopCollection(
+                    new[] {
+                        new GradientStop(Color.FromRgb(0xff,0x00,0x00), 0),
+                        new GradientStop(Color.FromRgb(0xff,0xff,0x00), 0.167),
+                        new GradientStop(Color.FromRgb(0x00,0xff,0x00), 0.333),
+                        new GradientStop(Color.FromRgb(0x00,0xff,0xff), 0.5),
+                        new GradientStop(Color.FromRgb(0x00,0x00,0xff), 0.667),
+                        new GradientStop(Color.FromRgb(0xff,0x00,0xff), 0.833),
+                        new GradientStop(Color.FromRgb(0xff,0x00,0x00), 1),
+                    }
+                ),
+                OnHueGradationsPropertyChanged
+            )
+        );
+
+        public GradientStopCollection HueGradations
+        {
+            get { return (GradientStopCollection)GetValue(HueGradationsProperty); }
+            set { SetValue(HueGradationsProperty, value); }
+        }
+
+        private static void OnHueGradationsPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var ctrl = (ColorCanvas)d;
+            ctrl.HueGradations = (GradientStopCollection)e.NewValue;
+        }
+
+        #endregion
+
+        #region function
+
+        /// <summary>
+        /// https://stackoverflow.com/a/9651053
+        /// </summary>
+        /// <param name="gsc"></param>
+        /// <param name="offset"></param>
+        /// <returns></returns>
+        public static Color GetRelativeColor(GradientStopCollection gsc, double offset)
+        {
+            var point = gsc.SingleOrDefault(f => f.Offset == offset);
+            if(point != null) {
+                return point.Color;
+            }
+
+            GradientStop before = gsc.Where(w => w.Offset == gsc.Min(m => m.Offset)).First();
+            GradientStop after = gsc.Where(w => w.Offset == gsc.Max(m => m.Offset)).First();
+
+            foreach(var gs in gsc) {
+                if(gs.Offset < offset && gs.Offset > before.Offset) {
+                    before = gs;
+                }
+                if(gs.Offset > offset && gs.Offset < after.Offset) {
+                    after = gs;
+                }
+            }
+
+            var color = new Color();
+
+            color.ScA = (float)((offset - before.Offset) * (after.Color.ScA - before.Color.ScA) / (after.Offset - before.Offset) + before.Color.ScA);
+            color.ScR = (float)((offset - before.Offset) * (after.Color.ScR - before.Color.ScR) / (after.Offset - before.Offset) + before.Color.ScR);
+            color.ScG = (float)((offset - before.Offset) * (after.Color.ScG - before.Color.ScG) / (after.Offset - before.Offset) + before.Color.ScG);
+            color.ScB = (float)((offset - before.Offset) * (after.Color.ScB - before.Color.ScB) / (after.Offset - before.Offset) + before.Color.ScB);
+
+            return color;
+        }
+
+        #endregion
+
+        private void PART_HueSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            var color = GetRelativeColor(HueGradations, e.NewValue);
+            BaseColor = color;
+        }
     }
 }
