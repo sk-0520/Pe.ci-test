ec6fb523ff6d88df97efb68bbf720d2e5293ac0d
sk <content.type.text.net@gmail.com>
2019-10-14 17:31:37 +0900

    めっちゃ吸い取られるやん・・・

--- Source/Pe/Pe/Views/CustomizeLauncherItem/CustomizeLauncherItemWindow.xaml.cs
+++ Source/Pe/Pe/Views/CustomizeLauncherItem/CustomizeLauncherItemWindow.xaml.cs
@@ -33,5 +33,56 @@ namespace ContentTypeTextNet.Pe.Main.Views.CustomizeLauncherItem
 
         #endregion
 
+        #region Window
+
+        protected override void OnPreviewMouseWheel(MouseWheelEventArgs e)
+        {
+            var avalonEdit = UIUtility.GetVisualClosest<ICSharpCode.AvalonEdit.TextEditor>((DependencyObject)e.OriginalSource);
+            if(avalonEdit != null) {
+                var scrollViewer = UIUtility.GetVisualClosest<System.Windows.Controls.ScrollViewer>((DependencyObject)e.OriginalSource);
+                if(scrollViewer != null) {
+                    Logger.LogDebug(e.OriginalSource.ToString());
+                    Logger.LogDebug("HorizontalOffset: {0}, ContentHorizontalOffset: {1}", scrollViewer.HorizontalOffset, scrollViewer.ContentHorizontalOffset);
+                    Logger.LogDebug("VerticalOffset: {0}, ContentVerticalOffset: {1}", scrollViewer.VerticalOffset, scrollViewer.ContentVerticalOffset);
+                    Logger.LogDebug("ScrollableHeight: {0}", scrollViewer.ScrollableHeight);
+                    if(scrollViewer.VerticalOffset == 0) {
+                        Logger.LogDebug("top");
+                        e.Handled = true;
+                    }
+                    if(scrollViewer.VerticalOffset == scrollViewer.ScrollableHeight) {
+                        Logger.LogDebug("bottom");
+                        e.Handled = true;
+                    }
+                }
+            }
+
+            base.OnPreviewMouseWheel(e);
+        }
+
+        protected override void OnMouseWheel(MouseWheelEventArgs e)
+        {
+            var avalonEdit = UIUtility.GetVisualClosest<ICSharpCode.AvalonEdit.TextEditor>((DependencyObject)e.OriginalSource);
+            if(avalonEdit != null) {
+                var scrollViewer = UIUtility.GetVisualClosest<System.Windows.Controls.ScrollViewer>((DependencyObject)e.OriginalSource);
+                if(scrollViewer != null) {
+                    Logger.LogDebug(e.OriginalSource.ToString());
+                    Logger.LogDebug("HorizontalOffset: {0}, ContentHorizontalOffset: {1}", scrollViewer.HorizontalOffset, scrollViewer.ContentHorizontalOffset);
+                    Logger.LogDebug("VerticalOffset: {0}, ContentVerticalOffset: {1}", scrollViewer.VerticalOffset, scrollViewer.ContentVerticalOffset);
+                    Logger.LogDebug("ScrollableHeight: {0}", scrollViewer.ScrollableHeight);
+                    if(scrollViewer.VerticalOffset == 0) {
+                        Logger.LogDebug("top");
+                        e.Handled = true;
+                    }
+                    if(scrollViewer.VerticalOffset == scrollViewer.ScrollableHeight) {
+                        Logger.LogDebug("bottom");
+                        e.Handled = true;
+                    }
+                }
+            }
+
+            base.OnMouseWheel(e);
+        }
+
+        #endregion
     }
 }
