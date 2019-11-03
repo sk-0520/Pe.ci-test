3520b4febcdf75f8f71709654d31b9e92059609b
sk <content.type.text.net@gmail.com>
2019-11-03 15:56:43 +0900

    起動用C++プロジェクト追加

---
+++ Source/Pe/Pe/Pe.c
@@ -0,0 +1,7 @@
+#include <windows.h>
+#include <tchar.h>
+
+int CALLBACK WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
+{
+    return 0;
+}
