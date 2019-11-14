d49ab5caf54bf9e679befc834936ab2f14825f14
sk <content.type.text.net@gmail.com>
2019-11-15 00:44:41 +0900

    ねむいけどねれない、なんだこれ

--- Source/Pe/Pe.Main/Models/Data/KeyData.cs
+++ Source/Pe/Pe.Main/Models/Data/KeyData.cs
@@ -6,6 +6,75 @@ using System.Windows.Input;
 namespace ContentTypeTextNet.Pe.Main.Models.Data
 {
     /// <summary>
+    /// キーボード処理の種別。
+    /// <para>修飾キー単体での起動は行わない方針。</para>
+    /// </summary>
+    public enum KeyActionKind
+    {
+        /// <summary>
+        /// キー置き換え。
+        /// <para>マッピングデータは起点となる一つのみ。</para>
+        /// <para>アクション内容は置き換えキー。</para>
+        /// </summary>
+        Replace,
+        /// <summary>
+        /// キー無効化。
+        /// <para>無効化するが二重入力で活性とする。</para>
+        /// <para>マッピングデータは起点となる一つのみ。</para>
+        /// </summary>
+        Disable,
+        /// <summary>
+        /// コマンド型ランチャーの起動。
+        /// </summary>
+        Command,
+        /// <summary>
+        /// ランチャーアイテムの起動。
+        /// <para>アクション内容は起動する方法。</para>
+        /// <para>アクションオプションは起動するアイテム。</para>
+        /// </summary>
+        Launcher,
+        /// <summary>
+        /// ノートの処理。
+        /// <para>アクション内容は処理内容。</para>
+        /// </summary>
+        Note,
+    }
+
+    /// <summary>
+    /// <see cref="KeyActionKind.Launcher"/>で処理する内容。
+    /// </summary>
+    public enum KeyActionContentLauncher
+    {
+        /// <summary>
+        /// 実行。
+        /// </summary>
+        Execute,
+        /// <summary>
+        /// 指定して実行。
+        /// </summary>
+        ExtendsExecute,
+    }
+
+    /// <summary>
+    /// <see cref="KeyActionKind.Note"/>で処理する内容。
+    /// </summary>
+    public enum KeyActionContentNote
+    {
+        /// <summary>
+        /// 新規ノートの作成。
+        /// </summary>
+        Create,
+        /// <summary>
+        /// 表示中ノートの最前面移動。
+        /// </summary>
+        ZOrderTop,
+        /// <summary>
+        /// 表示中ノートの最後面移動。
+        /// </summary>
+        ZOrderBottom,
+    }
+
+    /// <summary>
     /// 修飾キー。
     /// </summary>
     public enum ModifierKey
@@ -30,6 +99,26 @@ namespace ContentTypeTextNet.Pe.Main.Models.Data
         /// 両方。
         /// </summary>
         All,
+    }
+
+    public interface IReadOnlyKeyActionCommonData
+    {
+        #region property
+
+        Guid KeyActionId { get; }
+        KeyActionKind KeyActionKind { get; }
+
+        #endregion
+    }
+
+    public class KeyActionCommonData : IReadOnlyKeyActionCommonData
+    {
+        #region IReadOnlyKeyActionCommonData
+
+        public Guid KeyActionId { get; set; }
+        public KeyActionKind KeyActionKind { get; set; }
+
+        #endregion
     }
 
     public interface IReadOnlyKeyMappingItemData
@@ -57,6 +146,5 @@ namespace ContentTypeTextNet.Pe.Main.Models.Data
 
         #endregion
     }
-
 
 }
