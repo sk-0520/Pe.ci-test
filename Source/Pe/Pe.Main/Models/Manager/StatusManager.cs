f021d16d511fd28a9eaa86a86e6246065d625026
sk <content.type.text.net@gmail.com>
2019-11-19 21:38:03 +0900

    冗長的に一旦書いた
    あとでいい感じにする

--- Source/Pe/Pe.Main/Models/Manager/StatusManager.cs
+++ Source/Pe/Pe.Main/Models/Manager/StatusManager.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
 using ContentTypeTextNet.Pe.Core.Models;
 using Microsoft.Extensions.Logging;
@@ -7,14 +9,88 @@ using Microsoft.Extensions.Logging;
 namespace ContentTypeTextNet.Pe.Main.Models.Manager
 {
     /// <summary>
+    /// ステータス変更モード。
+    /// </summary>
+    public enum StatusChangedMode
+    {
+        /// <summary>
+        /// 永続的変更。
+        /// </summary>
+        Changed,
+        /// <summary>
+        /// 一時的変更。
+        /// </summary>
+        TemporaryChanged,
+        /// <summary>
+        /// 一時的変更の戻し。
+        /// </summary>
+        TemporaryRestore,
+    }
+
+    public enum StatusProperty
+    {
+        /// <summary>
+        /// 💩＜ｳﾝﾁ!
+        /// </summary>
+        Unknown,
+        /// <summary>
+        /// <see cref="IStatusManager.CanCallNotifyAreaMenu"/>
+        /// </summary>
+        CanCallNotifyAreaMenu,
+    }
+
+    public class StatusChangedEventArgs : EventArgs
+    {
+        public StatusChangedEventArgs(StatusChangedMode mode, StatusProperty statusProperty, Type valueType, object? oldValue, object? newValue)
+        {
+            Mode = mode;
+            ValueType = valueType;
+            OldValue = oldValue;
+            NewValue = newValue;
+            StatusProperty = statusProperty;
+        }
+
+        #region property
+
+        public StatusChangedMode Mode { get; }
+        public Type ValueType { get; }
+        public StatusProperty StatusProperty { get; }
+        public object? OldValue { get; }
+        public object? NewValue { get; }
+
+        #endregion
+    }
+
+
+    /// <summary>
     /// アプリケーションの状態管理。
     /// </summary>
     public interface IStatusManager
     {
-        #region property
+        #region event
+
+        event EventHandler<StatusChangedEventArgs> StatusChanged;
+
         #endregion
 
         #region function
+
+        IResult ChangeBoolean(StatusProperty statusProperty, bool newValue);
+        IResultSuccessValue<IDisposable> ChangeLimitedBoolean(StatusProperty statusProperty, bool newValue);
+
+        #endregion
+
+        #region property
+
+        bool CanCallNotifyAreaMenu { get; }
+
+        #endregion
+
+        #region function
+
+
+
+
         #endregion
     }
 
@@ -24,10 +100,85 @@ namespace ContentTypeTextNet.Pe.Main.Models.Manager
             : base(diContainer, loggerFactory)
         { }
 
+        #region property
+
+        #endregion
+
         #region function
+
+        void OnStatusChanged(StatusChangedMode mode, StatusProperty statusProperty, Type valueType, object? oldValue, object? newValue)
+        {
+            var e = new StatusChangedEventArgs(mode, statusProperty, valueType, oldValue, newValue);
+            StatusChanged?.Invoke(this, e);
+        }
+
+        void OnStatusChanged<T>(StatusChangedMode mode, StatusProperty statusProperty, T oldValue, T newValue)
+        {
+            OnStatusChanged(mode, statusProperty, typeof(T), oldValue, newValue);
+        }
+
         #endregion
 
         #region IStatusManager
+
+        public event EventHandler<StatusChangedEventArgs>? StatusChanged;
+
+        public IResult ChangeBoolean(StatusProperty statusProperty, bool value)
+        {
+            var oldValue = statusProperty switch
+            {
+                StatusProperty.CanCallNotifyAreaMenu => CanCallNotifyAreaMenu,
+                _ => throw new NotImplementedException(),
+            };
+
+            if(oldValue == value) {
+                return new Result(false);
+            }
+
+            var newValue = statusProperty switch
+            {
+                StatusProperty.CanCallNotifyAreaMenu => CanCallNotifyAreaMenu = value,
+                _ => throw new NotImplementedException(),
+            };
+
+            OnStatusChanged(StatusChangedMode.Changed, statusProperty, oldValue, newValue);
+
+            return new Result(true);
+        }
+
+        public IResultSuccessValue<IDisposable> ChangeLimitedBoolean(StatusProperty statusProperty, bool value)
+        {
+            var oldValue = statusProperty switch
+            {
+                StatusProperty.CanCallNotifyAreaMenu => CanCallNotifyAreaMenu,
+                _ => throw new NotImplementedException(),
+            };
+
+            if(oldValue == value) {
+                return ResultSuccessValue.Failure<IDisposable>();
+            }
+
+            var newValue = statusProperty switch
+            {
+                StatusProperty.CanCallNotifyAreaMenu => CanCallNotifyAreaMenu = value,
+                _ => throw new NotImplementedException(),
+            };
+
+            OnStatusChanged(StatusChangedMode.TemporaryChanged, statusProperty, oldValue, newValue);
+
+            return ResultSuccessValue.Success((IDisposable)new ActionDisposer(() => {
+                var restoreValue = statusProperty switch
+                {
+                    StatusProperty.CanCallNotifyAreaMenu => CanCallNotifyAreaMenu = oldValue,
+                    _ => throw new NotImplementedException(),
+                };
+                OnStatusChanged(StatusChangedMode.TemporaryRestore, statusProperty, newValue, oldValue);
+            }));
+        }
+
+        public bool CanCallNotifyAreaMenu { get; private set; }
+
+
         #endregion
 
     }
