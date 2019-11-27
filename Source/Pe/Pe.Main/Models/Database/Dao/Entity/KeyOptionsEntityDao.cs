81d78c4e24f749b24ba6c7262046ce9870ba9a7e
sk <content.type.text.net@gmail.com>
2019-11-27 23:17:06 +0900

    ぶっ壊す準備OK

---
+++ Source/Pe/Pe.Main/Models/Database/Dao/Entity/KeyOptionsEntityDao.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ContentTypeTextNet.Pe.Core.Models.Database;
+using Microsoft.Extensions.Logging;
+
+namespace ContentTypeTextNet.Pe.Main.Models.Database.Dao.Entity
+{
+    public class KeyOptionsEntityDao : EntityDaoBase
+    {
+        public KeyOptionsEntityDao(IDatabaseCommander commander, IDatabaseStatementLoader statementLoader, IDatabaseImplementation implementation, ILoggerFactory loggerFactory)
+            : base(commander, statementLoader, implementation, loggerFactory)
+        { }
+
+        #region property
+
+        public static class Column
+        {
+            #region property
+
+            public static string KeyActionId { get; } = "KeyActionId";
+            public static string KeyOption { get; } = "KeyOption";
+
+            #endregion
+        }
+
+        #endregion
+
+        #region function
+        #endregion
+    }
+}
