a159dcb7ac27727a86ca8e67649a8eee24ff2e61
sk <content.type.text.net@gmail.com>
2019-12-06 23:33:53 +0900

    dao, dto のひな形だけ作成

---
+++ Source/Pe/Pe.Main/Models/Data/Dto/Entity/AppUpdateSettingEntityDao.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContentTypeTextNet.Pe.Main.Models.Data.Dto.Entity
+{
+    public class AppUpdateSettingEntityDao : CommonDtoBase
+    {
+        #region property
+
+        #endregion
+    }
+}
---
+++ Source/Pe/Pe.Main/Models/Database/Dao/Entity/AppGeneralSettingEntityDao.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ContentTypeTextNet.Pe.Bridge.Models.Data;
+using ContentTypeTextNet.Pe.Core.Models.Database;
+using ContentTypeTextNet.Pe.Main.Models.Data;
+using ContentTypeTextNet.Pe.Main.Models.Data.Dto.Entity;
+using Microsoft.Extensions.Logging;
+
+namespace ContentTypeTextNet.Pe.Main.Models.Database.Dao.Entity
+{
+    public class AppGeneralSettingEntityDao : EntityDaoBase
+    {
+        public AppGeneralSettingEntityDao(IDatabaseCommander commander, IDatabaseStatementLoader statementLoader, IDatabaseImplementation implementation, ILoggerFactory loggerFactory)
+            : base(commander, statementLoader, implementation, loggerFactory)
+        { }
+
+        #region property
+
+        public static class Column
+        {
+            #region property
+
+
+            #endregion
+        }
+
+        #endregion
+
+        #region function
+
+
+
+
+        #endregion
+    }
+}
