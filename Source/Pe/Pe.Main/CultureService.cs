8918c5471210cad611d5bd9463fc3ec323d12150
sk <content.type.text.net@gmail.com>
2019-12-31 20:29:05 +0900

    fx 2 から CultureService 移植

---
+++ Source/Pe/Pe.Main/CultureService.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Text;
+
+namespace ContentTypeTextNet.Pe.Main
+{
+    public sealed class CultureService : INotifyPropertyChanged
+    {
+        #region event
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        #endregion
+
+        #region property
+
+        static CultureInfo StartupCulture { get; } = CultureInfo.CurrentCulture;
+
+        public static CultureService Current { get; } = new CultureService();
+
+        public Properties.Resources Resources { get; } = new Properties.Resources();
+
+        public CultureInfo Culture => Properties.Resources.Culture;
+
+        #endregion
+
+        #region function
+
+        void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        void ChangeCultureCore(CultureInfo culture)
+        {
+            Properties.Resources.Culture = culture;
+
+            CultureInfo.CurrentCulture = culture;
+            CultureInfo.CurrentUICulture = culture;
+
+            OnPropertyChanged(nameof(Resources));
+        }
+
+        public void ChangeCulture(string name)
+        {
+            var culture = CultureInfo.GetCultureInfo(name);
+            ChangeCultureCore(culture);
+        }
+
+        public void ChangeAutoCulture()
+        {
+            ChangeCultureCore(StartupCulture);
+        }
+
+        #endregion
+    }
+}
