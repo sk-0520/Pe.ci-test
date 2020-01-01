a846848a59311a8e19bf302d701b925b2ebe3547
sk <content.type.text.net@gmail.com>
2020-01-02 05:54:32 +0900

    ColorPair 移動
    これ構造体でよくない？

--- Source/Pe/Pe.Bridge/Source/Pe.Bridge/Plugin/Theme/Theme.cs
+++ Source/Pe/Pe.Bridge/Source/Pe.Bridge/Plugin/Theme/Theme.cs
@@ -14,6 +14,35 @@ namespace ContentTypeTextNet.Pe.Bridge.Plugin.Theme
         #endregion
     }
 
+    //TODO: 構造体で一本化したい
+    public class ColorPair<T> : IReadOnlyColorPair<T>
+    {
+        public ColorPair(T foreground, T background)
+        {
+            Foreground = foreground;
+            Background = background;
+        }
+
+        #region property
+
+        public T Foreground { get; }
+        public T Background { get; }
+
+        #endregion
+    }
+
+    public static class ColorPair
+    {
+        #region function
+        public static ColorPair<T> Create<T>(T foreground, T background)
+        {
+            return new ColorPair<T>(foreground, background);
+        }
+
+        #endregion
+    }
+
+
     public enum ViewState
     {
         Active,
