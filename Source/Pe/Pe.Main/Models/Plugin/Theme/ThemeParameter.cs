b9808d3de9ab04622c2994db292b3a7d5d56f236
sk <content.type.text.net@gmail.com>
2020-01-01 22:01:48 +0900

    PluginParameterBase

---
+++ Source/Pe/Pe.Main/Models/Plugin/Theme/ThemeParameter.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ContentTypeTextNet.Pe.Bridge.Models;
+using ContentTypeTextNet.Pe.Bridge.Plugin.Theme;
+using Microsoft.Extensions.Logging;
+
+namespace ContentTypeTextNet.Pe.Main.Models.Plugin.Theme
+{
+    public class ThemeParameter : PluginParameterBase, IThemeParameter
+    {
+        public ThemeParameter(IPlatformTheme platformTheme, IDispatcherWrapper dispatcherWrapper, ILoggerFactory loggerFactory)
+            : base(platformTheme, dispatcherWrapper, loggerFactory)
+        { }
+
+        #region IThemeParameter
+
+        #endregion
+    }
+}
