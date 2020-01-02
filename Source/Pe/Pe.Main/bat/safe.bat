30f594a6106ed101ac5267d9825e59b4da1c17e6
sk <content.type.text.net@gmail.com>
2020-01-02 17:50:04 +0900

    バッチファイルのメッセージ整理

--- Source/Pe/Pe.Main/bat/safe.bat
+++ Source/Pe/Pe.Main/bat/safe.bat
@@ -2,7 +2,17 @@ cd /d %~dp0
 
 echo off
 
-echo "セーフモード起動"
-..\Pe.exe --log .\data\logs --force-log --user-dir .\data\user --machine-dir .\data\machine --temp-dir .\data\temp
+set DATA_DIR=safe-data
 
-pause
+echo == セーフモード起動 ==
+echo.
+echo 各種データは %DATA_DIR% ディレクトリに保存されます。
+echo 実行後に不要であれば削除してください。
+echo.
+echo 実行しています...
+..\Pe.exe --log .\%DATA_DIR%\logs --force-log --user-dir .\%DATA_DIR%\user --machine-dir .\%DATA_DIR%\machine --temp-dir .\%DATA_DIR%\temp
+
+echo.
+echo キー入力でプロンプトを閉じます。
+pause > nul
+
