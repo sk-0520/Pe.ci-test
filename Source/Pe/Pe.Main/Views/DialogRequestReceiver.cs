d72785b83e15a1f80341c9e98df92b592663666d
sk <content.type.text.net@gmail.com>
2020-01-06 22:22:45 +0900

    無理やりだけど一応タブのitemssourceから独立させた

--- Source/Pe/Pe.Main/Views/DialogRequestReceiver.cs
+++ Source/Pe/Pe.Main/Views/DialogRequestReceiver.cs
@@ -94,8 +94,8 @@ namespace ContentTypeTextNet.Pe.Main.Views
         {
             Debug.Assert(View != null);
 
-            View.Loaded -= View_Loaded;
-            View.Unloaded += View_Unloaded;
+            //View.Loaded -= View_Loaded;
+            //View.Unloaded += View_Unloaded;
             OwnerWindow = Window.GetWindow(View);
         }
 
@@ -103,9 +103,9 @@ namespace ContentTypeTextNet.Pe.Main.Views
         {
             Debug.Assert(View != null);
 
-            View.Unloaded -= View_Unloaded;
-            View = null;
-            OwnerWindow = null;
+            //View.Unloaded -= View_Unloaded;
+            //View = null;
+            //OwnerWindow = null;
         }
 
 
