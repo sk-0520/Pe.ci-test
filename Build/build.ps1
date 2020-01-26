8f5607cde5dc569b85c1373b66b09b6c3c7a631d
sk <content.type.text.net@gmail.com>
2020-01-26 14:33:17 +0900

    バージョン取得

--- Build/build.ps1
+++ Build/build.ps1
@@ -24,17 +24,25 @@ Write-Output ("dotnet: " + (dotnet --version))
 
 # SCM 的に現行状態に未コミットがあれば死ぬ
 if((git status -s | Measure-Object).Count -ne 0) {
-    #throw "変更あり"
+    throw "変更あり"
 }
+
 
 $rootDirectory = Split-Path -Path $currentDirPath -Parent
 try {
     Push-Location $rootDirectory
 
+    $projectXml = [XML](Get-Content -Path Source/Pe/Pe.Main/Pe.Main.csproj)
+    $projectNav = $projectXml.CreateNavigator()
+    $vesion = $projectNav.Select('//PropertyGroup/Version').Value
+    $revision = (git rev-parse HEAD)
+
+
     # ビルド開始
-    msbuild        Source/Pe.Boot/Pe.Boot.sln                          /p:Configuration=Release                          /p:Platform=$platform /p:DefineConstants=$buildType
-    dotnet build   Source/Pe/Pe.sln                  --verbosity normal --configuration Release --runtime win-$platform  /p:Platform=$platform /p:DefineConstants=$buildType
-    dotnet publish Source/Pe/Pe.Main/Pe.Main.csproj  --verbosity normal --configuration Release --runtime win-$platform  /p:Platform=$platform /p:DefineConstants=$buildType --output Output/Release/$platform/Pe/bin --self-contained true
+    # msbuild        Source/Pe.Boot/Pe.Boot.sln                          /p:Configuration=Release                          /p:Platform=$platform /p:DefineConstants=$buildType
+    # dotnet build   Source/Pe/Pe.sln                  --verbosity normal --configuration Release --runtime win-$platform  /p:Platform=$platform /p:DefineConstants=$buildType
+    # dotnet publish Source/Pe/Pe.Main/Pe.Main.csproj  --verbosity normal --configuration Release --runtime win-$platform  /p:Platform=$platform /p:DefineConstants=$buildType --output Output/Release/$platform/Pe/bin --self-contained true
 } finally {
+    git reset --hard
     Pop-Location
 }
