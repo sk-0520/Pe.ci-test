ca07627317052f193ebbc2e44c85ec1be6b418d5
sk <content.type.text.net@gmail.com>
2020-01-26 13:10:26 +0900

    層を薄めにした

--- Build/build.ps1
+++ Build/build.ps1
@@ -2,20 +2,24 @@
 $currentDirPath = Split-Path -Parent $MyInvocation.MyCommand.Path
 
 $scriptFileNames = @(
-    'test-command.ps1',
+    'command.ps1',
     'buildvar-appveyor.ps1',
-    'buildvar.ps1',
-    'scm-git.ps1',
-    'scm.ps1'
+    'buildvar.ps1'
 );
 foreach ($scriptFileName in $scriptFileNames) {
     $scriptFilePath = Join-Path $currentDirPath $scriptFileName
     . $scriptFilePath
 }
+
+Set-Command 'git' 'BUILD_GIT_PATH' "%PROGRAMFILES%\git\bin"
+Set-Command 'msbuild' 'BUILD_MSBUILD_PATH' "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin"
+
+Write-Output ("git: " + (git --version))
+Write-Output ("msbuild: " + (msbuild -version -noLogo ))
+
 $buildVariables = Get-BuildVariable
 Write-Output $buildVariables
 
 # SCM 的に現行状態に未コミットがあれば死ぬ
-Initialize-Scm
 
-echo (Get-ChangedScm)
+
--- Build/scm.ps1
+++
@@ -1,8 +0,0 @@
-
-function Initialize-Scm {
-    Initialize-Scm_Git
-}
-
-function Get-ChangedScm {
-    return Get-ChangedScm_Git
-}
