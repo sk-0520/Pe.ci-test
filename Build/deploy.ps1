8e1858049300c18a76414ea32c72bd4c28a82774
sk <content.type.text.net@gmail.com>
2020-02-15 20:58:53 +0900

    送信できるかねー

---
+++ Build/deploy.ps1
@@ -0,0 +1,17 @@
+Param(
+    [parameter(mandatory = $true)][ValidateSet("bitbucket")][string] $TargetRepository,
+    [parameter(mandatory = $true)][string] $DeployRootDirectory,
+    [parameter(mandatory = $true)][string] $DeployApiDownloadUrl,
+    [parameter(mandatory = $true)][string] $DeployApiTagUrl,
+    [parameter(mandatory = $true)][string] $DeployAccount,
+    [parameter(mandatory = $true)][string] $DeployPassword
+)
+$ErrorActionPreference = 'Stop'
+
+$password = ConvertTo-SecureString $DeployPassword -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential($DeployAccount, $password)
+
+$archiveItems = Get-ChildItem -Path $DeployRootDirectory -Filter "*.zip"
+foreach($archiveItem in $archiveItems) {
+    Invoke-RestMethod -Uri $DeployApiDownloadUrl -Method "POST" -InFile $archiveItem -Credential $credential
+}
