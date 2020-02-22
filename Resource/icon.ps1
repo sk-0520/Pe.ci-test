e0bec390278326a236967956e95e4e3684e327bb
sk <content.type.text.net@gmail.com>
2020-02-22 14:18:14 +0900

    バッチ処理を ps に移植

---
+++ Resource/icon.ps1
@@ -0,0 +1,131 @@
+﻿Param(
+	[string] $FirstInput = '',
+	[switch] $OneLife = ''
+)
+$ErrorActionPreference = 'Stop'
+Set-StrictMode -Version Latest
+$currentDirPath = Split-Path -Parent $MyInvocation.MyCommand.Path
+$iconDirPath = Join-Path $currentDirPath 'Icon'
+$workDirPath = Join-Path $iconDirPath '@work'
+
+$exeIncspace = if ($env:INKSCAPE) { $env:INKSCAPE } else { [Environment]::ExpandEnvironmentVariables('%PROGRAMFILES%\Inkscape\inkscape.exe') }
+$exeImageMagic = if ($env:IMAGEMAGIC) { $env:IMAGEMAGIC } else { [Environment]::ExpandEnvironmentVariables('C:\Applications\tool\ImageMagick\convert.exe') }
+
+$appIcons = @(
+	@{
+		name  = 'app-release'
+		color = '#ffffff'
+	}
+	@{
+		name  = 'app-debug'
+		color = '#8888ff'
+	}
+	@{
+		name  = 'app-beta'
+		color = '#ff8888'
+	}
+)
+$iconSize = @(16, 32)
+
+
+
+
+function ConvertAppSvgToPng() {
+	# App.svg から release(そのまま), debug, beta を生成
+	$appPath = Join-Path $iconDirPath 'App.svg'
+	$appXml = [xml](Get-Content $appPath -Raw -Encoding UTF8)
+
+	$nodes = @(
+		$appXml.SelectSingleNode('//*[@id="backgroundLinearGradientStart"]')
+		$appXml.SelectSingleNode('//*[@id="backgroundLinearGradientEnd"]')
+	)
+
+	New-Item -Path $workDirPath -ItemType Directory -Force | Out-Null
+
+	$savedPaths = @()
+	foreach ($appIcon in $appIcons) {
+		foreach ($node in $nodes) {
+			$style = $node.Attributes['style'].Value
+			$node.Attributes['style'].Value = $style -replace "stop-color:(#[0-9a-f]{6})", "stop-color:$($appIcon.color)"
+		}
+		$savePath = Join-Path $workDirPath "$($appIcon.name).svg"
+		$appXml.Save($savePath)
+		$savedPaths += $savePath
+	}
+	foreach ($savePath in $savedPaths) {
+		ConvertSvgToPng "$savePath"
+	}
+}
+
+function PackAppIcon {
+	foreach ($appIcon in $appIcons) {
+		$pattern = "$($appIcon.name)_*.png"
+		$outputPath = Join-Path $workDirPath "$($appIcon.name).ico"
+		PackIcon $workDirPath $pattern $outputPath
+	}
+}
+
+function ConvertSvgToPng([string] $srcSvgPath) {
+	$pngBasePath = Join-Path (Split-Path -Parent $srcSvgPath) ([System.IO.Path]::GetFileNameWithoutExtension($srcSvgPath))
+	Write-Host "[SRC] $srcSvgPath";
+	foreach ($size in $iconSize) {
+		$pngPath = "${pngBasePath}_${size}.png"
+		Write-Host "   -> $pngPath"
+		& $exeIncspace `
+			--file=$srcSvgPath `
+			--export-dpi=96 `
+			--export-width=$size `
+			--export-height=$size `
+			--export-png=$pngPath
+	}
+}
+
+function PackIcon([string] $directoryPath, [string] $pngPattern, [string] $outputPath) {
+	Write-Host "$directoryPath $pngPattern"
+	$inputFiles = (
+		Get-ChildItem -Path $directoryPath -Filter $pngPattern -File `
+		| Select-Object -ExpandProperty FullName
+	)
+	Write-Host $inputFiles
+	& $exeImageMagic $inputFiles $outputPath
+}
+
+
+
+while ($true) {
+	Write-Host "0: Pe: SVG -> PNG"
+	Write-Host "1: Pe: PNG -> ICO"
+	Write-Host "x: 終了"
+	if ($FirstInput) {
+		$inputValue = $FirstInput
+		$FirstInput = ''
+	}
+ else {
+		$inputValue = Read-Host "処理"
+	}
+	try {
+		switch ($inputValue) {
+			'1' {
+				ConvertAppSvgToPng
+			}
+			'2' {
+				PackAppIcon
+			}
+			'x' {
+				exit 0;
+			}
+			default {
+				Write-Host "[$inputValue] は未定義"
+			}
+		}
+	}
+ catch {
+		Write-Host $Error[0] -ForegroundColor Red -BackgroundColor Black
+	}
+	Write-Host ''
+	if ($OneLife) {
+		exit 0
+	}
+}
+
+
