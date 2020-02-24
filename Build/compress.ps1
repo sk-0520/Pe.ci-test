87a1f378561582a765f09c03dba46d43ae80e0f4
sk <content.type.text.net@gmail.com>
2020-02-24 14:54:41 +0900

    多分不要なディレクトリ指定を破棄

--- Build/compress.ps1
+++ Build/compress.ps1
@@ -1,6 +1,5 @@
 ﻿Param(
 	[switch] $Diet,
-	[parameter(mandatory = $true)][string] $SourceDirectory,
 	[parameter(mandatory = $true)][ValidateSet("zip", "7z")][string] $Archive,
 	[parameter(mandatory = $true)][string[]] $Platforms
 )
@@ -21,20 +20,21 @@ $version = GetAppVersion
 
 foreach($platform in $Platforms) {
 	$archiveFileName = (ConvertAppArchiveFileName $version $platform $Archive)
+	$binDirPath = "Output\Release\$platform\Pe"
 
 	switch ($Archive) {
 		'zip' {
 			$destinationPath = Join-Path 'Output' $archiveFileName
-			Compress-Archive -Force -Path (Join-Path $SourceDirectory "*") -DestinationPath $destinationPath
+			Compress-Archive -Force -Path (Join-Path $binDirPath "*") -DestinationPath $destinationPath
 		}
 		'7z' {
 			try {
-				Push-Location $SourceDirectory
+				Push-Location $binDirPath
 				7z a -t7z -m0=lzma2 -mx=9 -mfb=64 -md=64m -ms=on "$archiveFileName" * -r
 			} finally {
 				Pop-Location
 			}
-			Move-Item -Path (Join-Path $SourceDirectory $archiveFileName) -Destination 'Output'
+			Move-Item -Path (Join-Path $binDirPath $archiveFileName) -Destination 'Output'
 		}
 	}
 }
