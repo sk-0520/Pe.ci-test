4f47f505ed863608fea781bdf32a70f012f6c77f
sk <content.type.text.net@gmail.com>
2020-02-26 07:38:02 +0900

    パスを優先

--- Source/Pe/Pe.Main/Models/WebView/WebViewinItializer.cs
+++ Source/Pe/Pe.Main/Models/WebView/WebViewinItializer.cs
@@ -51,7 +51,7 @@ namespace ContentTypeTextNet.Pe.Main.Models.WebView
             }
             var crtDir = Path.Combine(binDirPath, "lib", "Redist.MSVC.CRT", ProcessArchitecture.ApplicationArchitecture);
 
-            var addPath = orgPath + ";" + crtDir;
+            var addPath = crtDir + ";" + orgPath;
             Logger.LogDebug("新規PATH: {0}", addPath);
             Environment.SetEnvironmentVariable("PATH", crtDir);
         }
