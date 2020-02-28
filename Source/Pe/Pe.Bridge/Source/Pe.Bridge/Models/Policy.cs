f92d713548ff4c6992baf22cf609288097b31329
sk <content.type.text.net@gmail.com>
2020-02-28 18:28:53 +0900

    IPolicy

---
+++ Source/Pe/Pe.Bridge/Source/Pe.Bridge/Models/Policy.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContentTypeTextNet.Pe.Bridge.Models
+{
+    //TODO: Pe 側で Polly を使ったような処理を隠蔽したい想いの実装
+
+    public interface IPolicy
+    {
+        #region function
+        #endregion
+    }
+}
