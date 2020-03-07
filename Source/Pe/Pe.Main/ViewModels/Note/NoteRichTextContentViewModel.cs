be0a5344a1a82e1e9b2f2caea4fc2492d8db298c
sk <content.type.text.net@gmail.com>
2020-03-07 20:24:14 +0900

    選択中のあれこれ対応

--- Source/Pe/Pe.Main/ViewModels/Note/NoteRichTextContentViewModel.cs
+++ Source/Pe/Pe.Main/ViewModels/Note/NoteRichTextContentViewModel.cs
@@ -10,6 +10,7 @@ using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Media;
 using ContentTypeTextNet.Pe.Bridge.Models;
 using ContentTypeTextNet.Pe.Core.Models;
 using ContentTypeTextNet.Pe.Main.Models.Element.Note;
@@ -26,6 +27,11 @@ namespace ContentTypeTextNet.Pe.Main.ViewModels.Note
 
         bool _isOpenToolbar;
 
+        FontFamily? _selectionFontFamily;
+        decimal _selectionFontHeight;
+        Color _foregroundColor;
+        Color _backgroundColor;
+
         #endregion
 
         public NoteRichTextContentViewModel(NoteContentElement model, IClipboardManager clipboardManager, IDispatcherWrapper dispatcherWrapper, ILoggerFactory loggerFactory)
@@ -37,9 +43,9 @@ namespace ContentTypeTextNet.Pe.Main.ViewModels.Note
         #region property
 
         //Xceed.Wpf.Toolkit.RichTextBox Control { get; set; }
-        RichTextBox? RichTextBox { get; set; }
-        FlowDocument Document => RichTextBox?.Document ?? throw new NullReferenceException(nameof(RichTextBox));
-        Popup? Popup { get; set; }
+        RichTextBox? ContentElement { get; set; }
+        FlowDocument Document => ContentElement?.Document ?? throw new NullReferenceException(nameof(ContentElement));
+        Popup? PopupElement { get; set; }
 
         LazyAction TextChangeLazyAction { get; }
 
@@ -49,12 +55,105 @@ namespace ContentTypeTextNet.Pe.Main.ViewModels.Note
             set => SetProperty(ref this._isOpenToolbar, value);
         }
 
+        bool NowSelectionProcess { get; set; }
+
+        public FontFamily? SelectionFontFamily
+        {
+            get => this._selectionFontFamily;
+            set
+            {
+                SetProperty(ref this._selectionFontFamily, value);
+                if(!NowSelectionProcess) {
+                    ApplySelectionFontFamily();
+                }
+            }
+        }
+
+        public decimal SelectionFontHeight
+        {
+            get => this._selectionFontHeight;
+            set
+            {
+                SetProperty(ref this._selectionFontHeight, value);
+                if(!NowSelectionProcess) {
+                    ApplySelectionFontHeight();
+                }
+            }
+        }
+
+        public Color SelectionForegroundColor
+        {
+            get => this._foregroundColor;
+            set
+            {
+                SetProperty(ref this._foregroundColor, value);
+                if(!NowSelectionProcess) {
+                    ApplySelectionForegroundColor();
+                }
+            }
+        }
+
+        public Color SelectionBackgroundColor
+        {
+            get => this._backgroundColor;
+            set
+            {
+                SetProperty(ref this._backgroundColor, value);
+                if(!NowSelectionProcess) {
+                    ApplySelectionBackgroundColor();
+                }
+            }
+        }
+
+
         #endregion
 
         #region command
         #endregion
 
         #region function
+
+        private void ApplySelectionCore(Action<RichTextBox, TextSelection> action)
+        {
+            if(ContentElement == null) {
+                return;
+            }
+            if(ContentElement.Selection.IsEmpty) {
+                return;
+            }
+            action(ContentElement, ContentElement.Selection);
+        }
+        private void ApplySelectionPropertyValue(DependencyProperty dependencyProperty, object value)
+        {
+            ApplySelectionCore((r, s) => s.ApplyPropertyValue(dependencyProperty, value));
+        }
+
+        private void ApplySelectionFontFamily()
+        {
+            if(SelectionFontFamily != null) {
+                ApplySelectionPropertyValue(Run.FontFamilyProperty, SelectionFontFamily);
+            }
+        }
+
+        private void ApplySelectionFontHeight()
+        {
+            if(0 < SelectionFontHeight) {
+                ApplySelectionPropertyValue(Run.FontSizeProperty, Convert.ToDouble(SelectionFontHeight));
+            }
+        }
+
+        private void ApplySelectionForegroundColor()
+        {
+            var brush = FreezableUtility.GetSafeFreeze(new SolidColorBrush(SelectionForegroundColor));
+            ApplySelectionPropertyValue(Run.ForegroundProperty, brush);
+        }
+
+        private void ApplySelectionBackgroundColor()
+        {
+            var brush = FreezableUtility.GetSafeFreeze(new SolidColorBrush(SelectionBackgroundColor));
+            ApplySelectionPropertyValue(Run.ForegroundProperty, brush);
+        }
+
         #endregion
 
         #region NoteContentViewModelBase
@@ -62,11 +161,11 @@ namespace ContentTypeTextNet.Pe.Main.ViewModels.Note
         protected override Task LoadContentAsync(FrameworkElement baseElement)
         {
             //Control = (Xceed.Wpf.Toolkit.RichTextBox)control;
-            RichTextBox = (RichTextBox)baseElement.FindName("content");
-            Popup = (Popup)baseElement.FindName("popup");
+            ContentElement = (RichTextBox)baseElement.FindName("content");
+            PopupElement = (Popup)baseElement.FindName("popup");
 
-            RichTextBox.TextChanged += Control_TextChanged;
-            RichTextBox.SelectionChanged += RichTextBox_SelectionChanged;
+            ContentElement.TextChanged += Control_TextChanged;
+            ContentElement.SelectionChanged += RichTextBox_SelectionChanged;
 
             return Task.Run(() => {
                 var content = Model.LoadRichTextContent();
@@ -90,15 +189,15 @@ namespace ContentTypeTextNet.Pe.Main.ViewModels.Note
         {
             Flush();
 
-            if(RichTextBox == null) {
+            if(ContentElement == null) {
                 return;
             }
 
-            RichTextBox.TextChanged -= Control_TextChanged;
-            RichTextBox.SelectionChanged -= RichTextBox_SelectionChanged;
+            ContentElement.TextChanged -= Control_TextChanged;
+            ContentElement.SelectionChanged -= RichTextBox_SelectionChanged;
 
-            Popup = null;
-            RichTextBox = null;
+            PopupElement = null;
+            ContentElement = null;
             IsOpenToolbar = false;
         }
 
@@ -158,24 +257,43 @@ namespace ContentTypeTextNet.Pe.Main.ViewModels.Note
         private void RichTextBox_SelectionChanged(object sender, RoutedEventArgs e)
         {
             // ツールバー的なの何かできればいいなぁと
-            Debug.Assert(RichTextBox != null);
-            Debug.Assert(Popup != null);
+            Debug.Assert(ContentElement != null);
+            Debug.Assert(PopupElement != null);
 
-            if(RichTextBox.Selection.IsEmpty) {
+            NowSelectionProcess = true;
+            using var _cleanup_ = new ActionDisposer(() => NowSelectionProcess = false);
+
+            if(ContentElement.Selection.IsEmpty) {
+                // 未選択。さよなら
                 IsOpenToolbar = false;
                 return;
             }
 
-            // Popup.Placement = PlacementMode.AbsolutePoint;
-            var startRect = RichTextBox.Selection.Start.GetCharacterRect(LogicalDirection.Forward);
-            var endRect = RichTextBox.Selection.End.GetCharacterRect(LogicalDirection.Forward);
+            // 選択状態から表示可能項目の抜出
+            var fontFamliyProperty = ContentElement.Selection.GetPropertyValue(RichTextBox.FontFamilyProperty);
+            var fontSizeProperty = ContentElement.Selection.GetPropertyValue(RichTextBox.FontSizeProperty);
+            Logger.LogDebug("fontFamliyProperty: {0}", fontFamliyProperty);
+            Logger.LogDebug("fontSizeProperty: {0}", fontSizeProperty);
+
+            if(fontFamliyProperty == DependencyProperty.UnsetValue) {
+                SelectionFontFamily = null;
+            } else {
+                SelectionFontFamily = (FontFamily)fontFamliyProperty;
+            }
+
+            if(fontSizeProperty != DependencyProperty.UnsetValue) {
+                // 複数サイズの場合は直近のを表示しとくしかないなぁ(自家製 NumericUpDown が値無しを表示できるほど柔軟じゃない)
+                SelectionFontHeight = Convert.ToDecimal(fontSizeProperty);
+            }
+
+            // 表示位置補正
+            var startRect = ContentElement.Selection.Start.GetCharacterRect(LogicalDirection.Forward);
+            var endRect = ContentElement.Selection.End.GetCharacterRect(LogicalDirection.Forward);
             var rect = new Rect(
-                RichTextBox.PointToScreen(new Point(startRect.X, startRect.Y + startRect.Height)),
+                ContentElement.PointToScreen(new Point(startRect.X, startRect.Y + startRect.Height)),
                 new Size(endRect.Width, endRect.Height)
             );
-
-            Popup.PlacementRectangle = rect;
-            Logger.LogInformation(ObjectDumper.GetDumpString(rect));
+            PopupElement.PlacementRectangle = rect;
 
             IsOpenToolbar = true;
         }
