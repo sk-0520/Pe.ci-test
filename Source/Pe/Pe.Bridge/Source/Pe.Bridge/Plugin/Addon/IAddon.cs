3f653a8f33d5d15b36b1c6b527e7a7cf517aac35
sk <content.type.text.net@gmail.com>
2020-03-10 17:34:17 +0900

    💀

--- Source/Pe/Pe.Bridge/Source/Pe.Bridge/Plugin/Addon/IAddon.cs
+++ Source/Pe/Pe.Bridge/Source/Pe.Bridge/Plugin/Addon/IAddon.cs
@@ -4,7 +4,33 @@ using System.Text;
 
 namespace ContentTypeTextNet.Pe.Bridge.Plugin.Addon
 {
+    public enum AddonKind
+    {
+        /// <summary>
+        /// ランチャーアイテムとして処理可能アドオン。
+        /// </summary>
+        LauncherItem,
+        /// <summary>
+        /// コマンド入力として処理可能アドオン。
+        /// <para><see cref="LauncherItem"/>では登録内容がコマンドに表示される可能性があるが、こちらは完全に独立した何か。</para>
+        /// </summary>
+        CommandFinder,
+        /// <summary>
+        /// ウィジェットとして処理可能アドオン。
+        /// <para>1 プラグインにつき、 1 ウィンドウを想定。</para>
+        /// </summary>
+        Widget,
+        /// <summary>
+        /// 後ろでなんかしてるやつ。
+        /// </summary>
+        Background,
+    }
+
     public interface IAddon: IPlugin
     {
+        #region function
+
+
+        #endregion
     }
 }
