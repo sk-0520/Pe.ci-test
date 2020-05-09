23900ea2107130e604c45e8a1764cd93a0d7d0dd
sk <content.type.text.net@gmail.com>
2020-05-09 20:08:16 +0900

    0.95.000

--- Source/Documents/source/script/changelogs.ts
+++ Source/Documents/source/script/changelogs.ts
@@ -91,52 +91,30 @@ const changelogs = [
 	*/
 	/*--------RELEASE HEAD--------*/
 	{
-		"date": "YYYY/MM/DD",
-		"version": "0.94.001",
+		"date": "2020/05/09",
+		"version": "0.95.000",
 		"contents": [
-			{
-				"type": "note",
-				"logs": [
-					{
-						"revision": "",
-						"class": "",
-						"subject": ""
-					},
-					{
-						"revision": "",
-						"subject": ""
-					},
-					{
-						"revision": "",
-						"subject": ""
-					}
-				]
-			},
 			{
 				"type": "features",
 				"logs": [
 					{
-						"revision": "",
+						"revision": "6f2f4f6729441b57ce3e6fe5963bfd8e0bdd9d98",
 						"subject": "#603: マウスクリックでキーボード入力待機を解除する"
 					},
 					{
-						"revision": "",
+						"revision": "93b3df1af98152dc7b87f8104ddbd2b156ef7ae0",
 						"subject": "#531: 本体用特別コマンドの実装",
 						"comments": [
 							"コマンド入力時に先頭が「.」の場合に本体用コマンドとして扱うようにした"
 						]
 					},
 					{
-						"revision": "",
+						"revision": "8a646840355b1d79f6957d6753809bc703a033c3",
 						"subject": "#613: ノート内でタブを入力できるようにする"
 					},
 					{
-						"revision": "",
+						"revision": "d05797710abb575c8c141cc8a328a2716ae0e66e",
 						"subject": "#602: キーボード設定をキー入力から行えるようにする"
-					},
-					{
-						"revision": "",
-						"subject": ""
 					}
 				]
 			},
@@ -144,51 +122,43 @@ const changelogs = [
 				"type": "fixes",
 				"logs": [
 					{
-						"revision": "",
+						"revision": "76d8addc5d42c0417694c18696836355f784433f",
 						"subject": "#601: コマンド型ランチャーの横幅が保存されてない",
 						"comments": [
 							"正確には保存されてたんだけど保存値がちょっと頭おかしかった"
 						]
 					},
 					{
-						"revision": "",
+						"revision": "3e03ec1a35538ed9b12f4a39a5891011f159d7f4",
 						"subject": "#607: ヘルプのメニュースクロール位置がリンク遷移時にリセットされる"
 					},
 					{
-						"revision": "",
+						"revision": "9862776bd0a0c63cdce79cdd5f1ca0ad0f625694",
 						"subject": "#610: アイコンのあるコントロール系UIが二重のタブ移動対象になっている"
 					},
 					{
-						"revision": "",
+						"revision": "fc09c9b8e1f0a6ef91a19b1aef17587d4ac73023",
 						"subject": "#604: 文言をもうちっと分かり易くする"
 					},
 					{
-						"revision": "",
+						"revision": "b7e7e86052a226c9ce8c9fa22ebdb0438338fdb9",
 						"subject": "#606: 毎月1日のクッソしょうもないアイコン切り替えが常時稼働状態だと切り替わらない"
 					},
 					{
-						"revision": "",
+						"revision": "c59fdf04ac39dd4000d04bc673df0a5538147f72",
 						"subject": "#614: ランチャーアイコンが保存されていない疑惑"
 					},
 					{
-						"revision": "",
+						"revision": "86b7817b0f649fb5ecbdafb58c9a097a37405558",
 						"subject": "#615: 本体ディレクトリ読み込み時に不要なディレクトリが作成される"
 					},
 					{
-						"revision": "",
+						"revision": "60119f359108eb67945ec06d68ff8929a618f50d",
 						"subject": "#605: ランチャーアイテム修正時にコマンド型ランチャーに即時反映されない"
 					},
 					{
-						"revision": "",
+						"revision": "c2bedc4ae10af111a0b1e59e676ab2a98efba739",
 						"subject": "アイコン制御処理SQLが上手くいってなかった"
-					},
-					{
-						"revision": "",
-						"subject": ""
-					},
-					{
-						"revision": "",
-						"subject": ""
 					}
 				]
 			},
@@ -196,28 +166,28 @@ const changelogs = [
 				"type": "developer",
 				"logs": [
 					{
-						"revision": "",
+						"revision": "ea659d24ad4f329f09fc15b713e10a5aee0106fc",
 						"subject": "フック処理の登録処理を初期化から若干ずらした"
 					},
 					{
-						"revision": "",
+						"revision": "ee01edf5db43db7d66f233f7361bbb42feda86a4",
 						"subject": "#608: UserControl のバインド周りを調整"
 					},
 					{
-						"revision": "",
+						"revision": "8f0bc8a95e3cc8b57121c108f026a45cb897b81a",
 						"class": "nuget",
 						"subject": "System.Data.SQLite.Core: 1.0.112.1 -> 1.0.112.2"
 					},
 					{
-						"revision": "",
+						"revision": "4b11f25409c3a27f462e8895993cad8302e49340",
 						"subject": "#584: 0.95.000 公開時時に 0.83.0-0.90.000 からのアップデートサポートを破棄"
 					},
 					{
-						"revision": "",
+						"revision": "60e254596d40fc626f698d8e0bbb9044c959d876",
 						"subject": "#616: Dao と内部実装SQL読み込み処理に対する事故防止対策委員会"
 					},
 					{
-						"revision": "",
+						"revision": "518650078c359bfabe454b7db2e2cb32cb850b28",
 						"subject": "過去バージョンはもう tag から適当に再現してくれ"
 					}
 				]
