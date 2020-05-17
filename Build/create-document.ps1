ccd44f4db28b916f041928c863318b53e6c49981
sk <content.type.text.net@gmail.com>
2020-05-17 16:12:05 +0900

    これはもう webpack 入れた方がいいのでは・・・

--- Build/create-document.ps1
+++ Build/create-document.ps1
@@ -16,6 +16,19 @@ $rootDirectoryPath = Split-Path -Parent $currentDirPath
 $documentDirectoryPath = Join-Path $rootDirectoryPath 'Source\Documents'
 $buildOutputDirectoryPath = Join-Path $documentDirectoryPath 'build'
 
+$documentScriptDirectoryPath = Join-Path $documentDirectoryPath 'source\script'
+$changelogCurrentFilePath = Join-Path $documentScriptDirectoryPath 'changelogs.ts'
+$changelogArchiveFilePath = Join-Path $documentScriptDirectoryPath 'changelogs-archive.json'
+
+$changelogCurrentContent = Get-Content -Path $changelogCurrentFilePath -Raw -Encoding UTF8
+$changelogArchiveContent = Get-Content -Path $changelogArchiveFilePath -Raw -Encoding UTF8
+
+$embeddedMark = '/*--------BUILD-EMBEDDED-JSON--------*/'
+
+$embeddedScript = "changelogs.push($changelogArchiveContent);"
+
+$changelogNewContent = $changelogCurrentContent.Replace($embeddedMark, $embeddedScript)
+Set-Content $changelogCurrentFilePath -Value $changelogNewContent -Encoding UTF8
 
 try{
 	Push-Location $documentDirectoryPath
