ed0dcbc89c49f6373b4cafc0bd817266022b0cc3
sk <content.type.text.net@gmail.com>
2020-06-10 12:30:08 +0900

    ウィジェット用のIF作ったけど中身は考え中

---
+++ Source/Pe/Pe.Bridge/Source/Pe.Bridge/Plugin/Addon/IWidget.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContentTypeTextNet.Pe.Bridge.Plugin.Addon
+{
+    /// <summary>
+    /// ウィジェット！
+    /// </summary>
+    public interface IWidget
+    {
+    }
+}
