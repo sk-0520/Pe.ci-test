3fa4ca770d56e809c2fd621666de50c62f7b7b13
sk <content.type.text.net@gmail.com>
2020-06-12 17:07:10 +0900

    チープ時計

--- Source/Plugins/Pe.Plugins.Clock/ViewModels/ClockWidgetViewModel.cs
+++ Source/Plugins/Pe.Plugins.Clock/ViewModels/ClockWidgetViewModel.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Threading;
 using ContentTypeTextNet.Pe.Bridge.ViewModels;
 using ContentTypeTextNet.Pe.Plugins.Clock.Models.Data;
 using Microsoft.Extensions.Logging;
@@ -13,15 +14,25 @@ namespace ContentTypeTextNet.Pe.Plugins.Clock.ViewModels
 
         DateTime _currentTime;
 
+        double _hourAngle;
+        double _minutesAngle;
+        double _secondsAngle;
+
         #endregion
+
         public ClockWidgetViewModel(ClockWidgetSetting setting, ISkeletonImplements skeletonImplements, ILoggerFactory loggerFactory)
             : base(skeletonImplements, loggerFactory)
         {
             Setting = setting;
-
+            ClockTimer = new DispatcherTimer(DispatcherPriority.Normal);
+            ClockTimer.Tick += ClockTimer_Tick;
         }
 
         #region property
+
+        ClockWidgetSetting Setting { get; }
+
+        DispatcherTimer ClockTimer { get; }
 
         public DateTime CurrentTime
         {
@@ -29,16 +40,52 @@ namespace ContentTypeTextNet.Pe.Plugins.Clock.ViewModels
             set => SetProperty(ref this._currentTime, value);
         }
 
-        public ClockWidgetSetting Setting { get; }
+        public ClockWidgetKind ClockWidgetKind => Setting.ClockWidgetKind;
+
+        public double HourAngle
+        {
+            get => this._hourAngle;
+            private set => SetProperty(ref this._hourAngle, value);
+        }
+        public double MinutesAngle
+        {
+            get => this._minutesAngle;
+            private set => SetProperty(ref this._minutesAngle, value);
+        }
+        public double SecondsAngle
+        {
+            get => this._secondsAngle;
+            private set => SetProperty(ref this._secondsAngle, value);
+        }
+
+
+        #endregion
+
+        #region function
 
         public void StartClock()
         {
-
+            ClockTimer.Interval = TimeSpan.FromMilliseconds(500);
+            ClockTimer.Start();
         }
 
         public void StopClock()
         {
 
+        }
+
+        private void ClockTimer_Tick(object? sender, EventArgs e)
+        {
+            ClockTimer.Stop();
+
+            CurrentTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, Setting.TimeZone);
+
+            SecondsAngle = CurrentTime.Second * 6;
+            MinutesAngle = CurrentTime.Minute * 6;
+            HourAngle = (CurrentTime.Hour * 30) + (CurrentTime.Minute * 0.5);
+
+            ClockTimer.Interval = TimeSpan.FromMilliseconds(TimeSpan.FromSeconds(1).TotalMilliseconds - CurrentTime.Millisecond);
+            ClockTimer.Start();
         }
 
         #endregion
