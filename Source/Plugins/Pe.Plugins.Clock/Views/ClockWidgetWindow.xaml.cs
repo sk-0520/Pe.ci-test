6f592fb80e63aec9eab852c6094eb32f609adadd
sk <content.type.text.net@gmail.com>
2020-06-13 15:18:51 +0900

    リサイズむずない？

--- Source/Plugins/Pe.Plugins.Clock/Views/ClockWidgetWindow.xaml.cs
+++ Source/Plugins/Pe.Plugins.Clock/Views/ClockWidgetWindow.xaml.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -18,6 +20,19 @@ namespace ContentTypeTextNet.Pe.Plugins.Clock.Views
     /// </summary>
     public partial class ClockWidgetWindow: Window
     {
+        #region define
+
+        const int WM_SYSCOMMAND = 0x0112;
+        const int SC_SIZE = 0xF000;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr SendMessage(IntPtr hWnd, int Msg, int wParam, IntPtr lParam);
+        [DllImport("User32.dll")]
+        static extern bool SetCapture(IntPtr hWnd);
+        [DllImport("user32.dll")]
+        static extern bool ReleaseCapture();
+
+        #endregion
         public ClockWidgetWindow()
         {
             InitializeComponent();
@@ -25,22 +40,49 @@ namespace ContentTypeTextNet.Pe.Plugins.Clock.Views
 
         #region proeprty
 
-        Storyboard? PrevStoryboard { get; set; }
+        //Storyboard? PrevStoryboard { get; set; }
+
+        //bool Dragging { get; set; }
 
         #endregion
 
-        private void Window_MouseEnter(object sender, MouseEventArgs e)
+        //private void Window_MouseEnter(object sender, MouseEventArgs e)
+        //{
+        //    ////PrevStoryboard?.Stop();
+        //    //PrevStoryboard = (Storyboard)FindResource("fadein");
+        //    //this.background.BeginStoryboard(PrevStoryboard);
+        //}
+
+        //private void Window_MouseLeave(object sender, MouseEventArgs e)
+        //{
+        //    //Debug.WriteLine(e.GetPosition(this));
+        //    //var element = VisualTreeHelper.HitTest(this.resize, e.GetPosition(this));
+        //    //if(element != null) {
+
+        //    //}
+        //    ////PrevStoryboard?.Stop();
+        //    //PrevStoryboard = (Storyboard)FindResource("fadeout");
+        //    //this.background.BeginStoryboard(PrevStoryboard);
+        //}
+
+        private void resize_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            PrevStoryboard?.Stop();
-            PrevStoryboard = (Storyboard)FindResource("fadein");
-            this.resize.BeginStoryboard(PrevStoryboard);
+            if(!IsEnabled) {
+                return;
+            }
+
+            var helper = new System.Windows.Interop.WindowInteropHelper(this);
+            var hWnd = helper.Handle;
+            SetCapture(hWnd);
+            ReleaseCapture();
+            e.Handled = true;
+
+            SendMessage(hWnd, WM_SYSCOMMAND, SC_SIZE + 8, IntPtr.Zero);
         }
 
-        private void Window_MouseLeave(object sender, MouseEventArgs e)
-        {
-            PrevStoryboard?.Stop();
-            PrevStoryboard = (Storyboard)FindResource("fadeout");
-            this.resize.BeginStoryboard(PrevStoryboard);
-        }
+        //private void resize_MouseMove(object sender, MouseEventArgs e)
+        //{
+
+        //}
     }
 }
