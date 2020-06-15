510623da28ee91ea708c0af4b821ca30d4980d05
sk <content.type.text.net@gmail.com>
2020-06-15 13:00:44 +0900

    引数のコンテキスト化

---
+++ Source/Pe/Pe.Bridge/Source/Pe.Bridge/Plugin/Addon/BackgroundAddonContext.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+using ContentTypeTextNet.Pe.Bridge.Models;
+
+namespace ContentTypeTextNet.Pe.Bridge.Plugin.Addon
+{
+    public interface IBackgroundAddonKeyboardContext
+    {
+        #region property
+
+        Key Key { get; }
+        bool IsDown { get; }
+
+        #endregion
+    }
+
+    public interface IBackgroundAddonMouseMoveContext
+    {
+        #region proeprty
+
+        /// <summary>
+        /// マウスカーソルの物理座標。
+        /// </summary>
+        [PixelKind(Px.Device)]
+        Point Location { get; }
+
+        #endregion
+    }
+
+    public interface IBackgroundAddonMouseButtonContext
+    {
+        #region proeprty
+
+        MouseButton Button { get; }
+        MouseButtonState State { get; }
+        bool IsDown { get; }
+
+        #endregion
+    }
+
+    public interface IBackgroundAddonDatabaseStatementContext
+    {
+        #region proeprty
+
+        /// <summary>
+        /// SQL文。他プラグインによる変更の可能性あり。
+        /// </summary>
+        string Statement { get; }
+        /// <summary>
+        /// パラメータ。
+        /// </summary>
+        object? Parameter { get; }
+
+        #endregion
+    }
+
+    public interface IBackgroundAddonDatabaseParameterContext
+    {
+        #region proeprty
+
+        /// <summary>
+        /// SQL文。他プラグインによる変更の可能性あり。
+        /// </summary>
+        string Statement { get; }
+        /// <summary>
+        /// パラメータ。
+        /// <para>直接こいつを書き換えるのではなく<see cref="ObjectCloner"/>を経由もしくは自力で複製して書き換え処理を行うこと。他プラグインによる変更の可能性あり。</para>
+        /// </summary>
+        object? Parameter { get; }
+        /// <summary>
+        /// パラメータ複製装置。
+        /// </summary>
+        Func<object, object> ObjectCloner { get; }
+
+        #endregion
+    }
+}
--- Source/Pe/Pe.Bridge/Source/Pe.Bridge/Plugin/Addon/IBackground.cs
+++ Source/Pe/Pe.Bridge/Source/Pe.Bridge/Plugin/Addon/IBackground.cs
@@ -52,52 +52,47 @@ namespace ContentTypeTextNet.Pe.Bridge.Plugin.Addon
         /// キーが押下された。
         /// <para>非同期で呼び出される。</para>
         /// </summary>
-        /// <param name="key"></param>
-        void HookKeyDown(Key key);
+        void HookKeyDown(IBackgroundAddonKeyboardContext backgroundAddonKeyboardContext);
         /// <summary>
         /// キーが離された。
         /// <para>非同期で呼び出される。</para>
         /// </summary>
-        /// <param name="key"></param>
-        void HookKeyUp(Key key);
+        void HookKeyUp(IBackgroundAddonKeyboardContext backgroundAddonKeyboardContext);
 
 
         /// <summary>
         /// マウスが移動した。
         /// <para>非同期で呼び出される。</para>
         /// </summary>
-        /// <param name="location">マウスカーソルの物理座標。</param>
-        void HookMouseMouve([PixelKind(Px.Device)] Point location);
+        void HookMouseMouve(IBackgroundAddonMouseMoveContext backgroundAddonMouseMoveContext);
         /// <summary>
         /// マウスのボタンが押された。
         /// <para>非同期で呼び出される。</para>
         /// </summary>
         /// <param name="mouseButton"></param>
         /// <param name="mouseButtonState"></param>
-        void HookMouseDown(MouseButton mouseButton, MouseButtonState mouseButtonState);
+        void HookMouseDown(IBackgroundAddonMouseButtonContext backgroundAddonMouseButtonContext);
         /// <summary>
         /// マウスのボタンが離された。
         /// <para>非同期で呼び出される。</para>
         /// </summary>
         /// <param name="mouseButton"></param>
         /// <param name="mouseButtonState"></param>
-        void HookMouseUp(MouseButton mouseButton, MouseButtonState mouseButtonState);
+        void HookMouseUp(IBackgroundAddonMouseButtonContext backgroundAddonMouseButtonContext);
 
         /// <summary>
         /// DBアクセス時にSQLを書き換える。
         /// </summary>
-        /// <param name="statement">SQL文。他プラグインによる変更の可能性あり。</param>
-        /// <param name="parameter">パラメータ。</param>
-        /// <returns>書き換え後のSQL文。</returns>
-        string HookDatabaseStatement(string statement, object? parameter);
+        /// <param name="backgroundAddonDatabaseStatementContext"></param>
+        // <returns>書き換え後のSQL文。</returns>
+        string HookDatabaseStatement(IBackgroundAddonDatabaseStatementContext backgroundAddonDatabaseStatementContext);
+
         /// <summary>
         /// DBアクセス時にパラメータを書き換える。
         /// </summary>
-        /// <param name="statement">SQL文。他プラグインによる変更の可能性あり。</param>
-        /// <param name="parameter">パラメータ。直接こいつを書き換えるのではなく<paramref name="objectCloner"/>を経由もしくは自力で複製して書き換え処理を行うこと。他プラグインによる変更の可能性あり。</param>
-        /// <param name="objectCloner"></param>
-        /// <returns>書き換え後のパラメータ。<paramref name="parameter"/>と同じであれば書き換えがなかったとみなされる。</returns>
-        object? HookDatabaseParameter(string statement, object? parameter, Func<object, object> objectCloner);
+        /// <param name="backgroundAddonDatabaseParameterContext"></param>
+        /// <returns>書き換え後のパラメータ。入力と同じ(参照)であれば書き換えがなかったとみなされる。</returns>
+        object? HookDatabaseParameter(IBackgroundAddonDatabaseParameterContext backgroundAddonDatabaseParameterContext);
 
 
         #endregion
