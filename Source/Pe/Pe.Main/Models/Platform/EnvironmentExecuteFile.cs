8a26fd174981c600bd32ecdb52d90ed654091be8
sk <content.type.text.net@gmail.com>
2020-07-02 23:15:30 +0900

    なじぇぇ

--- Source/Pe/Pe.Main/Models/Platform/EnvironmentExecuteFile.cs
+++ Source/Pe/Pe.Main/Models/Platform/EnvironmentExecuteFile.cs
@@ -4,7 +4,6 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.Extensions.Logging;
 
 namespace ContentTypeTextNet.Pe.Main.Models.Platform
