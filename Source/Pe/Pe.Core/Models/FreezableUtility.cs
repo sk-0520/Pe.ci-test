982102a4421c58be233f01cac05dd09725f9fc7e
sk <content.type.text.net@gmail.com>
2020-07-02 10:50:42 +0900

    FreezableUtility に対して null を許容した

--- Source/Pe/Pe.Core/Models/FreezableUtility.cs
+++ Source/Pe/Pe.Core/Models/FreezableUtility.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Text;
 using System.Windows;
 
@@ -16,8 +17,12 @@ namespace ContentTypeTextNet.Pe.Core.Models
         /// </summary>
         /// <param name="freezable"></param>
         /// <returns></returns>
-        public static bool SafeFreeze(Freezable freezable)
+        public static bool SafeFreeze(Freezable? freezable)
         {
+            if(freezable == null) {
+                return false;
+            }
+
             var result = freezable.CanFreeze;
             if(result) {
                 freezable.Freeze();
@@ -26,7 +31,17 @@ namespace ContentTypeTextNet.Pe.Core.Models
             return result;
         }
 
-        public static TFreezable GetSafeFreeze<TFreezable>(TFreezable freezable)
+        /// <summary>
+        /// 引数のオブジェクトに対して<see cref="SafeFreeze(Freezable?)"/>を実施したオブジェクトをそのまま返す。
+        /// </summary>
+        /// <code>
+        /// var brush = FreezableUtility.GetSafeFreeze(new SolidColorBrush(Colors.Red));
+        /// </code>
+        /// <typeparam name="TFreezable"></typeparam>
+        /// <param name="freezable"></param>
+        /// <returns></returns>
+        [return: NotNullIfNotNull("freezable")]
+        public static TFreezable? GetSafeFreeze<TFreezable>(TFreezable? freezable)
             where TFreezable : Freezable
         {
             SafeFreeze(freezable);
