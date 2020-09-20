7ad35e50acbd58e5fad7f4848e9ac10b00685ce7
sk <content.type.text.net@gmail.com>
2020-09-20 20:01:45 +0900

    GeneralConfiguration

--- Source/Pe/Pe.Main/Models/Applications/Configuration/GeneralConfiguration.cs
+++ Source/Pe/Pe.Main/Models/Applications/Configuration/GeneralConfiguration.cs
@@ -14,18 +14,18 @@ namespace ContentTypeTextNet.Pe.Main.Models.Applications.Configuration
             //LoggingConfigFileName = section.GetValue<string>("log_conf_file_name");
             SupportCultures = section.GetSection("support_cultures").Get<string[]>();
 
-            LicenseName = section.GetValue<string>("license_name");
-            LicenseUri = section.GetValue<Uri>("license_uri");
+            //LicenseName = section.GetValue<string>("license_name");
+            //LicenseUri = section.GetValue<Uri>("license_uri");
 
-            ProjectRepositoryUri = section.GetValue<Uri>("project_repository_uri");
-            ProjectForumUri = section.GetValue<Uri>("project_forum_uri");
-            ProjectWebSiteUri = section.GetValue<Uri>("project_website_uri");
-            UpdateCheckUri = section.GetValue<Uri>("version_check_uri");
-            UpdateWait = section.GetValue<TimeSpan>("update_wait");
-            DispatcherWait = section.GetValue<TimeSpan>("dispatcher_wait");
+            //ProjectRepositoryUri = section.GetValue<Uri>("project_repository_uri");
+            //ProjectForumUri = section.GetValue<Uri>("project_forum_uri");
+            //ProjectWebSiteUri = section.GetValue<Uri>("project_website_uri");
+            //UpdateCheckUri = section.GetValue<Uri>("version_check_uri");
+            //UpdateWait = section.GetValue<TimeSpan>("update_wait");
+            //DispatcherWait = section.GetValue<TimeSpan>("dispatcher_wait");
 
-            CanSendCrashReport = section.GetValue<bool>("can_send_crash_report");
-            UnhandledExceptionHandled = section.GetValue<bool>("unhandled_exception_handled");
+            //CanSendCrashReport = section.GetValue<bool>("can_send_crash_report");
+            //UnhandledExceptionHandled = section.GetValue<bool>("unhandled_exception_handled");
         }
 
         #region property
@@ -39,17 +39,27 @@ namespace ContentTypeTextNet.Pe.Main.Models.Applications.Configuration
 
         public IReadOnlyList<string> SupportCultures { get; }
 
-        public string LicenseName { get; }
-        public Uri LicenseUri { get; }
+        [Configuration]
+        public string LicenseName { get; } = default!;
+        [Configuration]
+        public Uri LicenseUri { get; } = default!;
 
-        public Uri ProjectRepositoryUri { get; }
-        public Uri ProjectForumUri { get; }
-        public Uri ProjectWebSiteUri { get; }
-        public Uri UpdateCheckUri { get; }
+        [Configuration]
+        public Uri ProjectRepositoryUri { get; } = default!;
+        [Configuration]
+        public Uri ProjectForumUri { get; } = default!;
+        [Configuration("project_website_uri")]
+        public Uri ProjectWebSiteUri { get; } = default!;
+        [Configuration("version_check_uri")]
+        public Uri UpdateCheckUri { get; } = default!;
+        [Configuration]
         public TimeSpan UpdateWait { get; }
+        [Configuration]
         public TimeSpan DispatcherWait { get; }
 
+        [Configuration]
         public bool CanSendCrashReport { get; }
+        [Configuration]
         public bool UnhandledExceptionHandled { get; }
 
         #endregion
