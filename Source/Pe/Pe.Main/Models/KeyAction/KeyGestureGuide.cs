fba6fd8d785d1394c841cb401b33628857c22a71
sk <content.type.text.net@gmail.com>
2020-10-03 23:38:44 +0900

    キー状態表示のために処理作るのもなぁ・・・

---
+++ Source/Pe/Pe.Main/Models/KeyAction/KeyGestureGuide.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ContentTypeTextNet.Pe.Core.Models.Database;
+using ContentTypeTextNet.Pe.Main.Models.Applications;
+using Microsoft.Extensions.Logging;
+
+namespace ContentTypeTextNet.Pe.Main.Models.KeyAction
+{
+    public interface IKeyGestureGuide
+    {
+        #region proeprty
+
+        #endregion
+
+        #region function
+
+        #endregion
+    }
+
+    public class KeyGestureGuide: IKeyGestureGuide
+    {
+        public KeyGestureGuide(IMainDatabaseBarrier mainDatabaseBarrier, IDatabaseStatementLoader databaseStatementLoader, ILoggerFactory loggerFactory)
+        {
+            Logger = loggerFactory.CreateLogger(GetType());
+            MainDatabaseBarrier = mainDatabaseBarrier;
+            DatabaseStatementLoader = databaseStatementLoader;
+        }
+
+        #region proeprty
+
+        ILogger Logger { get; }
+
+        IMainDatabaseBarrier MainDatabaseBarrier { get; }
+        IDatabaseStatementLoader DatabaseStatementLoader { get; }
+
+        #endregion
+
+        #region function
+
+        #endregion
+    }
+}
