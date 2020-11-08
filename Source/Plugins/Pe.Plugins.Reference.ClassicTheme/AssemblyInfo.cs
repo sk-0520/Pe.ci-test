98659a65f42b7d54d7e9ad7aefca3d804a69731d
sk <content.type.text.net@gmail.com>
2020-11-08 12:07:51 +0900

    プラグインの名前に関する制約を追加

--- Source/Plugins/Pe.Plugins.Reference.ClassicTheme/AssemblyInfo.cs
+++ Source/Plugins/Pe.Plugins.Reference.ClassicTheme/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Text;
 using ContentTypeTextNet.Pe.Bridge.Plugin;
 using ContentTypeTextNet.Pe.Embedded.Attributes;
 
-[assembly: PluginIdentifiers("Reference-ClassicTheme", "67F0FA7D-52D3-4889-B595-BE3703B224EB")]
+[assembly: PluginIdentifiers("Pe.Plugins.Reference.ClassicTheme", "67F0FA7D-52D3-4889-B595-BE3703B224EB")]
 [assembly: PluginSupportVersions("0.0.0", "0.0.0")]
 [assembly: PluginAuthors("sk", PluginLicense.DoWhatTheF_ckYouWantToPublicLicense2, "https://content-type-text.net/", "https://bitbucket.org/sk_0520/pe/wiki/Home")]
 [assembly: PluginCategory(PluginCategories.Design)]
