dce6193cba50a3caa94c115c48eb7b3c8c9508d9
sk <content.type.text.net@gmail.com>
2021-02-09 22:58:34 +0900

    DatabaseContextException を独立宣言

--- Source/Pe/Pe.Core/Models/Database/DatabaseContextExtensions.cs
+++ Source/Pe/Pe.Core/Models/Database/DatabaseContextExtensions.cs
@@ -7,18 +7,6 @@ using System.Text.RegularExpressions;
 
 namespace ContentTypeTextNet.Pe.Core.Models.Database
 {
-
-    [Serializable]
-    public class DatabaseContextException: Exception
-    {
-        public DatabaseContextException() { }
-        public DatabaseContextException(string message) : base(message) { }
-        public DatabaseContextException(string message, Exception inner) : base(message, inner) { }
-        protected DatabaseContextException(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context)
-            : base(info, context)
-        { }
-    }
-
     /// <summary>
     /// 読み込み処理の安全のしおり。
     /// </summary>
