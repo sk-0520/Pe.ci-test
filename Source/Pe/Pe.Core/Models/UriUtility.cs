c9f3eb734d47a887d832e694a662ca06ff7d4f88
sk <content.type.text.net@gmail.com>
2021-02-25 17:24:00 +0900

    UriUtility 整理

--- Source/Pe/Pe.Core/Models/UriUtility.cs
+++ Source/Pe/Pe.Core/Models/UriUtility.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Text;
 
 namespace ContentTypeTextNet.Pe.Core.Models
 {
@@ -8,27 +10,78 @@ namespace ContentTypeTextNet.Pe.Core.Models
     {
         #region function
 
-        public static string CombinePath(IEnumerable<string> hierarchies)
+        static Uri CombinePathCore(Uri uri, bool appendLastSeparator, string[] paths)
         {
-            return hierarchies.Aggregate((left, right) => left.TrimEnd('/') + "/" + right.TrimStart('/'));
+            Debug.Assert(paths.Any());
+
+            var noLastSegment = string.IsNullOrEmpty(uri.UserInfo)
+                ? $"{uri.Scheme}://{uri.Authority}"
+                : $"{uri.Scheme}://{uri.UserInfo}@{uri.Authority}"
+            ;
+            var builder = new StringBuilder();
+            foreach(var segment in uri.Segments) {
+                builder.Append(segment);
+            }
+            foreach(var path in paths) {
+                if(builder[builder.Length - 1] != '/') {
+                    builder.Append('/');
+                }
+                builder.Append(path.Trim('/'));
+            }
+            if(appendLastSeparator) {
+                if(builder[builder.Length - 1] != '/') {
+                    builder.Append('/');
+                }
+            }
+
+            if(!string.IsNullOrEmpty(uri.Query)) {
+                builder.Append(uri.Query);
+            }
+
+            return new Uri(noLastSegment + builder.ToString());
         }
 
-        public static Uri CombineUri(string baseUri, IEnumerable<string> hierarchies)
-        {
-            var list = new List<string>() {
-                baseUri,
-            };
-            list.AddRange(hierarchies);
-            //hierarchy
-            var originUri = CombinePath(list);
+        /// <summary>
+        /// <inheritdoc cref="CombinePath(Uri, bool, string, string[])"/>
+        /// <para>パス末尾に / は付与されない。</para>
+        /// </summary>
+        /// <inheritdoc cref="CombinePath(Uri, bool, string, string[])"/>
+        public static Uri CombinePath(Uri uri, string path, params string[] paths) => CombinePath(uri, false, path, paths);
 
-            return new Uri(originUri);
+        /// <summary>
+        /// 指定URLのパスを順に結合。
+        /// </summary>
+        /// <param name="uri">対象URI。</param>
+        /// <param name="appendLastSeparator">パス末尾に / を付与するか。</param>
+        /// <param name="path">結合するパス。</param>
+        /// <param name="paths">さらに結合するパス。</param>
+        /// <returns>結合されたURI。クエリは最後に付与される。</returns>
+        public static Uri CombinePath(Uri uri, bool appendLastSeparator, string path, params string[] paths)
+        {
+            if(uri == null) {
+                throw new ArgumentNullException(nameof(uri));
+            }
+            if(path == null) {
+                throw new ArgumentNullException(nameof(path));
+            }
+            if(paths == null) {
+                throw new ArgumentNullException(nameof(paths));
+            }
+            foreach(var p in paths) {
+                if(p == null) {
+                    throw new ArgumentNullException(nameof(paths));
+                }
+            }
+
+            var allPaths = new string[1 + paths.Length];
+            allPaths[0] = path;
+            for(var i = 0; i < paths.Length; i++) {
+                allPaths[i + 1] = paths[i];
+            }
+            return CombinePathCore(uri, appendLastSeparator, allPaths);
         }
 
-        public static Uri CombineUri(string baseUri, params string[] hierarchies) => CombineUri(baseUri, hierarchies.AsEnumerable<string>());
 
-        public static Uri CombineUri(Uri baseUri, IEnumerable<string> hierarchies) => CombineUri(baseUri.OriginalString, hierarchies);
-        public static Uri CombineUri(Uri baseUri, params string[] hierarchies) => CombineUri(baseUri, hierarchies.AsEnumerable<string>());
         #endregion
     }
 }
