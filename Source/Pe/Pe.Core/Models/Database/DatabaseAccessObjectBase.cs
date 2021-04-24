8538f6671e7259b6eb8c52a4920bea8c89dfa5a4
sk <content.type.text.net@gmail.com>
2021-04-24 20:35:12 +0900

    SQL加工処理のメソッドを作成

--- Source/Pe/Pe.Core/Models/Database/DatabaseAccessObjectBase.cs
+++ Source/Pe/Pe.Core/Models/Database/DatabaseAccessObjectBase.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using Microsoft.Extensions.Logging;
 
@@ -57,6 +58,36 @@ namespace ContentTypeTextNet.Pe.Core.Models.Database
             return StatementLoader.LoadStatementByCurrent(type, callerMemberName);
         }
 
+        /// <summary>
+        /// 指定条件に合わせて文を加工する。
+        /// </summary>
+        /// <example>
+        /// select *
+        /// from /*/!*//*KEY
+        /// KEY-A: { <paramref name="blocks"/>["KEY"] -> KEY-A
+        ///     TABLE_A 
+        /// }
+        /// KEY-B: { <paramref name="blocks"/>["KEY"] -> KEY-B
+        ///     TABLE_B
+        /// }
+        /// KEY-C: NAME <paramref name="blocks"/>["KEY"] -> KEY-C, callerMemberName.NAME.sql
+        /// */TABLE_C/*!/*/ <paramref name="blocks"/>["KEY"] not KEY-A,KEY-B,KEY-C
+        /// </example>
+        /// <param name="statement"></param>
+        /// <param name="blocks"></param>
+        /// <param name="callerMemberName"></param>
+        /// <returns></returns>
+        protected string ProcessStatement(string statement, IReadOnlyDictionary<string, string> blocks, [CallerMemberName] string callerMemberName = "")
+        {
+            if(string.IsNullOrEmpty(statement)) {
+                return statement;
+            }
+            if(blocks.Count == 0) {
+                return statement;
+            }
+
+            throw new NotImplementedException();
+        }
 
         #endregion
     }
