7e1218dee661e24d2974a7930644b4a91b89459a
sk <content.type.text.net@gmail.com>
2021-05-05 20:35:17 +0900

    #52

--- Source/Pe/Pe.Core/Models/SimpleKeyedCollection.cs
+++ Source/Pe/Pe.Core/Models/SimpleKeyedCollection.cs
@@ -4,6 +4,12 @@ using System.Collections.ObjectModel;
 
 namespace ContentTypeTextNet.Pe.Core.Models
 {
+    /// <summary>
+    /// <see cref="KeyedCollection{TKey, TItem}"/>をラムダで対応する暫定クラス。
+    /// <para>クラス内で完結する場合のみに使用する前提、複数個所で使用する場合はちゃんと<see cref="KeyedCollection{TKey, TItem}"/>の実装を作成すべき。</para>
+    /// </summary>
+    /// <typeparam name="TKey">コレクション内のキーの型。</typeparam>
+    /// <typeparam name="TValue">コレクション内の項目の型。</typeparam>
     public sealed class SimpleKeyedCollection<TKey, TValue>: KeyedCollection<TKey, TValue>
         where TKey : notnull
     {
