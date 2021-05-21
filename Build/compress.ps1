2e028a854d0ad838157a03ee8bf621d790f704d2
sk <content.type.text.net@gmail.com>
2021-05-22 01:24:35 +0900

    7z圧縮処理にプラグイン対応追加

--- Build/compress.ps1
+++ Build/compress.ps1
@@ -19,12 +19,12 @@ $builToolDirPath = Join-Path $rootDirectory "Output\tools"
 
 $version = GetAppVersion
 
-foreach($platform in $Platforms) {
+foreach ($platform in $Platforms) {
 	$archiveFileName = (ConvertAppArchiveFileName $version $platform $Archive)
 	$binRootDirPath = "Output\Release\$platform\Pe"
 	$pluginsRootDirPath = "Output\Release\$platform\Plugins"
 
-	if($Diet) {
+	if ($Diet) {
 		$removeTargets = @(
 			Get-ChildItem -Path $binRootDirPath -File -Filter "*.ilk"
 			Get-ChildItem -Path $binRootDirPath -File -Filter "*.iobj"
@@ -41,7 +41,7 @@ foreach($platform in $Platforms) {
 		$removeTargets += (Join-Path $binRootDirPath "bin\$unsupportPlatform")
 		$removeTargets += (Join-Path $binRootDirPath "bin\lib\Redist.MSVC.CRT\$unsupportPlatform")
 
-		foreach($removeTarget in $removeTargets) {
+		foreach ($removeTarget in $removeTargets) {
 			Write-Output "DIET: $removeTarget"
 			Remove-Item $removeTarget -Recurse -Force
 		}
@@ -50,7 +50,7 @@ foreach($platform in $Platforms) {
 		$sqlDilrPath = Join-Path $binRootDirPath "etc\sql"
 		$packSqlName = "sql.sqlite3"
 		$packSqlPath = Join-Path $sqlDilrPath $packSqlName
-		if(Test-Path $packSqlPath) {
+		if (Test-Path $packSqlPath) {
 			Remove-Item -Path $packSqlPath
 		}
 		& "$builToolDirPath\SqlPack.exe" --sql-root-dir $sqlDilrPath --output $packSqlPath
@@ -68,21 +68,30 @@ foreach($platform in $Platforms) {
 
 			Compress-Core $binRootDirPath $archiveFileName
 
-			foreach($pluginDir in $pluginDirs) {
+			foreach ($pluginDir in $pluginDirs) {
 				Compress-Core $pluginDir.FullName ($pluginDir.Name + '.' + $Archive)
 			}
 		}
 		'7z' {
-			try {
-				Push-Location $binRootDirPath
-				if(Test-Path "$archiveFileName") {
-					Remove-Item "$archiveFileName"
+			function Compress-Core([string] $directoryPath, [string] $outputFileName) {
+				try {
+					Push-Location "$directoryPath"
+					if (Test-Path "$outputFileName") {
+						Remove-Item "$outputFileName"
+					}
+					7z a -t7z -m0=lzma2 -mx=9 -mfb=64 -md=64m -ms=on -mmt=on "$outputFileName" * -r -bsp1
 				}
-				7z a -t7z -m0=lzma2 -mx=9 -mfb=64 -md=64m -ms=on -mmt=on "$archiveFileName" * -r -bsp1
-			} finally {
-				Pop-Location
+				finally {
+					Pop-Location
+				}
+				Move-Item -Path (Join-Path "$directoryPath" "$outputFileName") -Destination 'Output'
 			}
-			Move-Item -Path (Join-Path $binRootDirPath $archiveFileName) -Destination 'Output'
+
+			Compress-Core $binRootDirPath $archiveFileName
+
+			foreach ($pluginDir in $pluginDirs) {
+				Compress-Core $pluginDir.FullName ($pluginDir.Name + '.' + $Archive)
+			}
 		}
 	}
 }
