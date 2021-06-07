3729c2dd17d461479538832a9894f69350e16aad
sk <content.type.text.net@gmail.com>
2021-06-07 10:56:21 +0900

    スペルミス修正

--- Source/Pe/Pe.Main/Models/Applications/Configuration/LauncherItemConfiguration.cs
+++ Source/Pe/Pe.Main/Models/Applications/Configuration/LauncherItemConfiguration.cs
@@ -19,7 +19,7 @@ namespace ContentTypeTextNet.Pe.Main.Models.Applications.Configuration
         /// <para>正規表現・大文字小文字を区別しない。</para>
         /// </summary>
         [Configuration]
-        public IReadOnlyList<string> AutoImportUntargetPatterns { get; } = default!;
+        public IReadOnlyList<string> AutoImportExcludePatterns { get; } = default!;
 
         #endregion
     }
