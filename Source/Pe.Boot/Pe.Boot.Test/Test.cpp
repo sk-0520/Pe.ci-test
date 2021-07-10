e7c124f7e7bf6128b5fad3811905003f21feb70d
sk <content.type.text.net@gmail.com>
2021-07-10 23:35:08 +0900

    CRT諦め処理破棄

--- Source/Pe.Boot/Pe.Boot.Test/Test.cpp
+++ Source/Pe.Boot/Pe.Boot.Test/Test.cpp
@@ -1,49 +1,7 @@
 #include "pch.h"
 
-#if 0
-extern "C" {
-#   include "../Pe.Boot/tstring.h"
-}
-
 namespace PeBootTest
 {
-    void Test::failCore(const TCHAR* message, const TestInfo& info)
-    {
-        Microsoft::VisualStudio::CppUnitTestFramework::__LineInfo lineInfo = Microsoft::VisualStudio::CppUnitTestFramework::__LineInfo(
-            info.file,
-            info.function,
-            info.line
-        );
-        Microsoft::VisualStudio::CppUnitTestFramework::Assert::Fail(message, &lineInfo);
-    }
-
-    void Test::isTrue(bool condition, const TestInfo& info)
-    {
-    }
-
-    void Test::isFalse(bool condition, const TestInfo& info)
-    {
-    }
-
-    void Test::isNull(void* p, const TestInfo& info)
-    {
-
-    }
-
-    void Test::areEqual(int expected, int actual, const TestInfo& info)
-    {
-        if (expected != actual) {
-            TCHAR message[1024];
-            formatString(message, _T("expected: %d, actual: %d"), expected, actual);
-            failCore(message, info);
-        }
-    }
-
-    void Test::areEqual(const TCHAR* expected, const TCHAR* actual, const TestInfo& info)
-    {
-
-    }
-
 }
 
-#endif
+
--- Source/Pe.Boot/Pe.Boot.Test/Test.h
+++ Source/Pe.Boot/Pe.Boot.Test/Test.h
@@ -1,44 +1,8 @@
 ﻿#pragma once
-
-#if 0
-
 #include <tchar.h>
 
 namespace PeBootTest
 {
-    struct TestInfo
-    {
-        TestInfo(const TCHAR* args, const char* function, const TCHAR* file, int line) {
-            this->args = args;
-            this->function = function;
-            this->file = file;
-            this->line = line;
-        }
-
-        const TCHAR* args;
-        const char* function;
-        const TCHAR* file;
-        int line;
-    };
-
-    class Test {
-    private:
-        static void failCore(const TCHAR* message, const TestInfo& info);
-
-    public:
-        static void isTrue(bool actual, const TestInfo& info);
-        static void isFalse(bool actual, const TestInfo& info);
-        static void isNull(void* p, const TestInfo& info);
-        static void areEqual(int expected, int actual, const TestInfo& info);
-        static void areEqual(const TCHAR* expected, const TCHAR* actual, const TestInfo& info);
-    };
-
-#define INFO(args) TestInfo(_T(#args), __func__, __FILEW__, __LINE__)
-#define Test_IsTrue(condition) Test::isTrue((condition), INFO(condition))
-#define Test_IsFalse(condition) Test::isFalse((condition), INFO(condition))
-#define Test_IsNull(p) Test::isNull((p), INFO(p))
-#define Test_AreEqual(expected, actual) Test::areEqual((expected), (actual), INFO(expected "," actual))
-
 }
 
-#endif
+
