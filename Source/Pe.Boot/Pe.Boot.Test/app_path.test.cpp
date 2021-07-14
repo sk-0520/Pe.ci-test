592b73df6e96f19d41db0866335728554fb96dce
sk <content.type.text.net@gmail.com>
2021-07-14 16:34:13 +0900

    getMainModulePath2Test

---
+++ Source/Pe.Boot/Pe.Boot.Test/app_path.test.cpp
@@ -0,0 +1,25 @@
+﻿#include "pch.h"
+
+extern "C" {
+#   include "../Pe.Boot/app_path.h"
+}
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace PeBootTest
+{
+    TEST_CLASS(appPathTest)
+    {
+    public:
+
+        TEST_METHOD(getMainModulePath2Test)
+        {
+            TEXT expected = wrap("C:\\abc\\bin\\Pe.Main.exe");
+            TEXT input = wrap("C:\\abc\\");
+            TEXT actual = getMainModulePath2(&input);
+
+            Assert::AreEqual(expected.value, actual.value);
+            freeText(&actual);
+        }
+    };
+}
