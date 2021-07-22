4ac06220238c87bd2a98ab916c02e3b9d464b71f
sk <content.type.text.net@gmail.com>
2021-07-22 13:01:43 +0900

    fsio.test

--- Source/Pe.Boot/Pe.Boot.Test/fsio.test.cpp
+++ Source/Pe.Boot/Pe.Boot.Test/fsio.test.cpp
@@ -27,5 +27,40 @@ namespace PeBootTest
             Assert::IsFalse(is_directory_path(&file_path));
             Assert::IsFalse(is_directory_path(&unknown_path));
         }
+
+        TEST_METHOD(exists_file_path_test)
+        {
+            TEST_INIT_DIR;
+
+            TEST_GET_PATH(dir_path, _T("dir"));
+            TEST::create_directory(test_dir_path);
+
+            TEST_GET_PATH(file_path, _T("file"));
+            TEST::create_file(test_file_path);
+
+            TEST_GET_PATH(unknown_path, _T("unknown"));
+
+            Assert::IsFalse(exists_file_path(&dir_path));
+            Assert::IsTrue(exists_file_path(&file_path));
+            Assert::IsFalse(exists_file_path(&unknown_path));
+        }
+
+        TEST_METHOD(exists_directory_path_test)
+        {
+            TEST_INIT_DIR;
+
+            TEST_GET_PATH(dir_path, _T("dir"));
+            TEST::create_directory(test_dir_path);
+
+            TEST_GET_PATH(file_path, _T("file"));
+            TEST::create_file(test_file_path);
+
+            TEST_GET_PATH(unknown_path, _T("unknown"));
+
+            Assert::IsTrue(exists_directory_path(&dir_path));
+            Assert::IsFalse(exists_directory_path(&file_path));
+            Assert::IsFalse(exists_directory_path(&unknown_path));
+        }
+
     };
 }
