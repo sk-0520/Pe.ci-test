a58da42a7a681af6582c1a1a3a71f79865315976
sk <content.type.text.net@gmail.com>
2021-07-23 23:50:02 +0900

    文字列のだんだん追加処理無いと死ぬわ

--- Source/Pe.Boot/Pe.Boot/tstring.c
+++ Source/Pe.Boot/Pe.Boot/tstring.c
@@ -46,4 +46,60 @@ void RC_HEAP_FUNC(free_string, const TCHAR* s)
     RC_HEAP_CALL(free_memory, (void*)s);
 }
 
+STRING_BUILDER RC_HEAP_FUNC(initialize_string_builder, const TCHAR* s, size_t capacity)
+{
+    assert_debug(s);
+
+    size_t length = get_string_length(s);
+    size_t bytes = length * sizeof(TCHAR);
+
+    TCHAR* buffer = RC_HEAP_CALL(allocate_memory, bytes, false);
+    copy_memory(buffer, s, bytes);
+
+    STRING_BUILDER result = {
+        .buffer = buffer,
+        .length = length,
+        .library = {
+            .capacity = MAX(length, capacity),
+    }
+    };
+
+    return result;
+}
+
+STRING_BUILDER RC_HEAP_FUNC(create_string_builder, size_t capacity)
+{
+    assert_debug(capacity);
+
+    size_t bytes = capacity * sizeof(TCHAR);
+    TCHAR* buffer = RC_HEAP_CALL(allocate_memory, bytes, false);
+    buffer[0] = 0;
+
+    STRING_BUILDER result = {
+        .buffer = buffer,
+        .length = 0,
+        .library = {
+            .capacity = capacity,
+    }
+    };
+
+    return result;
+}
+
+bool RC_HEAP_FUNC(free_string_builder, STRING_BUILDER* string_builder)
+{
+    if (!string_builder) {
+        return false;
+    }
+    if (!string_builder->buffer) {
+        return false;
+    }
+
+    RC_HEAP_CALL(free_memory, string_builder->buffer);
+    string_builder->buffer = NULL;
+    string_builder->length = 0;
+    string_builder->library.capacity = 0;
+
+    return false;
+}
 
--- Source/Pe.Boot/Pe.Boot/tstring.h
+++ Source/Pe.Boot/Pe.Boot/tstring.h
@@ -5,6 +5,28 @@
 
 #include "memory.h"
 
+#define STRING_BUILDER_DEFAULT_CAPACITY (64)
+
+/// <summary>
+/// 可変長文字列生成処理。
+/// </summary>
+typedef struct tag_STRING_BUILDER
+{
+    /// <summary>
+    /// バッファ。
+    /// <para>番兵を前提として扱わないこと。</para>
+    /// <para>構築中に使用する場合、追加・削除処理を行ってはいけない(再確保でヒープが無効の可能性あり)。</para>
+    /// </summary>
+    TCHAR* buffer;
+    /// <summary>
+    /// 現在の長さ。
+    /// </summary>
+    size_t length;
+    struct
+    {
+        size_t capacity;
+    } library;
+} STRING_BUILDER;
 
 /// <summary>
 /// 文字列長を取得。
@@ -34,8 +56,8 @@ TCHAR* copy_string(TCHAR* result, const TCHAR* value);
 /// 文字列を複製。
 /// </summary>
 /// <param name="source"></param>
-/// <returns>複製された文字列。<c>freeString(freeMemory)</c>にて解放する必要あり。</returns>
-TCHAR* RC_HEAP_FUNC(clone_string, const TCHAR * source);
+/// <returns>複製された文字列。解放が必要。</returns>
+TCHAR* RC_HEAP_FUNC(clone_string, const TCHAR* source);
 #ifdef RES_CHECK
 #   define clone_string(source) RC_HEAP_WRAP(clone_string, source)
 #endif
@@ -55,9 +77,37 @@ TCHAR* RC_HEAP_FUNC(allocate_string, size_t length);
 /// ドメインとしての関数で<c>freeMemory</c>のラッパー。
 /// </summary>
 /// <param name="s"></param>
-void RC_HEAP_FUNC(free_string, const TCHAR * s);
+void RC_HEAP_FUNC(free_string, const TCHAR* s);
 #ifdef RES_CHECK
 #   define free_string(s) RC_HEAP_WRAP(free_string, (s))
 #endif
 
+/// <summary>
+/// <c>STRING_BUILDER</c>を初期化文字列から生成。
+/// </summary>
+/// <param name="s">初期化に使用する文字列。</param>
+/// <param name="capacity">初期予約領域。特に指定しない場合は<c>STRING_BUILDER_DEFAULT_CAPACITY</c>を使用する。<c>s</c>より小さい場合、<c>s</c>の長さまで拡張される。</param>
+/// <returns>生成した<c>STRING_BUILDER</c>。解放が必要。</returns>
+STRING_BUILDER RC_HEAP_FUNC(initialize_string_builder, const TCHAR* s, size_t capacity);
+#ifdef RES_CHECK
+#   define initialize_string_builder(s, capacity) RC_HEAP_WRAP(initialize_string_builder, (s), (capacity))
+#endif
+
+/// <summary>
+/// <c>STRING_BUILDER</c>を生成。
+/// </summary>
+/// <param name=""></param>
+/// <param name="capacity">初期予約領域。特に指定しない場合は<c>STRING_BUILDER_DEFAULT_CAPACITY</c>を使用する。</param>
+/// <returns>生成した<c>STRING_BUILDER</c>。解放が必要。</returns>
+STRING_BUILDER RC_HEAP_FUNC(create_string_builder, size_t capacity);
+#ifdef RES_CHECK
+#   define create_string_builder(capacity) RC_HEAP_WRAP(create_string_builder, (capacity))
+#endif
+
+/// <summary>
+/// <c>STRING_BUILDER</c>の解放。
+/// </summary>
+/// <param name=""></param>
+/// <param name="string_builder"></param>
+bool RC_HEAP_FUNC(free_string_builder, STRING_BUILDER* string_builder);
 
