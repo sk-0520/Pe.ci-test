f84885a966ea3ea2bbce03562b5c2bb9ea672da0
sk <content.type.text.net@gmail.com>
2021-07-24 13:06:04 +0900

    0埋めと空白埋めと左右指定が混乱してる

--- Source/Pe.Boot/Pe.Boot.Test/writer.test.cpp
+++ Source/Pe.Boot/Pe.Boot.Test/writer.test.cpp
@@ -66,33 +66,48 @@ namespace PeBootTest
         TEST_METHOD(write_to_primitive_integer_test)
         {
             auto tests = {
-                DATA(_T("0"), 0, WRITE_PADDING_NONE, WRITE_ALIGN_LEFT, false, 0, '\0'),
-                DATA(_T("1234"), 1234, WRITE_PADDING_NONE, WRITE_ALIGN_LEFT, false, 0, '\0'),
-                DATA(_T("12345678"), 12345678, WRITE_PADDING_NONE, WRITE_ALIGN_LEFT, false, 0, '\0'),
+                DATA(_T("0"), 0, WRITE_PADDING_NONE, WRITE_ALIGN_LEFT, false, 0, _T('\0')),
+                DATA(_T("1234"), 1234, WRITE_PADDING_NONE, WRITE_ALIGN_LEFT, false, 0, _T('\0')),
+                DATA(_T("12345678"), 12345678, WRITE_PADDING_NONE, WRITE_ALIGN_LEFT, false, 0, _T('\0')),
 
-                DATA(_T("+0"), 0, WRITE_PADDING_NONE, WRITE_ALIGN_LEFT, true, 0, '\0'),
-                DATA(_T("+123"), 123, WRITE_PADDING_NONE, WRITE_ALIGN_LEFT, true, 0, '\0'),
-                DATA(_T("-123"), -123, WRITE_PADDING_NONE, WRITE_ALIGN_LEFT, true, 0, '\0'),
+                DATA(_T("+0"), 0, WRITE_PADDING_NONE, WRITE_ALIGN_LEFT, true, 0, _T('\0')),
+                DATA(_T("+123"), 123, WRITE_PADDING_NONE, WRITE_ALIGN_LEFT, true, 0, _T('\0')),
+                DATA(_T("-123"), -123, WRITE_PADDING_NONE, WRITE_ALIGN_LEFT, true, 0, _T('\0')),
 
-                DATA(_T("0"), 0, WRITE_PADDING_NONE, WRITE_ALIGN_LEFT, false, 4, '\0'),
-                DATA(_T("   0"), 0, WRITE_PADDING_NONE, WRITE_ALIGN_RIGHT, false, 4, '\0'),
-                DATA(_T("1234"), 1234, WRITE_PADDING_NONE, WRITE_ALIGN_RIGHT, false, 4, '\0'),
-                DATA(_T("12345"), 12345, WRITE_PADDING_NONE, WRITE_ALIGN_RIGHT, false, 4, '\0'),
+                DATA(_T("0"), 0, WRITE_PADDING_NONE, WRITE_ALIGN_LEFT, false, 4, _T('\0')),
+                DATA(_T("   0"), 0, WRITE_PADDING_NONE, WRITE_ALIGN_RIGHT, false, 4, _T('\0')),
+                DATA(_T("1234"), 1234, WRITE_PADDING_NONE, WRITE_ALIGN_RIGHT, false, 4, _T('\0')),
+                DATA(_T("12345"), 12345, WRITE_PADDING_NONE, WRITE_ALIGN_RIGHT, false, 4, _T('\0')),
 
-                DATA(_T("+1"), 1, WRITE_PADDING_NONE, WRITE_ALIGN_LEFT, true, 4, '\0'),
-                DATA(_T("  +1"), 1, WRITE_PADDING_NONE, WRITE_ALIGN_RIGHT, true, 4, '\0'),
-                DATA(_T("-1"), -1, WRITE_PADDING_NONE, WRITE_ALIGN_LEFT, true, 4, '\0'),
-                DATA(_T("  -1"), -1, WRITE_PADDING_NONE, WRITE_ALIGN_RIGHT, true, 4, '\0'),
-                DATA(_T("-1"), -1, WRITE_PADDING_NONE, WRITE_ALIGN_LEFT, false, 4, '\0'),
-                DATA(_T("  -1"), -1, WRITE_PADDING_NONE, WRITE_ALIGN_RIGHT, false, 4, '\0'),
+                DATA(_T("+1"), 1, WRITE_PADDING_NONE, WRITE_ALIGN_LEFT, true, 4, _T('\0')),
+                DATA(_T("  +1"), 1, WRITE_PADDING_NONE, WRITE_ALIGN_RIGHT, true, 4, _T('\0')),
+                DATA(_T("-1"), -1, WRITE_PADDING_NONE, WRITE_ALIGN_LEFT, true, 4, _T('\0')),
+                DATA(_T("  -1"), -1, WRITE_PADDING_NONE, WRITE_ALIGN_RIGHT, true, 4, _T('\0')),
+                DATA(_T("-1"), -1, WRITE_PADDING_NONE, WRITE_ALIGN_LEFT, false, 4, _T('\0')),
+                DATA(_T("  -1"), -1, WRITE_PADDING_NONE, WRITE_ALIGN_RIGHT, false, 4, _T('\0')),
 
-                DATA(_T("0001"), 1, WRITE_PADDING_ZERO, WRITE_ALIGN_LEFT, false, 4, '\0'),
-                DATA(_T("0001"), 1, WRITE_PADDING_ZERO, WRITE_ALIGN_RIGHT, false, 4, '\0'),
-                DATA(_T("-001"), -1, WRITE_PADDING_ZERO, WRITE_ALIGN_LEFT, false, 4, '\0'),
-                DATA(_T("-001"), -1, WRITE_PADDING_ZERO, WRITE_ALIGN_RIGHT, false, 4, '\0'),
-                DATA(_T("+001"), 1, WRITE_PADDING_ZERO, WRITE_ALIGN_LEFT, true, 4, '\0'),
-                DATA(_T("+001"), 1, WRITE_PADDING_ZERO, WRITE_ALIGN_RIGHT, true, 4, '\0'),
+                DATA(_T("0001"), 1, WRITE_PADDING_ZERO, WRITE_ALIGN_LEFT, false, 4, _T('\0')),
+                DATA(_T("0001"), 1, WRITE_PADDING_ZERO, WRITE_ALIGN_RIGHT, false, 4, _T('\0')),
+                DATA(_T("-001"), -1, WRITE_PADDING_ZERO, WRITE_ALIGN_LEFT, false, 4, _T('\0')),
+                DATA(_T("-001"), -1, WRITE_PADDING_ZERO, WRITE_ALIGN_RIGHT, false, 4, _T('\0')),
+                DATA(_T("+001"), 1, WRITE_PADDING_ZERO, WRITE_ALIGN_LEFT, true, 4, _T('\0')),
+                DATA(_T("+001"), 1, WRITE_PADDING_ZERO, WRITE_ALIGN_RIGHT, true, 4, _T('\0')),
 
+                DATA(_T("0"), 0, WRITE_PADDING_NONE, WRITE_ALIGN_LEFT, false, 0, _T(',')),
+                DATA(_T("1,234"), 1234, WRITE_PADDING_NONE, WRITE_ALIGN_LEFT, false, 0, _T(',')),
+                DATA(_T("12,345,678"), 12345678, WRITE_PADDING_NONE, WRITE_ALIGN_LEFT, false, 0, _T(',')),
+
+                DATA(_T("       0"), 0, WRITE_PADDING_NONE, WRITE_ALIGN_RIGHT, false, 8, _T(',')),
+                DATA(_T("   1,234"), 1234, WRITE_PADDING_NONE, WRITE_ALIGN_RIGHT, false, 8, _T(',')),
+                DATA(_T("12,345,678"), 12345678, WRITE_PADDING_NONE, WRITE_ALIGN_RIGHT, false, 8, _T(',')),
+
+                DATA(_T("      +0"), 0, WRITE_PADDING_NONE, WRITE_ALIGN_RIGHT, true, 8, _T(',')),
+                DATA(_T("  +1,234"), 1234, WRITE_PADDING_NONE, WRITE_ALIGN_RIGHT, true, 8, _T(',')),
+                DATA(_T("+12,345,678"), 12345678, WRITE_PADDING_NONE, WRITE_ALIGN_RIGHT, true, 8, _T(',')),
+
+                DATA(_T("+0000000"), 0, WRITE_PADDING_ZERO, WRITE_ALIGN_RIGHT, true, 8, _T(',')),
+                DATA(_T("+001,234"), 1234, WRITE_PADDING_ZERO, WRITE_ALIGN_RIGHT, true, 8, _T(',')),
+                DATA(_T("+12,345,678"), 12345678, WRITE_PADDING_ZERO, WRITE_ALIGN_RIGHT, true, 8, _T(',')),
             };
             for (auto test : tests) {
                 BUF actual;
