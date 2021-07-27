a6f970a8d8d32e11ef69abe03cc751a417579dfb
sk <content.type.text.net@gmail.com>
2021-07-27 18:04:27 +0900

    文字コード変換準備

---
+++ Source/Pe.Boot/Pe.Boot/encoding.h
@@ -0,0 +1,40 @@
+﻿#pragma once
+#include <windows.h>
+
+#include "text.h"
+
+typedef enum tag_ENCODE_TYPE
+{
+    STRINIG_ENCODING_SYSTEM,
+    STRINIG_ENCODING_ASCII,
+    STRINIG_ENCODING_UTF8,
+    STRINIG_ENCODING_UTF16LE,
+    STRINIG_ENCODING_UTF16BE,
+    STRINIG_ENCODING_UTF32LE,
+    STRINIG_ENCODING_UTF32BE,
+} ENCODE_TYPE;
+
+/// <summary>
+/// エンコード結果。
+/// </summary>
+typedef struct tag_ENCODE_RESULT
+{
+    /// <summary>
+    /// エンコードデータ格納バッファ。
+    /// </summary>
+    void* buffer;
+    /// <summary>
+    /// <c>buffer</c>のバイト単位の長さ。
+    /// </summary>
+    size_t bytes;
+    /// <summary>
+    /// <c>buffer</c>の最小要素幅。
+    /// </summary>
+    size_t width;
+} ENCODE_RESULT;
+
+//bool free_encode_result(ENCODE_RESULT* encode_result);
+
+//ENCODE_RESULT encode_to_string(const TEXT* input, ENCODE_TYPE encode_type);
+
+
