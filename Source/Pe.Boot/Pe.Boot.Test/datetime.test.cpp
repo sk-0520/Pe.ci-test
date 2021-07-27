88ef511c1ce447e65e017085c49bd922e8dcae2c
sk <content.type.text.net@gmail.com>
2021-07-27 11:32:42 +0900

    日時情報
    * テストのためにタイムゾーン定義したけどもういっか
    * UTC<->LOCALがわからん！けどJPでの云々は出来たからもう知らん！
    * UTCかローカル時間かわからんぞこれ
    * 日付の無理やり定義
    * 日付を扱えるようにするため必要ファイルを用意

---
+++ Source/Pe.Boot/Pe.Boot.Test/datetime.test.cpp
@@ -0,0 +1,35 @@
+﻿#include "pch.h"
+
+extern "C" {
+#   include "../Pe.Boot/datetime.h"
+}
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace PeBootTest
+{
+    TEST_CLASS(datetime_test)
+    {
+    public:
+#if 0
+        TEST_METHOD(local_jp_test)
+        {
+            auto local_input = create_datetime(false, 2021, 7, 27, 0, 0, 0, 0);
+            auto local_utc = datetime_to_timestamp(&local_input, true);
+            auto local_local = datetime_to_timestamp(&local_input, false);
+            Assert::AreEqual(26, (int)local_utc.day);
+            Assert::AreEqual(15, (int)local_utc.hour);
+            Assert::AreEqual(27, (int)local_local.day);
+            Assert::AreEqual(0, (int)local_local.hour);
+
+            auto utc_input = create_datetime(true, 2021, 7, 27, 16, 0, 0, 0);
+            auto utc_utc = datetime_to_timestamp(&utc_input, true);
+            auto utc_local = datetime_to_timestamp(&utc_input, false);
+            Assert::AreEqual(27, (int)utc_utc.day);
+            Assert::AreEqual(16, (int)utc_utc.hour);
+            Assert::AreEqual(28, (int)utc_local.day);
+            Assert::AreEqual(1, (int)utc_local.hour);
+        }
+#endif
+    };
+}
---
+++ Source/Pe.Boot/Pe.Boot/datetime.h
@@ -0,0 +1,109 @@
+﻿#pragma once
+#include <stdint.h>
+#include <stdbool.h>
+
+#include <windows.h>
+
+typedef struct tag_TIME_ZONE
+{
+    TIME_ZONE_INFORMATION tzi;
+    struct
+    {
+        bool enabled;
+    } library;
+} TIME_ZONE;
+
+
+/// <summary>
+/// 日付の持ち運び用データ。
+/// <para>アプリケーション側で明示的に生成しない。</para>
+/// </summary>
+typedef union  tag_DATETIME
+{
+    /// <summary>
+    /// 実データ。
+    /// <para>常にUTC。</para>
+    /// </summary>
+    FILETIME filetime;
+    /// <summary>
+    /// あれこれ演算用。
+    /// </summary>
+    uint64_t epoch;
+} DATETIME;
+
+/// <summary>
+/// 曜日。
+/// </summary>
+typedef enum tag_DAY_OF_WEEK
+{
+    /// <summary>
+    /// 日曜日。
+    /// </summary>
+    DAY_OF_WEEK_SUNDAY,
+    /// <summary>
+    /// 月曜日。
+    /// </summary>
+    DAY_OF_WEEK_MONDAY,
+    /// <summary>
+    /// 火曜日。
+    /// </summary>
+    DAY_OF_WEEK_TUESDAY,
+    /// <summary>
+    /// 水曜日。
+    /// </summary>
+    DAY_OF_WEEK_WEDNESDAY,
+    /// <summary>
+    /// 木曜日。
+    /// </summary>
+    DAY_OF_WEEK_THURSDAY,
+    /// <summary>
+    /// 金曜日。
+    /// </summary>
+    DAY_OF_WEEK_FRIDAY,
+    /// <summary>
+    /// 土曜日。
+    /// </summary>
+    DAY_OF_WEEK_SATURDAY,
+} DAY_OF_WEEK;
+
+/// <summary>
+/// 日付の分解データ。
+/// <para>アプリケーション側で明示的に生成しない。</para>
+/// </summary>
+typedef struct tag_TIMESTAMP
+{
+    int16_t year;
+    int16_t month;
+    uint8_t day;
+    uint8_t hour;
+    uint8_t minute;
+    uint8_t second;
+    uint16_t milli_sec;
+    DAY_OF_WEEK day_of_week : 16;
+    struct
+    {
+        /// <summary>
+        /// UTCか。
+        /// </summary>
+        bool is_utc;
+    } library;
+} TIMESTAMP;
+
+TIME_ZONE get_current_time_zone();
+
+/// <summary>
+/// 現在時間(UTC)の取得。
+/// </summary>
+/// <returns></returns>
+DATETIME get_current_datetime();
+
+DATETIME create_datetime(bool is_utc, unsigned int year, unsigned int month, unsigned int day, unsigned int hour, unsigned int minute, unsigned int second, unsigned int milli_sec);
+
+/// <summary>
+/// <c>DATETIME</c>を<c>TIMESTAMP</c>に変換。
+/// </summary>
+/// <param name="datetime"></param>
+/// <param name="to_utc">UTCにするか。</param>
+/// <returns></returns>
+TIMESTAMP datetime_to_timestamp(const DATETIME* datetime, bool to_utc);
+
