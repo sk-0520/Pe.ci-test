b99f9b46b2771542b5d81be67fd96f1a06df3d09
sk <content.type.text.net@gmail.com>
2021-08-07 20:55:33 +0900

    やっぱDB定義変更必要やん！

---
+++ Source/Pe/Pe.Main/etc/sql/ContentTypeTextNet.Pe.Main/Models/Database/Setupper/Setupper_V_00_99_160/ExecuteMainDDL.sql
@@ -0,0 +1,16 @@
+--// table: PluginVersionChecks
+create table [PluginVersionChecks] (
+	[PluginId] text not null /* プラグインID  */,
+	[Sequence] integer not null /* シーケンス  */,
+	[CreatedTimestamp] datetime not null /* 作成タイムスタンプ UTC */,
+	[CreatedAccount] text not null /* 作成ユーザー名  */,
+	[CreatedProgramName] text not null /* 作成プログラム名  */,
+	[CreatedProgramVersion] text not null /* 作成プログラムバージョン  */,
+	[VersionCheckUri] text not null /* 更新確認URI  */,
+	primary key(
+		[PluginId],
+		[Sequence]
+	),
+	foreign key([PluginId]) references [Plugins]([PluginId])
+)
+;
