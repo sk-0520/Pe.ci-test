21a49d790dc92e771b855b4b934f3d086d60c416
sk <content.type.text.net@gmail.com>
2021-10-31 20:18:45 +0900

    32/64bit を柔軟にしたい思い

--- Source/Pe.Boot/Pe.Library/text_conv.h
+++ Source/Pe.Boot/Pe.Library/text_conv.h
@@ -54,6 +54,11 @@ TEXT_PARSED_I32_RESULT parse_i32_from_text(const TEXT* input, bool support_hex);
 TEXT_PARSED_I64_RESULT parse_i64_from_text(const TEXT* input, bool support_hex);
 #endif
 
+TEXT_PARSED_I32_RESULT parse_i32_from_bin_text(const TEXT* input);
+#ifdef _WIN64
+TEXT_PARSED_I64_RESULT parse_i64_from_bin_text(const TEXT* input);
+#endif
+
 #ifdef _UNICODE
 
 typedef enum tag_MULTIBYTE_CHARACTER_TYPE
@@ -88,11 +93,6 @@ bool RC_HEAP_FUNC(free_multibyte_character_result, MULTIBYTE_CHARACTER_RESULT* m
 #   define free_multibyte_character_result(mbcr) RC_HEAP_WRAP(free_multibyte_character_result, (mbcr))
 #endif
 
-TEXT_PARSED_I32_RESULT parse_i32_from_bin_text(const TEXT* input);
-#ifdef _WIN64
-TEXT_PARSED_I64_RESULT parse_i64_from_bin_text(const TEXT* input);
-#endif
-
 /// <summary>
 ///
 /// </summary>
@@ -105,5 +105,15 @@ TEXT RC_HEAP_FUNC(make_text_from_multibyte, const uint8_t* input, size_t length,
 #   define make_text_from_multibyte(input, length, mbc_type) RC_HEAP_WRAP(make_text_from_multibyte, (input), (length), (mbc_type))
 #endif
 
+#endif
+
+#ifdef _WIN64
+#   define TEXT_PARSED_SSIZE_RESULT TEXT_PARSED_I64_RESULT
+#   define parse_ssize_from_text parse_i64_from_text
+#   define parse_ssize_from_bin_text parse_i64_from_bin_text
+#else
+#   define TEXT_PARSED_SSIZE_RESULT TEXT_PARSED_I32_RESULT
+#   define parse_ssize_from_text parse_i32_from_text
+#   define parse_ssize_from_bin_text parse_i32_from_bin_text
 #endif
 
