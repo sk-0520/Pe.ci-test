cf454fe26f2fbb90c4998bc6a63724a8652625ce
sk <content.type.text.net@gmail.com>
2022-04-17 21:11:40 +0900

    共通化してみたけど共通も何も一つだけだしなんだこれ・・・

---
+++ Source/Pe/Pe.Main/Models/SettingChangedTarget.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace ContentTypeTextNet.Pe.Main.Models
+{
+    /// <summary>
+    /// 設定変更を受け付けるメンバに対して付与する属性。
+    /// <para><see cref="EventHandler{NotifyEventArgs}"/>で使用する前提。</para>
+    /// <see cref=""/>
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+    public class SettingChangedTargetAttribute: Attribute
+    { }
+
+    public static class SettingChangedTargetHelper
+    {
+        #region function
+
+        /// <summary>
+        /// <see cref="SettingChangedTargetAttribute"/>の付与されたメンバーを取得する。
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public static IEnumerable<MemberInfo> GetMembers(Type type)
+        {
+            var flags = BindingFlags.Public | BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.GetField | BindingFlags.GetProperty;
+            var members = type.GetMembers(flags)
+                .Where(i => i.GetCustomAttribute<SettingChangedTargetAttribute>() is not null)
+            ;
+
+            return members;
+        }
+
+        #endregion
+    }
+}
