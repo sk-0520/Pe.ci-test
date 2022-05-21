24cdde47085975d3f40b237711f2162181f9c018
sk <content.type.text.net@gmail.com>
2022-05-22 00:54:59 +0900

    fsio.c の番兵対応

--- Source/Pe.Boot/Pe.Library/fsio.c
+++ Source/Pe.Boot/Pe.Library/fsio.c
@@ -1,9 +1,31 @@
 ﻿#include "fsio.h"
 
+static DWORD get_file_attributes_core(const TEXT* path)
+{
+    if (!path) {
+        return INVALID_FILE_ATTRIBUTES;
+    }
+
+    if (path->library.sentinel) {
+        return GetFileAttributes(path->value);
+    }
+
+    const MEMORY_RESOURCE* memory_resource = path->library.memory_resource
+        ? path->library.memory_resource
+        : get_default_memory_resource()
+        ;
+
+    TCHAR* cstr = text_to_string(path, memory_resource);
+    DWORD attr = GetFileAttributes(path->value);
+    release_string(cstr, memory_resource);
+
+    return attr;
+}
+
 bool exists_directory_fsio(const TEXT* path)
 {
-    DWORD attr = GetFileAttributes(path->value);
-    if (attr == -1) {
+    DWORD attr = get_file_attributes_core(path);
+    if (attr == INVALID_FILE_ATTRIBUTES) {
         return false;
     }
     return (attr & FILE_ATTRIBUTE_DIRECTORY) == FILE_ATTRIBUTE_DIRECTORY;
@@ -11,8 +33,8 @@ bool exists_directory_fsio(const TEXT* path)
 
 bool exists_file_fsio(const TEXT* path)
 {
-    DWORD attr = GetFileAttributes(path->value);
-    if (attr == -1) {
+    DWORD attr = get_file_attributes_core(path);
+    if (attr == INVALID_FILE_ATTRIBUTES) {
         return false;
     }
     return (attr & FILE_ATTRIBUTE_DIRECTORY) != FILE_ATTRIBUTE_DIRECTORY;
@@ -20,8 +42,8 @@ bool exists_file_fsio(const TEXT* path)
 
 bool exists_fsio(const TEXT* path)
 {
-    DWORD attr = GetFileAttributes(path->value);
-    if (attr == -1) {
+    DWORD attr = get_file_attributes_core(path);
+    if (attr == INVALID_FILE_ATTRIBUTES) {
         return false;
     }
 
