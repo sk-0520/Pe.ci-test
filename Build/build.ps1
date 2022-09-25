cd905963fa4554cf5f6e7ebe0f623e950297c27e
sk <content.type.text.net@gmail.com>
2022-09-25 21:19:32 +0900

    le とか ge がいつも分からん。雰囲気でやってる

--- Build/build.ps1
+++ Build/build.ps1
@@ -232,7 +232,7 @@ try {
 
 			# ライブラリの移送
 			robocopy /R:3 /S /E "Resource\Library\" "Output\Release\$platform\Pe\bin\lib\"
-			IF ($LastExitCode -ge 1) {
+			IF ($LastExitCode -le 1) {
 				$LastExitCode = 0;
 			}
 		}
