853976221ade8bfb5149b82f724ee965499cd27e
sk <content.type.text.net@gmail.com>
2022-09-26 22:33:10 +0900

    これでええの？ もうこれでやめさせて

--- .github/workflows/artifact.bat
+++ .github/workflows/artifact.bat
@@ -12,6 +12,12 @@ for /F %%D in ('dir Pe.Plugins.* /A D /B') do (
 	echo   with:
 	echo     name: !DIR_NAME!-${{ matrix.PLATFORM }}
 	echo     path: Output\!DIR_NAME!_*.${{ env.DEFAULT_ARCHIVE }}
+
+	echo - name: ^<Artifact^> !DIR_NAME!.json
+	echo   uses: actions/upload-artifact@v3
+	echo   with:
+	echo     name: !DIR_NAME!-${{ matrix.PLATFORM }}.json
+	echo     path: Output\!DIR_NAME!.json
 )
 echo #endregion
 
