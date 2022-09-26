bf576bcf8eae9aaad8ccee99c833c4067760a09c
sk <content.type.text.net@gmail.com>
2022-09-26 22:42:01 +0900

    固めたんが再圧縮されるのよくない。。。

--- .github/workflows/artifact.bat
+++ .github/workflows/artifact.bat
@@ -7,17 +7,17 @@ echo #region artifact.bat
 for /F %%D in ('dir Pe.Plugins.* /A D /B') do (
 	set DIR_NAME=%%D
 
-	echo - name: ^<Artifact^> !DIR_NAME!
+	echo - name: ^<Artifact^> !DIR_NAME!.${{ env.DEFAULT_ARCHIVE }}
 	echo   uses: actions/upload-artifact@v3
 	echo   with:
-	echo     name: !DIR_NAME!-${{ matrix.PLATFORM }}
+	echo     name: !DIR_NAME!-${{ matrix.PLATFORM }}.${{ env.DEFAULT_ARCHIVE }}
 	echo     path: Output\!DIR_NAME!_*.${{ env.DEFAULT_ARCHIVE }}
 
 	echo - name: ^<Artifact^> !DIR_NAME!.json
 	echo   uses: actions/upload-artifact@v3
 	echo   with:
 	echo     name: !DIR_NAME!-${{ matrix.PLATFORM }}.json
-	echo     path: Output\!DIR_NAME!.json
+	echo     path: Output\update-!DIR_NAME!.json
 )
 echo #endregion
 
