3b38da54954f8e3a4219dba61f5c07a85f320c1b
sk <content.type.text.net@gmail.com>
2022-09-26 22:09:28 +0900

    これが正か

--- .github/workflows/artifact.bat
+++ .github/workflows/artifact.bat
@@ -5,11 +5,11 @@ pushd ..\..\Source\Pe
 for /F %%D in ('dir Pe.Plugins.* /A D /B') do (  
 	set DIR_NAME=%STR% %%D
 	
-	echo - name: ^<Artifact^> e.Plugins.Reference.ClassicTheme
+	echo - name: ^<Artifact^> %DIR_NAME%
 	echo   uses: actions/upload-artifact@v3
 	echo   with:
-	echo     name: Pe.Plugins.Reference.ClassicTheme-${{ matrix.PLATFORM }}
-	echo     path: Output\Pe.Plugins.Reference.ClassicTheme_*.${{ env.DEFAULT_ARCHIVE }}
+	echo     name: %DIR_NAME%-${{ matrix.PLATFORM }}
+	echo     path: Output\%DIR_NAME%_*.${{ env.DEFAULT_ARCHIVE }}
 )
 
 
