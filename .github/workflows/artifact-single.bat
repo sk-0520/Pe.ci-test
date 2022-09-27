a72c34ee77ef0f1511b739896a168fb5f7bc09f2
sk <content.type.text.net@gmail.com>
2022-09-27 23:13:03 +0900

    bat しんどいなぁ

--- .github/workflows/artifact-single.bat
+++ .github/workflows/artifact-single.bat
@@ -6,37 +6,44 @@ set INDENT=      %INDENT%
 
 pushd ..\..\Source\Pe
 
-set PLATFORMS[0] = PLATFORMS[%I]
-set PLATFORMS[1] = x64
+set PLATFORMS_0 = x86
+set PLATFORMS_1 = x64
 
 echo %INDENT%#region artifact-single.bat
 for /l %%I in (0,1) do (
+	set INDEX=%%I
+
+	echo "INDEX: !INDEX!"
+
 	for /F %%D in ('dir Pe.Plugins.Reference.* /A D /B') do (
 		set DIR_NAME=%%D
+		set PLATFORM=!PLATFORMS_%INDEX%!
 
-		echo %INDENT%- name: ^<Artifact^> Plugin - archive - !DIR_NAME!
-		echo %INDENT%  uses: actions/upload-artifact@v3
-		echo %INDENT%  with:
-		echo %INDENT%    name: !DIR_NAME!-PLATFORMS[%%I].${{ env.DEFAULT_ARCHIVE }}
-		echo %INDENT%    path: Output\!DIR_NAME!_*.${{ env.DEFAULT_ARCHIVE }}
+		echo "PLATFORM: !PLATFORM!"
 
-		echo.
+		@REM echo %INDENT%- name: ^<Artifact^> Plugin - archive - !DIR_NAME!
+		@REM echo %INDENT%  uses: actions/upload-artifact@v3
+		@REM echo %INDENT%  with:
+		@REM echo %INDENT%    name: !DIR_NAME!-!PLATFORM!.${{ env.DEFAULT_ARCHIVE }}
+		@REM echo %INDENT%    path: Output\!DIR_NAME!_*.${{ env.DEFAULT_ARCHIVE }}
 
-		echo %INDENT%- name: ^<Artifact^> Plugin - update info - !DIR_NAME!
-		echo %INDENT%  uses: actions/upload-artifact@v3
-		echo %INDENT%  with:
-		echo %INDENT%    name: !DIR_NAME!-PLATFORMS[%%I].json
-		echo %INDENT%    path: Output\update-!DIR_NAME!.json
+		@REM echo.
 
-		echo.
+		@REM echo %INDENT%- name: ^<Artifact^> Plugin - update info - !DIR_NAME!
+		@REM echo %INDENT%  uses: actions/upload-artifact@v3
+		@REM echo %INDENT%  with:
+		@REM echo %INDENT%    name: !DIR_NAME!-!PLATFORM!.json
+		@REM echo %INDENT%    path: Output\update-!DIR_NAME!.json
 
-		echo %INDENT%- name: ^<Artifact^> Plugin - html - !DIR_NAME!
-		echo %INDENT%  uses: actions/upload-artifact@v3
-		echo %INDENT%  with:
-		echo %INDENT%    name: !DIR_NAME!-PLATFORMS[%I].html
-		echo %INDENT%    path: Output\!DIR_NAME!.html
+		@REM echo.
 
-		echo.
+		@REM echo %INDENT%- name: ^<Artifact^> Plugin - html - !DIR_NAME!
+		@REM echo %INDENT%  uses: actions/upload-artifact@v3
+		@REM echo %INDENT%  with:
+		@REM echo %INDENT%    name: !DIR_NAME!-!PLATFORM!.html
+		@REM echo %INDENT%    path: Output\!DIR_NAME!.html
+
+		@REM echo.
 	)
 )
 echo %INDENT%#endregion
