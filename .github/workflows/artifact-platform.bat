54b35d908981a705958aa07325629c93afb64bc1
sk <content.type.text.net@gmail.com>
2022-09-27 14:44:55 +0900

    うー

--- .github/workflows/artifact-platform.bat
+++ .github/workflows/artifact-platform.bat
@@ -6,7 +6,7 @@ set INDENT=      %INDENT%
 
 pushd ..\..\Source\Pe
 
-echo %INDENT%#region artifact.bat
+echo %INDENT%#region artifact-platform.bat
 for /F %%D in ('dir Pe.Plugins.Reference.* /A D /B') do (
 	set DIR_NAME=%%D
 
@@ -23,6 +23,15 @@ for /F %%D in ('dir Pe.Plugins.Reference.* /A D /B') do (
 	echo %INDENT%  with:
 	echo %INDENT%    name: !DIR_NAME!-${{ matrix.PLATFORM }}.json
 	echo %INDENT%    path: Output\update-!DIR_NAME!.json
+
+	echo.
+
+	echo %INDENT%- name: ^<Artifact^> html - !DIR_NAME!
+	echo %INDENT%  uses: actions/upload-artifact@v3
+	echo %INDENT%  with:
+	echo %INDENT%    name: !DIR_NAME!-${{ matrix.PLATFORM }}.html
+	echo %INDENT%    path: Output\!DIR_NAME!.html
+
 
 	echo.
 	echo.
