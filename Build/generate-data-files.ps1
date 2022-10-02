dfd260c7cd610b1b6f0adb987a560b02fa38ba06
sk <content.type.text.net@gmail.com>
2022-10-02 12:34:31 +0900

    test

--- Build/generate-data-files.ps1
+++ Build/generate-data-files.ps1
@@ -14,4 +14,5 @@ $outputDirectory = Join-Path $rootDirPath 'Output'
 
 $utf8nEncoding = New-Object System.Text.UTF8Encoding $False
 
-[System.IO.File]::WriteAllLines((Join-Path $outputDirectory 'gen-version.txt'), (GetAppVersion), $utf8nEncoding)
+[System.IO.File]::WriteAllLines((Join-Path $outputDirectory 'gen-version-dot.txt'), (GetAppVersion), $utf8nEncoding)
+[System.IO.File]::WriteAllLines((Join-Path $outputDirectory 'gen-version-hyphen.txt'), (ConvertVersion (GetAppVersion) '-'), $utf8nEncoding)
