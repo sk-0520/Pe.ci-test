3201627e86c7dc0d0a2bf7baf3e066252386d044
sk <content.type.text.net@gmail.com>
2022-12-14 21:47:11 +0900

    #835: ヘルプファイルのリンクが腐ってる
    * 無関係なフォーラムURLもついでに修正

--- Source/Help/script/link.ts
+++ Source/Help/script/link.ts
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', ev => {
 	const anchorElements = document.getElementsByTagName('a');
 	for (const anchorElement of anchorElements) {
 		const url = new URL(anchorElement.href);
-		if (!url.hostname.startsWith('localhost') && anchorElement.target == '') {
+		if (url.protocol.startsWith('http') && !url.hostname.startsWith('localhost') && anchorElement.target == '') {
 			anchorElement.target = '_blank';
 		}
 	}
