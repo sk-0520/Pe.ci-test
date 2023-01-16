97ffe0ab7ce8c70db95179ea3d0c788c3fa66006
sk <content.type.text.net@gmail.com>
2023-01-16 21:06:50 +0900

    dotnet_naming_rule.async_methods_end_in_async.severity -> warning

---
+++ Source/Pe/Pe.Core.Test/GlobalSuppressions.cs
@@ -0,0 +1,13 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Style", "IDE1006:命名スタイル", Justification = "<保留中>", Scope = "member", Target = "~M:ContentTypeTextNet.Pe.Core.Test.Models.Database.Vender.Public.SQLite.SqliteSimpleTest.QueryAsync_Dynamic_Test~System.Threading.Tasks.Task")]
+[assembly: SuppressMessage("Style", "IDE1006:命名スタイル", Justification = "<保留中>", Scope = "member", Target = "~M:ContentTypeTextNet.Pe.Core.Test.Models.Database.Vender.Public.SQLite.SqliteSimpleTest.QueryAsyncTest~System.Threading.Tasks.Task")]
+[assembly: SuppressMessage("Style", "IDE1006:命名スタイル", Justification = "<保留中>", Scope = "member", Target = "~M:ContentTypeTextNet.Pe.Core.Test.Models.Database.Vender.Public.SQLite.SqliteSimpleTest.QueryFirstAsyncTest~System.Threading.Tasks.Task")]
+[assembly: SuppressMessage("Style", "IDE1006:命名スタイル", Justification = "<保留中>", Scope = "member", Target = "~M:ContentTypeTextNet.Pe.Core.Test.Models.Database.Vender.Public.SQLite.SqliteSimpleTest.QueryFirstOrDefaultAsyncTest~System.Threading.Tasks.Task")]
+[assembly: SuppressMessage("Style", "IDE1006:命名スタイル", Justification = "<保留中>", Scope = "member", Target = "~M:ContentTypeTextNet.Pe.Core.Test.Models.Database.Vender.Public.SQLite.SqliteSimpleTest.QuerySingleAsyncTest~System.Threading.Tasks.Task")]
+[assembly: SuppressMessage("Style", "IDE1006:命名スタイル", Justification = "<保留中>", Scope = "member", Target = "~M:ContentTypeTextNet.Pe.Core.Test.Models.Database.Vender.Public.SQLite.SqliteSimpleTest.QuerySingleOrDefaultAsyncTest~System.Threading.Tasks.Task")]
