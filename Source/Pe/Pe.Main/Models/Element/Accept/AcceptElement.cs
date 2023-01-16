6c5d45e7278b62e8f520a8864183f1a999af038c
sk <content.type.text.net@gmail.com>
2023-01-16 20:24:48 +0900

    Pe.Server の URI を使用許諾に適用

--- Source/Pe/Pe.Main/Models/Element/Accept/AcceptElement.cs
+++ Source/Pe/Pe.Main/Models/Element/Accept/AcceptElement.cs
@@ -39,7 +39,8 @@ namespace ContentTypeTextNet.Pe.Main.Models.Element.Accept
                 ["COPYRIGHT"] = BuildStatus.Copyright,
                 ["PROJECT-URI"] = ApplicationConfiguration.General.ProjectRepositoryUri.ToString(),
                 ["FORUM-URI"] = ApplicationConfiguration.General.ProjectForumUri.ToString(),
-                ["WEBSITE-URI"] = ApplicationConfiguration.General.AuthorWebSiteUri.ToString(),
+                ["WEBSITE-URI"] = ApplicationConfiguration.General.ProjectWebSiteUri.ToString(),
+                ["AUTHOR-URI"] = ApplicationConfiguration.General.AuthorWebSiteUri.ToString(),
             }.ToDictionary(i => i.Key, i => SecurityElement.Escape(i.Value)!)
             ;
             var rawXml = TextUtility.ReplaceFromDictionary(Properties.Resources.File_Accept_AcceptDocument, map);
