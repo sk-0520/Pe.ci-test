1dbf22426e4ece55d52fa3316520f378b3d9cfcf
sk <content.type.text.net@gmail.com>
2023-01-26 18:27:44 +0900

    こんな感じかなぁ？

--- Source/Pe/Pe.Core/Models/WeakEvent.cs
+++ Source/Pe/Pe.Core/Models/WeakEvent.cs
@@ -1,71 +1,128 @@
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Data.SQLite;
 using System.Diagnostics.Tracing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using Microsoft.Extensions.Logging;
 
 namespace ContentTypeTextNet.Pe.Core.Models
 {
-    public class WeakEvent<TEventSource, TEventArgs>: WeakEventManager
-        where TEventSource : class
+    public class WeakEvent<TEventListener, TEventArgs>
+        where TEventListener : class
         where TEventArgs : EventArgs
     {
-        public WeakEvent()
-            : base()
+        #region define
+
+        private readonly record struct WeakHandler
         {
+            public WeakHandler(EventHandler<TEventArgs> handler)
+            {
+                var target = handler.Target;
+                if(target is TEventListener listener) {
+                    Listener = new WeakReference<TEventListener>(listener);
+                } else {
+                    throw new ArgumentException($"{nameof(handler)}.{nameof(handler.Target)}", nameof(handler));
+                }
+
+                MethodInfo = handler.GetMethodInfo();
+            }
+
+            #region property
+
+            public WeakReference<TEventListener> Listener { get; }
+            public MethodInfo MethodInfo { get; }
+
+            #endregion
+        }
+
+        #endregion
+
+        #region variable
+
+        private readonly object _locker = new();
+
+        #endregion
+
+        public WeakEvent(string eventName, ILoggerFactory loggerFactory)
+        {
+            Logger = loggerFactory.CreateLogger(eventName + ": " + GetType());
         }
 
         #region property
 
+        ILogger Logger { get; }
+
+        private IList<WeakHandler> Handlers { get; } = new List<WeakHandler>();
 
         #endregion
 
         #region function
 
-        public void Invoke(TEventSource sender, TEventArgs e)
+        public void Raise(object sender, TEventArgs eventArgs)
         {
-            DeliverEvent(sender, e);
+            IReadOnlyList<WeakHandler> weakHandlers;
+
+            lock(this._locker) {
+                weakHandlers = Handlers.ToList();
+            }
+
+            var parameters = new object[] { sender, eventArgs };
+
+            for(var i = weakHandlers.Count - 1; 0 <= i; i--) {
+                var handler = weakHandlers[i];
+
+                if(handler.Listener.TryGetTarget(out var listener)) {
+                    handler.MethodInfo.Invoke(listener, parameters);
+                }
+            }
+
+            Refresh();
         }
 
-        public void Register(TEventSource sender)
+        public void Add(EventHandler<TEventArgs> eventHandler)
         {
+            var weakHandler = new WeakHandler(eventHandler);
+
+            lock(this._locker) {
+                Handlers.Add(weakHandler);
+            }
         }
 
-        #endregion
-
-        #region WeakEventManager
-
-        protected override void StartListening(object source)
+        public bool Remove(EventHandler<TEventArgs> eventHandler)
         {
-            throw new NotImplementedException();
+            lock(this._locker) {
+                for(var i = 0; i < Handlers.Count; i++) {
+                    var handler = Handlers[i];
+
+                    if(handler.Listener.TryGetTarget(out var listener)) {
+                        if(eventHandler.Target == listener && handler.MethodInfo == eventHandler.Method) {
+                            Handlers.RemoveAt(i);
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            return false;
         }
 
-        protected override void StopListening(object source)
+        private void Refresh()
         {
-            throw new NotImplementedException();
+            lock(this._locker) {
+                for(var i = Handlers.Count - 1; 0 <= i; i--) {
+                    var handler = Handlers[i];
+
+                    if(!handler.Listener.TryGetTarget(out _)) {
+                        Handlers.RemoveAt(i);
+                    }
+                }
+            }
         }
-
-        #endregion
-    }
-
-    public class WeakEvent2<TEventSource, TEventArgs>
-        where TEventSource : class
-        where TEventArgs : EventArgs
-    {
-        public WeakEvent2()
-        {
-        }
-
-        #region property
-
-        // 辞書の中に弱い参照ぶちこんどきゃいいんじゃないかなぁ。
-
-        #endregion
-
-        #region function
 
         #endregion
     }
