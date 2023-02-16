639492ffa1cccfbfd0ecd31ec3ad3c13549e0f36
sk <content.type.text.net@gmail.com>
2023-02-16 20:45:14 +0900

    HashUtility 分離

--- Source/Pe/Pe.Standard.Base/Models/HashUtility.cs
+++ Source/Pe/Pe.Standard.Base/Models/HashUtility.cs
@@ -1,12 +1,9 @@
 using System;
 using System.Collections.Generic;
-using System.DirectoryServices;
-using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace ContentTypeTextNet.Pe.Core.Models
+namespace ContentTypeTextNet.Pe.Standard.Base.Models
 {
     public static class HashUtility
     {
