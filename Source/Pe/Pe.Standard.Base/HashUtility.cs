7e994254b0a886f10c27e6583c4c4504b8747038
sk <content.type.text.net@gmail.com>
2023-02-23 22:55:37 +0900

    ハッシュ生成処理の生成は enum に寄せた

--- Source/Pe/Pe.Standard.Base/HashUtility.cs
+++ Source/Pe/Pe.Standard.Base/HashUtility.cs
@@ -5,9 +5,31 @@ using System.Text;
 
 namespace ContentTypeTextNet.Pe.Standard.Base
 {
+    public enum HashAlgorithmKind
+    {
+        Unknown,
+        SHA1,
+        SHA256,
+        SHA384,
+        SHA512,
+        MD5,
+    }
+
     public static class HashUtility
     {
         #region function
+
+        public static HashAlgorithm Create(HashAlgorithmKind hashAlgorithmKind)
+        {
+            return hashAlgorithmKind switch {
+                HashAlgorithmKind.SHA1 => SHA1.Create(),
+                HashAlgorithmKind.SHA256 => SHA256.Create(),
+                HashAlgorithmKind.SHA384 => SHA384.Create(),
+                HashAlgorithmKind.SHA512 => SHA512.Create(),
+                HashAlgorithmKind.MD5 => MD5.Create(),
+                _ => throw new NotImplementedException()
+            };
+        }
 
         /// <summary>
         /// .NET7 で使えなくなった <see cref="HashAlgorithm.Create(string)"/> のラッパー。
@@ -21,22 +43,22 @@ namespace ContentTypeTextNet.Pe.Standard.Base
             switch(algorithmName.ToUpperInvariant()) {
                 case "SHA":
                 case "SHA1":
-                    return SHA1.Create();
+                    return Create(HashAlgorithmKind.SHA1);
 
                 case "SHA256":
                 case "SHA-256":
-                    return SHA256.Create();
+                    return Create(HashAlgorithmKind.SHA256);
 
                 case "SHA384":
                 case "SHA-384":
-                    return SHA384.Create();
+                    return Create(HashAlgorithmKind.SHA384);
 
                 case "SHA512":
                 case "SHA-512":
-                    return SHA512.Create();
+                    return Create(HashAlgorithmKind.SHA512);
 
                 case "MD5":
-                    return MD5.Create();
+                    return Create(HashAlgorithmKind.MD5);
 
                 case "SYSTEM.SECURITY.CRYPTOGRAPHY.SHA1":
                 case "SYSTEM.SECURITY.CRYPTOGRAPHY.SHA256":
