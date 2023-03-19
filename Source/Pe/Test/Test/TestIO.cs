2f601790d9bc529fe539ae6bb416efea25d42587
sk <content.type.text.net@gmail.com>
2023-03-19 21:01:27 +0900

    Squashed commit of the following:
    commit bfc7f7c42494946c2a019fd745030d728a64c8ce
    Author: sk <content.type.text.net@gmail.com>
    Date:   Sun Mar 19 20:47:05 2023 +0900
    
        ファイルめんどいなぁ
    
    commit 728ecd95b871eb21a715d1dc53be426578e3fecf
    Author: sk <content.type.text.net@gmail.com>
    Date:   Sun Mar 19 15:36:11 2023 +0900
    
        rename
    
    commit d0f8e40502e29259932c39937e232f65e77eb9c1
    Author: sk <content.type.text.net@gmail.com>
    Date:   Sun Mar 19 10:19:10 2023 +0900
    
        増えるやもしれんし * でなぁなぁにしておく
    
    commit 0cc1e5286b52538bc5934c175d661964d500e475
    Author: sk <content.type.text.net@gmail.com>
    Date:   Sun Mar 19 00:08:04 2023 +0900
    
        test: MakeFileParentDirectory
    
    commit d41f14e13395405f1502b2856464c00124f7cac5
    Author: sk <content.type.text.net@gmail.com>
    Date:   Sat Mar 18 23:39:49 2023 +0900
    
        多分これでいいはず
    
    commit c06edbad9b29ddba2340e1af3f4654502c6e0cdb
    Author: sk <content.type.text.net@gmail.com>
    Date:   Sat Mar 18 23:04:18 2023 +0900
    
        ファイルテスト用の共通処理のファイルを追加

---
+++ Source/Pe/Test/Test/TestIO.cs
@@ -0,0 +1,157 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using Microsoft.VisualStudio.TestPlatform.PlatformAbstractions.Interfaces;
+
+namespace ContentTypeTextNet.Pe.Test
+{
+    /// <summary>
+    /// IO系のテスト系インフラ。
+    /// </summary>
+    public static class TestIO
+    {
+        #region property
+
+        private static string RootDirectoryName { get; } = "_test_io_";
+
+        private static bool InitializedProjectDirectory { get; set; }
+        private static ISet<string> InitializedClassDirectory { get; set; } = new HashSet<string>();
+        private static ISet<string> InitializedMethodDirectory { get; set; } = new HashSet<string>();
+
+        #endregion
+
+        #region function
+
+        private static DirectoryInfo GetProjectDirectory()
+        {
+            var projectTestPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!;
+            var projectTestDirPath = Path.Combine(projectTestPath, RootDirectoryName);
+
+            return new DirectoryInfo(projectTestDirPath);
+        }
+
+        private static DirectoryInfo GetClassDirectory(object test)
+        {
+            var project = GetProjectDirectory();
+            var classTestDirPath = Path.Combine(project.FullName, test.GetType().FullName ?? throw new Exception(test.ToString()));
+
+            return new DirectoryInfo(classTestDirPath);
+        }
+
+        private static DirectoryInfo GetMethodDirectory(object test, string callerMemberName, int callerLineNumber)
+        {
+            var classTestDirPath = GetClassDirectory(test);
+            var methodTestDirPath = Path.Combine(classTestDirPath.FullName, callerMemberName + "-L" + callerLineNumber.ToString(CultureInfo.InvariantCulture));
+
+            return new DirectoryInfo(methodTestDirPath);
+        }
+
+        public static DirectoryInfo CreateDirectory(DirectoryInfo dir)
+        {
+            dir.Refresh();
+            if(dir.Exists) {
+                dir.Delete(true);
+            }
+            dir.Create();
+
+            return dir;
+        }
+
+        /// <summary>
+        /// テストプロジェクト用テスト初期化。
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public static DirectoryInfo InitializeProject()
+        {
+            if(InitializedProjectDirectory) {
+                throw new TestException();
+            }
+
+            var dir = GetProjectDirectory();
+            var result = CreateDirectory(dir);
+
+            InitializedProjectDirectory = true;
+
+            return result;
+        }
+
+        /// <summary>
+        /// テストクラス用テスト初期化。
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public static DirectoryInfo InitializeClass(Type type)
+        {
+            var dir = GetClassDirectory(type);
+
+            if(InitializedClassDirectory.Contains(dir.FullName)) {
+                throw new TestException();
+            }
+
+            var result = CreateDirectory(dir);
+
+            InitializedClassDirectory.Add(dir.FullName);
+
+            return result;
+        }
+
+        /// <summary>
+        /// テストメソッド用テスト初期化。
+        /// </summary>
+        /// <param name="test"></param>
+        /// <param name="callerMemberName"></param>
+        /// <returns></returns>
+        public static DirectoryInfo InitializeMethod(object test, [CallerMemberName] string callerMemberName = "", [CallerLineNumber] int callerLineNumber = 0)
+        {
+            var dir = GetMethodDirectory(test, callerMemberName, callerLineNumber);
+
+            if(InitializedMethodDirectory.Contains(dir.FullName)) {
+                throw new TestException();
+            }
+
+            var result = CreateDirectory(dir);
+
+            InitializedMethodDirectory.Add(dir.FullName);
+
+            return result;
+        }
+
+        public static DirectoryInfo CreateDirectory(DirectoryInfo directory, string name)
+        {
+            var dirPath = Path.Combine(directory.FullName, name);
+            return Directory.CreateDirectory(dirPath);
+        }
+
+        public static FileInfo CreateEmptyFile(DirectoryInfo directory, string name)
+        {
+            var filePath = Path.Combine(directory.FullName, name);
+            using(File.Create(filePath)) {
+            }
+
+            return new FileInfo(filePath);
+        }
+
+        public static FileInfo CreateTextFile(DirectoryInfo directory, string name, string content, Encoding encoding)
+        {
+            var filePath = Path.Combine(directory.FullName, name);
+
+            using(var stream = File.Create(filePath)) {
+                using var writer = new StreamWriter(stream, encoding);
+                writer.Write(content);
+            }
+
+            return new FileInfo(filePath);
+        }
+        public static FileInfo CreateTextFile(DirectoryInfo directory, string name, string content) => CreateTextFile(directory, name, content, Encoding.UTF8);
+
+        #endregion
+    }
+}
