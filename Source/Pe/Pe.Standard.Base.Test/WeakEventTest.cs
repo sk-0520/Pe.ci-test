6ab807380316d6d6acdf8d9c792a5c49431e91fb
sk <content.type.text.net@gmail.com>
2023-03-21 00:45:18 +0900

    非ジェネリクス弱いイベント

--- Source/Pe/Pe.Standard.Base.Test/WeakEventTest.cs
+++ Source/Pe/Pe.Standard.Base.Test/WeakEventTest.cs
@@ -10,7 +10,7 @@ namespace ContentTypeTextNet.Pe.Standard.Base.Test
 {
     file class EventSource
     {
-        #region define
+        #region event
 
         public event EventHandler<EventArgs>? Strong;
         public event EventHandler<EventArgs>? Weak
@@ -18,12 +18,18 @@ namespace ContentTypeTextNet.Pe.Standard.Base.Test
             add => WeakEvent.Add(value);
             remove => WeakEvent.Remove(value);
         }
+        public event EventHandler? NoGenerics
+        {
+            add => NoGenericsEvent.Add(value);
+            remove => NoGenericsEvent.Remove(value);
+        }
 
         #endregion
 
         #region property
 
         private WeakEvent<EventArgs> WeakEvent { get; } = new WeakEvent<EventArgs>(nameof(Weak));
+        private WeakEvent NoGenericsEvent { get; } = new WeakEvent(nameof(NoGenerics));
 
         #endregion
 
@@ -39,6 +45,11 @@ namespace ContentTypeTextNet.Pe.Standard.Base.Test
             WeakEvent?.Raise(this, EventArgs.Empty);
         }
 
+        public void RaiseNoGenerics()
+        {
+            NoGenericsEvent?.Raise(this, EventArgs.Empty);
+        }
+
         #endregion
     }
 
@@ -46,8 +57,9 @@ namespace ContentTypeTextNet.Pe.Standard.Base.Test
     {
         #region property
 
-        public int StrongCount { get; set; } = 0;
-        public int WeakCount { get; set; } = 0;
+        public int StrongCount { get; private set; } = 0;
+        public int WeakCount { get; private set; } = 0;
+        public int NoGenericsCount { get; private set; } = 0;
 
         #endregion
 
@@ -69,6 +81,16 @@ namespace ContentTypeTextNet.Pe.Standard.Base.Test
             }
         }
 
+        public void NoGenerics(object? sender, EventArgs e)
+        {
+            if(NoGenericsCount == 0) {
+                NoGenericsCount += 1;
+                Assert.IsTrue(true);
+            } else {
+                Assert.Fail();
+            }
+        }
+
         #endregion
     }
 
@@ -79,6 +101,7 @@ namespace ContentTypeTextNet.Pe.Standard.Base.Test
 
         public int StrongTestCount { get; set; } = 0;
         public int WeakTestCount { get; set; } = 0;
+        public int NoGenericsTestCount { get; set; } = 0;
 
         #endregion
 
@@ -144,6 +167,36 @@ namespace ContentTypeTextNet.Pe.Standard.Base.Test
             Assert.AreEqual(3, WeakTestCount);
         }
 
+        [TestMethod]
+        public void NoGenericsTest()
+        {
+            var source = new EventSource();
+
+            source.RaiseNoGenerics();
+            Assert.AreEqual(0, NoGenericsTestCount);
+
+            source.NoGenerics += Source_NoGenerics;
+            source.RaiseNoGenerics();
+            Assert.AreEqual(1, NoGenericsTestCount);
+
+            static void Scope(EventSource source)
+            {
+                var listener = new EventListener();
+                source.NoGenerics += listener.NoGenerics;
+                source.RaiseNoGenerics();
+                listener = null;
+            }
+            Scope(source);
+            Assert.AreEqual(2, NoGenericsTestCount);
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+
+            source.RaiseNoGenerics();
+            Assert.AreEqual(3, NoGenericsTestCount);
+        }
+
         #endregion
 
         private void Source_Strong(object? sender, EventArgs e)
@@ -155,6 +208,10 @@ namespace ContentTypeTextNet.Pe.Standard.Base.Test
         {
             WeakTestCount += 1;
         }
-    }
 
+        private void Source_NoGenerics(object? sender, EventArgs e)
+        {
+            NoGenericsTestCount += 1;
+        }
+    }
 }
