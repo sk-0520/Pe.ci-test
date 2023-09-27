1515579ef73852627366cd11eda268d8d1a38656
sk <content.type.text.net@gmail.com>
2023-09-27 19:16:49 +0900

    #905: Pe.Publish は Pe に統合すべき
    Squashed commit of the following:
    
    commit 86182362a6557ca9a1781d8d2d9e42b64b904253
    Author: sk <content.type.text.net@gmail.com>
    Date:   Wed Sep 27 19:16:01 2023 +0900
    
        doc
    
    commit e115eb9b21ab44784375a124de5b3adb24435f04
    Author: sk <content.type.text.net@gmail.com>
    Date:   Wed Sep 27 19:05:04 2023 +0900
    
        rename
    
    commit 92f3578fae1e4164eea06b8d05ed41283c475279
    Author: sk <content.type.text.net@gmail.com>
    Date:   Wed Sep 27 19:02:14 2023 +0900
    
        extension
    
    commit 0bb046379682fd875f85ffb5fcd5581bb99ad71f
    Author: sk <content.type.text.net@gmail.com>
    Date:   Wed Sep 27 19:00:42 2023 +0900
    
        ^v^
    
    commit 55df8b8a142b0f050d5b2c607c88bba39c684185
    Author: sk <content.type.text.net@gmail.com>
    Date:   Wed Sep 27 18:58:58 2023 +0900
    
        何がどうなってんのか知らん
    
    commit f3be3e433fb5a5669df78de1468f1cb5aaf1f174
    Author: sk <content.type.text.net@gmail.com>
    Date:   Wed Sep 27 18:56:19 2023 +0900
    
        aaaa
    
    commit 5ada3e6117bccc229c9c76838ec671e89c48dbf0
    Author: sk <content.type.text.net@gmail.com>
    Date:   Wed Sep 27 18:55:26 2023 +0900
    
        なんか知らんけどコピペしまくった

---
+++ Build/package-latest-version.ps1
@@ -0,0 +1,83 @@
+$ErrorActionPreference = 'Stop'
+Set-StrictMode -Version Latest
+$currentDirPath = Split-Path -Parent $MyInvocation.MyCommand.Path
+$scriptFileNames = @(
+	'version.ps1',
+	'command.ps1'
+);
+foreach ($scriptFileName in $scriptFileNames) {
+	$scriptFilePath = Join-Path $currentDirPath $scriptFileName
+	. $scriptFilePath
+}
+
+$verionEndPoint = [uri]"https://peserver.gq/api/application/version/update"
+$extension = '7z'
+$response = Invoke-WebRequest -Uri $verionEndPoint -Method Get
+$latestResult = ConvertFrom-Json ([System.Text.Encoding]::UTF8.GetString($response.Content))
+$latestItem = $latestResult.items[0]
+$latestVersion = $latestItem.version
+
+$workDirectoryPath = ".\work"
+$outputDirectoryPath = ".\package"
+$gitHubReleaseUrl = "https://github.com/sk-0520/Pe/releases/download"
+$7zipPath = "%PROGRAMFILES%\7-Zip\7z.exe"
+
+$_workDirectoryPath = [System.IO.Path]::GetFullPath([System.Environment]::ExpandEnvironmentVariables($workDirectoryPath))
+$_outputDirectoryPath = [System.IO.Path]::GetFullPath([System.Environment]::ExpandEnvironmentVariables($outputDirectoryPath))
+$_7zipPath = [System.Environment]::ExpandEnvironmentVariables($7zipPath)
+
+Write-Verbose "[`$WorkDirectoryPath] $_workDirectoryPath"
+Write-Verbose "[`$OutputDirectoryPath] $_outputDirectoryPath"
+Write-Verbose "[`$7zipPath] $_7zipPath"
+
+Write-Host "Version = $latestVersion"
+
+$versions = @{
+	"path" = ConvertVersion $latestVersion "."
+	"file" = "Pe_" + (ConvertVersion $latestVersion "-")
+}
+
+$platforms = @(
+	"x86",
+	"x64"
+)
+
+$archiveFiles = @()
+
+if (!(Test-Path -Path $_workDirectoryPath)) {
+	New-Item -Path $_workDirectoryPath -ItemType Directory
+}
+
+foreach ($platform in $platforms) {
+	$url = $gitHubReleaseUrl.Trim("/") + "/" + $versions["path"] + "/" + $versions["file"] + "_${platform}.${extension}";
+	$fileName = $versions["file"] + "_${platform}.${extension}"
+
+	Write-Verbose "[$platform] $url $fileName"
+
+	$filePath = Join-Path -Path $_workDirectoryPath -ChildPath $fileName
+	Invoke-WebRequest -Uri $url -Method Get -OutFile $filePath
+	$archiveFiles += $filePath
+}
+
+if(!(Test-Path -Path $_outputDirectoryPath)) {
+	New-Item -Path $_outputDirectoryPath -ItemType Directory
+}
+Remove-Item -Path (Join-Path -Path $_outputDirectoryPath -ChildPath "*") -Force -Recurse
+
+foreach($filePath in $archiveFiles) {
+	Write-Verbose "[TARGET] $filePath"
+
+	$parentDirPath = [System.IO.Path]::GetDirectoryName($filePath)
+	$baseFileName = [System.IO.Path]::GetFileNameWithoutExtension($filePath)
+	$sfxName = $baseFileName + '.exe'
+	$outputPath = [System.IO.Path]::Combine($_outputDirectoryPath, $sfxName)
+
+	try {
+		Push-Location $parentDirPath
+		& "${_7zipPath}" x -y "-o$baseFileName" "$filePath"
+		& "${_7zipPath}" a $outputPath -sfx -m0=lzma2 -mx=9 -mfb=64 -md=64m -ms=on $baseFileName\* -r
+	} finally {
+		Pop-Location
+	}
+
+}
