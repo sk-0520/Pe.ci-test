de40a6fef5ec6b9874844e5aa9bfa8d2ceb549e8
sk <content.type.text.net@gmail.com>
2023-10-29 17:12:32 +0900

    まぁそんな感じ

--- Build/reconstitute-application.ps1
+++ Build/reconstitute-application.ps1
@@ -1,5 +1,6 @@
 Param(
 	[Parameter(mandatory = $true)][ValidateSet('x86', 'x64')][string] $Platform,
+	[Parameter(mandatory = $true)][ValidateSet('zip', '7z', 'tar')][string] $Archive,
 	[switch] $ProductMode,
 	[Parameter(mandatory = $true)][string] $InputDirectory,
 	[Parameter(mandatory = $true)][string] $OutputDirectory
@@ -18,4 +19,21 @@ foreach ($scriptFileName in $scriptFileNames) {
 #/*[FUNCTIONS]-------------------------------------
 #*/[FUNCTIONS]-------------------------------------
 
-Write-Output "HELLO"
+$inputDirectories = @{
+	buildTools = Join-Path $InputDirectory 'buildtools'
+	help = Join-Path $InputDirectory 'help'
+	boot = Join-Path $InputDirectory 'boot'
+	main = Join-Path $InputDirectory 'main-bin'
+}
+
+# 出力ディレクトリになんかあっても面倒なので更地にしてからあれこれする
+if ( Test-Path -Path $OutputDirectory ) {
+	Remove-Item -Path $OutputDirectory -Force -Recurse
+}
+New-Item -Path $OutputDirectory -ItemType Directory
+
+# boot 処理の整理
+Copy-Item -Path (Join-Path -Path $inputDirectories.boot -ChildPath '*') -Destination $OutputDirectory
+
+
+
