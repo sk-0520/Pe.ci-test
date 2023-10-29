9b895b25b97bf4ee810164f2ec650a84a0e761ec
sk <content.type.text.net@gmail.com>
2023-10-29 18:52:09 +0900

    sql.sqlite3

---
+++ Build/buildtools-sql.ps1
@@ -0,0 +1,23 @@
+Param(
+	[Parameter(mandatory = $true)][string] $BuildToolsSqlPack,
+	[Parameter(mandatory = $true)][string] $OutputFile
+)
+$ErrorActionPreference = 'Stop'
+Set-StrictMode -Version Latest
+$currentDirPath = Split-Path -Parent $MyInvocation.MyCommand.Path
+$scriptFileNames = @(
+	'project.ps1'
+);
+foreach ($scriptFileName in $scriptFileNames) {
+	$scriptFilePath = Join-Path $currentDirPath $scriptFileName
+	. $scriptFilePath
+}
+
+$sourceDirectory = GetSourceDirectory 'main'
+$sqlDirectory = Join-Path -Path $sourceDirectory -ChildPath 'Pe.Main' | Join-Path -ChildPath 'etc' | Join-Path -ChildPath  'sql'
+
+Write-Output "pack SQL"
+if (Test-Path $OutputFile) {
+	Remove-Item -Path $OutputFile
+}
+& "$BuildToolsSqlPack" --sql-root-dir $sqlDirectory --output $OutputFile
