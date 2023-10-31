dab277db9b207b31668de53bea930cebd246db0d
sk <content.type.text.net@gmail.com>
2023-10-31 18:56:00 +0900

    x86 <-> x64 でお互いに不要なリソースを破棄

--- Build/reconstitute-application.ps1
+++ Build/reconstitute-application.ps1
@@ -59,7 +59,7 @@ Move-Item -Path $srcLibDir -Destination $dstLibDir
 
 # etc/appsettings.*.json の整理
 $outputEtcDir = Join-Path -Path $OutputDirectory -ChildPath 'etc'
-if($BuildType -ne "BETA") {
+if ($BuildType -ne "BETA") {
 	Remove-Item -Path (Join-Path -Path $outputEtcDir -ChildPath "appsettings.beta.json")
 }
 Remove-Item -Path (Join-Path -Path $outputEtcDir -ChildPath "@appsettings.debug.json")
@@ -73,3 +73,20 @@ Move-Item -Path $inputItems.sql -Destination $outputSqlDir
 # doc/help を生成済みヘルプに置き換え(ダミーのindex.htmlがあるので上書きOK)
 $helpRootDir = Join-Path -Path $OutputDirectory -ChildPath 'doc'
 Move-Item $inputItems.help -Destination $helpRootDir -Force
+
+
+# プラットフォームに合わないディレクトリを破棄(機械的にやってもいいけどちょっと自信ないのです)
+$unsupportPlatform = switch ($Platform) {
+	'x64' { 'x86' }
+	'x86' { 'x64' }
+	Default { throw "error: $Platform" }
+}
+$unsupportTargets = @(
+	Join-Path -Path $outputMainDir -ChildPath $unsupportPlatform
+	Join-Path -Path $dstLibDir -ChildPath 'Redist.MSVC.CRT' | Join-Path -ChildPath $unsupportPlatform
+)
+foreach ($unsupportTarget in $unsupportTargets) {
+	Write-Output "Remove: $unsupportTarget"
+	Remove-Item $unsupportTarget -Recurse -Force
+}
+
