c26d4eac7d4279cb44ed9015fcb251f7a920d360
sk <content.type.text.net@gmail.com>
2023-11-05 23:15:05 +0900

    多分使ってないビルド処理を破棄

--- Build/make-buildtools.ps1
+++ Build/make-buildtools.ps1
@@ -3,14 +3,9 @@ Param(
 $ErrorActionPreference = 'Stop'
 Set-StrictMode -Version Latest
 $currentDirPath = Split-Path -Parent $MyInvocation.MyCommand.Path
-$scriptFileNames = @(
-	'command.ps1'
-);
-foreach ($scriptFileName in $scriptFileNames) {
-	$scriptFilePath = Join-Path $currentDirPath $scriptFileName
-	. $scriptFilePath
-}
+
 $rootDirectory = Split-Path -Path $currentDirPath -Parent
+
 $builToolDirPath = Join-Path $rootDirectory "Output\tools"
 
 dotnet build Source/BuildTools/SqlPack/SqlPack.csproj --verbosity normal --configuration Debug /p:Platform=x86 --runtime win-x86 --output $builToolDirPath --no-self-contained
