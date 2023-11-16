1e365a63dc3c5489d829157ec65e4eb6160552f3
sk <content.type.text.net@gmail.com>
2023-11-16 21:32:17 +0900

    各種内部プロジェクトの .NET バージョンを切り替え

--- Source/Pe/Test/Test/TestException.cs
+++ Source/Pe/Test/Test/TestException.cs
@@ -1,16 +1,10 @@
 namespace ContentTypeTextNet.Pe.Test
 {
-    /// <summary>
-    /// 単体テストで何かしら変になった時に投げられる例外。
-    /// <para>テストの対象処理自体で投げられることはない。</para>
-    /// </summary>
+    [System.Serializable]
     public class TestException: System.Exception
     {
         public TestException() { }
         public TestException(string message) : base(message) { }
         public TestException(string message, System.Exception inner) : base(message, inner) { }
-        protected TestException(
-          System.Runtime.Serialization.SerializationInfo info,
-          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
     }
 }
