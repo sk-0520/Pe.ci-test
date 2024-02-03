2ad88246e171670c316f53cf5a6fc7a8d119472c
sk <content.type.text.net@gmail.com>
2024-02-03 18:50:50 +0900

    下準備

--- Build/upload-release-assets.ps1
+++ Build/upload-release-assets.ps1
@@ -8,7 +8,46 @@ Param(
 $ErrorActionPreference = 'Stop'
 Set-StrictMode -Version Latest
 
+$rootDirectory = Get-RootDirectory
+
 #/*[FUNCTIONS]-------------------------------------
+
+function Invoke-GithubAsset {
+	param (
+		[Parameter(mandatory = $true)][string] $Token,
+		[Parameter(mandatory = $true)][string] $ReleaseId,
+		[Parameter(mandatory = $true)][string] $RepositoryOwner,
+		[Parameter(mandatory = $true)][string] $RepositoryName,
+		[Parameter(mandatory = $true)][string] $FilePath,
+		[string] $ContentType,
+		[string] $FileName
+	)
+}
+
+function Invoke-Asset {
+	param (
+		[Parameter(mandatory = $true)][ValidateSet('github')][string] $TargetRepository,
+		[Parameter(mandatory = $true)][string] $Token,
+		[Parameter(mandatory = $true)][string] $ReleaseId,
+		[Parameter(mandatory = $true)][string] $RepositoryOwner,
+		[Parameter(mandatory = $true)][string] $RepositoryName,
+		[Parameter(mandatory = $true)][string] $FilePath,
+		[string] $ContentType,
+		[string] $FileName
+	)
+
+	switch ($TargetRepository) {
+		'github' {
+			Invoke-GithubAsset -Token $Token -ReleaseId $ReleaseId -RepositoryOwner $RepositoryOwner -RepositoryName $RepositoryName -FilePath $FilePath -ContentType $ContentType -FileName $FileName
+		}
+
+		Default {
+			throw $TargetRepository
+		}
+	}
+
+}
+
 #*/[FUNCTIONS]-------------------------------------
 
 Write-Host "TargetRepository: $TargetRepository"
