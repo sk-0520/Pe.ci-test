665350201eda4e3baf7360fd93375ee65547be5e
sk <content.type.text.net@gmail.com>
2024-02-07 00:41:12 +0900

    しんどい

--- Build/generate-update-information.ps1
+++ Build/generate-update-information.ps1
@@ -48,7 +48,7 @@ function Convert-Template {
 
 	$work = $Source
 
-	$work = $work.Replace('@VERSION@', (Get-ApplicationVersion -Version $version -Separator '.'))
+	$work = $work.Replace('@VERSION@', (Convert-Version -Version $version -Separator '.'))
 	$work = $work.Replace('@REVISION@', $Revision)
 
 	return $work
