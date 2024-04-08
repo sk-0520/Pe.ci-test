0cd719a91b3ed2523b6753a1c7da5632711f8ffa
sk <content.type.text.net@gmail.com>
2024-04-08 21:34:30 +0900

    Pe.Plugins.Reference.Html の破棄

--- Source/Pe/Pe.Plugins.Reference.Html/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-using ContentTypeTextNet.Pe.Bridge.Plugin;
-using ContentTypeTextNet.Pe.Embedded.Attributes;
-
-[assembly: PluginIdentifiers("Pe.Plugins.Reference.Html", "4FA1A634-6B32-4762-8AE8-3E1CF6DF9DB1")]
-[assembly: PluginSupportVersions("0.0.0", "0.0.0")]
-[assembly: PluginAuthors("sk", PluginLicense.DoWhatTheF_ckYouWantToPublicLicense2, "https://content-type-text.net/", "https://github.com/sk-0520/Pe")]
