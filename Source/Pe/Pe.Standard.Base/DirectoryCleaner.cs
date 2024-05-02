812231ebb45772c61606bae7474e94fbb819c85d
sk <content.type.text.net@gmail.com>
2024-05-02 21:17:38 +0900

    なんかそれっぽく

--- Source/Pe/Pe.Standard.Base/DirectoryCleaner.cs
+++ Source/Pe/Pe.Standard.Base/DirectoryCleaner.cs
@@ -14,7 +14,7 @@ namespace ContentTypeTextNet.Pe.Standard.Base
     public class DirectoryCleaner
     {
         /// <summary>
-        /// 
+        /// 生成。
         /// </summary>
         /// <param name="directory">対象ディレクトリ。</param>
         /// <param name="retryCount">ディレクトリ作成失敗時の再試行回数。</param>
@@ -33,7 +33,7 @@ namespace ContentTypeTextNet.Pe.Standard.Base
         /// <summary>
         /// 対象ディレクトリ。
         /// </summary>
-        private DirectoryInfo Directory { get; }
+        public DirectoryInfo Directory { get; }
         /// <summary>
         /// 待機回数。
         /// </summary>
