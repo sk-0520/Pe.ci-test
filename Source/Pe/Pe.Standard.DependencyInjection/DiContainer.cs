eb075cd0cc15c2ad76dda9ad00513aac09ae2526
sk <content.type.text.net@gmail.com>
2024-05-03 21:11:59 +0900

    ArrayPoolObject

--- Source/Pe/Pe.Standard.DependencyInjection/DiContainer.cs
+++ Source/Pe/Pe.Standard.DependencyInjection/DiContainer.cs
@@ -170,7 +170,7 @@ namespace ContentTypeTextNet.Pe.Standard.DependencyInjection
 
         private static List<KeyValuePair<Type, object?>> BuildManualParameters(IReadOnlyList<object> manualParameters)
         {
-            using var arrayPoolDisposer = new ArrayPoolValue<KeyValuePair<Type, object?>>(manualParameters.Count);
+            using var arrayPoolDisposer = new ArrayPoolObject<KeyValuePair<Type, object?>>(manualParameters.Count);
             int resultIndex = 0;
             foreach(var manualParameter in manualParameters) {
                 if(manualParameter is null) {
