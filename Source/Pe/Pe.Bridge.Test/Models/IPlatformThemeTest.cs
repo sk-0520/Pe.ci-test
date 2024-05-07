e2447e42ff23ae83a2ced5c4ca91a4c5be9196a4
sk <content.type.text.net@gmail.com>
2024-05-07 19:36:33 +0900

    PlatformThemeColorsTest

--- Source/Pe/Pe.Bridge.Test/Models/IPlatformThemeTest.cs
+++ Source/Pe/Pe.Bridge.Test/Models/IPlatformThemeTest.cs
@@ -10,6 +10,43 @@ using Xunit;
 
 namespace ContentTypeTextNet.Pe.Bridge.Test.Models
 {
+    public class PlatformThemeColorsTest
+    {
+        #region function
+
+        [Fact]
+        public void Test()
+        {
+            var actual = new PlatformThemeColors(Colors.Lime, Colors.Red, Colors.Gray, Colors.DarkGray);
+
+            Assert.Equal(Colors.Lime, actual.Background);
+            Assert.Equal(Colors.Red, actual.Foreground);
+            Assert.Equal(Colors.Gray, actual.Control);
+            Assert.Equal(Colors.DarkGray, actual.Border);
+        }
+
+        #endregion
+    }
+
+    public class PlatformAccentColorsTest
+    {
+        #region function
+
+        [Fact]
+        public void Test()
+        {
+            var actual = new PlatformAccentColors(Colors.Lime, Colors.Red, Colors.Gray, Colors.DarkGray, Colors.Yellow);
+
+            Assert.Equal(Colors.Lime, actual.Accent);
+            Assert.Equal(Colors.Red, actual.Base);
+            Assert.Equal(Colors.Gray, actual.Highlight);
+            Assert.Equal(Colors.DarkGray, actual.Active);
+            Assert.Equal(Colors.Yellow, actual.Disable);
+        }
+
+        #endregion
+    }
+
     public class IPlatformThemeLoaderExtensionsTest
     {
         #region function
