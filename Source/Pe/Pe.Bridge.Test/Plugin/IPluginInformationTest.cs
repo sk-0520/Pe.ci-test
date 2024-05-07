c9fd353b87390ee897a1dbc5cbab8e3faa4e387e
sk <content.type.text.net@gmail.com>
2024-05-07 21:22:19 +0900

    PluginVersionsTest, PluginAuthorsTest, PluginInformationTest

--- Source/Pe/Pe.Bridge.Test/Plugin/IPluginInformationTest.cs
+++ Source/Pe/Pe.Bridge.Test/Plugin/IPluginInformationTest.cs
@@ -35,4 +35,63 @@ namespace ContentTypeTextNet.Pe.Bridge.Test.Plugin
         }
         #endregion
     }
+
+    public class PluginVersionsTest
+    {
+        #region function
+
+        [Fact]
+        public void ConstructorTest()
+        {
+            var versions = new {
+                Plugin = new Version(50, 0),
+                Minimum = new Version(10, 0),
+                Maximum = new Version(100, 0)
+            };
+            var actual = new PluginVersions(versions.Plugin, versions.Minimum, versions.Maximum, ["a", "b", "c"]);
+            Assert.Equal(versions.Plugin, actual.PluginVersion);
+            Assert.Equal(versions.Minimum, actual.MinimumSupportVersion);
+            Assert.Equal(versions.Maximum, actual.MaximumSupportVersion);
+            Assert.Equal(["a", "b", "c"], actual.CheckUrls);
+        }
+
+        #endregion
+    }
+
+    public class PluginAuthorsTest
+    {
+        #region function
+
+        [Fact]
+        public void ConstructorTest()
+        {
+            var actual = new PluginAuthors(new Author("author"), "license");
+            Assert.Equal("author", actual.PluginAuthor.Name);
+            Assert.Empty(actual.PluginAuthor.Contacts);
+            Assert.Equal("license", actual.PluginLicense);
+        }
+
+        #endregion
+    }
+
+    public class PluginInformationTest
+    {
+        #region function
+
+        [Fact]
+        public void ConstructorTest()
+        {
+            var plugin = new {
+                Id = new PluginIdentifiers(PluginId.NewId(), "pluginName"),
+                Versions = new PluginVersions(new Version(10, 0), new Version(1, 0), new Version(100, 0), ["a, b"]),
+                Authors = new PluginAuthors(new Author("author"), "license"),
+            };
+            var actual = new PluginInformation(plugin.Id, plugin.Versions, plugin.Authors);
+            Assert.Equal(plugin.Id, actual.PluginIdentifiers);
+            Assert.Equal(plugin.Versions, actual.PluginVersions);
+            Assert.Equal(plugin.Authors, actual.PluginAuthors);
+        }
+
+        #endregion
+    }
 }
