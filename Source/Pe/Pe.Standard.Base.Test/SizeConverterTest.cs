606ff0aca3ba8649440eea7adf65777146be570e
sk <content.type.text.net@gmail.com>
2024-05-08 20:30:31 +0900

    SizeConverter テスト追加

--- Source/Pe/Pe.Standard.Base.Test/SizeConverterTest.cs
+++ Source/Pe/Pe.Standard.Base.Test/SizeConverterTest.cs
@@ -17,10 +17,43 @@ namespace ContentTypeTextNet.Pe.Standard.Base.Test
         [InlineData("0.00 byte", 0, "{0:0.00} {1}", new[] { "byte" })]
         [InlineData("1023.00 byte", 1023, "{0:0.00} {1}", new[] { "byte", "KB" })]
         [InlineData("1.00 KB", 1024, "{0:0.00} {1}", new[] { "byte", "KB" })]
-        public void ConvertHumanReadableByteTest(string expected, long byteSize, string sizeFormat, IReadOnlyList<string> units)
+        public void ConvertHumanReadableByte_3_Test(string expected, long byteSize, string sizeFormat, IReadOnlyList<string> units)
         {
             var sc = new SizeConverter();
             var actual = sc.ConvertHumanReadableByte(byteSize, sizeFormat, units);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("0.00 byte", 0, new[] { "byte" })]
+        [InlineData("1023.00 byte", 1023, new[] { "byte", "KB" })]
+        [InlineData("1.00 KB", 1024, new[] { "byte", "KB" })]
+        public void ConvertHumanReadableByte_2_1_Test(string expected, long byteSize, IReadOnlyList<string> units)
+        {
+            var sc = new SizeConverter();
+            var actual = sc.ConvertHumanReadableByte(byteSize, units);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("0.00 byte", 0, "{0:0.00} {1}")]
+        [InlineData("1023.00 byte", 1023, "{0:0.00} {1}")]
+        [InlineData("1.00 KB", 1024, "{0:0.00} {1}")]
+        public void ConvertHumanReadableByte_2_2_Test(string expected, long byteSize, string sizeFormat)
+        {
+            var sc = new SizeConverter();
+            var actual = sc.ConvertHumanReadableByte(byteSize, sizeFormat);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("0.00 byte", 0)]
+        [InlineData("1023.00 byte", 1023)]
+        [InlineData("1.00 KB", 1024)]
+        public void ConvertHumanReadableByte_1_Test(string expected, long byteSize)
+        {
+            var sc = new SizeConverter();
+            var actual = sc.ConvertHumanReadableByte(byteSize);
             Assert.Equal(expected, actual);
         }
 
