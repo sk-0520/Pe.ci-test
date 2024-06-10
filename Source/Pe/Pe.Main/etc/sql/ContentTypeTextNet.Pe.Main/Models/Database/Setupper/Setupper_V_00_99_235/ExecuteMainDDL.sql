171d83e5764d23e2db1de97a3412437a6fdbd5ad
sk <content.type.text.net@gmail.com>
2024-06-10 21:27:38 +0900

    add LauncherSeparators

---
+++ Source/Pe/Pe.Main/etc/sql/ContentTypeTextNet.Pe.Main/Models/Database/Setupper/Setupper_V_00_99_235/ExecuteMainDDL.sql
@@ -0,0 +1,20 @@
+--// table: LauncherSeparators
+create table [LauncherSeparators] (
+	[LauncherItemId] text not null /* ランチャーアイテムID  */,
+	[CreatedTimestamp] datetime not null /* 作成タイムスタンプ UTC */,
+	[CreatedAccount] text not null /* 作成ユーザー名  */,
+	[CreatedProgramName] text not null /* 作成プログラム名  */,
+	[CreatedProgramVersion] text not null /* 作成プログラムバージョン  */,
+	[UpdatedTimestamp] datetime not null /* 更新タイムスタンプ UTC */,
+	[UpdatedAccount] text not null /* 更新ユーザー名  */,
+	[UpdatedProgramName] text not null /* 更新プログラム名  */,
+	[UpdatedProgramVersion] text not null /* 更新プログラムバージョン  */,
+	[UpdatedCount] integer not null /* 更新回数 0始まり */,
+	[SeparatorKind] text not null /* 区切り種別  */,
+	[SeparatorWidth] integer not null /* 区切り幅 SeparatorKind に依存 */,
+	primary key(
+		[LauncherItemId]
+	),
+	foreign key([LauncherItemId]) references [LauncherItems]([LauncherItemId])
+)
+;
