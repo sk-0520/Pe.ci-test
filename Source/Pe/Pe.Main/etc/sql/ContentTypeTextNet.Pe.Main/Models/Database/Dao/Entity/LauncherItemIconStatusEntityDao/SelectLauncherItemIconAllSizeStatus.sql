ebd9cf7bef3ffb280e75fa95fd6923d46a9c4b69
sk <content.type.text.net@gmail.com>
2024-08-22 18:22:15 +0900

    たぶん不要なファイル群破棄

--- Source/Pe/Pe.Main/etc/sql/ContentTypeTextNet.Pe.Main/Models/Database/Dao/Entity/LauncherItemIconStatusEntityDao/SelectLauncherItemIconAllSizeStatus.sql
+++
@@ -1,8 +0,0 @@
-select
-	LauncherItemIconStatus.IconBox,
-	LauncherItemIconStatus.IconScale,
-	LauncherItemIconStatus.LastUpdatedTimestamp
-from
-	LauncherItemIconStatus
-where
-	LauncherItemIconStatus.LauncherItemId = @LauncherItemId
