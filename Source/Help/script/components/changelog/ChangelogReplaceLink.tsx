fa2fd8172e84c42b3fbf00482c34fb260b2af801
sk <content.type.text.net@gmail.com>
2024-08-26 23:04:35 +0900

    くっそみたいな非効率処理っぽいけど動くから正しい

--- Source/Help/script/components/changelog/ChangelogReplaceLink.tsx
+++ Source/Help/script/components/changelog/ChangelogReplaceLink.tsx
@@ -9,66 +9,45 @@ interface Token {
 	value: string;
 }
 
-const IssueRegex = /(#(?<ISSUE>\d+))/;
-const UrlRegex = /(?<URL>(https?:\/\/[\w?=&./\-;#~%]+(?![\w?&./;#~%"=-]*>)))/;
+const IssueRegex = /(^#(?<ISSUE>\d+))/;
+const UrlRegex = /(?<URL>^(https?:\/\/[\w?=&./\-;#~%]+(?![\w?&./;#~%"=-]*>)))/;
 
 function splitTokens(s: string): Token[] {
 	const result: Token[] = [];
 
-	// if(s.startsWith(".NET Framework 4.6 は")) {
-	// 	debugger
-	// }
-
-	let prevIndex = 0;
 	let currentIndex = 0;
 	while (currentIndex < s.length) {
 		const work = s.substring(currentIndex);
 
 		const issueMatch = work.match(IssueRegex);
 		if (issueMatch?.groups && "ISSUE" in issueMatch.groups) {
-			if(issueMatch.index && prevIndex < issueMatch.index) {
-				result.push({
-					kind: "text",
-					value: work.substring(0, issueMatch.index),
-				});
-			}
-
 			result.push({
 				kind: "issue",
 				value: issueMatch.groups.ISSUE,
 			});
-			prevIndex = currentIndex += issueMatch.groups.ISSUE.length + 1 + 1;
+			currentIndex += issueMatch.groups.ISSUE.length + 1 + 1;
 			continue;
 		}
 
 		const urlMatch = work.match(UrlRegex);
 		if (urlMatch?.groups && "URL" in urlMatch.groups) {
-			if(urlMatch.index && prevIndex < urlMatch.index) {
-				result.push({
-					kind: "text",
-					value: work.substring(0, urlMatch.index),
-				});
-			}
-
 			result.push({
 				kind: "url",
 				value: urlMatch.groups.URL,
 			});
-			prevIndex = currentIndex += urlMatch.groups.URL.length + 1;
+			currentIndex += urlMatch.groups.URL.length + 1;
 			continue;
 		}
+
+		result.push({
+			kind: "text",
+			value: work.substring(0, 1),
+		});
 
 		currentIndex += 1;
 	}
 
-	if(prevIndex < currentIndex) {
-		result.push({
-			kind: "text",
-			value: s.substring(prevIndex, currentIndex),
-		});
-	}
-
-	console.debug(result)
+	console.debug(result);
 
 	return result;
 }
