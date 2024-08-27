30eb2ba8b7bb0769433b24b3d2b555a9f2d92590
sk <content.type.text.net@gmail.com>
2024-08-27 22:53:39 +0900

    前実装参考にせず地道に組んだ方がいいかもしれん

--- Source/Help/script/utils/table.ts
+++ Source/Help/script/utils/table.ts
@@ -1,9 +1,19 @@
+const ColumnIndex = {
+	primaryKey: 0,
+	notNull: 1,
+	foreignKey: 2,
+	logicalName: 3,
+	absoluteName: 4,
+	logicalType: 5,
+	cliType: 6,
+	check: 7,
+	comment: 8,
+};
 
 export function splitEntities(markdown: string): string[] {
-	if(!markdown) {
-		return []
+	if (!markdown) {
+		return [];
 	}
 
-	return markdown.split(/^___$/gm).map(a => a.trim());
+	return markdown.split(/^___$/gm).map((a) => a.trim());
 }
-
