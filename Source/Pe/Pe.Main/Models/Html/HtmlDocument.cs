71eca691643d5db1fe15b810ab2305a0d4f28842
sk <content.type.text.net@gmail.com>
2024-09-24 20:39:07 +0900

    おおぉっと、意味の分からんコードを書きだしましたよ

---
+++ Source/Pe/Pe.Main/Models/Html/HtmlDocument.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContentTypeTextNet.Pe.Main.Models.Html
+{
+    public class HtmlDocument
+    {
+    }
+}
